e4de940dfe8eb92578d43695aabb269502f1ee4d
Christoph Hartmann <chris@lollyrock.com>
2015-08-14 00:49:31 -0700

    improve windows detection

--- lib/utils/detect.rb
+++ lib/utils/detect.rb
@@ -6,10 +6,15 @@
 
 require 'json'
 
+if os[:family] == 'windows'
+  res = JSON.parse(command('New-Object -Type PSObject | Add-Member -MemberType NoteProperty -Name OS -Value (Get-WmiObject -Class Win32_OperatingSystem) -PassThru | Add-Member -MemberType NoteProperty -Name OSVersion -Value ([Environment]::OSVersion) -PassThru | ConvertTo-Json').stdout)
+  release = res["OS"]["Caption"]
+end
+
 # hijack os-detection from serverspec
 puts JSON.dump({
   os_family:   os[:family],
-  os_release:  os[:release],
+  os_release:  release || os[:release],
   os_arch:     os[:arch]
 })
 exit 0
