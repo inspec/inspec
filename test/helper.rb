cf77e56118b67717563d89abdc6cfec3133cb0fc
Christoph Hartmann <chris@lollyrock.com>
2015-09-03 17:33:19 +0200

    add unit test for bond resource

--- test/helper.rb
+++ test/helper.rb
@@ -1,4 +1,30 @@
+# encoding: utf-8
+
 require 'minitest/autorun'
 require 'minitest/spec'
-
 require 'vulcano/backend'
+
+# loads a resource class and instantiates the class with the given arguments
+def loadResource (resource, *args)
+
+  # test mappings
+  scriptpath = IO::File.realpath(IO::File.dirname(__FILE__))
+  @mapping = {
+    '/proc/net/bonding/bond0' => IO::File.join(scriptpath, '/unit/mock/bond0')
+  }
+
+  # create mock backend
+  conf = Vulcano::Backend.target_config({})
+  backend_class = Vulcano::Backend.registry['mock']
+  @backend = backend_class.new(conf, @mapping)
+
+  # load resource
+  @rclass = Vulcano::Resource.registry[resource]
+
+  # merge arguments
+  args = [@backend] | args
+
+  # initialize resource with backend and parameters
+  @resource = @rclass.new(*args)
+
+end
