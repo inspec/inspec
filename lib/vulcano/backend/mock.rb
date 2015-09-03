ceb68f94cff38cdb04633ce40402e3aa3b148bd6
Christoph Hartmann <chris@lollyrock.com>
2015-09-03 17:09:39 +0200

    feature: extend mock framework to support file mappings

--- lib/vulcano/backend/mock.rb
+++ lib/vulcano/backend/mock.rb
@@ -4,14 +4,19 @@ module Vulcano::Backends
   class Mock < Vulcano.backend(1)
     name 'mock'
 
-    def initialize( conf )
+    def initialize( conf , mapping = {})
       @conf = conf
       @files = {}
+      @mapping = mapping
     end
 
     def file(path)
-      puts "--> get file #{path}"
-      @files[path] ||= File.new(self, path)
+      newpath = @mapping[path]
+      if newpath != nil
+        @files[path] ||= File.new(self, newpath, false)
+      else
+        @files[path] ||= File.new(self, path)
+      end
     end
 
     def run_command(cmd)
@@ -26,18 +31,27 @@ module Vulcano::Backends
   class Mock
     class File
 
-      def initialize(runtime, path)
+      def initialize(runtime, path, mock = true)
         @path = path
-        @exists = (rand < 0.8) ? true : false
-        @is_file = (@exists && rand < 0.7) ? true : false
-        @size = 0
-        @content = ''
-        if @exists && @is_file
-          @size = ( rand ** 3 * 1000 ).to_i
-          @size = 0 if rand < 0.2
-        end
-        if @size > 0
-          @content = (0...50).map { ('a'..'z').to_a[rand(26)] }.join
+        # mock dataset
+        if mock
+          @exists = (rand < 0.8) ? true : false
+          @is_file = (@exists && rand < 0.7) ? true : false
+          @size = 0
+          @content = ''
+          if @exists && @is_file
+            @size = ( rand ** 3 * 1000 ).to_i
+            @size = 0 if rand < 0.2
+          end
+          if @size > 0
+            @content = (0...50).map { ('a'..'z').to_a[rand(26)] }.join
+          end
+        # we have a mapping file
+        else
+          @exists = IO::File.exists?(path)
+          @is_file = IO::File.file?(path)
+          @size = IO::File.size(path)
+          @content = IO::File.read(path)
         end
       end
 
