6a00ce8ebea8c915e8ca8a09a0195566ba7e6b60
Christoph Hartmann <chris@lollyrock.com>
2015-09-03 18:04:13 +0200

    add utf-8 header to tests

--- test/docker.rb
+++ test/docker.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 require 'docker'
 require 'yaml'
 require_relative '../lib/vulcano'
--- test/unit/plugins_v1_backend_file_test.rb
+++ test/unit/plugins_v1_backend_file_test.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 require 'helper'
 
 describe 'Vulcano::Plugins::Backend::FileCommon' do
