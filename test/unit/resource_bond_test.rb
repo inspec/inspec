abae94ba4e5e04d047db5e34eb930585b4197721
Christoph Hartmann <chris@lollyrock.com>
2015-09-03 18:03:51 +0200

    improve test style

--- test/unit/resource_bond_test.rb
+++ test/unit/resource_bond_test.rb
@@ -6,27 +6,26 @@ require 'vulcano/resource'
 describe 'Vulcano::Resources::Bond' do
 
   describe 'parse bond config' do
-    before do
-      @resource = loadResource('bond', 'bond0')
-    end
+
+    let(:resource) { loadResource('bond', 'bond0') }
 
     it 'bond must be available' do
-      @resource.exists?.must_equal true
+      resource.exists?.must_equal true
     end
 
     it 'eth0 is part of bond' do
-      _(@resource.has_interface?('eth0')).must_equal true
-      _(@resource.has_interface?('eth1')).must_equal false
-      _(@resource.has_interface?('eth2')).must_equal true
+      _(resource.has_interface?('eth0')).must_equal true
+      _(resource.has_interface?('eth1')).must_equal false
+      _(resource.has_interface?('eth2')).must_equal true
     end
 
     it 'get all interfaces' do
-      _(@resource.interfaces).must_equal ['eth0','eth2']
+      _(resource.interfaces).must_equal ['eth0','eth2']
     end
 
     it 'get proc content' do
-      _(@resource.content).wont_equal nil
-      _(@resource.content).wont_equal ''
+      _(resource.content).wont_equal nil
+      _(resource.content).wont_equal ''
     end
 
   end
