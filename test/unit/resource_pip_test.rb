238f1b20162feb8f211ae79dfc7965ad966a9a00
Christoph Hartmann <chris@lollyrock.com>
2015-09-08 23:13:08 +0100

    add pip resource to verify gem packages

---
+++ test/unit/resource_pip_test.rb
@@ -0,0 +1,17 @@
+# encoding: utf-8
+
+require 'helper'
+require 'vulcano/resource'
+
+describe 'Vulcano::Resources::Passwd' do
+  describe 'pip' do
+    let(:resource) { loadResource('pip', 'jinja2') }
+
+    it 'verify pip package detail parsing' do
+      pkg = {:name=>"Jinja2", :installed=>true, :version=>"2.8", :type=>"pip"}
+      _(resource.installed?).must_equal true
+      _(resource.info).must_equal pkg
+    end
+
+  end
+end
