1901d64f212d55b6582fa42128d2d8634704a031
Dominik Richter <dominik.richter@gmail.com>
2015-09-08 14:11:15 +0200

    use specinfra+docker for now
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- test/docker.rb
+++ test/docker.rb
@@ -45,7 +45,7 @@ class DockerTester
   end
 
   def test_container(container_id)
-    opts = { 'target' => "docker://#{container_id}" }
+    opts = { 'target' => "specinfra+docker://#{container_id}" }
     runner = Vulcano::Runner.new(nil, opts)
     runner.add_tests(@tests)
     runner.run
