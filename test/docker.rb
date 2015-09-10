ee11a25f6e0d513c91485d00102775a248d50f1b
Dominik Richter <dominik.richter@gmail.com>
2015-09-10 13:01:12 +0200

    run docker tests start/stop concurrently
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- test/docker.rb
+++ test/docker.rb
@@ -2,6 +2,7 @@
 
 require 'docker'
 require 'yaml'
+require 'concurrent'
 require_relative '../lib/vulcano'
 
 tests = ARGV
@@ -18,14 +19,14 @@ class DockerTester
   end
 
   def run
-    puts ["Running tests:", @tests].flatten.join("\n- ")
+    puts ['Running tests:', @tests].flatten.join("\n- ")
     puts ''
     # test all images
-    promises = @conf['images'].map{|n|
-      Concurrent::Promise.new{ prepare_image(n) }.execute
+    promises = @conf['images'].map { |n|
+      Concurrent::Promise.new { prepare_image(n) }.execute
     }
 
-    sleep(0.1) while !promises.all?{|x|x.fulfilled?}
+    sleep(0.1) until promises.all?(&:fulfilled?)
 
     res = promises.map do |promise|
       container = promise.value
@@ -33,11 +34,11 @@ class DockerTester
     end
 
     done = promises.map do |promise|
-      promise.then{|container| stop_container(container) }
+      promise.then { |c| stop_container(c) }
     end
-    sleep(0.1) while !done.all?{|x| x.fulfilled?}
+    sleep(0.1) until done.all?(&:fulfilled?)
 
-    res.all? or raise 'Test failures'
+    res.all? or fail 'Test failures'
   end
 
   def docker_images_by_tag
@@ -53,38 +54,38 @@ class DockerTester
 
   def tests_conf
     # get the test configuration
-    conf_path = File::join(File::dirname(__FILE__), '..', '.tests.yaml')
-    raise "Can't find tests config in #{conf_path}" unless File::file?(conf_path)
-    conf = YAML.load(File::read(conf_path))
+    conf_path = File.join(File.dirname(__FILE__), '..', '.tests.yaml')
+    fail "Can't find tests config in #{conf_path}" unless File.file?(conf_path)
+    YAML.load(File.read(conf_path))
   end
 
   def test_container(container_id)
+    puts "--> run test on docker #{container_id}"
     opts = { 'target' => "docker://#{container_id}" }
     runner = Vulcano::Runner.new(nil, opts)
     runner.add_tests(@tests)
     runner.run
   end
 
-  def test_image(name)
+  def prepare_image(name)
     dname = "docker-#{name}:latest"
     image = @images[dname]
-    raise "Can't find docker image #{dname}" if image.nil?
+    fail "Can't find docker image #{dname}" if image.nil?
 
     puts "--> start docker #{name}"
     container = Docker::Container.create(
-      'Cmd' => [ '/bin/bash' ],
+      'Cmd' => ['/bin/bash'],
       'Image' => image.id,
       'OpenStdin' => true,
     )
     container.start
+    container
+  end
 
-    puts "--> run test on docker #{name}"
-    res = test_container(container.id)
-
-    puts "--> killrm docker #{name}"
+  def stop_container(container)
+    puts "--> killrm docker #{container.id}"
     container.kill
     container.delete(force: true)
-    res
   end
 end
 
