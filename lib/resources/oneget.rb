b8ce0311c915f67cdecc4f008399c089f25e929d
Christoph Hartmann <chris@lollyrock.com>
2015-09-10 12:35:15 +0100

    add oneget resource

---
+++ lib/resources/oneget.rb
@@ -0,0 +1,48 @@
+# encoding: utf-8
+
+# This resource talks with OneGet (https://github.com/OneGet/oneget)
+# Its part of Windows Management Framework 5.0 and part of Windows 10
+#
+# Usage:
+# describe oneget('zoomit') do
+#   it { should be_installed }
+# end
+class OneGetPackage < Vulcano.resource(1)
+  name 'oneget'
+
+  def initialize(package_name)
+    @package_name = package_name
+    @cache = nil
+  end
+
+  def info
+    return @cache if !@cache.nil?
+    cmd = vulcano.run_command("Get-Package -Name '#{@package_name}' | ConvertTo-Json")
+    # cannot rely on exit code for now, successful command returns exit code 1
+    # return nil if cmd.exit_status != 0
+    # try to parse json
+    begin
+      pkgs = JSON.parse(cmd.stdout)
+    rescue JSON::ParserError => _e
+      return nil
+    end
+    @cache = {
+      name: pkgs['Name'],
+      version: pkgs['Version'],
+      type: 'oneget',
+    }
+  end
+
+  def installed?
+    !info.nil?
+  end
+
+  def version
+    return nil if info.nil?
+    info[:version]
+  end
+
+  def to_s
+    "oneget package #{@package_name}"
+  end
+end
---
+++ test/unit/resource_oneget_test.rb
@@ -0,0 +1,16 @@
+# encoding: utf-8
+
+require 'helper'
+require 'vulcano/resource'
+
+describe 'Vulcano::Resources::Passwd' do
+  describe 'oneget' do
+    let(:resource) { loadResource('oneget', 'Mozilla Firefox') }
+
+    it 'verify oneget package detail parsing' do
+      pkg = { name: 'Mozilla Firefox 40.0.3 (x86 en-US)', version: '40.0.3', type: 'oneget'}
+      _(resource.installed?).must_equal true
+      _(resource.info).must_equal pkg
+    end
+  end
+end
