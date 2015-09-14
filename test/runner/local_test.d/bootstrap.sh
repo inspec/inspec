1e7d365cadd7560331a74074e56908d8d6c7fb4f
Dominik Richter <dominik.richter@gmail.com>
2015-09-14 14:42:18 +0200

    switch kitchen provisioner shell -> chef
    Reason: Tempering around older ubuntu distros to get their ruby to work just right is error-prone and wasteful. Just install chef from omnibus, grab its ruby to run the tests and use chef to bootstrap the nodes.

--- test/runner/local_test.d/bootstrap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-apt-get update && apt-get -y install ruby ruby-dev gem gcc
-gem install bundler
-
-cd /tmp/kitchen/data
-bundle install
-bundle exec ruby -I lib test/runner/local_test.d/test.rb
