909676b7808fe9feece0bc86f634e3ec90be5e01
Dominik Richter <dominik.richter@gmail.com>
2015-09-15 12:38:51 +0200

    clean test.rb file
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- test/runner/test.rb
+++ test/runner/test.rb
@@ -11,9 +11,7 @@ backends[:local] = proc {
 }
 
 backends[:specinfra_local] = proc {
-  backend_conf = Vulcano::Backend.target_config({
-    'backend' => 'exec',
-  })
+  backend_conf = Vulcano::Backend.target_config({ 'backend' => 'exec' })
   backend_class = Vulcano::Backend.registry['specinfra']
   backend_class.new(backend_conf)
 }
@@ -21,7 +19,6 @@ backends[:specinfra_local] = proc {
 tests = ARGV
 
 backends.each do |type, get_backend|
-  puts "run on backend #{type.to_s}"
   tests.each do |test|
     instance_eval(File.read(test))
   end
