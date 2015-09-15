b04cfc688dd8aabc9235c387ec6e92e70ad982cf
Dominik Richter <dominik.richter@gmail.com>
2015-09-15 13:18:48 +0200

    add folder integration tests
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- test/runner/tests/file_test.rb
+++ test/runner/tests/file_test.rb
@@ -77,6 +77,76 @@ describe 'file interface' do
     end
   end
 
+  describe 'regular folder' do
+    let(:file) { backend.file('/tmp/folder') }
+
+    it 'exists' do
+      file.exists?.must_equal(true)
+    end
+
+    it 'is a directory' do
+      file.directory?.must_equal(true)
+    end
+
+    it 'has type :directory' do
+      file.type.must_equal(:directory)
+    end
+
+    it 'has no content' do
+      file.content.must_equal(nil)
+    end
+
+    it 'has owner name root' do
+      file.owner.must_equal('root')
+    end
+
+    it 'has group name root' do
+      file.group.must_equal('root')
+    end
+
+    it 'has mode 0567' do
+      file.mode.must_equal(00567)
+    end
+
+    it 'checks mode? 0567' do
+      file.mode?(00567).must_equal(true)
+    end
+
+    it 'has no link_path' do
+      file.link_path.must_be_nil
+    end
+
+    it 'has an md5sum' do
+      file.md5sum.must_equal(nil)
+    end
+
+    it 'has an sha256sum' do
+      file.sha256sum.must_equal(nil)
+    end
+
+    it 'has a modified time' do
+      # Must be in within 1800s (30min) of the current time
+      file.mtime.must_be_close_to(Time.now.to_i, 1000)
+    end
+
+    it 'has inode size' do
+      # Must be around 11 Bytes, +- 4
+      file.size.must_be_close_to(200, 198)
+    end
+
+    it 'has no selinux_label' do
+      file.selinux_label.must_equal(nil)
+    end
+
+    it 'has no product_version' do
+      file.product_version.must_equal(nil)
+    end
+
+    it 'has no file_version' do
+      file.file_version.must_equal(nil)
+    end
+  end
+
   describe 'symlink file' do
     let(:file) { backend.file('/tmp/symlink') }
 
