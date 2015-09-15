8cf43b377a905ba65582e148db4bf571530f1f03
Dominik Richter <dominik.richter@gmail.com>
2015-09-15 14:23:32 +0200

    add pipe/fifo integration tests
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- test/runner/tests/file_test.rb
+++ test/runner/tests/file_test.rb
@@ -287,6 +287,77 @@ describe 'file interface' do
     end
   end
 
+  describe 'pipe / fifo' do
+    let(:file) { backend.file('/tmp/pipe') }
+
+    it 'exists' do
+      file.exists?.must_equal(true)
+    end
+
+    it 'is a pipe' do
+      file.pipe?.must_equal(true)
+    end
+
+    it 'has type :pipe' do
+      file.type.must_equal(:pipe)
+    end
+
+    # # TODO add back content
+    # it 'has no content' do
+    #   file.content.must_be_nil
+    # end
+
+    it 'has owner name root' do
+      file.owner.must_equal('root')
+    end
+
+    it 'has group name' do
+      file.group.must_equal('root')
+    end
+
+    it 'has mode 0644' do
+      file.mode.must_equal(00644)
+    end
+
+    it 'checks mode? 0644' do
+      file.mode?(00644).must_equal(true)
+    end
+
+    it 'has no link_path' do
+      file.link_path.must_be_nil
+    end
+
+    # # TODO add back content
+    # it 'has no md5sum' do
+    #   file.md5sum.must_be_nil
+    # end
+    #
+    # # TODO add back content
+    # it 'has no sha256sum' do
+    #   file.sha256sum.must_be_nil
+    # end
+
+    it 'has a modified time' do
+      file.mtime.must_be_close_to(Time.now.to_i - MTime/2, MTime)
+    end
+
+    it 'has inode size of 0' do
+      file.size.must_equal(0)
+    end
+
+    it 'has no selinux_label' do
+      file.selinux_label.must_equal(nil)
+    end
+
+    it 'has no product_version' do
+      file.product_version.must_equal(nil)
+    end
+
+    it 'has no file_version' do
+      file.file_version.must_equal(nil)
+    end
+  end
+
   describe 'symlink file' do
     let(:file) { backend.file('/tmp/symlink') }
 
