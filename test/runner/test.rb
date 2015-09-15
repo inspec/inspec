ffcd12ba9712aa5096972bc93e94d83f5dd440d1
Dominik Richter <dominik.richter@gmail.com>
2015-09-15 14:38:19 +0200

    split up file integration tests
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- test/runner/test.rb
+++ test/runner/test.rb
@@ -1,3 +1,14 @@
+# encoding: utf-8
+
+# Tests configuration:
+
+# MTime tracks the maximum range of modification time in seconds.
+# i.e. MTime == 60*60*1 is 1 hour of modification time range,
+# which translates to a modification time range of:
+#   [ now-1hour, now ]
+MTime = 60 * 60 * 1
+
+# Run all tests
 require 'minitest/autorun'
 require 'minitest/spec'
 require 'vulcano/backend'
---
+++ test/runner/tests/path_folder_test.rb
@@ -0,0 +1,74 @@
+# encoding: utf-8
+
+describe 'file interface' do
+  let(:backend) { get_backend.call }
+
+  describe 'a folder' do
+    let(:file) { backend.file('/tmp/folder') }
+
+    it 'exists' do
+      file.exists?.must_equal(true)
+    end
+
+    it 'is a directory' do
+      file.directory?.must_equal(true)
+    end
+
+    it 'has type :directory' do
+      file.type.must_equal(:directory)
+    end
+
+    it 'has no content' do
+      file.content.must_equal(nil)
+    end
+
+    it 'has owner name root' do
+      file.owner.must_equal('root')
+    end
+
+    it 'has group name root' do
+      file.group.must_equal('root')
+    end
+
+    it 'has mode 0567' do
+      file.mode.must_equal(00567)
+    end
+
+    it 'checks mode? 0567' do
+      file.mode?(00567).must_equal(true)
+    end
+
+    it 'has no link_path' do
+      file.link_path.must_be_nil
+    end
+
+    it 'has an md5sum' do
+      file.md5sum.must_equal(nil)
+    end
+
+    it 'has an sha256sum' do
+      file.sha256sum.must_equal(nil)
+    end
+
+    it 'has a modified time' do
+      file.mtime.must_be_close_to(Time.now.to_i - MTime/2, MTime)
+    end
+
+    it 'has inode size' do
+      # Must be around 11 Bytes, +- 4
+      file.size.must_be_close_to(200, 198)
+    end
+
+    it 'has no selinux_label' do
+      file.selinux_label.must_equal(nil)
+    end
+
+    it 'has no product_version' do
+      file.product_version.must_equal(nil)
+    end
+
+    it 'has no file_version' do
+      file.file_version.must_equal(nil)
+    end
+  end
+end
