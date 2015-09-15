1c522d185ef7dcf1279e98de9c41e79e4a7f216b
Dominik Richter <dominik.richter@gmail.com>
2015-09-15 08:43:49 +0200

    extend file tests

--- test/runner/tests/file_test.rb
+++ test/runner/tests/file_test.rb
@@ -5,21 +5,44 @@ describe 'file interface' do
   describe 'regular file' do
     let(:file) { backend.file('/tmp/file') }
 
-    it 'can identify it as a file' do
+    it 'exists' do
+      file.exists?.must_equal(true)
+    end
+
+    it 'is a file' do
       file.file?.must_equal(true)
+    end
+
+    it 'has type :file' do
       file.type.must_equal(:file)
     end
 
-    it 'can read its content' do
+    it 'has content' do
       file.content.must_equal('hello world')
     end
 
-    it 'can get the owner by name' do
+    it 'has owner name root' do
       file.owner.must_equal('root')
     end
 
-    it 'can get the group by name' do
+    it 'has group name root' do
       file.group.must_equal('root')
+    end
+
+    it 'has mode 0765' do
+      file.mode.must_equal(00765)
+    end
+
+    it 'checks mode? 0765' do
+      file.mode?(00765).must_equal(true)
+    end
+
+    it 'doesnt check mode? 0764' do
+      file.mode?(00764).must_equal(false)
+    end
+
+    it 'has no link_target' do
+      file.link_target.must_equal('')
     end
   end
 end
