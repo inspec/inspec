4885798aeee7648424948a7e7b9b3fa6160f8427
Christoph Hartmann <chris@lollyrock.com>
2015-09-20 17:42:56 +0200

    add unit tests for windows feature

---
+++ test/unit/resource_windows_feature.rb
@@ -0,0 +1,18 @@
+# encoding utf-8
+
+require 'helper'
+require 'vulcano/resource'
+
+describe 'Vulcano:Resources::Feature' do
+  describe 'feature' do
+
+    let(:resource) { loadResource('windows_feature', 'dhcp') }
+
+    # TODO: set windows as mock os
+    it 'verify windows feature parsing' do
+      pkg = { name: 'DHCP', description: 'Dynamic Host Configuration Protocol (DHCP) Server enables you to centrally configure, manage, and provide temporary IP addresses and related information for client computers.', installed: false, type: 'windows-feature' }
+      _(resource.info).must_equal pkg
+      _(resource.installed?).must_equal false
+    end
+  end
+end
