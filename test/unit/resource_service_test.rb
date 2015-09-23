cf8a75f233ef65ade05277dff6e5a63d5ae778c5
Christoph Hartmann <chris@lollyrock.com>
2015-09-23 16:30:48 +0200

    improvement: make service tests os dependent

--- test/unit/resource_service_test.rb
+++ test/unit/resource_service_test.rb
@@ -4,15 +4,14 @@ require 'helper'
 require 'vulcano/resource'
 
 describe 'Vulcano::Resources::Service' do
-  # TODO: make OS dependent
-  let(:resource) { load_resource('service', 'dhcp') }
 
   # windows
-  # it 'verify service parsing' do
-  #   pkg = { name: 'dhcp', description: 'DHCP Client', installed: true, running: true, enabled: true, type: 'windows' }
-  #   _(resource.info).must_equal pkg
-  #   _(resource.installed?).must_equal true
-  #   _(resource.enabled?).must_equal true
-  #   _(resource.running?).must_equal true
-  # end
+  it 'verify service parsing' do
+    resource = MockLoader.new(:windows).load_resource('service', 'dhcp')
+    srv = { name: 'dhcp', description: 'DHCP Client', installed: true, running: true, enabled: true, type: 'windows' }
+    _(resource.info).must_equal srv
+    _(resource.installed?).must_equal true
+    _(resource.enabled?).must_equal true
+    _(resource.running?).must_equal true
+  end
 end
