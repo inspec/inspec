624bea943dabddc22ec51954d9932c2b8c4f7032
Dominik Richter <dominik.richter@gmail.com>
2015-09-23 12:21:10 +0200

    Add Delivery build cookbook

---
+++ .delivery/build-cookbook/metadata.rb
@@ -0,0 +1,5 @@
+name 'build-cookbook'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+version '0.1.0'
---
+++ .delivery/build-cookbook/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: build-cookbook
+# Recipe:: default
+#
+# Copyright (c) 2015 The Authors, All Rights Reserved.
