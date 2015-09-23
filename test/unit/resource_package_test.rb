943eef4ef33e37870c50fca99c3cec0d31377516
Christoph Hartmann <chris@lollyrock.com>
2015-09-23 15:50:23 +0200

    make package unit tests os dependent

--- test/unit/resource_package_test.rb
+++ test/unit/resource_package_test.rb
@@ -5,31 +5,29 @@ require 'vulcano/resource'
 
 describe 'Vulcano::Resources::Passwd' do
   describe 'package' do
-
-    # TODO: set operating system to arch
-    # let(:resource) { loadResource('package', 'curl') }
-
     # arch linux
-    # it 'verify package parsing' do
-    #   pkg = { name: 'curl', installed: true, version: '7.37.0-1', type: 'pacman' }
-    #   _(resource.installed?).must_equal true
-    #   _(resource.info).must_equal pkg
-    # end
+    it 'verify arch linux package parsing' do
+      resource = MockLoader.new(:arch).load_resource('package', 'curl')
+      pkg = { name: 'curl', installed: true, version: '7.37.0-1', type: 'pacman' }
+      _(resource.installed?).must_equal true
+      _(resource.info).must_equal pkg
+    end
 
     # ubuntu
-    # it 'verify package parsing' do
-    #   pkg = { name: 'curl', installed: true, version: '7.35.0-1ubuntu2', type: 'deb' }
-    #   _(resource.installed?).must_equal true
-    #   _(resource.info).must_equal pkg
-    # end
+    it 'verify ubuntu package parsing' do
+      resource = MockLoader.new(:ubuntu1404).load_resource('package', 'curl')
+      pkg = { name: 'curl', installed: true, version: '7.35.0-1ubuntu2', type: 'deb' }
+      _(resource.installed?).must_equal true
+      _(resource.info).must_equal pkg
+    end
 
     # centos
-    #
-    # it 'verify package parsing' do
-    #   pkg = { name: 'curl', installed: true, version: '7.29.0', type: 'rpm' }
-    #   _(resource.installed?).must_equal true
-    #   _(resource.info).must_equal pkg
-    # end
+    it 'verify centos package parsing' do
+      resource = MockLoader.new(:centos7).load_resource('package', 'curl')
+      pkg = { name: 'curl', installed: true, version: '7.29.0', type: 'rpm' }
+      _(resource.installed?).must_equal true
+      _(resource.info).must_equal pkg
+    end
 
   end
 end
