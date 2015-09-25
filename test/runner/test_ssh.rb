31a960e2b6e3111026f85eb60e14718b571c2cf0
Dominik Richter <dominik.richter@gmail.com>
2015-09-25 14:09:41 +0200

    add ssh backend to runner kitchen tests
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- test/runner/test_ssh.rb
+++ test/runner/test_ssh.rb
@@ -3,13 +3,18 @@ require_relative 'helper'
 require 'vulcano/backend'
 
 backends = {}
+backend_conf = Vulcano::Backend.target_config({
+  'target' => 'ssh://vagrant@localhost',
+  'key_file' => '/root/.ssh/id_rsa',
+})
 
 backends[:specinfra_ssh] = proc {
-  backend_conf = Vulcano::Backend.target_config({
-    'target' => 'ssh://vagrant@localhost',
-    'key_file' => '/root/.ssh/id_rsa',
-  })
   backend_class = Vulcano::Backend.registry['specinfra']
+  backend_class.new(backend_conf)
+}
+
+backends[:ssh] = proc {
+  backend_class = Vulcano::Backend.registry['ssh']
   backend_class.new(backend_conf)
 }
 
