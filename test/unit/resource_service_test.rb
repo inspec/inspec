86cd56c5c371d40e294ec3ac56189da62da256ca
Christoph Hartmann <chris@lollyrock.com>
2015-09-25 10:31:42 +0200

    add unit test for service resource on ubuntu 15.04

--- test/unit/resource_service_test.rb
+++ test/unit/resource_service_test.rb
@@ -15,10 +15,20 @@ describe 'Vulcano::Resources::Service' do
     _(resource.running?).must_equal true
   end
 
-  # # ubuntu 14.04 with upstart
+  # ubuntu 14.04 with upstart
   it 'verify ubuntu package parsing' do
     resource = MockLoader.new(:ubuntu1404).load_resource('service', 'ssh')
     srv = { name: 'ssh', description: '', installed: true, running: true, enabled: true, type: 'upstart' }
+    _(resource.info).must_equal srv
+    _(resource.installed?).must_equal true
+    _(resource.enabled?).must_equal true
+    _(resource.running?).must_equal true
+  end
+
+  # ubuntu 15.04 with systemd
+  it 'verify ubuntu package parsing' do
+    resource = MockLoader.new(:ubuntu1504).load_resource('service', 'sshd')
+    srv = { name: 'sshd.service', description: 'OpenSSH server daemon', installed: true, running: true, enabled: true, type: 'systemd' }
     _(resource.info).must_equal srv
     _(resource.installed?).must_equal true
     _(resource.enabled?).must_equal true
