536d9401fab0f92f305946bc50ffb15a1e5d223c
Dominik Richter <dominik.richter@gmail.com>
2015-09-30 15:48:42 +0200

    run dockerized tests in delivery

--- .delivery/build-cookbook/recipes/default.rb
+++ .delivery/build-cookbook/recipes/default.rb
@@ -1,5 +1,23 @@
-#
+# encoding: utf-8
 # Cookbook Name:: build-cookbook
 # Recipe:: default
 #
-# Copyright (c) 2015 The Authors, All Rights Reserved.
+# Copyright (c) 2015 Chef Software Inc., All Rights Reserved.
+
+# clean up the build node first
+
+group 'docker' do
+  members [node['delivery_builder']['build_user']]
+end
+
+docker_service 'dockerd' do
+  action [:delete, :create, :start]
+  host 'unix:///var/run/docker.sock'
+  group 'docker'
+  provider Chef::Provider::DockerService::Execute
+end
+
+log 'system info' do
+  message `uname -a; docker --version; ls -lha /var/run/docker.sock`
+  level :warn
+end
--- .delivery/build-cookbook/recipes/unit.rb
+++ .delivery/build-cookbook/recipes/unit.rb
@@ -7,8 +7,16 @@
 
 include_recipe 'build-cookbook::prepare'
 
+home = node['delivery_builder']['repo']
+
 execute 'rake test' do
-  command 'bundle exec rake'
-  cwd node['delivery_builder']['repo']
+  command 'bundle exec rake test'
+  cwd home
+  user node['delivery_builder']['build_user']
+end
+
+execute 'dockerized rake' do
+  command 'bundle exec rake test:resources config=test/test.yaml'
+  cwd home
   user node['delivery_builder']['build_user']
 end
