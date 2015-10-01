86bc130176af5ed67842bd3b44e06b9756ea3c2c
Dominik Richter <dominik.richter@gmail.com>
2015-10-01 22:23:33 +0200

    add delivery publish to github
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- .delivery/build-cookbook/recipes/default.rb
+++ .delivery/build-cookbook/recipes/default.rb
@@ -4,12 +4,26 @@
 #
 # Copyright (c) 2015 Chef Software Inc., All Rights Reserved.
 
+include_recipe 'delivery-truck::default'
+
+# create a persistant gem cache per builder for the entire project
+gem_cache = File.join(node['delivery']['workspace']['root'], "../../../project_gem_cache")
+directory gem_cache do
+  # set the owner to the dbuild so that the other recipes can write to here
+  owner node['delivery_builder']['build_user']
+  mode "0755"
+  recursive true
+  action :create
+end
+
+# ensure we always have a docker group with the build user as a member
 group 'docker' do
   members [node['delivery_builder']['build_user']]
 end
 
 package 'build-essential'
 
+# get docker
 docker_service 'dockerd' do
   action [:delete, :create, :start]
   host 'unix:///var/run/docker.sock'
