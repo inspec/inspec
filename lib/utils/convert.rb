14c5c3f393be0ded0007238824ff41dba9a22227
Christoph Hartmann <chris@lollyrock.com>
2015-10-05 12:28:28 +0200

    lint: remove whitespace

--- lib/utils/convert.rb
+++ lib/utils/convert.rb
@@ -1,12 +1,10 @@
 # encoding: utf-8
 
 module Converter
-
   # convert the value to an integer if we have numbers only
   # otherwise we return the string
   def convert_to_i(val)
     val = val.to_i if val.match(/^\d+$/)
     val
   end
-
 end
