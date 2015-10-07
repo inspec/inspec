67f6ae5be268b7f6d825c96511057326a72acc78
Christoph Hartmann <chris@lollyrock.com>
2015-10-07 13:04:40 +0200

    implement run script resource for windows

---
+++ lib/resources/script.rb
@@ -0,0 +1,39 @@
+# encoding: utf-8
+# copyright: 2015, Vulcano Security GmbH
+# author: Christoph Hartmann
+# author: Dominik Richter
+# license: All rights reserved
+
+class Script < Cmd
+  name 'script'
+  def initialize(script)
+    case vulcano.os[:family]
+    when 'windows'
+      # encodes a script as base64 to run as powershell encodedCommand
+      # this comes with performance issues: @see https://gist.github.com/fnichol/7b20596b950e65fb96f9
+      require 'winrm'
+      script = WinRM::PowershellScript.new(script)
+      cmd = "powershell -encodedCommand #{script.encoded}"
+    else
+      return skip_resource 'The `script` resource is not supported on your OS yet.'
+    end
+
+    @command = cmd
+  end
+
+  def result
+    @result ||= vulcano.command(@command)
+  end
+
+  def stdout
+    result.stdout
+  end
+
+  def stderr
+    result.stderr
+  end
+
+  def exit_status
+    result.exit_status.to_i
+  end
+end
