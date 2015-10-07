b7739a84be16e0ae3ec2d9a22008d3baf8fd93c6
Christoph Hartmann <chris@lollyrock.com>
2015-10-07 11:36:59 +0200

    implement group resource for windows

--- lib/resources/group.rb
+++ lib/resources/group.rb
@@ -11,11 +11,14 @@
 #  it { should have_gid 0 }
 # end
 
+require 'utils/convert'
+
 class Group < Vulcano.resource(1)
   name 'group'
 
-  def initialize(group)
-    @group = group
+  def initialize(groupname, domain = nil)
+    @group = groupname
+    @domain = domain
 
     # select group manager
     @group_provider = nil
@@ -23,6 +26,8 @@ class Group < Vulcano.resource(1)
     when 'ubuntu', 'debian', 'redhat', 'fedora', 'arch', 'darwin', 'freebsd'
       # use /etc/group
       @group_provider = UnixGroup.new(vulcano)
+    when 'windows'
+      @group_provider = WindowsGroup.new(vulcano)
     else
       return skip_resource 'The `group` resource is not supported on your OS yet.'
     end
@@ -31,18 +36,18 @@ class Group < Vulcano.resource(1)
   # verifies if a group exists
   def exists?
     # ensure that we found one group
-    !group_info.nil? && !group_info.groups.nil? && group_info.groups.size > 0
+    !group_info.nil? && group_info.size > 0
   end
 
   def gid
-    if group_info.nil? || group_info.gids.nil? || group_info.gids.size == 0
+    if group_info.nil? || group_info.size == 0
       return nil
-    elsif group_info.gids.size == 1
+    elsif group_info.size == 1
       # the default case should be one group
-      return group_info.gids[0]
+      return group_info[0][:gid]
     else
       # return array if we got multiple gids
-      return group_info.gids
+      return group_info.map { |grp| grp[:gid] }
     end
   end
 
@@ -51,11 +56,23 @@ class Group < Vulcano.resource(1)
     gid == compare_gid
   end
 
+  def local
+    if group_info.nil? || group_info.size == 0
+      return nil
+    elsif group_info.size == 1
+      # the default case should be one group
+      return group_info[0][:local]
+    else
+      # return array if we got multiple gids
+      return group_info.map { |grp| grp[:local] }
+    end
+  end
+
   private
 
   def group_info
     return @cache if !@cache.nil?
-    @cache = @group_provider.group_info(@group) if !@group_provider.nil?
+    @cache = @group_provider.group_info(@group, @domain) if !@group_provider.nil?
   end
 end
 
@@ -65,9 +82,47 @@ class GroupInfo
   end
 end
 
-# implements generic unix id handling
+# implements generic unix groups via /etc/group
 class UnixGroup < GroupInfo
-  def group_info(group)
-    @vulcano.etc_group.where(name: group)
+  include Converter
+
+  def group_info(group, _domain = nil)
+    @vulcano.etc_group.where(name: group).groups.map { |grp|
+      {
+        name: grp[0],
+        gid: convert_to_i(grp[2]),
+      }
+    }
+  end
+end
+
+class WindowsGroup < GroupInfo
+  def group_info(compare_group, compare_domain = nil)
+    cmd = @vulcano.command('Get-WmiObject Win32_Group | Select-Object -Property Caption, Domain, Name, SID, LocalAccount | ConvertTo-Json')
+
+    # cannot rely on exit code for now, successful command returns exit code 1
+    # return nil if cmd.exit_status != 0, try to parse json
+    begin
+      groups = JSON.parse(cmd.stdout)
+    rescue JSON::ParserError => _e
+      return nil
+    end
+
+    # ensure we have an array of groups
+    groups = [groups] if !groups.is_a?(Array)
+
+    # reduce list
+    groups.each_with_object([]) do |grp, grp_collection|
+      # map object
+      grp_info = {
+        name: grp['Name'],
+        domain: grp['Domain'],
+        caption: grp['Caption'],
+        gid: nil,
+        sid: grp['SID'],
+        local: grp['LocalAccount'],
+      }
+      grp_collection.push(grp_info) if grp_info[:name] == compare_group && (compare_domain.nil? || grp_info[:domain] == compare_domain)
+    end
   end
 end
