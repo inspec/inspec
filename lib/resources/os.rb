55e09963c841125ed94092d741e4b0e7ba99b77e
Christoph Hartmann <chris@lollyrock.com>
2015-10-07 18:28:34 +0200

    improvement: expose os detector function in os resource

--- lib/resources/os.rb
+++ lib/resources/os.rb
@@ -6,6 +6,13 @@ module Vulcano::Resources
   class OS < Vulcano.resource(1)
     name 'os'
 
+    # reuse helper methods from backend
+    %w{redhat? debian? suse? bsd? solaris? linux? unix? windows?}.each do |os_family|
+      define_method((os_family).to_sym) do
+        vulcano.backend.os.send(os_family)
+      end
+    end
+
     def [](name)
       vulcano.backend.os[name]
     end
