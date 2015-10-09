6f1181bc276f50c5bdb1650154627c6037756044
Christoph Hartmann <chris@lollyrock.com>
2015-10-09 15:26:31 +0200

    add windows support for bridge

--- lib/resources/bridge.rb
+++ lib/resources/bridge.rb
@@ -17,6 +17,8 @@ class Bridge < Vulcano.resource(1)
     @bridge_provider = nil
     if vulcano.os.linux?
       @bridge_provider = LinuxBridge.new(vulcano)
+    elsif vulcano.os.windows?
+      @bridge_provider = WindowsBridge.new(vulcano)
     else
       return skip_resource 'The `bridge` resource is not supported on your OS yet.'
     end
@@ -67,5 +69,41 @@ class LinuxBridge < BridgeDetection
       name: bridge_name,
       interfaces: interfaces,
     }
+  end
+end
+
+# Windows Bridge
+# select netadapter by adapter binding for windows
+# Get-NetAdapterBinding -ComponentID ms_bridge | Get-NetAdapter
+# @see https://technet.microsoft.com/en-us/library/jj130921(v=wps.630).aspx
+# RegKeys: HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Class\{4D36E972-E325-11CE-BFC1-08002BE10318}
+class WindowsBridge < BridgeDetection
+  def bridge_info(bridge_name)
+    # find all bridge adapters
+    cmd = @vulcano.command('Get-NetAdapterBinding -ComponentID ms_bridge | Get-NetAdapter | Select-Object -Property Name, InterfaceDescription | ConvertTo-Json')
+
+    # filter network interface
+    begin
+      bridges = JSON.parse(cmd.stdout)
+    rescue JSON::ParserError => _e
+      return nil
+    end
+
+    # ensure we have an array of groups
+    bridges = [bridges] if !bridges.is_a?(Array)
+
+    # select the requested interface
+    bridges = bridges.each_with_object([]) do |adapter, adapter_collection|
+      # map object
+      info = {
+        name: adapter['Name'],
+        interfaces: nil,
+      }
+      adapter_collection.push(info) if info[:name].casecmp(bridge_name) == 0
+    end
+
+    return nil if bridges.size == 0
+    warn "[Possible Error] detected multiple bridges interfaces with the name #{bridge_name}" if bridges.size > 1
+    bridges[0]
   end
 end
