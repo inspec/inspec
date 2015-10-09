2176039120b2432219231545ebe34687effd4826
Christoph Hartmann <chris@lollyrock.com>
2015-10-09 19:27:29 +0200

    add linux tests for host

--- test/unit/resource_host_test.rb
+++ test/unit/resource_host_test.rb
@@ -7,6 +7,20 @@ require 'vulcano/resource'
 
 describe 'Vulcano::Resources::Host' do
 
+  it 'check host on ubuntu' do
+    resource = MockLoader.new(:ubuntu1404).load_resource('host', 'example.com')
+    _(resource.resolvable?).must_equal true
+    _(resource.reachable?).must_equal true
+    _(resource.ipaddress).must_equal ['2606:2800:220:1:248:1893:25c8:1946']
+  end
+
+  it 'check host on centos 7' do
+    resource = MockLoader.new(:centos7).load_resource('host', 'example.com')
+    _(resource.resolvable?).must_equal true
+    _(resource.reachable?).must_equal true
+    _(resource.ipaddress).must_equal ['2606:2800:220:1:248:1893:25c8:1946']
+  end
+
   it 'check host on windows' do
     resource = MockLoader.new(:windows).load_resource('host', 'microsoft.com')
     _(resource.resolvable?).must_equal true
