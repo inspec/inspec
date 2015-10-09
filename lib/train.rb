54875705a11721ff8b55ba78ea0c318e6ff7b02e
Dominik Richter <dominik.richter@gmail.com>
2015-10-09 23:59:58 +0200

    create transport backend in train

--- lib/train.rb
+++ lib/train.rb
@@ -2,6 +2,19 @@
 #
 # Author:: Dominik Richter (<dominik.richter@gmail.com>)
 
+require 'train/version'
+require 'train/plugins'
+
 module Train
-  VERSION = '0.0.0'
+  # Create a new transport instance, with the plugin indicated by the
+  # given name.
+  #
+  # @param [String] name of the plugin
+  # @param [Array] *args list of arguments for the plugin
+  # @return [Transport] instance of the new transport or nil
+  def self.create(name, *args)
+    transport_class = Train::Plugins.registry[name]
+    return nil if transport_class.nil?
+    transport_class.new(*args)
+  end
 end
