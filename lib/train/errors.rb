3cdc83cd12cc85cb3524164afef8dbc7c2695728
Dominik Richter <dominik.richter@gmail.com>
2015-10-10 00:00:21 +0200

    provide common errors

---
+++ lib/train/errors.rb
@@ -0,0 +1,8 @@
+# encoding: utf-8
+#
+# Author:: Dominik Richter (<dominik.richter@gmail.com>)
+
+module Train
+  class TransportError < ::StandardError; end
+  class ClientError < ::StandardError; end
+end
