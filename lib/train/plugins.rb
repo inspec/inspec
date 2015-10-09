f1df1f608eea6bf187bfacca0bd246c5b379ffcb
Dominik Richter <dominik.richter@gmail.com>
2015-10-10 00:01:18 +0200

    add plugins structure

---
+++ lib/train/plugins.rb
@@ -0,0 +1,40 @@
+# encoding: utf-8
+#
+# Author:: Dominik Richter (<dominik.richter@gmail.com>)
+# Author:: Christoph Hartmann (<chris@lollyrock.com>)
+
+require 'train/errors'
+
+module Train
+  class Plugins
+    autoload :Transport, 'train/plugins/transport'
+
+    class << self
+      # Retrieve the current plugin registry, containing all plugin names
+      # and their transport handlers.
+      #
+      # @return [Hash] map with plugin names and plugins
+      def registry
+        @registry ||= {}
+      end
+    end
+  end
+
+  # Create a new plugin by inheriting from the class returned by this method.
+  # Create a versioned plugin by providing the transport layer plugin version
+  # to this method. It will then select the correct class to inherit from.
+  #
+  # The plugin version determins what methods will be available to your plugin.
+  #
+  # @param [Int] version = 1 the plugin version to use
+  # @return [Transport] the versioned transport base class
+  def self.plugin(version = 1)
+    if version != 1
+      fail ClientError,
+           'Only understand train plugin version 1. You are trying to '\
+           "initialize a train plugin #{version}, which is not supported "\
+           'in the current release of train.'
+    end
+    Train::Plugins::Transport
+  end
+end
---
+++ test/unit/plugins_test.rb
@@ -0,0 +1,22 @@
+# encoding: utf-8
+require 'helper'
+
+describe Train::Plugins do
+  it 'provides a method to create new v1 transport plugins' do
+    Train.plugin(1).must_equal Train::Plugins::Transport
+  end
+
+  it 'fails when called with an unsupported plugin version' do
+    proc {
+      Train.plugin(2)
+    }.must_raise Train::ClientError
+  end
+
+  it 'defaults to v1 plugins' do
+    Train.plugin.must_equal Train::Plugins::Transport
+  end
+
+  it 'provides a registry of plugins' do
+    Train::Plugins.registry.must_equal({})
+  end
+end
