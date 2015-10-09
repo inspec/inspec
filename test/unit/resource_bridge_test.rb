74729f5779f1c02008226a1ed9e30a972e00e3e6
Christoph Hartmann <chris@lollyrock.com>
2015-10-09 15:10:43 +0200

    add more unit test cases

--- test/unit/resource_bridge_test.rb
+++ test/unit/resource_bridge_test.rb
@@ -7,7 +7,7 @@ require 'vulcano/resource'
 
 describe 'Vulcano::Resources::Bridge' do
 
-  it 'check linux bridge' do
+  it 'check linux bridge on ubuntu' do
     resource = MockLoader.new(:ubuntu1404).load_resource('bridge', 'br0')
     _(resource.exists?).must_equal true
 
@@ -18,5 +18,31 @@ describe 'Vulcano::Resources::Bridge' do
 
     # get associated interfaces
     _(resource.interfaces).must_equal %w{eth1 eth2}
+  end
+
+  it 'check linux bridge on centos 7' do
+    resource = MockLoader.new(:centos7).load_resource('bridge', 'br0')
+    _(resource.exists?).must_equal true
+
+    # check network interfaced attached to bridge
+    _(resource.has_interface?('eth0')).must_equal false
+    _(resource.has_interface?('eth1')).must_equal true
+    _(resource.has_interface?('eth2')).must_equal true
+
+    # get associated interfaces
+    _(resource.interfaces).must_equal %w{eth1 eth2}
+  end
+
+  it 'check bridge on unsupported os' do
+    resource = MockLoader.new(:undefined).load_resource('bridge', 'br0')
+    _(resource.exists?).must_equal false
+
+    # check network interfaced attached to bridge
+    _(resource.has_interface?('eth0')).must_equal false
+    _(resource.has_interface?('eth1')).must_equal false
+    _(resource.has_interface?('eth2')).must_equal false
+
+    # get associated interfaces
+    _(resource.interfaces).must_equal nil
   end
 end
