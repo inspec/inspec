f7434c987f23b77f820471021ae9121bcc50001e
Dominik Richter <dominik.richter@gmail.com>
2015-10-11 18:03:39 +0200

    add back file functionality to local transport

--- lib/train/transports/local.rb
+++ lib/train/transports/local.rb
@@ -1,5 +1,8 @@
 # encoding: utf-8
 #
+# author: Dominik Richter
+# author: Christoph Hartmann
+
 require 'train/plugins'
 require 'mixlib/shellout'
 
@@ -7,12 +10,22 @@ module Train::Transports
   class Local < Train.plugin(1)
     name 'local'
 
+    autoload :File, 'train/transports/local_file'
+
+    def initialize
+      @files = {}
+    end
+
     def run_command(cmd)
       res = Mixlib::ShellOut.new(cmd)
       res.run_command
       CommandResult.new(res.stdout, res.stderr, res.exitstatus)
     rescue Errno::ENOENT => _
       CommandResult.new(nil, nil, 1)
+    end
+
+    def file(path)
+      @files[path] ||= File.new(self, path)
     end
   end
 end
