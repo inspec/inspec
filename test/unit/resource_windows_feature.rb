dd2fb7938fb9cadb7f3267bac4bc9025060dd561
Christoph Hartmann <chris@lollyrock.com>
2015-10-12 10:38:28 +0200

    ease windows feature test

--- test/unit/resource_windows_feature.rb
+++ test/unit/resource_windows_feature.rb
@@ -7,11 +7,8 @@ require 'vulcano/resource'
 
 describe 'Vulcano:Resources::Feature' do
   describe 'feature' do
-
-    let(:resource) { MockLoader.new(:windows).load_resource('windows_feature', 'dhcp') }
-
-    # TODO: set windows as mock os
     it 'verify windows feature parsing' do
+      resource = MockLoader.new(:windows).load_resource('windows_feature', 'dhcp')
       pkg = { name: 'DHCP', description: 'Dynamic Host Configuration Protocol (DHCP) Server enables you to centrally configure, manage, and provide temporary IP addresses and related information for client computers.', installed: false, type: 'windows-feature' }
       _(resource.info).must_equal pkg
       _(resource.installed?).must_equal false
