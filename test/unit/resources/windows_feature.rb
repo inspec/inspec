24184154dd1a2f9561e961646f76989ad6d6db54
Christoph Hartmann <chris@lollyrock.com>
2015-10-21 22:16:13 +0200

    more unit tests for windows feature

--- test/unit/resources/windows_feature.rb
+++ test/unit/resources/windows_feature.rb
@@ -6,12 +6,24 @@ require 'helper'
 require 'inspec/resource'
 
 describe 'Inspec::Resources::Feature' do
-  describe 'feature' do
-    it 'verify windows feature parsing' do
-      resource = MockLoader.new(:windows).load_resource('windows_feature', 'dhcp')
-      pkg = { name: 'DHCP', description: 'Dynamic Host Configuration Protocol (DHCP) Server enables you to centrally configure, manage, and provide temporary IP addresses and related information for client computers.', installed: false, type: 'windows-feature' }
-      _(resource.info).must_equal pkg
-      _(resource.installed?).must_equal false
-    end
+  it 'verify windows feature parsing' do
+    resource = MockLoader.new(:windows).load_resource('windows_feature', 'dhcp')
+    pkg = { name: 'DHCP', description: 'Dynamic Host Configuration Protocol (DHCP) Server enables you to centrally configure, manage, and provide temporary IP addresses and related information for client computers.', installed: false, type: 'windows-feature' }
+    _(resource.info).must_equal pkg
+    _(resource.installed?).must_equal false
+  end
+
+  it 'windows feature on Ubuntu' do
+    resource = MockLoader.new(:ubuntu1404).load_resource('windows_feature', 'powershell')
+    pkg = { name: 'powershell', type: 'windows-feature' }
+    _(resource.info).must_equal pkg
+    _(resource.installed?).must_equal false
+  end
+
+  it 'windows feature on undefined os' do
+    resource = MockLoader.new(:undefined).load_resource('windows_feature', 'powershell')
+    pkg = { name: 'powershell', type: 'windows-feature' }
+    _(resource.info).must_equal pkg
+    _(resource.installed?).must_equal false
   end
 end
