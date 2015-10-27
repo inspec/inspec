8d682973b0c5c08574eb58ec0a03333a2753e6c5
Dominik Richter <dominik.richter@gmail.com>
2015-10-27 15:48:17 +0100

    extend csv tests, remove dot-resolver

--- test/unit/resources/csv_test.rb
+++ test/unit/resources/csv_test.rb
@@ -6,10 +6,30 @@ require 'helper'
 require 'inspec/resource'
 
 describe 'Inspec::Resources::CSV' do
-  it 'verify csv parsing' do
-    resource = load_resource('csv', 'example.csv')
-    _(resource.params).wont_equal nil
-    _(resource.send('1.addressable')).must_equal 'astrolabe'
-    _(resource.send('addressable')).must_equal %w{ast astrolabe berkshelf}
+  describe 'when loading a valid csv' do
+    let (:resource) { load_resource('csv', 'example.csv') }
+    let (:params) {
+      {}
+    }
+
+    it 'captures an array of params' do
+      _(resource.params).must_be_kind_of Array
+    end
+
+    it 'gets all value lines' do
+      _(resource.params.length).must_equal 3
+    end
+
+    it 'captures a hashmap of entries of a line' do
+      _(resource.params[0]).must_be_kind_of Hash
+    end
+
+    it 'gets params by header fields' do
+      _(resource.params[0]['addressable']).must_equal 'ast'
+    end
+
+    it 'retrieves nil if a param is missing' do
+      _(resource.params[0]['missing']).must_be_nil
+    end
   end
 end
