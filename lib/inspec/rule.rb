8daf8dfa86dc6485c1d482983e4d4c7701de76e7
Dominik Richter <dominik.richter@gmail.com>
2015-10-27 03:07:38 +0100

    lint

--- lib/inspec/rule.rb
+++ lib/inspec/rule.rb
@@ -29,7 +29,7 @@ module Inspec
 
       opts = { 'caller' => calls[0][3] }
       if !calls[0][3].nil? && !calls[0][3].empty? &&
-             (m = calls[0][3][0].match(/^([^:]*):(\d+):/))
+         (m = calls[0][3][0].match(/^([^:]*):(\d+):/))
         opts['file_path'] = m[0]
         opts['line_number'] = m[1]
       end
