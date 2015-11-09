b33717fa551b31db7bc69091463340f73bf28260
Christoph Hartmann <chris@lollyrock.com>
2015-11-10 00:30:23 +0100

    add test case for upstart with systemv service

--- test/integration/test/integration/default/service_spec.rb
+++ test/integration/test/integration/default/service_spec.rb
@@ -26,3 +26,12 @@ describe service(available_service) do
   it { should be_installed }
   it { should be_running }
 end
+
+# extra test for ubuntu upstart with systemv service
+if os[:family] == 'ubuntu'
+  describe service('ntp') do
+    it { should be_enabled }
+    it { should be_installed }
+    it { should be_running }
+  end
+end
