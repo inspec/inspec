83a1e7180e03a699b90edc20e0bf4847c5e687d9
Christoph Hartmann <chris@lollyrock.com>
2015-11-17 22:12:19 +0100

    add integration test for registry key

--- test/integration/cookbooks/os_prepare/recipes/registry_key.rb
+++ test/integration/cookbooks/os_prepare/recipes/registry_key.rb
@@ -3,42 +3,77 @@
 #
 # change a few Windows registry keys for testing purposes
 
-# rule 'windows-audit-103'
-# title 'Configure System Event Log (System)'
-registry_key 'HKLM\Software\Policies\Microsoft\Windows\EventLog\System' do
-  values [{ name: 'MaxSize', type: :dword, data: 67_108_864 }]
-  recursive true
-  action :create
-end
+if node['platform_family'] == 'windows'
 
-# rule 'windows-base-101'
-# title 'Safe DLL Search Mode is Enabled'
-registry_key 'HKLM\System\CurrentControlSet\Control\Session Manager' do
-  values [{ name: 'SafeDllSearchMode', type: :dword, data: 1 }]
-  recursive true
-  action :create
-end
+  # create test key
+  registry_key 'HKLM\System\Test' do
+    values [{
+      :name => '',
+      :type => :string,
+      :data => 'test'
+    },{
+      :name => 'string value',
+      :type => :string,
+      :data => nil
+    },{
+      :name => 'binary value',
+      :type => :binary,
+      :data => 'dfa0f066'
+    },{
+      :name => 'dword value',
+      :type => :dword,
+      :data => 0
+    },{
+      :name => 'qword value',
+      :type => :qword,
+      :data => 0
+    },{
+      :name => 'multistring value',
+      :type => :multi_string,
+      :data => ['test', 'multi','string','data']
+    }]
+    recursive true
+    action :create
+  end
 
-# rule 'windows-base-103'
-# title 'All Shares are Configured to Prevent Anonymous Access'
-registry_key 'HKLM\System\CurrentControlSet\Services\LanManServer\Parameters' do
-  values [{ name: 'NullSessionShares', type: :multi_string, data: [] }]
-  recursive true
-  action :create
-end
+  # rule 'windows-audit-103'
+  # title 'Configure System Event Log (System)'
+  registry_key 'HKLM\Software\Policies\Microsoft\Windows\EventLog\System' do
+    values [{ name: 'MaxSize', type: :dword, data: 67_108_864 }]
+    recursive true
+    action :create
+  end
 
-# rule 'windows-ie-101'
-# title 'IE 64-bit tab'
-registry_key 'HKLM\Software\Policies\Microsoft\Internet Explorer\Main' do
-  values [{ name: 'Isolation64Bit', type: :dword, data: 1 }]
-  recursive true
-  action :create
-end
+  # rule 'windows-base-101'
+  # title 'Safe DLL Search Mode is Enabled'
+  registry_key 'HKLM\System\CurrentControlSet\Control\Session Manager' do
+    values [{ name: 'SafeDllSearchMode', type: :dword, data: 1 }]
+    recursive true
+    action :create
+  end
 
-# rule 'windows-rdp-101'
-# title 'Strong Encryption for Windows Remote Desktop Required'
-registry_key 'HKLM\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services' do
-  values [{ name: 'MinEncryptionLevel', type: :dword, data: 3 }]
-  recursive true
-  action :create
+  # rule 'windows-base-103'
+  # title 'All Shares are Configured to Prevent Anonymous Access'
+  registry_key 'HKLM\System\CurrentControlSet\Services\LanManServer\Parameters' do
+    values [{ name: 'NullSessionShares', type: :multi_string, data: [] }]
+    recursive true
+    action :create
+  end
+
+  # rule 'windows-ie-101'
+  # title 'IE 64-bit tab'
+  registry_key 'HKLM\Software\Policies\Microsoft\Internet Explorer\Main' do
+    values [{ name: 'Isolation64Bit', type: :dword, data: 1 }]
+    recursive true
+    action :create
+  end
+
+  # rule 'windows-rdp-101'
+  # title 'Strong Encryption for Windows Remote Desktop Required'
+  registry_key 'HKLM\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services' do
+    values [{ name: 'MinEncryptionLevel', type: :dword, data: 3 }]
+    recursive true
+    action :create
+  end
+
 end
