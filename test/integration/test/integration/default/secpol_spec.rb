9210c97840233b80e048609adb257a8f7d17c4cb
Jeremy J. Miller <jm@chef.io>
2015-12-16 21:46:27 -0500

    Cleaned up extra spaces in file

--- test/integration/test/integration/default/secpol_spec.rb
+++ test/integration/test/integration/default/secpol_spec.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 
 if os.windows?
-    describe security_policy do
+  describe security_policy do
     its('EnableAdminAccount') { should eq 1 }
     its('EnableGuestAccount') { should eq 0 }
   end
