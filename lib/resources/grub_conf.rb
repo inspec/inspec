1b1690a3e3516da12f3f58f3b09f55e2b4c6818a
Thomas Cate <tcate@chef.io>
2015-12-18 08:41:29 -0600

    improve examples

--- lib/resources/grub_conf.rb
+++ lib/resources/grub_conf.rb
@@ -10,8 +10,9 @@ class GrubConfig < Inspec.resource(1)
   example "
     describe grub_conf('/etc/grub.conf') do
       its('kernel') { should include '/vmlinuz-2.6.32-573.7.1.el6.x86_64' }
-      its('kernel') { should include 'audit=1' }
-      its('default') { should_not include '1' }
+      its('initrd') { should include '/initramfs-2.6.32-573.el6.x86_64.img=1' }
+      its('default') { should_not eq '1' }
+      its('timeout') { should eq '5' }
     end
   "
 
