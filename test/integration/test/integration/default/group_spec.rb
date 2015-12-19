5f7dccc1b460f8db947c82927d07cf70eb8440ae
Jeremy W. Chalfant <jchalfan@us.ibm.com>
2015-12-18 18:57:06 -0600

    aix group tests

--- test/integration/test/integration/default/group_spec.rb
+++ test/integration/test/integration/default/group_spec.rb
@@ -30,3 +30,20 @@ if os[:family] == 'freebsd'
     its('gid') { should eq nil }
   end
 end
+
+if os[:family] == 'aix'
+  describe group('system') do
+    it { should exist }
+    its('gid') { should eq 0 }
+  end
+
+  describe group('bin') do
+    it { should exist }
+    its('gid') { should eq 2 }
+  end
+
+  describe group('noroot') do
+    it { should_not exist }
+    its('gid') { should eq nil }
+  end
+end
