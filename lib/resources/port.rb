f5780b69a4df748a98224e6b0af3498b115218a7
Mark Harrison <mark@mivok.net>
2016-01-14 16:05:22 -0500

    Correctly detect UDP ports on linux
    netstat on UDP lines doesn't display a port state (e.g. LISTEN), so make this
    field optional when parsing the netstat line.

--- lib/resources/port.rb
+++ lib/resources/port.rb
@@ -213,7 +213,7 @@ class LinuxPorts < PortsInfo
   def parse_netstat_line(line)
     # parse each line
     # 1 - Proto, 2 - Recv-Q, 3 - Send-Q, 4 - Local Address, 5 - Foreign Address, 6 - State, 7 - Inode, 8 - PID/Program name
-    parsed = /^(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)/.match(line)
+    parsed = /^(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)?\s+(\S+)\s+(\S+)\s+(\S+)/.match(line)
 
     return {} if parsed.nil? || line.match(/^proto/i)
 
