4afb22565e6f1bd47983400215e466d53b3037c9
Stephan Renatus <srenatus@chef.io>
2016-01-28 17:08:41 +0100

    auditd_rules: teach old dog new tricks

--- lib/resources/auditd_rules.rb
+++ lib/resources/auditd_rules.rb
@@ -43,8 +43,38 @@ class AuditdRulesLegacy
   end
 end
 
+class FilterArray
+  attr_accessor :rules
+
+  def initialize(rules)
+    @rules = rules
+  end
+
+  # allows for chaining
+  # syscall('open').field('arch', 'b32').field('key', 'access').rules
+  def field(key, value)
+    res = rules.find_all { |r| r[key.to_sym] == value }
+    FilterArray.new(res)
+  end
+
+  def method_missing(meth, *args)
+    field(meth, args[0])
+  end
+
+  # # use iptables' matcher have_rule('-w ...')
+  # def has_rule?(rule)
+  #   if is_file?(rule)
+  #     perms = get_permissions(rule)
+  #     key = get_key(rule)
+  #   elsif is_syscall?(rule)
+  #     # ...
+  #   end
+  # end
+end
+
 class AuditDaemonRules < Inspec.resource(1)
   extend Forwardable
+  attr_accessor :rules, :lines
 
   name 'auditd_rules'
   desc 'Use the auditd_rules InSpec audit resource to test the rules for logging that exist on the system. The audit.rules file is typically located under /etc/audit/ and contains the list of rules that define what is captured in log files.'
@@ -56,6 +86,8 @@ class AuditDaemonRules < Inspec.resource(1)
       its('LIST_RULES') {should contain_match(/^exit,always arch=.* key=time-change syscall=clock_settime/)}
       its('LIST_RULES') {should contain_match(/^exit,always watch=\/etc\/localtime perm=wa key=time-change/)}
     end
+
+    # TODO(sr) new interface
   "
 
   def initialize
@@ -70,13 +102,95 @@ class AuditDaemonRules < Inspec.resource(1)
   end
 
   # non-legacy instances are not asked for `its('LIST_RULES')`
-  def_delegator :@legacy, :'LIST_RULES'
+  def LIST_RULES
+    return @legacy.LIST_RULES if @legacy
+    fail 'Using legacy auditd_rules LIST_RULES interface with non-legacy audit package. Please use the new syntax.'
+  end
 
   def parse_content
+    @rules = {
+      syscalls: [],
+      files: []
+    }
+    @lines = @content.lines.map(&:chomp)
 
+    lines.each do |line|
+      if is_syscall?(line)
+        syscalls = get_syscalls line
+        action, list = get_action_list line
+        key = get_key line
+        opts = get_opts line
+
+        # create a 'flatter' structure because sanity
+        syscalls.each do |s|
+          @rules[:syscalls] << { syscall: s, list: list, action: action, key: key, opts: opts }
+        end
+      elsif is_file?(line)
+        file = get_file line
+        perms = get_permissions line
+        key = get_key line
+
+        @rules[:files] << { file: file, key: key, permissions: perms }
+      end
+    end
+  end
+
+  def syscall(name)
+    select_name(:syscall, name)
+  end
+
+  def file(name)
+    select_name(:file, name)
   end
 
   def to_s
     'Audit Daemon Rules'
+  end
+
+  private
+
+  def select_name(key, name)
+    plural = "#{key}s".to_sym
+    res = rules[plural].find_all { |rule| rule[key] == name }
+    FilterArray.new(res)
+  end
+
+  def is_syscall?(line)
+    line.match /\ -S /
+  end
+
+  def is_file?(line)
+    line.match /-w /
+  end
+
+  def get_syscalls(line)
+    line.scan(/-S ([^ ]+)/).flatten
+  end
+
+  def get_action_list(line)
+    line.scan(/-a ([^,]+),([^ ]+)/).flatten
+  end
+
+  def get_key(line)
+    line.match(/-k ([^ ]+)/)[1]
+  end
+
+  # NOTE there are NO precautions wrt. filenames containing spaces in auditctl
+  # `auditctl -w /foo\ bar` gives the following line: `-w /foo bar -p rwxa`
+  def get_file(line)
+     line.match(/-w (.+) -p/)[1]
+  end
+
+  def get_permissions(line)
+    line.match(/-p ([^ ]+)/)[1]
+  end
+
+  def get_opts(line)
+    opts = {}
+    line.scan(/-F ([^= ]+)=([^ ]+)/).each do |key, val|
+      opts[key.to_s] = val
+    end
+
+    opts
   end
 end
--- test/unit/resources/auditd_rules_test.rb
+++ test/unit/resources/auditd_rules_test.rb
@@ -6,16 +6,59 @@ require 'helper'
 require 'inspec/resource'
 
 describe 'Inspec::Resources::AuditDaemonRules' do
-  it 'check legacy audit policy parsing' do
+  it 'auditd_rules interface' do
     resource = MockLoader.new(:undefined).load_resource('auditd_rules')
-    _(resource.send('LIST_RULES')).must_equal [
-      'exit,always syscall=rmdir,unlink',
-      'exit,always auid=1001 (0x3e9) syscall=open',
-      'exit,always watch=/etc/group perm=wa',
-      'exit,always watch=/etc/passwd perm=wa',
-      'exit,always watch=/etc/shadow perm=wa',
-      'exit,always watch=/etc/sudoers perm=wa',
-      'exit,always watch=/etc/secret_directory perm=r',
+    _(resource.send('lines')).must_equal [
+       '-a always,exit -F arch=b32 -S open -S openat -F exit=-EACCES -k access',
+       '-w /etc/ssh/sshd_config -p rwxa -k CFG_sshd_config',
     ]
   end
+
+  it 'auditd_rules syscall interface' do
+    resource = MockLoader.new(:undefined).load_resource('auditd_rules')
+    _(resource.send('syscall', 'open').send('rules')).must_equal [
+      { syscall: 'open', list: 'exit', action: 'always', key: 'access', opts: { 'arch' => 'b32', 'exit' => '-EACCES' } },
+    ]
+  end
+
+  it 'auditd_rules syscall query chaining' do
+    resource = MockLoader.new(:undefined).load_resource('auditd_rules')
+    _(resource.send('syscall', 'open').field('key', 'access').send('rules')).must_equal [
+      { syscall: 'open', list: 'exit', action: 'always', key: 'access', opts: { 'arch' => 'b32', 'exit' => '-EACCES' } },
+    ]
+  end
+
+  it 'auditd_rules syscall query chaining with short syntax' do
+    resource = MockLoader.new(:undefined).load_resource('auditd_rules')
+    _(resource.send('syscall', 'open').key('access').list('exit').send('rules')).must_equal [
+      { syscall: 'open', list: 'exit', action: 'always', key: 'access', opts: { 'arch' => 'b32', 'exit' => '-EACCES' } },
+    ]
+  end
+
+  it 'check auditd_rules syscall query chaining empty results' do
+    resource = MockLoader.new(:undefined).load_resource('auditd_rules')
+    _(resource.send('syscall', 'open').field('key', 'access').field('foo', 'bar').send('rules')).must_equal []
+  end
+
+
+  it 'check auditd_rules file interface' do
+    resource = MockLoader.new(:undefined).load_resource('auditd_rules')
+    _(resource.send('file', '/etc/ssh/sshd_config').send('rules')).must_equal [
+      { file: '/etc/ssh/sshd_config', key: 'CFG_sshd_config', permissions: 'rwxa'},
+    ]
+  end
+
+  # TODO(sr) figure out how to feed resource the legacy auditctl mock cmd output
+  # it 'check legacy audit policy parsing' do
+  #   resource = MockLoader.new(:undefined).load_resource('auditd_rules')
+  #   _(resource.send('LIST_RULES')).must_equal [
+  #     'exit,always syscall=rmdir,unlink',
+  #     'exit,always auid=1001 (0x3e9) syscall=open',
+  #     'exit,always watch=/etc/group perm=wa',
+  #     'exit,always watch=/etc/passwd perm=wa',
+  #     'exit,always watch=/etc/shadow perm=wa',
+  #     'exit,always watch=/etc/sudoers perm=wa',
+  #     'exit,always watch=/etc/secret_directory perm=r',
+  #   ]
+  # end
 end
