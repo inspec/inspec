826d059b1974aa3384d4bc1b38cc3cfc5fac8e42
Christoph Hartmann <chris@lollyrock.com>
2016-02-03 08:50:46 +0100

    optimize json logger for line delimited JSON

--- lib/utils/json_log.rb
+++ lib/utils/json_log.rb
@@ -5,22 +5,7 @@ require 'json'
 
 # a simple streaming json logger
 class Logger::JSONFormatter < Logger::Formatter
-  def initialize
-    @first = true
-    puts '['
-  end
-
-  def stop
-    puts ']'
-  end
-
   def call(severity, time, progname, msg)
-    if @first == false
-      print ','
-    else
-      @first = false
-      print ' '
-    end
     puts JSON.generate(
       {
         'progname'=> progname,
