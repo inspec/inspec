7815cefdedf79185be9bb3ae060130a4bf122478
Stephan Renatus <srenatus@chef.io>
2016-02-09 19:18:48 +0100

    iptables: adapt integration tests
    _Only_ CentOS 6 does not strip the quotes from comments.

--- test/integration/test/integration/default/iptables_spec.rb
+++ test/integration/test/integration/default/iptables_spec.rb
@@ -13,6 +13,13 @@ when  'rhel', 'centos'
   describe iptables do
     it { should have_rule('-A INPUT -i eth0 -p tcp -m tcp --dport 80 -m state --state NEW -m comment --comment "http on 80" -j ACCEPT') }
     it { should_not have_rule('-A INPUT -i eth1 -p tcp -m tcp --dport 80 -j ACCEPT') }
-    it { should have_rule('-A derby-cognos-web -p tcp -m tcp --dport 80 -m comment --comment "derby-cognos-web" -j ACCEPT') }
   end
+
+  describe iptables do
+    it { should have_rule('-A derby-cognos-web -p tcp -m tcp --dport 80 -m comment --comment "derby-cognos-web" -j ACCEPT') }
+  end if os[:release] == 6
+
+  describe iptables do
+    it { should have_rule('-A derby-cognos-web -p tcp -m tcp --dport 80 -m comment --comment derby-cognos-web -j ACCEPT') }
+  end if os[:release] == 7
 end
