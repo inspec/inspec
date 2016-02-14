96d02ba4a260cf5adccbd352118ab4aeb204984f
Christoph Hartmann <chris@lollyrock.com>
2016-02-14 21:27:40 +0100

    add inspec profile as example

---
+++ lib/bundles/inspec-init/templates/profile/controls/example.rb
@@ -0,0 +1,20 @@
+# encoding: utf-8
+# copyright: 2015, The Authors
+# license: All rights reserved
+
+title 'sample section'
+
+# you can also use plain tests
+describe file('/tmp') do
+  it { should be_directory }
+end
+
+# you add controls here
+control 'tmp-1.0' do                        # A unique ID for this control
+  impact 0.7                                # The criticality, if this control fails.
+  title 'Create /tmp directory'             # A human-readable title
+  desc 'An optional description...'
+  describe file('/tmp') do                  # The actual test
+    it { should be_directory }
+  end
+end
