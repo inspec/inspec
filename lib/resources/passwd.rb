d9427b3aac0234c3d00a9938f0753d6301b75813
Dominik Richter <dominik.richter@gmail.com>
2016-02-17 12:35:46 +0100

    add filter to passwd

--- lib/resources/passwd.rb
+++ lib/resources/passwd.rb
@@ -13,88 +13,112 @@
 # - home directory
 # - command
 
-# usage:
-#
-# describe passwd do
-#   its(:usernames) { should eq ['root'] }
-#   its(:uids) { should eq [0] }
-# end
-#
-# describe passwd.uid(0) do
-#   its(:username) { should eq 'root' }
-#   its(:count) { should eq 1 }
-# end
-
 require 'utils/parser'
 
 class Passwd < Inspec.resource(1)
   name 'passwd'
   desc 'Use the passwd InSpec audit resource to test the contents of /etc/passwd, which contains the following information for users that may log into the system and/or as users that own running processes.'
   example "
+    describe passwd do
+      its('usernames') { should_not include 'forbidden_user' }
+    end
+
     describe passwd.uid(0) do
       its('username') { should eq 'root' }
       its('count') { should eq 1 }
+    end
+
+    describe passwd.name(/oot/) do
+      ...
     end
   "
 
   include PasswdParser
 
   attr_reader :uid
-  attr_reader :parsed
+  attr_reader :params
+  attr_reader :content
+  attr_reader :lines
 
-  def initialize(path = nil)
+  def initialize(path = nil, opts = nil)
+    opts ||= {}
     @path = path || '/etc/passwd'
-    @content = inspec.file(@path).content
-    @parsed = parse_passwd(@content)
+    @content = opts[:content] || inspec.file(@path).content
+    @lines = @content.to_s.split("\n")
+    @filters = opts[:filters] || ''
+    @params = parse_passwd(@content)
   end
 
-  # call passwd().uid(0)
-  # returns a seperate object with reference to this object
-  def uid(uid)
-    PasswdUid.new(self, uid)
+  def filter(hm = {})
+    return self if hm.nil? || hm.empty?
+    res = @params
+    filters = ''
+    hm.each do |attr, condition|
+      filters += " #{attr} = #{condition.inspect}"
+      res = res.find_all do |line|
+        case line[attr.to_s]
+        when condition
+          true
+        else
+          false
+        end
+      end
+    end
+    content = res.map { |x| x.values.join(':') }.join("\n")
+    Passwd.new(@path, content, @filters + filters)
   end
 
   def usernames
-    map_data('name')
+    warn '[DEPRECATION] `passwd.usernames` is deprecated. Please use `passwd.users` instead.'
+    users
+  end
+
+  def users
+    map_data('user')
+  end
+
+  def user(name)
+    filter(name: name)
   end
 
   def passwords
     map_data('password')
   end
 
+  def password(pw)
+    filter(password: pw)
+  end
+
   def uids
     map_data('uid')
+  end
+
+  def uid(uid)
+    filter(uid: uid)
   end
 
   def gids
     map_data('gid')
   end
 
+  def gid(gid)
+    filter(gid: gid)
+  end
+
   def to_s
-    '/etc/passwd'
+    f = @filters.empty? ? '' : ' with'+@filters
+    "/etc/passwd#{f}"
+  end
+
+  def count
+    @params.length
   end
 
   private
 
   def map_data(id)
-    @parsed.map {|x|
+    @params.map {|x|
       x[id]
     }
-  end
-end
-
-# object that hold a specifc uid view on passwd
-class PasswdUid
-  def initialize(passwd, uid)
-    @passwd = passwd
-    @users = @passwd.parsed.select { |x| x['uid'] == uid.to_s }
-  end
-
-  def username
-    @users.at(0)['name']
-  end
-
-  def count
-    @users.size
   end
 end
