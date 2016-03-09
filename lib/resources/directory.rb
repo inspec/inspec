387415859e9ce48cabae8dfc999256ec71501bd2
Dominik Richter <dominik.richter@gmail.com>
2016-03-09 10:35:35 +0100

    rename internal `File` -> `FileResource`

--- lib/resources/directory.rb
+++ lib/resources/directory.rb
@@ -5,7 +5,7 @@
 require 'resources/file'
 
 module Inspec::Resources
-  class Directory < File
+  class Directory < FileResource
     name 'directory'
     desc 'Use the directory InSpec audit resource to test if the file type is a directory. This is equivalent to using the file InSpec audit resource and the be_directory matcher, but provides a simpler and more direct way to test directories. All of the matchers available to file may be used with directory.'
     example "
