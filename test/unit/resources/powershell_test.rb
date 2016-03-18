32a065239caf210053c8b78a630dc0f96936fa30
Christoph Hartmann <chris@lollyrock.com>
2016-03-18 15:47:00 +0100

    update unit test, add integration test

--- test/unit/resources/powershell_test.rb
+++ test/unit/resources/powershell_test.rb
@@ -12,7 +12,16 @@ describe 'Inspec::Resources::Script' do
     Get-Help Get-Command
   EOH
 
-  it 'check if script for windows is properly generated ' do
+  it 'check if `powershell` for windows is properly generated ' do
+    resource = MockLoader.new(:windows).load_resource('powershell', ps1_script)
+    if Gem.loaded_specs['winrm'].version < Gem::Version.new('1.6.1')
+      _(resource.command).must_equal 'powershell -encodedCommand IAAgACAAIAAjACAAYwBhAGwAbAAgAGgAZQBsAHAAIABmAG8AcgAgAGcAZQB0ACAAYwBvAG0AbQBhAG4AZAAKACAAIAAgACAARwBlAHQALQBIAGUAbABwACAARwBlAHQALQBDAG8AbQBtAGEAbgBkAAoA'
+    else
+      _(resource.command).must_equal 'powershell -encodedCommand JABQAHIAbwBnAHIAZQBzAHMAUAByAGUAZgBlAHIAZQBuAGMAZQA9ACcAUwBpAGwAZQBuAHQAbAB5AEMAbwBuAHQAaQBuAHUAZQAnADsAIAAgACAAIAAjACAAYwBhAGwAbAAgAGgAZQBsAHAAIABmAG8AcgAgAGcAZQB0ACAAYwBvAG0AbQBhAG4AZAAKACAAIAAgACAARwBlAHQALQBIAGUAbABwACAARwBlAHQALQBDAG8AbQBtAGEAbgBkAAoA'
+    end
+  end
+
+  it 'check if legacy `script` for windows is properly generated ' do
     resource = MockLoader.new(:windows).load_resource('script', ps1_script)
     if Gem.loaded_specs['winrm'].version < Gem::Version.new('1.6.1')
       _(resource.command).must_equal 'powershell -encodedCommand IAAgACAAIAAjACAAYwBhAGwAbAAgAGgAZQBsAHAAIABmAG8AcgAgAGcAZQB0ACAAYwBvAG0AbQBhAG4AZAAKACAAIAAgACAARwBlAHQALQBIAGUAbABwACAARwBlAHQALQBDAG8AbQBtAGEAbgBkAAoA'
