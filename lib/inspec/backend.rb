856460054f10b40d59168165dc9c54b45a249539
Dominik Richter <dominik.richter@gmail.com>
2016-03-24 23:14:12 +0100

    dont fail with stacktrace on connection errors

--- lib/inspec/backend.rb
+++ lib/inspec/backend.rb
@@ -37,6 +37,11 @@ module Inspec
       end
 
       cls.new
+
+    rescue Train::ClientError => e
+      raise "Client error, can't connect to '#{name}' backend: #{e.message}"
+    rescue Train::TransportError => e
+      raise "Transport error, can't connect to '#{name}' backend: #{e.message}"
     end
   end
 end
