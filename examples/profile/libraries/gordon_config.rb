825702014ce748ad4ca719c83e132fe124226f00
Alex Pop <al3xpop@gmail.com>
2016-03-24 19:19:14 +0000

    update gordon with a bit of ruby and update README

--- examples/profile/libraries/gordon_config.rb
+++ examples/profile/libraries/gordon_config.rb
@@ -1,16 +1,42 @@
 require 'yaml'
 
+# Custom resource based on the InSpec resource DSL
 class GordonConfig < Inspec.resource(1)
   name 'gordon_config'
 
+  desc "
+    Gordon's resource description ...
+  "
+
+  example "
+    describe gordon_config do
+      its('version') { should eq('1.0') }
+      its('size') { should > 1 }
+    end
+  "
+
+  # Load the configuration file on initialization
   def initialize
-    @path = '/etc/gordon/config.yaml'
+    @path = '/tmp/gordon/config.yaml'
     @file = inspec.file(@path)
     return skip_resource "Can't find file \"#{@path}\"" if !@file.file?
 
-    @params = YAML.load(@file.content)
+    # Protect from invalid YAML content
+    begin
+      @params = YAML.load(@file.content)
+    rescue Exception
+      return skip_resource "#{@file}: #{$!}"
+    end
+    add_some_extra_params
   end
 
+  # Extra Ruby helper method
+  def add_some_extra_params
+    @params['size']=@file.size
+    @params['md5sum']=@file.md5sum
+  end
+
+  # Expose all parameters
   def method_missing(name)
     @params[name.to_s]
   end
