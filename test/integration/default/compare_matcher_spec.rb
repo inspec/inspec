34a22a290eed400ea2f7a2df668a18ea5a3deb60
Alex Pop <al3xpop@gmail.com>
2016-04-20 10:39:46 +0100

    add more cmp matcher tests

--- test/integration/default/compare_matcher_spec.rb
+++ test/integration/default/compare_matcher_spec.rb
@@ -35,24 +35,64 @@ if os.linux?
     it { should cmp len.to_s }
   end
 
+  describe 122 do
+    it { should cmp 122 }
+    it { should cmp < 133 }
+    it { should cmp > 111 }
+    it { should_not cmp < 122 }
+    it { should_not cmp > 122 }
+    it { should cmp <= 122 }
+    it { should cmp >= 122 }
+    it { should cmp >= -666 }
+    it { should_not cmp <= 111 }
+    it { should_not cmp >= 133 }
+    it { should cmp /122/ }
+    it { should_not cmp /133/ }
+  end
+
+  describe 0o640 do
+    it { should cmp 0o640 }
+    it { should cmp > 100 }
+    it { should_not cmp <= 0 }
+    it { should cmp == '416' }
+    it { should cmp /416$/ }
+  end
+
   describe '12' do
+    it { should cmp 12 }
     it { should cmp < 13 }
     it { should cmp > 11 }
     it { should_not cmp < 12 }
     it { should_not cmp > 12 }
     it { should cmp <= 12 }
     it { should cmp >= 12 }
+    it { should cmp >= -666 }
     it { should_not cmp <= 11 }
     it { should_not cmp >= 13 }
+  end
+
+  describe 'some 123' do
+    it { should cmp 'some 123' }
+    it { should cmp /^SOME\s\d+(1|2|3)3/i }
+    it { should_not cmp /^SOME/ }
+  end
+
+  describe ['some-array'] do
+    it { should cmp 'some-array' }
+    it { should cmp /.+/ }
+    it { should_not cmp /^SOME/ }
   end
 
   describe '' do
     it { should_not cmp >= 3 }
     it { should_not cmp < 3 }
+    it { should_not cmp /.+/ }
   end
 
   describe nil do
+    it { should cmp nil }
     it { should_not cmp >= 3 }
     it { should_not cmp < 3 }
+    it { should_not cmp /something/ }
   end
 end
