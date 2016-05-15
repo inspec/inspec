cfcc06a379968832d46d388000bbcc24a3d2f953
Jeremy J. Miller <jm@chef.io>
2016-05-15 11:04:23 -0400

    fix spelling

--- lib/utils/base_cli.rb
+++ lib/utils/base_cli.rb
@@ -30,7 +30,7 @@ module Inspec
       option :sudo_options, type: :string,
         desc: 'Additional sudo options for a remote scan.'
       option :sudo_command, type: :string,
-        desc: 'Altnerate command for sudo.'
+        desc: 'Alternate command for sudo.'
       option :ssl, type: :boolean,
         desc: 'Use SSL for transport layer encryption (WinRM).'
       option :self_signed, type: :boolean,
