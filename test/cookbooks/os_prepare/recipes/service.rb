1c9d998afde7dd7c7dd964586e285ee94eedd46b
Christoph Hartmann <chris@lollyrock.com>
2016-08-04 15:13:50 +0200

    do not run runit and upstart in docker since chef run fails

--- test/cookbooks/os_prepare/recipes/service.rb
+++ test/cookbooks/os_prepare/recipes/service.rb
@@ -13,7 +13,7 @@ when 'ubuntu'
 when 'centos'
   # install runit for alternative service mgmt
   if node['platform_version'].to_i == 6
-    include_recipe 'os_prepare::_runit_service_centos'
-    include_recipe 'os_prepare::_upstart_service_centos'
+    include_recipe 'os_prepare::_runit_service_centos' unless node['osprepare']['docker']
+    include_recipe 'os_prepare::_upstart_service_centos' unless node['osprepare']['docker']
   end
 end
