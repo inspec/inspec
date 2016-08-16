a381e406c41539fb6802cdcb074e122a53bd338d
Christoph Hartmann <chris@lollyrock.com>
2016-08-16 14:57:34 +0200

    add integration tests for file permissions on windows

--- test/integration/default/file_spec.rb
+++ test/integration/default/file_spec.rb
@@ -192,4 +192,10 @@ if os.windows?
     # Only works on Windows 2012 R2
     its('file_version') { should eq '6.3.9600.17415' }
   end
+
+  # read the owner of a file
+  describe directory('C:/opscode/chef') do
+    its('owner') { should cmp 'NT AUTHORITY\SYSTEM' }
+    it { should be_owned_by 'NT AUTHORITY\SYSTEM' }
+  end
 end
