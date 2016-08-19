95029203cd46c3af948bd93be9463bbd0be2c16f
Christoph Hartmann <chris@lollyrock.com>
2016-08-19 09:40:54 +0200

    unique controls for dependency tests

--- test/unit/mock/profiles/dependencies/inheritance/controls/example.rb
+++ test/unit/mock/profiles/dependencies/inheritance/controls/example.rb
@@ -3,11 +3,12 @@
 include_controls 'profile_a'
 include_controls 'profile_b'
 include_controls 'os-hardening' do
-  skip_control 'package-01'
-  skip_control 'package-02'
-  skip_control 'package-03'
-  skip_control 'package-04'
-  skip_control 'package-05'
+  skip_control 'os-01'
+
+  1.upto(5) do |i|
+    skip_control "package-%02d" % i
+  end
+
   1.upto(33) do |i|
     skip_control "sysctl-%02d" % i
   end
--- test/unit/mock/profiles/dependencies/profile_b/controls/example.rb
+++ test/unit/mock/profiles/dependencies/profile_b/controls/example.rb
@@ -10,7 +10,7 @@ describe file('/tmp') do
 end
 
 # you add controls here
-control 'tmp-1.0' do                        # A unique ID for this control
+control 'profileb-1' do                        # A unique ID for this control
   impact 0.7                                # The criticality, if this control fails.
   title 'Create /tmp directory'             # A human-readable title
   desc 'An optional description...'
