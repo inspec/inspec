11a7c9f0da2039536ff3fee3668f1e23cbc92656
Victoria Jeffrey <vjeffrey@chef.io>
2016-08-25 07:44:19 -0400

    fix lint error

--- tasks/get_command_output.rb
+++ tasks/get_command_output.rb
@@ -1,13 +1,13 @@
+# encoding: utf-8
 require 'open3'
 
-commands = {'inspec_exec' => 'inspec exec examples/profile/controls/example.rb', 'inspec_version' => 'inspec version'}
+commands = { 'inspec_exec' => 'inspec exec examples/profile/controls/example.rb', 'inspec_version' => 'inspec version' }
 
 commands.each do |keyname, command|
-  stdout, stdeerr, status = Open3.capture3(command)
+  stdout, _stdeerr, _status = Open3.capture3(command)
 
   # filename should be key.txt...and we should be saving it somewhere in www/
-  out_file = File.new("#{keyname}.txt", "w")
+  out_file = File.new("#{keyname}.txt", 'w')
   out_file.puts(stdout)
   out_file.close
 end
-
