0f501b811c6f4576364b528845709b292741de94
Steven Danna <steve@chef.io>
2016-09-02 12:11:01 +0100

    Add simple GitHub fetcher
    This adds a fetcher for sources that are on github.com. It resolves
    branch and tag references using the GitHub API and constructs an https
    url to a tar archive that GitHub provides.  This allows us to support
    GitHub-based sources without having the git executable on disk.

---
+++ test/unit/fetchers/github_test.rb
@@ -0,0 +1,55 @@
+# encoding: utf-8
+
+require 'helper'
+require 'uri'
+
+describe Fetchers::Github do
+  let(:fetcher) { Fetchers::Github }
+  let(:ref_for_test) { "0ca497e333a3222e4b517bc3ebe43eba01986c4c" }
+  let(:tag_ref_for_test) { "202d4e0f97b5dc2b88df13d686eae36320a5a53c" }
+  let(:uri_without_spec) { URI.parse("github://chef/inspec") }
+  let(:uri_with_branch) { URI.parse("github://chef/inspec/branch/master") }
+  let(:uri_with_tag) { URI.parse("github://chef/inspec/tag/v0.32.0") }
+  let(:uri_with_ref) { URI.parse("github://chef/inspec/ref/#{ref_for_test}") }
+
+  it 'registers with the fetchers registry' do
+    reg = Inspec::Fetcher.registry
+    reg['github'].must_equal fetcher
+  end
+
+  it "transforms a github url to an https url" do
+    f = fetcher.new(uri_without_spec)
+    f.expanded_target.must_match(/^https/)
+  end
+
+  it "transforms a github url with no branch, tag, or ref to an https pointing at master" do
+    f1 = fetcher.new(uri_without_spec)
+    f2 = fetcher.new(uri_with_branch)
+    f1.expanded_target.must_equal f2.expanded_target
+  end
+
+  it "transforms a github url with a branch to a tarball of an exact sha" do
+    f = fetcher.new(uri_with_branch)
+    # We don't know what master of inspec will point to.  We can clean
+    # this up if we mock out the API responses from Octocat
+    f.expanded_target.must_match(/[0-9a-z]{40}.tar.gz$/)
+  end
+
+  it "transforms a github url with a ref to a tarball of an exact sha" do
+    f = fetcher.new(uri_with_tag)
+    f.expanded_target.must_equal("https://github.com/chef/inspec/archive/#{tag_ref_for_test}.tar.gz")
+  end
+
+  it "transforms a github url with a ref to a tarball of that same ref" do
+    f = fetcher.new(uri_with_ref)
+    f.expanded_target.must_equal("https://github.com/chef/inspec/archive/#{ref_for_test}.tar.gz")
+  end
+
+  describe "resolve" do
+    it "handles our temporary api" do
+      source = "github://chef/inspec"
+      r = fetcher.resolve(source)
+      r.wont_be_nil
+    end
+  end
+end
