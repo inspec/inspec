9eb3711bbd9c746f28f40e3145f1e60cd144ca3a
Dominik Richter <dominik.richter@gmail.com>
2016-09-12 01:26:00 +0200

    add homepage

---
+++ www/config.rb
@@ -0,0 +1,50 @@
+require 'slim'
+
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+# With alternative layout
+# page 'docs/*', layout: :sidebar, locals: { sidebar_layout: 'docs' }
+
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy '/this-page-has-no-template.html', '/template-file.html', locals: {
+#  which_fake_page: 'Rendering a fake page with a local variable' }
+
+###
+# Helpers
+###
+
+# Reload the browser automatically whenever files change
+configure :development do
+  activate :livereload
+end
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     'Helping'
+#   end
+# end
+#
+# Build-specific configuration
+configure :build do
+  # Minify CSS on build
+  activate :minify_css
+
+  # Minify Javascript on build
+  activate :minify_javascript
+end
+
+activate :autoprefixer
+activate :directory_indexes
+set :trailing_slash, false
+set :markdown_engine, :kramdown
+set :markdown, coderay_line_numbers: :table
