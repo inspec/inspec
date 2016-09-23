2e3d287f7d3d39177bccee6984bcef7a31d5c3c6
Dominik Richter <dominik.richter@gmail.com>
2016-09-23 12:29:24 +0200

    bugfix: build all online tutorial assets

---
+++ tasks/shared.rb
@@ -0,0 +1,26 @@
+# encoding: utf-8
+# Copyright:: Copyright (c) 2015 Chef Software, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+module Log
+  def self.section(msg)
+    puts "\n\033[36;1m====> #{msg}\033[0m"
+  end
+
+  def self.info(msg)
+    puts "\033[32;1m----> #{msg}\033[0m"
+  end
+end
