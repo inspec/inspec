08b1a5da8872e16091f21b2cb64440e4e7c6ae47
Maggie Walker <magwalk@gmail.com>
2016-09-26 12:45:24 -0700

    Website: Fix docs nav functionality and optimize for mobile
    A few minor styling fixes for the docs nav for mobile, refactoring
    the markup to remove unneeded parent elements, and fixing a
    bug where the nav overlapped with the footer if the window was
    too short.

--- www/source/javascripts/nav.js
+++ www/source/javascripts/nav.js
@@ -2,6 +2,11 @@
 const $navLinks = $('.main-nav--links');
 const $navToggle = $('.main-nav--toggle');
 const navBreakpoint = 730; // this should match $nav-breakpoint in _nav.scss
+const $mainContent = $('#main-content');
+
+$(document).ready(function() {
+  $mainContent.css('min-height', $('#main-nav').outerHeight() - $('#main-nav-ctas').outerHeight());
+});
 
 $navToggle.click(function() {
   $(this).toggleClass('is-active');
@@ -13,4 +18,20 @@ $(window).resize(function() {
     $navToggle.removeClass('is-active');
     $navLinks.attr("style", "");
   }
+});
+
+// toggles fixed nav position when the window is too short
+const footerOffsetTop = $("#main-footer").offset().top;
+var navOffsetBottom;
+
+function toggleFixedNavPosition() {
+  navOffsetBottom = $("#main-nav").outerHeight() + $(window).scrollTop();
+
+  $("#main-nav").toggleClass("is-fixed-bottom", footerOffsetTop < navOffsetBottom)
+}
+
+toggleFixedNavPosition();
+
+$(window).scroll(function() {
+  toggleFixedNavPosition();
 });
