944040fcf9a87c39d0bcd1162d50b823155f55af
jkerry <john@kerryhouse.net>
2016-11-22 14:43:19 -0500

    adding functional tests for the junit rspec formatter
    Signed-off-by: jkerry <john@kerryhouse.net>

--- test/functional/inspec_exec_junit_test.rb
+++ test/functional/inspec_exec_junit_test.rb
@@ -4,13 +4,6 @@
 require 'functional/helper'
 require 'nokogiri'
 
-# <testsuite name="rspec" tests="2" failures="0" errors="0" time="0.004459" timestamp="2016-11-20T21:43:51-05:00">
-#   <!-- Randomized with seed 39231 -->
-#   <properties/>
-#   <testcase classname="lib.inspec.rule" name="File /tmp should be directory" file="./lib/inspec/rule.rb" time="0.003060"/>
-#   <testcase classname="lib.inspec.rule" name="File /tmp should be directory" file="./lib/inspec/rule.rb" time="0.000217"/>
-# </testsuite>
-
 describe 'inspec exec with junit formatter' do
   include FunctionalHelper
 
@@ -26,23 +19,9 @@ describe 'inspec exec with junit formatter' do
     out = inspec('exec ' + example_profile + ' --format junit --no-create-lockfile')
     out.stderr.must_equal ''
     out.exit_status.must_equal 0
-    puts out.stdout
     doc = Nokogiri::XML(out.stdout)
     doc.errors.length.must_equal 0
   end
-
-# <?xml version="1.0" encoding="UTF-8"?>
-# <testsuite name="rspec" tests="5" failures="0" errors="0" time="0.025467" timestamp="2016-11-20T22:42:53-05:00">
-#   <!-- Randomized with seed 35012 -->
-#   <properties/>
-#   <testcase classname="lib.inspec.rule" name="File /tmp should be directory" file="./lib/inspec/rule.rb" time="0.001841"/>
-#   <testcase classname="lib.inspec.rule" name="File /tmp should be directory" file="./lib/inspec/rule.rb" time="0.000128"/>
-#   <testcase classname="lib.inspec.runner" name="gordon_config Can't find file &quot;/tmp/gordon/config.yaml&quot;" file="./lib/inspec/runner.rb" time="0.000011">
-#     <skipped/>
-#   </testcase>
-#   <testcase classname="examples.profile.controls.gordon" name="File  content should match nil" file="./examples/profile/controls/gordon.rb" time="0.001176"/>
-#   <testcase classname="lib.inspec.rule" name="File /bin/sh should be owned by &quot;root&quot;" file="./lib/inspec/rule.rb" time="0.019848"/>
-# </testsuite>
 
   describe 'execute a profile with junit formatting' do
     let(:doc) { Nokogiri::XML(inspec('exec ' + example_profile + ' --format junit --no-create-lockfile').stdout) }
@@ -60,6 +39,10 @@ describe 'inspec exec with junit formatter' do
         suite.xpath("//properties").children.length.must_equal 0
       end
 
+      it 'must have 5 testcase children' do
+        suite.xpath("//testcase").length.must_equal 5
+      end
+
       it 'has the tests attribute with 5 total tests' do
         suite["tests"].must_equal "5"
       end
@@ -71,5 +54,23 @@ describe 'inspec exec with junit formatter' do
       it 'has the errors attribute with 0 total tests' do
         suite["errors"].must_equal "0"
       end
+
+      it 'has 2 elements named "File /tmp should be directory"' do
+        suite.xpath("//testcase[@name='File /tmp should be directory']").length.must_equal 2
+      end
+
+      describe 'the testcase named "gordon_config Can\'t find file ..."' do
+        let(:gordon_yml_tests) { suite.xpath("//testcase[@classname='lib.inspec.runner']") }
+        let(:first_gordon_test) {gordon_yml_tests.first}
+
+        it 'should be unique' do
+          gordon_yml_tests.length.must_equal 1
+        end
+
+        it 'should be skipped' do
+          first_gordon_test.xpath("//skipped").length.must_equal 1
+        end
+      end
     end
+  end
 end
