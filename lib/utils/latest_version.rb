f0cdad800ff36e344de1f5f8c305ed0c4fca282e
Christoph Hartmann <chris@lollyrock.com>
2016-12-15 12:02:01 +0100

    display if inspec version is outdated
    Signed-off-by: Christoph Hartmann <chris@lollyrock.com>

---
+++ lib/utils/latest_version.rb
@@ -0,0 +1,20 @@
+# encoding: utf-8
+# author: Christoph Hartmann
+
+require 'json'
+require 'net/http'
+
+class LatestInSpecVersion
+  # fetches the latest version from rubygems server
+  def latest
+    uri = URI('https://rubygems.org/api/v1/gems/inspec.json')
+    res = Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') {|http|
+      http.read_timeout = 0.5
+      http.get(uri.path)
+    }
+    inspec_info = JSON.parse(res.body)
+    inspec_info['version']
+  rescue Exception # rubocop:disable Lint/RescueException
+    nil
+  end
+end
