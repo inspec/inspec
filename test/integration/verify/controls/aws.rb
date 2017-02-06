a3a238aafc9dc1e88fbd205f1c432cf9ae867cd7
Chris Redekop <chris.redekop@d2l.com>
2017-02-06 12:06:01 +0000

    #13, Ec2#exists returns false positives
    Signed-off-by: Chris Redekop <chris.redekop@d2l.com>

--- test/integration/verify/controls/aws.rb
+++ test/integration/verify/controls/aws.rb
@@ -1,5 +1,9 @@
 describe aws_ec2(name: 'Example') do
-  it { should be_running }
+  it { should exist }
   its('image_id') { should eq 'ami-0d729a60' }
   its('instance_type') { should eq 't2.micro' }
+end
+
+describe aws_ec2('i-missing') do
+  it { should_not exist }
 end
