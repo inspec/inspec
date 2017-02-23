983900a135944e15a52895deaacfa29baa3f35e7
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2017-02-23 12:46:15 +0000

    Moved all libraries into one file #11
    Signed-off-by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

--- libraries/azure_rg.rb
+++ libraries/azure_rg.rb
@@ -1,5 +1,5 @@
 
-require File.expand_path(File.dirname(__FILE__) + '/common/helpers')
+require 'azure_backend'
 
 # Class to test the resources in Resource Groups
 #
--- libraries/azure_vm.rb
+++ libraries/azure_vm.rb
@@ -1,5 +1,5 @@
 
-require File.expand_path(File.dirname(__FILE__) + '/common/helpers')
+require 'azure_backend'
 
 # Class to retrieve information about the specified virtual machine
 #
