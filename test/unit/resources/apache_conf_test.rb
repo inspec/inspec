3bdf1563e89e05b4c52f63493831d03aa906a7fe
Carl Johnston <carldjohnston@gmail.com>
2017-02-24 16:13:22 +1100

    Added unit tests for apache_conf inclusion of symlinked files.
    Same test added to both Ubuntu and Centos for consistency.
    
    Signed-off-by: Carl Johnston <carldjohnston@gmail.com>

--- test/unit/resources/apache_conf_test.rb
+++ test/unit/resources/apache_conf_test.rb
@@ -13,6 +13,8 @@ describe 'Inspec::Resources::ApacheConf' do
     _(resource.content).must_be_kind_of String
     _(resource.params('ServerRoot')).must_equal ['"/etc/apache2"']
     _(resource.params('Listen').sort).must_equal ['443', '80']
+    # sourced using a linked file in conf-enabled/
+    _(resource.params('ServerSignature')).must_equal ['Off']
     # TODO(sr) currently, the parser only merges parameter across separate
     # source files, not in one file
     _(resource.params('Define')).must_equal ['ENABLE_USR_LIB_CGI_BIN',
@@ -29,5 +31,7 @@ describe 'Inspec::Resources::ApacheConf' do
 
     # sourced using an absolute path in httpd.conf
     _(resource.params('ExtendedStatus')).must_equal ['Off']
+    # sourced using a linked file in conf-enabled/
+    _(resource.params('ServerSignature')).must_equal ['Off']
   end
 end
