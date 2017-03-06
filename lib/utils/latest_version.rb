29f7321f7b7957775f1fa6a0505eb0aecc628cf7
Makoto Nozaki <makoto.nozaki@twosigma.com>
2017-03-05 22:19:54 -0500

    Code cleanup based on the discussion at #1538 Signed-off-by: Makoto Nozaki <makoto.nozaki@twosigma.com>

--- lib/utils/latest_version.rb
+++ lib/utils/latest_version.rb
@@ -9,8 +9,8 @@ class LatestInSpecVersion
   def latest
     uri = URI('https://rubygems.org/api/v1/gems/inspec.json')
     res = Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https',
-                          open_timeout: 0.5) {|http|
-      http.read_timeout = 0.5
+                          open_timeout: 0.5, read_timeout: 0.5
+                         ) {|http|
       http.get(uri.path)
     }
     inspec_info = JSON.parse(res.body)
