5b858260f90702e325d070410a29f8ada0975158
Nolan Davidson <ndavidson@chef.io>
2017-05-02 10:35:54 -0400

    Add support for Windows auth in mssql_resource
    This adds supports for connecting to MS SQL instances using Window
    authentication rather than SQL authentication.  By leaving either the
    user or password parameters blank causes the sqlcmd to leave off the -U
    and -P params.  This will cause sqlcmd to authenticate as the current
    Windows user.
    
    Signed-off-by: Nolan Davidson <ndavidson@chef.io>

--- lib/resources/mssql_session.rb
+++ lib/resources/mssql_session.rb
@@ -11,18 +11,31 @@ module Inspec::Resources
       describe sql.query('select * from sys.databases where name like \'*test*\') do
         its('stdout') {should_not match(/test/) }
       end
+
+      # Passing no credentials to mssql_session forces it to use Windows authentication
+      sql_windows_auth = mssql_session
+      describe sql_window_auth.query('select * from sys.databases where name like \'*test*\') do
+        its('stdout') {should_not match(/test/) }
+      end
     "
 
     def initialize(user = nil, pass = nil)
       @user = user
       @pass = pass
-      skip_resource('user and pass are required for MSSQL tests') if @user.nil? or @pass.nil?
     end
 
     def query(q)
       escaped_query = q.gsub(/\\/, '\\\\').gsub(/"/, '\\"').gsub(/\$/, '\\$').gsub(/\@/, '`@')
-      cmd = inspec.command("sqlcmd -U #{@user} -P #{@pass} -Q \"#{escaped_query}\"")
-
+      cmd_string =  if @user.nil? or @pass.nil?
+                      "sqlcmd -Q \"#{escaped_query}\""
+                    else
+                      "sqlcmd -U #{@user} -P #{@pass} -Q \"#{escaped_query}\""
+                    end
+      cmd = inspec.command(cmd_string)
+      out = cmd.stdout + "\n" + cmd.stderr
+      if out =~ /Sqlcmd: Error/
+        skip_resource("Can't connect to the MS SQL Server.")
+      end
       cmd
     end
 
