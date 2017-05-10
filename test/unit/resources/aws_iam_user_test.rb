beedecf247e6d20ddcf5dd93f29dc30da6f2d926
sfreeman <Steffanie.Freeman@d2l.com>
2017-05-10 15:41:03 -0400

    Add list_users
    Signed-off-by: sfreeman <Steffanie.Freeman@d2l.com>

--- test/unit/resources/aws_iam_user_test.rb
+++ test/unit/resources/aws_iam_user_test.rb
@@ -12,22 +12,22 @@ Username = "test"
   end
 
   def test_that_MFA_enable_returns_true_if_MFA_Enabled
-    @mock_user_provider.expect :get_user, {has_mfa_enabled?: true}, [Username]
+    @mock_user_provider.expect :user, {has_mfa_enabled?: true}, [Username]
     assert AwsIamUser.new(Username, @mock_user_provider).has_mfa_enabled?
   end
 
   def test_that_MFA_enable_returns_false_if_MFA_is_not_Enabled
-    @mock_user_provider.expect :get_user, {has_mfa_enabled?: false}, [Username]
+    @mock_user_provider.expect :user, {has_mfa_enabled?: false}, [Username]
     assert !AwsIamUser.new(Username, @mock_user_provider).has_mfa_enabled?
   end
 
   def test_that_console_Password_returns_true_if_console_Password_has_been_set
-    @mock_user_provider.expect :get_user, {has_console_password?: true}, [Username]
+    @mock_user_provider.expect :user, {has_console_password?: true}, [Username]
     assert AwsIamUser.new(Username, @mock_user_provider).has_console_password?
   end
 
   def test_that_console_Password_returns_false_if_console_Password_has_not_been_set
-    @mock_user_provider.expect :get_user, {has_console_password?: false}, [Username]
+    @mock_user_provider.expect :user, {has_console_password?: false}, [Username]
     assert !AwsIamUser.new(Username, @mock_user_provider).has_console_password?
   end
 end
