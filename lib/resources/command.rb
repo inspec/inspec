b17f0de7cf20cc72f26245b73f0d7668cf85f239
gbright <gary.bright@niu-solutions.com>
2017-05-25 20:50:11 +0100

    Amended to resolve issue 1839
    Signed-off-by: username-is-already-taken2 <gary.bright@niu-solutions.com>

--- lib/resources/command.rb
+++ lib/resources/command.rb
@@ -50,7 +50,7 @@ module Inspec::Resources
       if inspec.os.linux?
         res = inspec.backend.run_command("bash -c 'type \"#{@command}\"'")
       elsif inspec.os.windows?
-        res = inspec.backend.run_command("where.exe \"#{@command}\"")
+        res = inspec.backend.run_command("If(Test-Path \"#{@command}\"){exit 0}Else{exit 1}")
       elsif inspec.os.unix?
         res = inspec.backend.run_command("type \"#{@command}\"")
       else
