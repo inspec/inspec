0f9e840e9f347d5d6449d4dbc9e8508c1abcb3e8
Pete Higgins <pete@peterhiggins.org>
2017-06-09 15:24:04 -0700

    Define duplicated constant once in root rakefile.
    Signed-off-by: Pete Higgins <pete@peterhiggins.org>

--- tasks/docs.rb
+++ tasks/docs.rb
@@ -20,7 +20,6 @@ require 'ruby-progressbar'
 require 'fileutils'
 require_relative './shared'
 
-PROJECT_DIR = File.join(File.expand_path(File.dirname(__FILE__)), '..').freeze
 WWW_DIR     = File.join(PROJECT_DIR, 'www').freeze
 DOCS_DIR    = File.join(PROJECT_DIR, 'docs').freeze
 
