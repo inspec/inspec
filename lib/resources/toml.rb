d768b5647c2a4d4dacf4d6fd9aededd7b0bdab05
Nolan Davidson <ndavidson@chef.io>
2017-06-15 09:29:34 -0400

    Removing comments from toml resource
    Signed-off-by: Nolan Davidson <ndavidson@chef.io>

--- lib/resources/toml.rb
+++ lib/resources/toml.rb
@@ -3,13 +3,6 @@
 
 require 'toml'
 
-# Parses a TOML document
-# Usage
-# describe toml('default.toml') do
-#   its('key') { should eq('value') }
-#   its (['arr', 1]) { should eq 2 }
-#   its (['mytable', 'key1']) { should eq 'value1' }
-# end
 module Inspec::Resources
   class TomlConfig < JsonConfig
     name 'toml'
