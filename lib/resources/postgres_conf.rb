3bb98fa1e8d2ffff00655e491bfd65923dc7dda1
Aaron Lippold <lippold@gmail.com>
2017-06-23 11:31:27 -0400

    Fixes the postgres_conf parsing of complex paramerters (#1938)
    Fixes #1671
    
    Signed-off-by: Aaron Lippold <lippold@gmail.com>

--- lib/resources/postgres_conf.rb
+++ lib/resources/postgres_conf.rb
@@ -2,6 +2,7 @@
 # copyright: 2015, Vulcano Security GmbH
 # author: Dominik Richter
 # author: Christoph Hartmann
+# author: Aaron Lippold
 # license: All rights reserved
 
 require 'utils/simpleconfig'
@@ -19,6 +20,7 @@ module Inspec::Resources
     "
 
     include FindFiles
+    include ObjectTraverser
 
     def initialize(conf_path = nil)
       @conf_path = conf_path || inspec.postgres.conf_path
@@ -42,8 +44,13 @@ module Inspec::Resources
       res
     end
 
-    def method_missing(name)
-      param = params[name.to_s]
+    def value(key)
+      extract_value(key, @params)
+    end
+
+    def method_missing(*keys)
+      keys.shift if keys.is_a?(Array) && keys[0] == :[]
+      param = value(keys)
       return nil if param.nil?
       # extract first value if we have only one value in array
       return param[0] if param.length == 1
