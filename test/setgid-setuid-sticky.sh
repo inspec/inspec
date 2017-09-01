c94e5234f0d44a67c6705105de078423bc72ab7a
Clinton Wolfe <clintoncwolfe@gmail.com>
2017-09-01 16:51:29 -0400

    Revert "Provisioner script to setup resource tests for setgid/setuid/sticky bit tests.  This appears to be the correct mechanism per docker_run, but I don't see any other provisioner scripts, so I suspect there is a different Chef-internal mechanism at play here."
    This reverts commit 42e672f3b1cea824451f23bb825b486baaa77c02.
    
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- test/setgid-setuid-sticky.sh
+++
@@ -1,3 +0,0 @@
-echo 'echo i am setuid' > /root/i-am-setuid && chmod 4700 /root/i-am-setuid
-echo 'echo i am setgid' > /root/i-am-setgid && chmod 2700 /root/i-am-setgid
-mkdir /root/i-am-sticky && chmod 1700 /root/i-am-sticky
