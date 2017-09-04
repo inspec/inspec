fdc705464c32c1798aeb2d9052ad053496fd9e73
dromazmj <dromazmj@dukes.jmu.edu>
2017-09-03 23:02:12 -0400

    Modifications to new resource - firewalld
    Signed-off-by: dromazmj <dromazmj@dukes.jmu.edu>

--- test/unit/resources/firewalld_test.rb
+++ test/unit/resources/firewalld_test.rb
@@ -5,12 +5,11 @@ require 'helper'
 require 'inspec/resource'
 
 describe 'Inspec::Resources::FirewallD' do
-  resource = load_resource('firewalld')
   centResource = MockLoader.new(:centos7).load_resource('firewalld')
 
   it 'verify firewalld detects a zone' do
-    centResource.stubs(:has_zone?).with('public').returns(true)
-    centResource.stubs(:has_zone?).with('zonenotinfirewalld').returns(false)
+    _(centResource.has_zone?('public')).must_equal true
+    _(centResource.has_zone?('zonenotinfirewalld')).must_equal false
   end
 
   it 'verity firewalld is running' do
@@ -18,33 +17,41 @@ describe 'Inspec::Resources::FirewallD' do
   end
 
   it 'verify firewalld detects a default_zone' do
-    _(resource.default_zone).must_equal 'public'
+    _(centResource.default_zone).must_equal 'public'
   end
 
-  it 'verify firewalld detects attributes for active zones' do
-    entries = resource.where { zone == 'public' }
+  it 'parses zones with multiple interfaces' do
+    entries = centResource.where { zone == 'public' }
     _(entries.interfaces).must_equal [['enp0s3', 'eno2']]
+  end
+
+  it 'detects services in an active zone' do
+    entries = centResource.where { zone == 'public' }
     _(entries.services).must_equal [['ssh', 'icmp']]
+  end
+
+  it 'detects sources in an active zone' do
+    entries = centResource.where { zone == 'public' }
     _(entries.sources).must_equal [['192.168.1.0/24', '192.168.1.2']]
   end
 
   it 'verify firewalld detects a whether or not a service is allowed in a zone' do
-    resource.stubs(:has_service_enabled_in_zone?).with('public', 'ssh').returns(true)
+    _(centResource.has_service_enabled_in_zone?('ssh', 'public')).must_equal true
   end
 
   it 'verify firewalld detects ports enabled for a service in a zone' do
-    resource.stubs(:service_ports_enabled_in_zone).with('public', 'ssh').returns('22/udp')
+    _(centResource.service_ports_enabled_in_zone('ssh', 'public')).must_equal ['22/tcp']
   end
 
   it 'verify firewalld detects protocols enabled for a service in a zone' do
-    resource.stubs(:service_protocols_enabled_in_zone).with('public', 'ssh').returns('')
+    _(centResource.service_protocols_enabled_in_zone('ssh', 'public')).must_equal ['icmp']
   end
 
   it 'verify firewalld detects a whether or not a service is allowed in a zone' do
-    resource.stubs(:has_port_enabled_in_zone?).with('public', '22/udp').returns(true)
+    _(centResource.has_port_enabled_in_zone?('22/udp', 'public')).must_equal true
   end
 
   it 'verify firewalld detects a whether or not a rule is enabled in a zone' do
-    resource.stubs(:has_rule_enabled?).with('public', 'rule family=ipv4 source address=192.168.0.14 accept').returns(true)
+    _(centResource.has_rule_enabled?('family=ipv4 source address=192.168.0.14 accept', 'public')).must_equal true
   end
 end
