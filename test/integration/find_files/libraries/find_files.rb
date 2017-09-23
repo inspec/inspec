3d7244fb0713103dc80c497f26bb4efa7dfad298
Jerry Aldrich III <aldrich003@gmail.com>
2017-09-23 02:17:34 -0500

    Add wildcard support to `Utils::FindFiles` (#2159)
    Wildcards are evaluated prior to applying `sudo` permissions. This
    means that running `sudo find /some/path/*.conf` will fail if the user
    does not have read permissions on `/some/path/` because the wildcard
    cannot expand before `sudo` is applied and `*.conf` isn't a file.
    
    The solution for this is to run the command in a subshell that has the
    proper permissions (e.g. `sudo sh -c 'find /some/path/*.conf'`).
    
    This modifies `Utils::FindFiles` to use a subshell thus allowing
    wildcard support.
    
    This fixes #2157
    
    Signed-off-by: Jerry Aldrich <jerryaldrichiii@gmail.com>

---
+++ test/integration/find_files/libraries/find_files.rb
@@ -0,0 +1,23 @@
+# Custom resource for Utils::FindFIles
+class TestFindFiles < Inspec.resource(1)
+  name 'test_find_files'
+
+  desc "
+    Resource used for testing the funcitonality of Utils::FindFiles
+  "
+
+  example "
+    describe test_find_files('/some/filepath') do
+      its('results') { should include 'somefile.conf' }
+    end
+  "
+
+  require 'utils/find_files'
+  include FindFiles
+
+  attr_reader :results
+
+  def initialize(path)
+    @results = find_files(path)
+  end
+end
