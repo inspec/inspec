5c848a2cb90174e560b694b0972fbd9c614b645b
dromazmj <dromazmj@dukes.jmu.edu>
2017-09-25 14:49:15 -0400

    Fixing lint issues
    Signed-off-by: dromazmj <dromazmj@dukes.jmu.edu>

--- lib/resources/firewalld.rb
+++ lib/resources/firewalld.rb
@@ -55,7 +55,7 @@ module Inspec::Resources
     def running?
       return false unless installed?
       result = firewalld_command('--state')
-      result.match(/^running/) ? true : false
+      result =~ /^running/ ? true : false
     end
 
     def default_zone
