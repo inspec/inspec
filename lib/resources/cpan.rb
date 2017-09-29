0bfeafd810a1c7e5c9e37972b65a104faae0e2ee
Markus Grobelin <markus.grobelin@centogene.com>
2017-09-29 16:09:43 +0200

    cpan resource: pass custom PERL5LIB path

--- lib/resources/cpan.rb
+++ lib/resources/cpan.rb
@@ -11,19 +11,21 @@
 module Inspec::Resources
   class CpanPackage < Inspec.resource(1)
     name 'cpan'
-    desc 'Use the cpan InSpec audit resource to test Perl modules that are installed using the cpan installer.'
+    desc 'Use the `cpan` InSpec audit resource to test Perl modules that are installed by system packages or the CPAN installer.'
     example "
       describe cpan('DBD::Pg') do
         it { should be_installed }
       end
     "
 
-    def initialize(package_name, cpan_path = nil)
+    def initialize(package_name, perl_lib_path = nil)
       @package_name = package_name
-      @cpan_cmd = cpan_path || 'cpan'
+      @perl_lib_path = perl_lib_path
+      @perl_cmd = 'perl'
+
       # this resource is not supported on Windows
       skip_resource 'The `cpan` resource is not supported on your OS yet.' if inspec.os.windows?
-      return skip_resource 'cpan not found' unless inspec.command(@cpan_cmd).exist?
+      return skip_resource 'cpan not found' unless inspec.command(@perl_cmd).exist?
     end
 
     def info
@@ -31,7 +33,9 @@ module Inspec::Resources
 
       @info = {}
       @info[:type] = 'cpan'
-      cmd = inspec.command("perl -le 'eval \"require $ARGV[0]\" and print $ARGV[0]->VERSION or exit 1' #{@package_name}")
+      # set PERL5LIB environment variable if a custom lib path is given
+      lib_path = @perl_lib_path.nil? ? '' : "PERL5LIB=#{@perl_lib_path} "
+      cmd = inspec.command("#{lib_path+@perl_cmd} -le 'eval \"require $ARGV[0]\" and print $ARGV[0]->VERSION or exit 1' #{@package_name}")
       return @info if cmd.exit_status != 0
 
       @info[:name] = @package_name
