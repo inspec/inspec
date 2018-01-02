f1f2900866cbbb7a8b6096daee7acb11c5d9c9cd
Dominik Richter <dominik.richter@gmail.com>
2018-01-02 12:41:01 -0800

    bugfix: dependency chaining in libraries (#2428)
    * WIP require chaining
    
    * add a tiny comment
    
    * reapply fix doh
    
    * add a deprecation warning
    
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>
    
    * lint
    
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>
    
    * remove deprecation warning for require in control files
    
    as discussed with Adam Leff
    
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>
    
    * add tests for regular ruby gem require in libs
    
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- lib/inspec/dsl_shared.rb
+++ lib/inspec/dsl_shared.rb
@@ -18,6 +18,14 @@ module Inspec
         # We cannot rely on libraries residing on disk however.
         # TODO: Sandboxing.
         content, path, line = @require_loader.load(rbpath)
+
+        # If we are in the realm of libraries and the LibraryEvalContext
+        # we should have access to the __inspec_binding, which is a Binding
+        # context that provides the correct plane to evaluate all required files to.
+        # It will ensure that embedded calls to `require` still call this
+        # method and get loaded from their correct paths.
+        return __inspec_binding.eval(content, path, line) if defined?(__inspec_binding)
+
         eval(content, TOPLEVEL_BINDING, path, line) # rubocop:disable Security/Eval
       end
     end
