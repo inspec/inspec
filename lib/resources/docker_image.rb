892cab2d39d95ece02ca41a5e3415785ce8ccf9f
Matt Kulka <mkulka@parchment.com>
2018-01-12 13:39:46 -0700

    Docker Swarm service resource
    This change adds the `docker_service` resource for Docker swarm mode services. This
    branches off some of the common elements (id, exists) into a `DockerObject` module along
    with a utility function for parsing the image/repo string. That function was implemented
    separately by `docker_image` and `docker_container`, now with a third resource, it made
    sense to consolidate that into an included module. I used the most comprehensive
    implementation. Existing classes had to be slightly modified for the genericization.
    
    Signed-off-by: Matt Kulka <mkulka@parchment.com>

--- lib/resources/docker_image.rb
+++ lib/resources/docker_image.rb
@@ -6,8 +6,12 @@
 # author: Patrick Muench
 # author: Dominik Richter
 
+require_relative 'docker_object'
+
 module Inspec::Resources
   class DockerImage < Inspec.resource(1)
+    include Inspec::Resources::DockerObject
+
     name 'docker_image'
     desc ''
     example "
@@ -35,24 +39,16 @@ module Inspec::Resources
       @opts = sanitize_options(o)
     end
 
-    def exist?
-      image_info.exists?
-    end
-
-    def id
-      image_info.ids[0] if image_info.entries.size == 1
-    end
-
     def image
-      "#{repo}:#{tag}" if image_info.entries.size == 1
+      "#{repo}:#{tag}" if object_info.entries.size == 1
     end
 
     def repo
-      image_info.repositories[0] if image_info.entries.size == 1
+      object_info.repositories[0] if object_info.entries.size == 1
     end
 
     def tag
-      image_info.tags[0] if image_info.entries.size == 1
+      object_info.tags[0] if object_info.entries.size == 1
     end
 
     def to_s
@@ -79,46 +75,12 @@ module Inspec::Resources
       opts
     end
 
-    def image_info
+    def object_info
       return @info if defined?(@info)
       opts = @opts
       @info = inspec.docker.images.where {
         (repository == opts[:repo] && tag == opts[:tag]) || (!id.nil? && !opts[:id].nil? && (id == opts[:id] || id.start_with?(opts[:id])))
       }
-    end
-
-    def parse_components_from_image(image_string)
-      # if the user did not supply an image string, they likely supplied individual
-      # option parameters, such as repo and tag. Return empty data back to the caller.
-      return {} if image_string.nil?
-
-      first_colon = image_string.index(':') || -1
-      first_slash = image_string.index('/') || -1
-
-      if image_string.count(':') == 2
-        # If there are two colons in the image string, it contains a repo-with-port and a tag.
-        # example: localhost:5000/chef/inspec:1.46.3
-        partitioned_string = image_string.rpartition(':')
-        repo = partitioned_string.first
-        tag = partitioned_string.last
-      elsif image_string.count(':') == 1 && first_colon < first_slash
-        # If there's one colon in the image string, and it comes before a forward-slash,
-        # it contains a repo-with-port but no tag.
-        # example: localhost:5000/ubuntu
-        repo = image_string
-        tag = nil
-      else
-        # If there's one colon in the image string and it doesn't preceed a slash, or if
-        # there is no colon at all, then it separates the repo from the tag, if there is a tag.
-        # example: chef/inspec:1.46.3
-        # example: chef/inspec
-        # example: ubuntu:14.04
-        repo, tag = image_string.split(':')
-      end
-
-      # return the repo and tag parsed from the string, which can be merged into
-      # the rest of the user-supplied options
-      { repo: repo, tag: tag }
     end
   end
 end
--- test/unit/resources/docker_test.rb
+++ test/unit/resources/docker_test.rb
@@ -6,26 +6,30 @@ require 'inspec/resource'
 
 describe 'Inspec::Resources::Docker' do
   describe 'docker' do
+    let(:resource) { load_resource('docker') }
+
     it 'check docker container parsing' do
-      resource = load_resource('docker')
       _(resource.containers.ids).must_equal ['3def9aa450f8bd772c3d5b07e27ec934e5f58575e955367a0aca2d93e0687536', 'd94f854370d2b02912e8fc636502bc72b74fbd567a7eba3fc6a52045bb28904e', '5a83c301f30ccd48579a74a84af6fdd0c0e0d66aacc7bb52abfa2ba2544c6c0c', '5a83c301f30ccd48579a74a84af6fdd0c0e0d66aacc7bb52abfa2ba2544c6c0c']
       _(resource.containers.names).must_equal ['sleepy_khorana', 'laughing_austin', 'heuristic_almeida', 'laughing_lamport']
     end
 
     it 'check docker image parsing' do
-      resource = load_resource('docker')
       _(resource.images.ids).must_equal ['sha256:4a415e3663882fbc554ee830889c68a33b3585503892cc718a4698e91ef2a526', 'sha256:978d85d02b87aea199e4ae8664f6abf32fdea331884818e46b8a01106b114cee', 'sha256:0ef2e08ed3fabfc44002ccb846c4f2416a2135affc3ce39538834059606f32dd', 'sha256:c4e5744dbe11a4f1970ba36d0aa3944c347ab232bb58fb86b240f1bb18a360c2']
       _(resource.images.repositories).must_equal ["alpine", "debian", "ubuntu", "repo.example.com:5000/ubuntu"]
     end
 
+    it 'check docker service parsing' do
+      _(resource.services.ids).must_equal ["2ghswegspre1", "huhcawfiyddo", "msar8lb56wq2", "mdrfkxckau6c"]
+      _(resource.services.names).must_equal ["service1", "service2", "service3", "service4"]
+      _(resource.services.images).must_equal ["foo/image:1.0", "foo/image:1.1", "bar:latest", "bar:latest"]
+    end
+
     it 'check docker version parsing' do
-      resource = load_resource('docker')
       _(resource.version.Server.Version).must_equal '17.03.0-ce'
       _(resource.version.Client.Version).must_equal '17.03.0-ce'
     end
 
     it 'check docker info parsing' do
-      resource = load_resource('docker')
       _(resource.info.ID).must_equal 'HMKB:SOFR:Z3DM:J6ZY:WE6K:47EW:WVGV:C5C3:WNJC:TSG6:43YV:IOGU'
       _(resource.info.Containers).must_equal 93
       _(resource.info.Runtimes.runc.path).must_equal 'docker-runc'
@@ -33,13 +37,11 @@ describe 'Inspec::Resources::Docker' do
     end
 
     it 'check docker object parsing' do
-      resource = load_resource('docker')
       _(resource.object('71b5df59442b').Id).must_equal '71b5df59442be8215902ce7804bfbb0ab5d8b8ddab7cef6e00224a8c1f476e38'
       _(resource.object('71b5df59442b').Path).must_equal 'nginx'
     end
 
     it 'prints as a docker resource' do
-      resource = load_resource('docker')
       resource.to_s.must_equal 'Docker Host'
     end
   end
