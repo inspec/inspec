e81937413b473e280aaa7173e1300b1912ff0e6f
Sam Cornwell <14048146+samcornwell@users.noreply.github.com>
2018-01-18 10:51:06 -0500

    skeletal aws_vpcs (#182)
    Signed-off-by: Sam Cornwell <14048146+samcornwell@users.noreply.github.com>

---
+++ libraries/aws_vpcs.rb
@@ -0,0 +1,42 @@
+class AwsVpcs < Inspec.resource(1)
+  name 'aws_vpcs'
+  desc 'Verifies settings for AWS VPCs in bulk'
+  example '
+    describe aws_vpcs do
+      it { should exist }
+    end
+  '
+
+  # Underlying FilterTable implementation.
+  filter = FilterTable.create
+  filter.add_accessor(:entries)
+        .add(:exists?) { |x| !x.entries.empty? }
+  filter.connect(self, :vpc_data)
+
+  def vpc_data
+    @table
+  end
+
+  def to_s
+    'VPCs'
+  end
+
+  def initialize
+    backend = AwsVpcs::BackendFactory.create
+    @table = backend.describe_vpcs.to_h[:vpcs]
+  end
+
+  class BackendFactory
+    extend AwsBackendFactoryMixin
+  end
+
+  class Backend
+    class AwsClientApi
+      BackendFactory.set_default_backend(self)
+
+      def describe_vpcs(query = {})
+        AWSConnection.new.ec2_client.describe_vpcs(query)
+      end
+    end
+  end
+end
