b18dc745297f2df311b5e27da1910481b804c816
Jerry Aldrich <jerryaldrichiii@gmail.com>
2018-01-17 21:43:37 -0600

    packages resource: Add `architectures` support
    This adds support for `architectures` to the `packages` resource.
    
    Example:
    
    ```
    describe packages(/compat-libstdc++-33/) do
      its('architectures') { should include 'x86_64' }
      its('architectures') { should include 'i686' }
    end
    ```
    
    This also adds documentation for the `packages` resource
    
    Signed-off-by: Jerry Aldrich <jerryaldrichiii@gmail.com>

--- lib/resources/packages.rb
+++ lib/resources/packages.rb
@@ -48,6 +48,7 @@ module Inspec::Resources
           .add(:statuses,  field: 'status', style: :simple)
           .add(:names,     field: 'name')
           .add(:versions,  field: 'version')
+          .add(:architectures, field: 'architecture')
           .connect(self, :filtered_packages)
 
     private
@@ -69,7 +70,7 @@ module Inspec::Resources
   end
 
   class PkgsManagement
-    PackageStruct = Struct.new(:status, :name, :version)
+    PackageStruct = Struct.new(:status, :name, :version, :architecture)
     attr_reader :inspec
     def initialize(inspec)
       @inspec = inspec
@@ -80,7 +81,7 @@ module Inspec::Resources
   class Debs < PkgsManagement
     def build_package_list
       # use two spaces as delimiter in case any of the fields has a space in it
-      command = "dpkg-query -W -f='${db:Status-Abbrev}  ${Package}  ${Version}\\n'"
+      command = "dpkg-query -W -f='${db:Status-Abbrev}  ${Package}  ${Version}  ${Architecture}\\n'"
       cmd = inspec.command(command)
       all = cmd.stdout.split("\n")
       return [] if all.nil?
@@ -97,7 +98,7 @@ module Inspec::Resources
   class Rpms < PkgsManagement
     def build_package_list
       # use two spaces as delimiter in case any of the fields has a space in it
-      command = "rpm -qa --queryformat '%{NAME}  %{VERSION}-%{RELEASE}\\n'" # rubocop:disable Style/FormatStringToken
+      command = "rpm -qa --queryformat '%{NAME}  %{VERSION}-%{RELEASE}  %{ARCH}\\n'" # rubocop:disable Style/FormatStringToken
       cmd = inspec.command(command)
       all = cmd.stdout.split("\n")
       return [] if all.nil?
