711396e4d384fa689085a017aad8c035945cc3a9
ViolentOr <github@violentor.me>
2018-01-21 19:28:26 +0300

    fixed mistype
    Signed-off-by: ViolentOr <github@violentor.me>

--- test/helper.rb
+++ test/helper.rb
@@ -480,7 +480,7 @@ class MockLoader
       "curl -H 'Content-Type: application/json' http://elasticsearch.mycompany.biz:1234/_nodes" => cmd.call('elasticsearch-cluster-url'),
 
       #security_policy resource SID translation
-      "New-Object System.Security.Principal.SecurityIdentifier(\"S-1-5-32-544\")).Translate( [System.Security.Principal.NTAccount]).Value" => cmd.call('securitt-policy-sid-translated'),
+      "New-Object System.Security.Principal.SecurityIdentifier(\"S-1-5-32-544\")).Translate( [System.Security.Principal.NTAccount]).Value" => cmd.call('security-policy-sid-translated'),
       "curl -H 'Content-Type: application/json' http://localhost:9200/_nodes" => empty.call,
     }
     @backend
