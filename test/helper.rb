80a2c70551682110638e68c5b787fc3acaae1138
ViolentOr <github@violentor.me>
2018-01-21 19:21:47 +0300

    Required tests added
    Signed-off-by: ViolentOr <github@violentor.me>

--- test/helper.rb
+++ test/helper.rb
@@ -478,6 +478,10 @@ class MockLoader
       "curl -k -H 'Content-Type: application/json' http://localhost:9200/_nodes" => cmd.call('elasticsearch-cluster-no-ssl'),
       "curl -H 'Content-Type: application/json'  -u es_admin:password http://localhost:9200/_nodes" => cmd.call('elasticsearch-cluster-auth'),
       "curl -H 'Content-Type: application/json' http://elasticsearch.mycompany.biz:1234/_nodes" => cmd.call('elasticsearch-cluster-url'),
+
+      #security_policy resource SID translation
+      "New-Object System.Security.Principal.SecurityIdentifier(\"S-1-5-32-544\")).Translate( [System.Security.Principal.NTAccount]).Value" => cmd.call('securitt-policy-sid-translated'),
+      "curl -H 'Content-Type: application/json' http://localhost:9200/_nodes" => empty.call,
     }
     @backend
   end
