ca96a74199599866a6d0bc0cef59645e6e919844
ViolentOr <github@violentor.me>
2018-01-21 19:45:20 +0300

    replaced empty call with empty file
    Signed-off-by: ViolentOr <github@violentor.me>

--- test/helper.rb
+++ test/helper.rb
@@ -481,7 +481,7 @@ class MockLoader
 
       #security_policy resource SID translation
       "New-Object System.Security.Principal.SecurityIdentifier(\"S-1-5-32-544\")).Translate( [System.Security.Principal.NTAccount]).Value" => cmd.call('security-policy-sid-translated'),
-      "New-Object System.Security.Principal.SecurityIdentifier(\"S-1-5-32-555\")).Translate( [System.Security.Principal.NTAccount]).Value" => empty.call,
+      "New-Object System.Security.Principal.SecurityIdentifier(\"S-1-5-32-555\")).Translate( [System.Security.Principal.NTAccount]).Value" => cmd.call('security-policy-sid-untranslated'),
     }
     @backend
   end
