0af7105ebab0ea96b031af56e5687fcb577c5aa7
Rony Xavier <rx294@nyu.edu>
2018-01-23 11:26:27 -0500

    Functional aws_cloudtrail_trail resource (#186)
    Signed-off-by: Rony Xavier <rx294@nyu.edu>

---
+++ libraries/aws_cloudtrail_trail.rb
@@ -0,0 +1,74 @@
+class AwsCloudTrailTrail < Inspec.resource(1)
+  name 'aws_cloudtrail_trail'
+  desc 'Verifies settings for an individual AWS CloudTrail Trail'
+  example "
+    describe aws_cloudtrail_trail('trail-name') do
+      it { should exist }
+    end
+  "
+
+  include AwsResourceMixin
+  attr_reader :s3_bucket_name, :trail_arn, :cloud_watch_logs_role_arn,
+              :cloud_watch_logs_log_group_arn, :kms_key_id, :home_region
+
+  def to_s
+    "CloudTrail #{@trail_name}"
+  end
+
+  def multi_region_trail?
+    @is_multi_region_trail
+  end
+
+  def log_file_validation_enabled?
+    @log_file_validation_enabled
+  end
+
+  def encrypted?
+    !kms_key_id.nil?
+  end
+
+  private
+
+  def validate_params(raw_params)
+    validated_params = check_resource_param_names(
+      raw_params: raw_params,
+      allowed_params: [:trail_name],
+      allowed_scalar_name: :trail_name,
+      allowed_scalar_type: String,
+    )
+
+    if validated_params.empty?
+      raise ArgumentError, "You must provide the parameter 'trail_name' to aws_cloudtrail_trail."
+    end
+
+    validated_params
+  end
+
+  def fetch_from_aws
+    backend = AwsCloudTrailTrail::BackendFactory.create
+
+    query = { trail_name_list: [@trail_name] }
+    resp = backend.describe_trails(query)
+
+    @trail = resp.trail_list[0].to_h
+    @exists = !@trail.empty?
+    @s3_bucket_name = @trail[:s3_bucket_name]
+    @is_multi_region_trail = @trail[:is_multi_region_trail]
+    @trail_arn = @trail[:trail_arn]
+    @log_file_validation_enabled = @trail[:log_file_validation_enabled]
+    @cloud_watch_logs_role_arn = @trail[:cloud_watch_logs_role_arn]
+    @cloud_watch_logs_log_group_arn = @trail[:cloud_watch_logs_log_group_arn]
+    @kms_key_id = @trail[:kms_key_id]
+    @home_region = @trail[:home_region]
+  end
+
+  class Backend
+    class AwsClientApi
+      BackendFactory.set_default_backend(self)
+
+      def describe_trails(query)
+        AWSConnection.new.cloudtrail_client.describe_trails(query)
+      end
+    end
+  end
+end
--- test/integration/default/build/aws.tf
+++ test/integration/default/build/aws.tf
@@ -7,6 +7,15 @@ provider "aws" {
 }
 
 data "aws_caller_identity" "creds" {}
+
 output "aws_account_id" {
   value = "${data.aws_caller_identity.creds.account_id}"
+}
+
+data "aws_region" "region" {
+  current = true
+}
+
+output "aws_region" {
+  value = "${data.aws_region.region.name}"
 }
