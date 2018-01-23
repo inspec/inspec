c067798fc5cb9b5198090bcd8dc44cc4326589ff
Matt Kulka <mattlqx@users.noreply.github.com>
2018-01-23 13:30:14 -0700

    Docker Swarm service resource (#2456)
    This change adds the `docker_service` resource for Docker swarm mode services. This
    branches off some of the common elements (id, exists) into a `DockerObject` module along
    with a utility function for parsing the image/repo string. That function was implemented
    separately by `docker_image` and `docker_container`, now with a third resource, it made
    sense to consolidate that into an included module. I used the most comprehensive
    implementation. Existing classes had to be slightly modified for the genericization.
    
    Signed-off-by: Matt Kulka <mkulka@parchment.com>

--- test/helper.rb
+++ test/helper.rb
@@ -403,6 +403,8 @@ class MockLoader
       "docker inspect 71b5df59442b" => cmd.call('docker-inspec'),
       # docker images
       "83c36bfade9375ae1feb91023cd1f7409b786fd992ad4013bf0f2259d33d6406" => cmd.call('docker-images'),
+      # docker services
+      %{docker service ls --format '{"ID": {{json .ID}}, "Name": {{json .Name}}, "Mode": {{json .Mode}}, "Replicas": {{json .Replicas}}, "Image": {{json .Image}}, "Ports": {{json .Ports}}}'} => cmd.call('docker-service-ls'),
       # modprobe for kernel_module
       "modprobe --showconfig" => cmd.call('modprobe-config'),
       # get-process cmdlet for processes resource
