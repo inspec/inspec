4f7d17108b656e8085f5af5c960e6f5bb41721c7
Danny Haynes <dhaynes@mitre.org>
2018-01-25 18:48:19 -0500

    Update etc_libuser.rb
    Fixed errors so the tests all run correctly.

--- test/unit/resources/etc_libuser.rb
+++ test/unit/resources/etc_libuser.rb
@@ -7,42 +7,42 @@ require 'inspec/resource'
 describe 'Inspec::Resources::LibUserConfig' do
 
   it 'verify login_defs' do
-    resource = MockLoader.new(:centos7).load_resource('libuser_conf')
-    _(resource.value(['import','login_defs']).must_equal '/etc/default/useradd'
+    resource = MockLoader.new(:centos7).load_resource('etc_libuser')
+    _(resource.value(['import','login_defs'])).must_equal ['/etc/login.defs']
   end
 
   it 'verify default_useradd' do
-    resource = MockLoader.new(:centos7).load_resource('libuser_conf')
-    _(resource.value(['import','default_useradd']).must_equal '/etc/default/useradd'
+    resource = MockLoader.new(:centos7).load_resource('etc_libuser')
+    _(resource.value(['import','default_useradd'])).must_equal ['/etc/default/useradd']
   end
 
   it 'verify crypt_style' do
-    resource = MockLoader.new(:centos7).load_resource('libuser_conf')
-    _(resource.value(['defaults','crypt_style']).must_equal 'md5'
+    resource = MockLoader.new(:centos7).load_resource('etc_libuser')
+    _(resource.value(['defaults','crypt_style'])).must_equal ['md5']
   end
 
   it 'verify modules' do
-    resource = MockLoader.new(:centos7).load_resource('libuser_conf')
-    _(resource.value(['defaults','modules']).must_equal 'files shadow'
+    resource = MockLoader.new(:centos7).load_resource('etc_libuser')
+    _(resource.value(['defaults','modules'])).must_equal ['files shadow']
   end
 
   it 'verify create_modules' do
-    resource = MockLoader.new(:centos7).load_resource('libuser_conf')
-    _(resource.value(['defaults','creates_modules']).must_equal 'files shadow'   
+    resource = MockLoader.new(:centos7).load_resource('etc_libuser')
+    _(resource.value(['defaults','create_modules'])).must_equal ['files shadow']   
   end
 
   it 'verify LU_USERNAME' do
-    resource = MockLoader.new(:centos7).load_resource('libuser_conf')
-    _(resource.value(['userdefaults','LD_USERNAME']).must_equal '%n'
+    resource = MockLoader.new(:centos7).load_resource('etc_libuser')
+    _(resource.value(['userdefaults','LU_USERNAME'])).must_equal ['%n']
   end
 
   it 'verify LU_GIDNUMBER' do
-    resource = MockLoader.new(:centos7).load_resource('libuser_conf')
-    _(resource.value(['userdefaults','LU_GIDNUMBER']).must_equal '%u'
+    resource = MockLoader.new(:centos7).load_resource('etc_libuser')
+    _(resource.value(['userdefaults','LU_GIDNUMBER'])).must_equal ['%u']
   end
 
   it 'verify LU_GROUPNAME' do
-    resource = MockLoader.new(:centos7).load_resource('libuser_conf')
-    _(resource.value(['groupdefaults','LU_GROUPNAME']).must_equal '$n'
+    resource = MockLoader.new(:centos7).load_resource('etc_libuser')
+    _(resource.value(['groupdefaults','LU_GROUPNAME'])).must_equal ['%n']
   end
 end
