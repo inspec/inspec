bb50fd54d68c57992fa519fc64bdea8a24291b53
kagarmoe <kgarmoe@chef.io>
2018-01-30 14:10:06 -0800

    Edits to audit_policy resource language
    Signed-off-by: kagarmoe <kgarmoe@chef.io>

--- lib/resources/audit_policy.rb
+++ lib/resources/audit_policy.rb
@@ -26,7 +26,7 @@
 module Inspec::Resources
   class AuditPolicy < Inspec.resource(1)
     name 'audit_policy'
-    desc 'Use the audit_policy InSpec audit resource to test auditing policies on the Microsoft Windows platform. An auditing policy is a category of security-related events to be audited. Auditing is disabled by default and may be enabled for categories like account management, logon events, policy changes, process tracking, privilege use, system events, or object access. For each auditing category property that is enabled, the auditing level may be set to No Auditing, Not Specified, Success, Success and Failure, or Failure.'
+    desc 'Use the audit_policy InSpec audit resource to test auditing policies on the Microsoft Windows platform. An auditing policy is a category of security-related events to be audited. Auditing is disabled by default and may be enabled for categories like account management, logon events, policy changes, process tracking, privilege use, system events, or object access. For each enabled auditing category property, the auditing level may be set to No Auditing, Not Specified, Success, Success and Failure, or Failure.'
     example "
       describe audit_policy do
         its('parameter') { should eq 'value' }
