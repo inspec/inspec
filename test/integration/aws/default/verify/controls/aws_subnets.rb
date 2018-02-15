95a1bf67626358826d5627f33fcfc3240d7cf467
Clinton Wolfe <clintoncwolfe@gmail.com>
2018-02-14 20:16:53 -0500

    Fix VPC ID fixture export for subnet testing
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- test/integration/aws/default/verify/controls/aws_subnets.rb
+++ test/integration/aws/default/verify/controls/aws_subnets.rb
@@ -1,7 +1,7 @@
 fixtures = {}
 [
-  'ec2_security_group_default_vpc_id',
-  'ec2_default_vpc_subnet_id',
+  'subnet_01_id',
+  'subnet_vpc_id',
 ].each do |fixture_name|
   fixtures[fixture_name] = attribute(
     fixture_name,
@@ -14,12 +14,12 @@ control "aws_subnets recall" do
   all_subnets = aws_subnets
   
   # You should be able to get a specific subnet given its id
-  describe all_subnets.where(subnet_id: fixtures['ec2_default_vpc_subnet_id']) do
+  describe all_subnets.where(subnet_id: fixtures['subnet_01_id']) do
     it { should exist }
   end
 
   # You should be able to get subnets given a vpc_id
-  describe all_subnets.where(vpc_id: fixtures['ec2_security_group_default_vpc_id']) do
+  describe all_subnets.where(vpc_id: fixtures['subnet_vpc_id']) do
     it { should exist }
   end
 
@@ -32,17 +32,17 @@ control "aws_subnets recall" do
   end
 end
 
-control "aws_subnets properties of default VPC subnet" do
+control "aws_subnets properties by subnet id" do
   # you should be able to test the cidr_block of a subnet
-  describe aws_subnets.where(subnet_id: fixtures['ec2_default_vpc_subnet_id']) do
+  describe aws_subnets.where(subnet_id: fixtures['subnet_01_id']) do
     its('cidr_blocks') { should include '172.31.96.0/20' }
     its('states') { should_not include 'pending' }
   end
 end
 
-control "aws_subnets properties of default VPC" do
+control "aws_subnets properties by vpc_id" do
   # you should be able to test the cidr_block of a subnet
-  describe aws_subnets.where(vpc_id: fixtures['ec2_security_group_default_vpc_id']) do
+  describe aws_subnets.where(vpc_id: fixtures['subnet_vpc_id']) do
     its('cidr_blocks') { should include '172.31.96.0/20' }
     its('states') { should include 'available' }
   end
