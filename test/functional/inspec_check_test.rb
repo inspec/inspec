20a0b0e025185e41b9c3cd6bb112b1accdad049e
Jared Quick <quick.jared@gmail.com>
2018-02-26 11:01:23 -0500

    Fix inspec check to work with platforms (#2737)
    * Fix inspec check to work with platforms.
    
    Signed-off-by: Jared Quick <jquick@chef.io>

--- test/functional/inspec_check_test.rb
+++ test/functional/inspec_check_test.rb
@@ -36,4 +36,18 @@ describe 'inspec check' do
       out.exit_status.must_equal 0
     end
   end
+
+  describe 'inspec check with a aws profile' do
+    it 'ignore train connection error' do
+      out = inspec('check ' + File.join(examples_path, 'profile-aws'))
+      out.exit_status.must_equal 0
+    end
+  end
+
+  describe 'inspec check with a azure profile' do
+    it 'ignore train connection error' do
+      out = inspec('check ' + File.join(examples_path, 'profile-azure'))
+      out.exit_status.must_equal 0
+    end
+  end
 end
