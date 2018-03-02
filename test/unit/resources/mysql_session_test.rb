965580ff5d657df9df5612dacd34befbb643dbbd
Tom Hodder <tom@limepepper.co.uk>
2018-03-02 14:45:05 +0000

    clarified test conditions
    Signed-off-by: Tom Hodder <tom@limepepper.co.uk>

--- test/unit/resources/mysql_session_test.rb
+++ test/unit/resources/mysql_session_test.rb
@@ -7,9 +7,9 @@ require 'helper'
 describe 'Inspec::Resources::MysqlSession' do
   it 'verify mysql_session escaped login details with single quotes correctly' do
     resource = load_resource('mysql_session',
-                             %q{$#!%''%$#'"'&%},
+                             'root',
                              %q{'%"'"&^*&()'*%})
-    _(resource.send(:create_mysql_cmd, 'SELECT 1 FROM DUAL;').must_equal(%q{mysql -u\$\#\!\%\'\'\%\$\#\'\"\'\&\% -p\'\%\"\'\"\&\^\*\&\(\)\'\*\% -h localhost -s -e "SELECT 1 FROM DUAL;"}))
+    _(resource.send(:create_mysql_cmd, 'SELECT 1 FROM DUAL;').must_equal(%q{mysql -uroot -p\'\%\"\'\"\&\^\*\&\(\)\'\*\% -h localhost -s -e "SELECT 1 FROM DUAL;"}))
   end
   it 'verify mysql_session omits optional username and password' do
     resource = load_resource('mysql_session')
