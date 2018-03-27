f97be7ed331e472f1b871f473ebbe92c8a3dd21d
Paul Welch <pwelch@chef.io>
2018-03-27 15:13:17 -0400

    Refactor recent pip changes for Windows
    Signed-off-by: Paul Welch <pwelch@chef.io>

--- lib/resources/pip.rb
+++ lib/resources/pip.rb
@@ -27,7 +27,6 @@ module Inspec::Resources
       @package_name = package_name
       @pip_cmd = pip_path || default_pip_path
 
-      return skip_resource 'python not found' if paths['Python'].nil?
       return skip_resource 'pip not found' if @pip_cmd.nil?
     end
 
@@ -68,7 +67,7 @@ module Inspec::Resources
     end
 
     def cmd_successful?
-      result = inspec.command("#{@pip_cmd} show #{@package_name}")
+      result = cmd
       return true if result.exit_status == 0
 
       if result.exit_status != 0
@@ -84,15 +83,15 @@ module Inspec::Resources
       false
     end
 
-    # Paths of Python and Pip
+    # Paths of Python and Pip on windows
     # {"Pip" => nil, "Python" => "/path/to/python"}
     #
-    # @return [Hash] of paths
-    def paths
-      return @__paths if @__paths
+    # @return [Hash] of windows_paths
+    def windows_paths
+      return @__windows_paths if @__windows_paths
       cmd = inspec.command('New-Object -Type PSObject | Add-Member -MemberType NoteProperty -Name Pip -Value (Invoke-Command -ScriptBlock {where.exe pip}) -PassThru | Add-Member -MemberType NoteProperty -Name Python -Value (Invoke-Command -ScriptBlock {where.exe python}) -PassThru | ConvertTo-Json')
 
-      @__paths = JSON.parse(cmd.stdout)
+      @__windows_paths = JSON.parse(cmd.stdout)
     end
 
     # Default path of python pip installation
@@ -101,15 +100,18 @@ module Inspec::Resources
     def default_pip_path
       return 'pip' unless inspec.os.windows?
 
+      # If python is not found, return with skip_resource
+      return skip_resource 'python not found' if windows_paths['Python'].nil?
+
       # Pip is not on the default path for Windows, therefore we do some logic
       # to find the binary on Windows
       begin
         # use pip if it on system path
-        pipcmd = paths['Pip']
+        pipcmd = windows_paths['Pip']
         # calculate path on windows
-        if defined?(paths['Python']) && pipcmd.nil?
-          return nil if paths['Pip'].nil?
-          pipdir = paths['Python'].split('\\')
+        if defined?(windows_paths['Python']) && pipcmd.nil?
+          return nil if windows_paths['Pip'].nil?
+          pipdir = windows_paths['Python'].split('\\')
           # remove python.exe
           pipdir.pop
           pipcmd = pipdir.push('Scripts').push('pip.exe').join('/')
