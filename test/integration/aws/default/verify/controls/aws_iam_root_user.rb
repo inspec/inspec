27203110cd07e7add64d42e812de832c461c68e9
Paul Welch <pwelch@users.noreply.github.com>
2018-04-03 09:13:52 -0400

    Add AWS hardware MFA matcher (#2892)
    * Add AWS hardware MFA matcher
    Adding a hardware as well as a virtual MFA matcher for aws_iam_root_user
    resource
    
    * Add New AWS Root Matcher Docs
    - Add documentation for new root MFA matchers
    - Fix logic for checking MFA devices from feedback on PR
    
    * Add Integration tests for MFA matchers
    - Add integration tests for virtual and hardware MFA matchers
    - Clean up logic for has_virtual_mfa_enabled? method
    
    Signed-off-by: Paul Welch <pwelch@chef.io>

--- test/integration/aws/default/verify/controls/aws_iam_root_user.rb
+++ test/integration/aws/default/verify/controls/aws_iam_root_user.rb
@@ -18,6 +18,14 @@ control "aws_iam_root_user has_mfa_enabled property" do
   end
 end
 
+#---------- Property - has_virtual_mfa_enabled ----------#
+# Negative test in 'minimal' test set.
+control "aws_iam_root_user has_virtual_mfa_enabled property" do
+  describe aws_iam_root_user do
+    it { should have_virtual_mfa_enabled }
+  end
+end
+
 #------------- Property - has_access_key -------------#
 # Positive test in 'minimal' test set
 control "aws_iam_root_user has_access_key property" do
