c04a98c9f83602c4a13f54565904138d07f73062
Matthew Dromazos <dromazmj@dukes.jmu.edu>
2018-04-05 12:51:22 -0400

    New Skeletal Resource aws_route_tables (#2643)
    * Initial commit of skeletal resource aws_route_tables
    * Fixes issues with documentation
    * Renames route table terraform resources to be more conventional
    * Removes tags terraform resources
    * Changes aws_route_table and aws_route_tables integration tests to use new terraform names
    * Removes unneeded data given in unit tests
    
    Signed-off-by: Matthew Dromazos <dromazmj@dukes.jmu.edu>

---
+++ lib/resources/aws/aws_route_tables.rb
@@ -0,0 +1,60 @@
+class AwsRouteTables < Inspec.resource(1)
+  name 'aws_route_tables'
+  desc 'Verifies settings for AWS Route Tables in bulk'
+  example '
+    describe aws_route_tables do
+      it { should exist }
+    end
+  '
+  supports platform: 'aws'
+
+  include AwsPluralResourceMixin
+  # Underlying FilterTable implementation.
+  filter = FilterTable.create
+  filter.add_accessor(:entries)
+        .add(:exists?) { |x| !x.entries.empty? }
+        .add(:vpc_ids, field: :vpc_id)
+        .add(:route_table_ids, field: :route_table_id)
+  filter.connect(self, :routes_data)
+
+  def routes_data
+    @table
+  end
+
+  def to_s
+    'Route Tables'
+  end
+
+  private
+
+  def validate_params(raw_criteria)
+    unless raw_criteria.is_a? Hash
+      raise 'Unrecognized criteria for fetching Route Tables. ' \
+            "Use 'criteria: value' format."
+    end
+
+    # No criteria yet
+    unless raw_criteria.empty?
+      raise ArgumentError, 'aws_route_tables does not currently accept resource parameters.'
+    end
+    raw_criteria
+  end
+
+  def fetch_from_api
+    backend = BackendFactory.create(inspec_runner)
+    catch_aws_errors do
+      @table = backend.describe_route_tables({}).to_h[:route_tables]
+    end
+  end
+
+  class Backend
+    class AwsClientApi < AwsBackendBase
+      BackendFactory.set_default_backend self
+      self.aws_client_class = Aws::EC2::Client
+
+      def describe_route_tables(query = {})
+        aws_service_client.describe_route_tables(query)
+      end
+    end
+  end
+end
---
+++ test/unit/resources/aws_route_tables_test.rb
@@ -0,0 +1,70 @@
+require 'helper'
+
+class EmptyAwsRouteTablesTest < Minitest::Test
+  def setup
+    AwsRouteTables::BackendFactory.select(AwsMRtbB::Empty)
+  end
+
+  def test_constructor_no_args_ok
+    AwsRouteTables.new
+  end
+  
+  def test_search_miss
+    refute AwsRouteTables.new.exists?
+  end
+
+  def test_constructor_reject_unknown_resource_params
+    assert_raises(ArgumentError) { AwsRouteTables.new(bla: 'blabla') }
+  end
+end
+
+class BasicAwsRouteTablesTest2 < Minitest::Test
+  def setup
+    AwsRouteTables::BackendFactory.select(AwsMRtbB::Basic)
+  end
+
+  def test_search_hit
+    assert AwsRouteTables.new.exists?
+  end
+
+  def test_property_vpc_ids
+    basic = AwsRouteTables.new
+    assert_kind_of(Array, basic.vpc_ids)
+    assert(basic.vpc_ids.include?('vpc-169f777e'))
+    assert(basic.vpc_ids.include?('vpc-169f777d'))
+    refute(basic.vpc_ids.include?(nil))
+  end
+
+  def test_property_route_table_ids
+    basic = AwsRouteTables.new
+    assert_kind_of(Array, basic.route_table_ids)
+    assert(basic.route_table_ids.include?('rtb-2c60ec44'))
+    assert(basic.route_table_ids.include?('rtb-58508630'))
+    refute(basic.route_table_ids.include?(nil))
+  end
+end
+
+# MRtbB = Mock Routetable Backend
+module AwsMRtbB
+  class Empty < AwsBackendBase
+    def describe_route_tables(query)
+      OpenStruct.new(route_tables: [])
+    end
+  end
+
+  class Basic < AwsBackendBase
+    def describe_route_tables(query)
+      fixtures = [
+        OpenStruct.new({
+                          route_table_id: 'rtb-2c60ec44',
+                          vpc_id: 'vpc-169f777e'
+        }),
+        OpenStruct.new({
+                          route_table_id: 'rtb-58508630',
+                          vpc_id: 'vpc-169f777d'
+        })
+      ]
+      OpenStruct.new({ route_tables: fixtures })
+    end
+  end
+end
