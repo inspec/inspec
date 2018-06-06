957abf4e042c120e526951ea3d82fac0fc5f559b
Clinton Wolfe <clintoncwolfe@gmail.com>
2018-06-05 23:02:18 -0400

    They and Their both pass functional tests
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- lib/inspec/they_their.rb
+++ lib/inspec/they_their.rb
@@ -1,5 +1,16 @@
 module RSpec
   module Its
+    # This is from the gem rspec-its.  We can just alias.
     alias their its
+  end
+end
+
+module RSpec
+  module Core
+    class ExampleGroup
+      # Can't just alias here, the methods are all handled by special macros.
+      # See https://github.com/rspec/rspec-core/blob/master/lib/rspec/core/example_group.rb#L158
+      define_example_method :they
+    end
   end
 end
