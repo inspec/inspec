eba212c2e4d0dfb923ecbc575a476085395de6d2
James Stocks <jstocks@chef.io>
2018-07-17 16:31:41 +0100

    Generate describe code for an array of strings
    Context:
    When testing a Windows registry key with a period character in it e.g. `explorer.exe` it is not possible to use `its("explorer.exe")` because the period would be interpreted as method chaining.
    In this case, you must instead use `its(["explorer", "exe"])`
    See https://github.com/inspec/inspec/issues/1281
    
    This commit fixes `to_ruby`in `Inspec::Describe` so that it produces an array in the generated Inspec code instead of a string.
    
    Signed-off-by: James Stocks <jstocks@chef.io>

--- test/unit/dsl/describe_test.rb
+++ test/unit/dsl/describe_test.rb
@@ -119,5 +119,15 @@ describe "unknown object" do
 end
 '.strip
     end
+
+    it 'constructs a test with an array of strings' do
+      obj.qualifier = [['resource']]
+      obj.add_test(['explorer', 'exe'], 'cmp', 1)
+      obj.to_ruby.must_equal '
+describe resource do
+  its(["explorer", "exe"]) { should cmp 1 }
+end
+'.strip
+    end
   end
 end
