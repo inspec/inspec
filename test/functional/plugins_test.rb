97caaa5f532126263e5b57cc17eb7f14e7ef4182
Clinton Wolfe <clintoncwolfe@gmail.com>
2018-08-14 09:57:56 -0400

    Looks like the cli help was fine after all
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- test/functional/plugins_test.rb
+++ test/functional/plugins_test.rb
@@ -58,6 +58,23 @@ describe 'cli command plugins' do
   it 'is able to respond to a plugin-based cli subcommand' do
     outcome = inspec_with_env('meaningoflife answer',  INSPEC_CONFIG_DIR: File.join(config_dir_path, 'meaning_by_path'))
     outcome.stderr.wont_include 'Could not find command "meaningoflife"'
+    outcome.stderr.must_equal ''
+    outcome.stdout.must_equal ''
     outcome.exit_status.must_equal 42
+  end
+
+  it 'is able to respond to [help subcommand] invocations' do
+    outcome = inspec_with_env('help meaningoflife',  INSPEC_CONFIG_DIR: File.join(config_dir_path, 'meaning_by_path'))
+    outcome.exit_status.must_equal 0
+    outcome.stderr.must_equal ''
+    outcome.stdout.must_include 'inspec meaningoflife answer'
+    outcome.stdout.must_include 'Exits immediately with an exit code reflecting the answer to life the universe, and everything.'
+  end
+
+  # This is an important test; usually CLI plugins are only activated when their name is present in ARGV
+  it 'includes plugin-based cli commands in top-level help' do
+    outcome = inspec_with_env('help',  INSPEC_CONFIG_DIR: File.join(config_dir_path, 'meaning_by_path'))
+    outcome.exit_status.must_equal 0
+    outcome.stdout.must_include 'inspec meaningoflife'
   end
 end
