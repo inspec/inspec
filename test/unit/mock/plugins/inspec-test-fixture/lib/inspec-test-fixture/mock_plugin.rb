b2f96a1e0a7899297c6e65666e44d4c84b971623
Clinton Wolfe <clintoncwolfe@gmail.com>
2018-08-16 12:13:27 -0400

    Make a test fixture gem, v0.1.0
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

---
+++ test/unit/mock/plugins/inspec-test-fixture/lib/inspec-test-fixture/mock_plugin.rb
@@ -0,0 +1,8 @@
+module InspecPlugins::TextFixture
+  class MockPlugin < Inspec.plugin(2, :mock_plugin_type)
+    def execute(opts = {})
+      # Check to see if Array responds to 'third'
+      Array.respond_to?(:third)
+    end
+  end
+end
