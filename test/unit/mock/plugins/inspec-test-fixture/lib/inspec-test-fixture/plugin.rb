d0201d44db818786b458fbe753d4a39b006e6df4
Clinton Wolfe <clintoncwolfe@gmail.com>
2018-08-16 12:42:50 -0400

    Add a fixture with a pre-installed gem
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- test/unit/mock/plugins/inspec-test-fixture/lib/inspec-test-fixture/plugin.rb
+++ test/unit/mock/plugins/inspec-test-fixture/lib/inspec-test-fixture/plugin.rb
@@ -1,13 +1,13 @@
 module InspecPlugins
-  module MeaningOfLife
+  module TestFixture
 
     class Plugin < Inspec.plugin(2)
-      plugin_name :meaning_of_life
-      # cli 'meaning-of-life-the-universe-and-everything' do
-      #   require_relative './cli'
-      #   InspecPlugins::MeaningOfLife::Cli
-      # end
-    end
+      plugin_name :test_fixture
 
+      mock_plugin :test_fixture do
+        require 'mock_plugin'
+        InspecPlugins::TestFixture
+      end
+    end
   end
 end
