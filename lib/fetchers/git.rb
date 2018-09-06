52e54d4a24fad205d915ab85fb514a0bfa4843e8
Noel Georgi <git@frezbo.com>
2018-09-06 12:27:44 +0530

    Remove the `.git` directory after vendor
    Fixes: https://github.com/inspec/inspec/issues/3357
    
    Signed-off-by: Noel Georgi <git@frezbo.com>

--- lib/fetchers/git.rb
+++ lib/fetchers/git.rb
@@ -50,6 +50,7 @@ module Fetchers
         Dir.mktmpdir do |tmpdir|
           checkout(tmpdir)
           Inspec::Log.debug("Checkout of #{resolved_ref} successful. Moving checkout to #{dir}")
+          FileUtils.rm_rf("#{tmpdir}/.git")
           FileUtils.cp_r(tmpdir + '/.', @repo_directory)
         end
       end
