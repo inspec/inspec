9ed57907182160f2bcbce1a2949f79fe2371833f
Christoph Hartmann <chris@lollyrock.com>
2018-09-10 14:38:35 +0200

    add profile with wrong character set
    Signed-off-by: Christoph Hartmann <chris@lollyrock.com>

---
+++ test/unit/mock/profiles/wrong-char-profile/controls/control.rb
@@ -0,0 +1,7 @@
+control 'char' do
+    title "ensure we have the right characterset"
+    desc "Ensure we do not freak out with “"
+    describe inspec.profile.file('ascii').force_encoding(Encoding::ISO_8859_1) do
+      it { should_not match(/conceptually/) }
+    end
+end
