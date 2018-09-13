31cb77f35e9fb6d1f7998dbfc10da153b66030a6
Jared Quick <jquick@chef.io>
2018-09-13 15:05:51 -0400

    Fix syntax in tests.
    Signed-off-by: Jared Quick <jquick@chef.io>

--- test/functional/inspec_schema_test.rb
+++ test/functional/inspec_schema_test.rb
@@ -12,7 +12,7 @@ describe 'inspec schema' do
       out.exit_status.must_equal 0
       data = JSON.parse(out.stdout)
       data.class.must_equal Array
-      data.size.must_be :>, 1
+      data.size.must_be(:>, 1)
       data[0]['name'].must_equal 'aix'
       families = ['aix', 'unix', 'os']
       data[0]['families'].must_equal families
