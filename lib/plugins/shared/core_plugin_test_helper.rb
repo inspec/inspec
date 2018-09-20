734adb3b8ebdbfa3ee2ba427be49dd90847a1998
Clinton Wolfe <clintoncwolfe@gmail.com>
2018-09-20 00:48:56 -0400

    Add a helper method that temporarily makes InSpec think a plugin is installed
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- lib/plugins/shared/core_plugin_test_helper.rb
+++ lib/plugins/shared/core_plugin_test_helper.rb
@@ -11,6 +11,8 @@ require 'ostruct'
 
 # Utilities often needed
 require 'fileutils'
+require 'tmpdir'
+require 'pathname'
 
 # Configure MiniTest to expose things like `let`
 class Module
@@ -42,9 +44,57 @@ module CorePluginFunctionalHelper
     end
     TRAIN_CONNECTION.run_command("#{prefix} #{inspec_bin_path} #{command_line}")
   end
+
+  # This helper does some fancy footwork to make InSpec think a plugin
+  # under development is temporarily installed.
+  def run_inspec_process_with_this_plugin(command_line, opts = {})
+    plugin_path = __find_plugin_path_from_caller
+    plugin_file_data = __make_plugin_file_data_structure(plugin_path)
+
+    Dir.mktmpdir do |tmp_dir|
+      File.write(File.join(tmp_dir, 'plugins.json'), JSON.generate(plugin_file_data))
+      opts[:env] ||= {}
+      opts[:env]['INSPEC_CONFIG_DIR'] = tmp_dir
+      run_inspec_process(command_line, opts)
+    end
+  end
+
+  def __find_plugin_path_from_caller(frames_back = 2)
+    caller_path = Pathname.new(caller_locations(frames_back, 1).first.absolute_path)
+    # Typical caller path:
+    # /Users/cwolfe/sandbox/inspec-resource-lister/test/functional/inspec_resource_lister_test.rb
+    # We want:
+    # /Users/cwolfe/sandbox/inspec-resource-lister/lib/inspec-resource-lister.rb
+    cursor = caller_path
+    until cursor.basename.to_s == 'test' && cursor.parent.basename.to_s =~ /^(inspec|train)-/
+      cursor = cursor.parent
+      break if cursor.nil?
+    end
+    raise 'Could not comprehend plugin project directory structure' if cursor.nil?
+
+    project_dir = cursor.parent
+    plugin_name = project_dir.basename
+    entry_point = File.join(project_dir.to_s, 'lib', plugin_name.to_s + '.rb')
+    raise 'Could not find plugin entry point' unless File.exist?(entry_point)
+    entry_point
+  end
+
+  def __make_plugin_file_data_structure(path)
+    # TODO: dry this up, refs #3350
+    plugin_name = File.basename(path, '.rb')
+    {
+      'plugins_config_version' => '1.0.0',
+      'plugins' => [
+        {
+          'name' => plugin_name,
+          'installation_type' => 'path',
+          'installation_path' => path,
+        },
+      ],
+    }
+  end
 end
 
 module CorePluginUnitHelper
   include CorePluginBaseHelper
-  require 'inspec'
 end
