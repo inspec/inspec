80d6ade71702c438b7cbc19fdb4c74b3d66d5979
Clinton Wolfe <clintoncwolfe@gmail.com>
2018-09-24 23:48:43 -0400

    Extend run_inspec_with_this_plugin to take two blocks allowing setup and teardown; and wrap the result in a delagating object, with a payload slot.
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- lib/plugins/shared/core_plugin_test_helper.rb
+++ lib/plugins/shared/core_plugin_test_helper.rb
@@ -13,10 +13,28 @@ require 'ostruct'
 require 'fileutils'
 require 'tmpdir'
 require 'pathname'
+require 'forwardable'
 
 # Configure MiniTest to expose things like `let`
 class Module
   include Minitest::Spec::DSL
+end
+
+module Inspec
+  class FuncTestRunResult
+    attr_reader :train_result
+    attr_reader :payload
+
+    extend Forwardable
+    def_delegator :train_result, :stdout
+    def_delegator :train_result, :stderr
+    def_delegator :train_result, :exit_status
+
+    def initialize(train_result)
+      @train_result = train_result
+      @payload = OpenStruct.new
+    end
+  end
 end
 
 module CorePluginBaseHelper
@@ -42,20 +60,47 @@ module CorePluginFunctionalHelper
     elsif opts.key?(:env)
       prefix = opts[:env].to_a.map { |assignment| "#{assignment[0]}=#{assignment[1]}" }.join(' ')
     end
-    TRAIN_CONNECTION.run_command("#{prefix} #{inspec_bin_path} #{command_line}")
+    Inspec::FuncTestRunResult.new(TRAIN_CONNECTION.run_command("#{prefix} #{inspec_bin_path} #{command_line}"))
   end
 
   # This helper does some fancy footwork to make InSpec think a plugin
   # under development is temporarily installed.
+  # @param String command_line Invocation, without the word 'inspec'
+  # @param Hash opts options as for run_inspec_process, with more options:
+  #    :pre_run: Proc(plugin_statefile_data, tmp_dir_path) - optional setup block.
+  #       Modify plugin_statefile_data as needed; it will be written to a plugins.json
+  #       in tmp_dir_path.  You may also copy in other things to the tmp_dir_path. Your PWD
+  #       will be in the tmp_dir, and it will exist and be empty.
+  #    :post_run: Proc(FuncTestRunResult, tmp_dir_path) - optional result capture block.
+  #       run_result will be populated, but you can add more to the ostruct .payload
+  #       Your PWD will be the tmp_dir, and it will still exist (for a moment!)
   def run_inspec_process_with_this_plugin(command_line, opts = {})
     plugin_path = __find_plugin_path_from_caller
-    plugin_file_data = __make_plugin_file_data_structure(plugin_path)
+
+    # If it looks like it is a core plugin under test, don't add it to the plugin file
+    # since the loader will auto-load it anyway
+    if plugin_path.include?('lib/plugins/inspec-')
+      plugin_file_data = __make_empty_plugin_file_data_structure
+    else
+      plugin_file_data = __make_plugin_file_data_structure_with_path(plugin_path)
+    end
 
     Dir.mktmpdir do |tmp_dir|
-      File.write(File.join(tmp_dir, 'plugins.json'), JSON.generate(plugin_file_data))
+      opts[:pre_run]&.call(plugin_file_data, tmp_dir)
+      plugin_file_path = File.join(tmp_dir, 'plugins.json')
+      # HACK: If the block cleared the hash, take that to mean it will provide a plugins.json file of its own.
+      File.write(plugin_file_path, JSON.generate(plugin_file_data)) unless plugin_file_data.empty?
       opts[:env] ||= {}
       opts[:env]['INSPEC_CONFIG_DIR'] = tmp_dir
-      run_inspec_process(command_line, opts)
+      run_result = run_inspec_process(command_line, opts)
+
+      # Read the resulting plugins.json into memory, if any
+      if File.exist?(plugin_file_path)
+        run_result.payload.plugin_data = JSON.parse(File.read(plugin_file_path))
+      end
+
+      opts[:post_run]&.call(run_result, tmp_dir)
+      run_result
     end
   end
 
@@ -79,18 +124,22 @@ module CorePluginFunctionalHelper
     entry_point
   end
 
-  def __make_plugin_file_data_structure(path)
+  def __make_plugin_file_data_structure_with_path(path)
     # TODO: dry this up, refs #3350
     plugin_name = File.basename(path, '.rb')
+    data = __make_empty_plugin_file_data_structure
+    data['plugins'] << {
+      'name' => plugin_name,
+      'installation_type' => 'path',
+      'installation_path' => path,
+    }
+  end
+
+  def __make_empty_plugin_file_data_structure
+    # TODO: dry this up, refs #3350
     {
       'plugins_config_version' => '1.0.0',
-      'plugins' => [
-        {
-          'name' => plugin_name,
-          'installation_type' => 'path',
-          'installation_path' => path,
-        },
-      ],
+      'plugins' => [],
     }
   end
 end
