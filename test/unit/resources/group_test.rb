7a6119e7a6e34c6cdac484f5050dde69dd1968e6
Miah Johnson <miah@chia-pet.org>
2018-10-18 13:48:30 -0700

    Add missing tests for groups resource, document members property, and assorted fixes. (#3467)
    * Add missing tests for groups resource, document members propery, and assorted fixes.
    
    Update existing documentation for group resource.
    Add documentation for groups resource.
    Update group resource tests to test members property.
    Change groups resource members property to have simple style. (this
    ensures members is a single array)
    remove deprecated have_gid propery.
    change `if !` to `unless`
    Remove early return from members method. This prevented members from
    working correctly on any OS other than Windows.
    Add missing tests for the groups resource.
    remove tests for has_gid
    
    Signed-off-by: Miah Johnson <miah@chia-pet.org>
    
    * Fix comments
    
    Signed-off-by: Jared Quick <jquick@chef.io>

--- test/unit/resources/group_test.rb
+++ test/unit/resources/group_test.rb
@@ -12,14 +12,18 @@ describe 'Inspec::Resources::Group' do
     resource = MockLoader.new(:ubuntu1404).load_resource('group', 'root')
     _(resource.exists?).must_equal true
     _(resource.gid).must_equal 0
-    _(resource.has_gid?(0)).must_equal true
   end
 
   it 'verify group on ubuntu with mixed case' do
     resource = MockLoader.new(:ubuntu1404).load_resource('group', 'GroupWithCaps')
     _(resource.exists?).must_equal true
     _(resource.gid).must_equal 999
-    _(resource.has_gid?(999)).must_equal true
+  end
+
+  it 'verify group on ubuntu with members' do
+    resource = MockLoader.new(:ubuntu1404).load_resource('group', 'www-data')
+    _(resource.exists?).must_equal true
+    _(resource.members).must_equal "www-data,root"
   end
 
   # ubuntu with non-existent group
@@ -27,7 +31,6 @@ describe 'Inspec::Resources::Group' do
     resource = MockLoader.new(:ubuntu1404).load_resource('group', 'nogroup')
     _(resource.exists?).must_equal false
     _(resource.gid).must_be_nil
-    _(resource.has_gid?(0)).must_equal false
   end
 
   # mac
@@ -35,7 +38,6 @@ describe 'Inspec::Resources::Group' do
     resource = MockLoader.new(:osx104).load_resource('group', 'root')
     _(resource.exists?).must_equal true
     _(resource.gid).must_equal 0
-    _(resource.has_gid?(0)).must_equal true
   end
 
   # freebsd
@@ -43,7 +45,6 @@ describe 'Inspec::Resources::Group' do
     resource = MockLoader.new(:freebsd10).load_resource('group', 'root')
     _(resource.exists?).must_equal true
     _(resource.gid).must_equal 0
-    _(resource.has_gid?(0)).must_equal true
   end
 
   # windows with local group
@@ -51,7 +52,6 @@ describe 'Inspec::Resources::Group' do
     resource = MockLoader.new(:windows).load_resource('group', 'Administrators')
     _(resource.exists?).must_equal true
     _(resource.gid).must_equal 'S-1-5-32-544'
-    _(resource.has_gid?(0)).must_equal false
     _(resource.members).must_equal ['Administrators', 'Domain Admins']
   end
 
@@ -59,7 +59,6 @@ describe 'Inspec::Resources::Group' do
     resource = MockLoader.new(:windows).load_resource('group', 'Power Users')
     _(resource.exists?).must_equal true
     _(resource.gid).must_equal 'S-1-5-32-547'
-    _(resource.has_gid?(0)).must_equal false
     _(resource.members).must_equal []
   end
 
@@ -68,8 +67,7 @@ describe 'Inspec::Resources::Group' do
     resource = MockLoader.new(:windows).load_resource('group', 'dhcp')
     _(resource.exists?).must_equal false
     _(resource.gid).must_be_nil
-    _(resource.has_gid?(0)).must_equal false
-    _(resource.members).must_equal nil
+    _(resource.members).must_be_nil
   end
 
   # undefined
@@ -77,6 +75,5 @@ describe 'Inspec::Resources::Group' do
     resource = MockLoader.new(:undefined).load_resource('group', 'root')
     _(resource.exists?).must_equal false
     _(resource.gid).must_be_nil
-    _(resource.has_gid?(0)).must_equal false
   end
 end
