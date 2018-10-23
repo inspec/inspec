f835c24e89b6c9fdea93e601b6db2fd101744730
Andrey Klyachkin <aklyachkin@gmail.com>
2018-10-23 15:16:24 +0200

    ksh support (#3533)
    Add ksh (Korn Shell) resource

---
+++ lib/resources/ksh.rb
@@ -0,0 +1,35 @@
+# encoding: utf-8
+
+require 'utils/command_wrapper'
+require 'resources/command'
+
+module Inspec::Resources
+  class Ksh < Cmd
+    name 'ksh'
+    supports platform: 'unix'
+    desc 'Run a command or script in KornShell.'
+    example "
+      describe ksh('ls -al /') do
+        its('stdout') { should match /bin/ }
+        its('stderr') { should eq '' }
+        its('exit_status') { should eq 0 }
+      end
+
+      # Specify the path of the executable:
+      ksh('...', path: '/usr/bin/ksh93')
+
+      # Specify arguments (defaults to -c)
+      ksh('...', args: '-x -c')
+    "
+
+    def initialize(command, options = {})
+      @raw_command = command
+      options[:shell] = 'ksh' if options.is_a?(Hash)
+      super(CommandWrapper.wrap(command, options))
+    end
+
+    def to_s
+      "KornShell command #{@raw_command}"
+    end
+  end
+end
---
+++ test/unit/resources/ksh_test.rb
@@ -0,0 +1,27 @@
+# encoding: utf-8
+
+require 'helper'
+require 'inspec/resource'
+
+describe Inspec::Resources::Ksh do
+  let(:x) { rand.to_s }
+  let(:resource) { load_resource('ksh', '$("'+x+'")') }
+
+  it 'prints as a ksh command' do
+    resource.to_s.must_equal 'KornShell command $("'+x+'")'
+  end
+
+  it 'wraps the command' do
+    resource.command.must_equal "ksh -c \\$\\(\\\"#{x}\\\"\\)"
+  end
+
+  it 'can specify an executable path' do
+    resource = load_resource('ksh', '$("'+x+'")', path: '/bin/ksh')
+    resource.command.must_equal "/bin/ksh -c \\$\\(\\\"#{x}\\\"\\)"
+  end
+
+  it 'can specify a arguments' do
+    resource = load_resource('ksh', '$("'+x+'")', args: '-x -c')
+    resource.command.must_equal "ksh -x -c \\$\\(\\\"#{x}\\\"\\)"
+  end
+end
