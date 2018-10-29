55862aebacc38f0942226912299b8973587a93e0
Clinton Wolfe <clintoncwolfe@gmail.com>
2018-10-29 19:58:21 -0400

    inspec check will now error on duplicate IDs
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- test/unit/mock/profiles/dupe-controls/controls/duplicates.rb
+++ test/unit/mock/profiles/dupe-controls/controls/duplicates.rb
@@ -1,9 +1,8 @@
 control 'dupe-01' do
   impact 1
   title 'dupe-01-title-one'
-  desc 'default-one'
-  description 'collision', 'one'
-  description 'uniq-one', 'one'
+  desc 'collision', 'one'
+  desc 'uniq-one', 'one'
 
   describe true do
     it { should eq true }
@@ -13,9 +12,8 @@ end
 control 'dupe-01' do
   impact 0
   title 'dupe-01-title-two'
-  desc 'default-two'
-  description 'collision', 'two'
-  description 'uniq-two', 'two'
+  desc 'collision', 'two'
+  desc 'uniq-two', 'two'
 
   describe true do
     it { should eq false }
