54fd031c0c7cc5ea695b27371c53711ac47424ce
mhackethal <31733219+mhackethal@users.noreply.github.com>
2018-11-13 13:12:57 +0100

    Improve filesystem.rb to support windows
    improve filesystem.rb to support windows.
    Split into 2 classes LinuxFileSystemResource / WindowsFileSystemResource
    Add filesystem to verify a FS-type ( currently not for linux because missing test server )
    Size on Windows is converted to GB - discussion about this welcome

--- lib/resources/filesystem.rb
+++ lib/resources/filesystem.rb
@@ -1,31 +1,113 @@
 module Inspec::Resources
-  class FileSystemResource < Inspec.resource(1)
-    name 'filesystem'
-    supports platform: 'linux'
-    desc 'Use the filesystem InSpec resource to test file system'
-    example "
-      describe filesystem('/') do
-        its('size') { should be >= 32000 }
-      end
-    "
-    attr_reader :partition
+	class FileSystemResource < Inspec.resource(1)
+		name 'filesystem'
+		supports platform: 'linux'
+		supports platform: 'windows'
+		desc 'Use the filesystem InSpec resource to test file system'
+		example "
+			'filesystem' currently not supported for Linux
+			describe filesystem('/') do
+				its('size') { should be >= 32000 }
+				its('filesystem') { should eq false }
+			end
+			
+			describe filesystem('c:') do
+				its('size') { should be >= 90 }
+				its('filesystem') { should eq 'NTFS' }
+			end
+			
+		"
+		attr_reader :partition
 
-    def initialize(partition)
-      @partition = partition
-    end
+		def initialize(partition)
+			@partition = partition
+			@cache = nil
+			#select file system manager
+			@fsman = nil
+			
+			os = inspec.os
+			if os.linux?
+				@fsman = LinuxFileSystemResource.new(inspec)
+			elsif os.windows?
+				@fsman = WindowsFileSystemResource.new(inspec)
+			else
+				raise Inspec::Exceptions::ResourceSkipped, 'The `filesystem` resource is not supported on your OS yet.'
+			end
+		end
+		
+		def info 
+			return @cache if !@cache.nil?
 
-    def size
-      @size ||= begin
-        cmd = inspec.command("df #{partition} --output=size")
-        raise Inspec::Exceptions::ResourceFailed, "Unable to get available space for partition #{partition}" if cmd.stdout.nil? || cmd.stdout.empty? || !cmd.exit_status.zero?
+			return {} if @fsman.nil?
+			
+			@fsman.info(@partition)
+		end
 
-        value = cmd.stdout.gsub(/\dK-blocks[\r\n]/, '').strip
-        value.to_i
-      end
-    end
+		
+		
+		def to_s
+			"FileSystem #{@partition}"
+		end	
 
-    def to_s
-      "Filesystem #{partition}"
-    end
-  end
+		def size
+			info = @fsman.info(@partition)
+			info[:size]
+		end		
+
+		def filesystem
+			info = @fsman.info(@partition)
+			info[:filesystem]
+		end
+		
+	end
+	
+	class FsManagement
+		attr_reader :inspec
+		def initialize(inspec)
+			@inspec = inspec
+		end
+	end
+	
+	class LinuxFileSystemResource < FsManagement
+		def info(partition)
+			begin
+				cmd = inspec.command("df #{partition} --output=size")
+				raise Inspec::Exceptions::ResourceFailed, "Unable to get available space for partition #{partition}" if cmd.stdout.nil? || cmd.stdout.empty? || !cmd.exit_status.zero?
+				value = cmd.stdout.gsub(/\dK-blocks[\r\n]/, '').strip
+
+				{
+				  name: partition,				
+				  size: value.to_i,
+				  filesystem: false,
+				}
+			end
+		end
+	end
+	
+	class WindowsFileSystemResource < FsManagement
+		def info(partition)
+
+			cmd = inspec.command <<-EOF.gsub(/^\s*/, '')
+			  $disk = Get-WmiObject Win32_LogicalDisk -Filter "DeviceID='#{partition}'"
+			  $disk.Size = $disk.Size / 1GB
+			  $disk | select -property DeviceID,Size,FileSystem | ConvertTo-Json
+			EOF
+
+			raise Inspec::Exceptions::ResourceSkipped, "Unable to get available space for partition #{partition}" if cmd.stdout == '' || cmd.exit_status.to_i != 0
+				
+			begin
+				fs = JSON.parse(cmd.stdout)
+			rescue JSON::ParserError => e
+			  raise Inspec::Exceptions::ResourceFailed,
+				'Failed to parse JSON from Powershell. ' \
+				"Error: #{e}"
+			end
+									
+			{
+			  name: fs['DeviceID'],
+			  size: fs['Size'].to_i,
+			  filesystem: fs['FileSystem'],
+			}
+		end
+	end	
 end
