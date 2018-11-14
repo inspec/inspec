e1fb7b43bcb91fde22bb67699b713b7aa4c9693f
Clinton Wolfe <clintoncwolfe@gmail.com>
2018-11-14 00:15:50 -0500

    High level formatters
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- lib/inspec/ui.rb
+++ lib/inspec/ui.rb
@@ -1,5 +1,5 @@
 module Inspec
-  # Provides simple UI interaction primitives for CLI commands and plugins.
+  # Provides simple terminal UI interaction primitives for CLI commands and plugins.
   class UI
 
     ANSI_CODES = {
@@ -14,18 +14,30 @@ module Inspec
       },
     }.freeze
 
+    GLYPHS = {
+      list_item: '•', # BULLET, Unicode: U+2022, UTF-8: E2 80 A2
+      check: '✔', #  HEAVY CHECK MARK, Unicode: U+2714, UTF-8: E2 9C 94
+      swirl: '↺', # ANTICLOCKWISE OPEN CIRCLE ARROW, Unicode U+21BA, UTF-8: E2 86 BA
+      script_x: '×', # MULTIPLICATION SIGN, Unicode: U+00D7, UTF-8: C3 97
+      question: '?', # normal ASCII question mark
+      em_dash: '—', # EM DASH Unicode: U+2014, UTF-8: E2 80 94'
+    }.freeze
+
     attr_reader :io
 
     def initialize(opts = {})
       @color = opts[:color].nil? ? true : opts[:color]
-      @io = $stdout
+      @io = opts[:io] || $stdout
     end
 
     def color?
       @color
     end
 
-    # Low-level formatting methods
+    #=========================================================================#
+    #                   Low-level formatting methods
+    #=========================================================================#
+
     def plain(str)
       io.print(str)
     end
@@ -38,6 +50,51 @@ module Inspec
       define_method(color) do |str|
         color? ? io.print(ANSI_CODES[:color][color] + str + ANSI_CODES[:reset]) : plain(str)
       end
+    end
+
+    #=========================================================================#
+    #                   High-Level formatting methods
+    #=========================================================================#
+
+    def headline(str)
+      if str.length < 76
+        dash_length = 80 - str.length - 4 # 4 spaces
+        dash_length = dash_length / 2
+      else
+        dash_length = 0
+      end
+
+      result = "\n\n"
+      result += ' ' + (color? ? GLYPHS[:em_dash] : '-') * dash_length + ' '
+      result += color? ? ANSI_CODES[:bold] + ANSI_CODES[:color][:white] : ''
+      result += str
+      result += color? ? ANSI_CODES[:reset] : ''
+      result += ' ' + (color? ? GLYPHS[:em_dash] : '-') * dash_length + ' '
+      result += "\n"
+
+      io.print result
+    end
+
+    def error(str)
+      result = ''
+      result += color? ? ANSI_CODES[:bold] + ANSI_CODES[:color][:red] : ''
+      result += 'ERROR:'
+      result += color? ? ANSI_CODES[:reset] : ''
+      result += ' '
+      result += str
+      result += "\n"
+      io.print result
+    end
+
+    def warning(str)
+      result = ''
+      result += color? ? ANSI_CODES[:bold] + ANSI_CODES[:color][:yellow] : ''
+      result += 'WARNING:'
+      result += color? ? ANSI_CODES[:reset] : ''
+      result += ' '
+      result += str
+      result += "\n"
+      io.print result
     end
 
   end
--- test/unit/ui_test.rb
+++ test/unit/ui_test.rb
@@ -3,6 +3,8 @@
 require 'minitest'
 require 'minitest/spec'
 require 'inspec/ui'
+require 'stringio'
+require 'byebug'
 
 # https://gist.github.com/chrisopedia/8754917
 # http://www.lihaoyi.com/post/BuildyourownCommandLinewithANSIescapecodes.html#256-colors
@@ -24,6 +26,7 @@ GLYPHS = {
   swirl: '↺', # ANTICLOCKWISE OPEN CIRCLE ARROW, Unicode U+21BA, UTF-8: E2 86 BA
   script_x: '×', # MULTIPLICATION SIGN, Unicode: U+00D7, UTF-8: C3 97
   question: '?', # normal ASCII question mark
+  em_dash: '—', # EM DASH Unicode: U+2014, UTF-8: E2 80 94'
 }.freeze
 
 
@@ -31,40 +34,51 @@ GLYPHS = {
 #                        Low-Level Formatting
 #=============================================================================#
 describe 'Inspec::UI low-level Formatting' do
-  let(:ui) { Inspec::UI.new() }
+  let(:fixture_io) { StringIO.new() }
+  let(:output) { fixture_io.string }
+  let(:ui) { Inspec::UI.new(io: fixture_io) }
+
   describe 'plain' do
     it 'uses no ANSI markers' do
-      ->{ ui.plain('test') }.must_output('test')
+      ui.plain('test')
+      output.must_include('test')
+      output.wont_include('\e[')
     end
   end
 
   describe 'when color is enabled' do
-    let(:ui) { Inspec::UI.new(color: true) }
+    let(:ui) { Inspec::UI.new(color: true, io: fixture_io) }
     describe 'bold' do
       it 'uses ANSI bold markers' do
-        ->{ ui.bold('test') }.must_output(ANSI_CODES[:bold] + 'test' + ANSI_CODES[:reset])
+        ui.bold('test')
+        output.must_equal(ANSI_CODES[:bold] + 'test' + ANSI_CODES[:reset])
       end
     end
     describe 'colors' do
       [:red, :green, :yellow, :white, :grey].each do |color|
         it ('uses the color code for ' + color.to_s) do
-          ->{ ui.send(color, 'test') }.must_output(ANSI_CODES[:color][color] + 'test' + ANSI_CODES[:reset])
+          ui.send(color, 'test')
+          output.must_equal(ANSI_CODES[:color][color] + 'test' + ANSI_CODES[:reset])
         end
       end
     end
   end
 
   describe 'when color is disabled' do
-    let(:ui) { Inspec::UI.new(color: false) }
+    let(:ui) { Inspec::UI.new(color: false, io: fixture_io) }
     describe 'bold' do
       it 'uses no ANSI codes' do
-        ->{ ui.bold('test') }.must_output('test')
+        ui.bold('test')
+        output.wont_include('\e[')
+        output.must_equal('test')
       end
     end
     describe 'colors' do
       [:red, :green, :yellow, :white, :grey].each do |color|
         it ('uses no ANSI codes for ' + color.to_s) do
-          ->{ ui.send(color, 'test') }.must_output('test')
+          ui.send(color, 'test')
+          output.wont_include('\e[')
+          output.must_equal('test')
         end
       end
     end
@@ -74,9 +88,109 @@ end
 #=============================================================================#
 #                        High-Level Formatting
 #=============================================================================#
-# headline
-# format error
-# format warning
+describe 'Inspec::UI High-Level Formatting' do
+  let(:fixture_io) { StringIO.new() }
+  let(:output) { fixture_io.string }
+
+  describe 'when color is enabled' do
+    let(:ui) { Inspec::UI.new(color: true, io: fixture_io) }
+
+    describe 'headline' do
+      it 'formats the headline when short' do
+        ui.headline('test')
+        output.must_match(/^\n\n/) # Start with two newlines
+        expected = ''
+        expected += ' ' + GLYPHS[:em_dash] * 36 + ' '
+        expected += ANSI_CODES[:bold] + ANSI_CODES[:color][:white] + 'test' + ANSI_CODES[:reset]
+        expected += ' ' + GLYPHS[:em_dash] * 36 + ' '
+        output.must_include(expected)
+        output.must_match(/^\n$/) # End with a newline
+      end
+      it 'formats the headline when longer' do
+        ui.headline('Testing is Such a Pleasure!')
+        expected = ''
+        expected += ' ' + GLYPHS[:em_dash] * 24 + ' '
+        expected += ANSI_CODES[:bold] + ANSI_CODES[:color][:white] + 'Testing is Such a Pleasure!' + ANSI_CODES[:reset]
+        expected += ' ' + GLYPHS[:em_dash] * 24 + ' '
+        output.must_include(expected)
+      end
+    end
+
+    describe 'error' do
+      it 'formats the message' do
+        ui.error('Everything has gone terribly wrong')
+        expected = ''
+        expected += ANSI_CODES[:bold] + ANSI_CODES[:color][:red]
+        expected += 'ERROR:'
+        expected += ANSI_CODES[:reset]
+        expected += ' '
+        expected += 'Everything has gone terribly wrong'
+        output.must_include(expected)
+        output.must_match(/\n$/) # End with a newline
+      end
+    end
+
+    describe 'warning' do
+      it 'formats the message' do
+        ui.warning('Maybe we can still pull through this')
+        expected = ''
+        expected += ANSI_CODES[:bold] + ANSI_CODES[:color][:yellow]
+        expected += 'WARNING:'
+        expected += ANSI_CODES[:reset]
+        expected += ' '
+        expected += 'Maybe we can still pull through this'
+        output.must_include(expected)
+        output.must_match(/\n$/) # End with a newline
+      end
+    end
+
+  end
+
+  describe 'when color is disabled' do
+    let(:ui) { Inspec::UI.new(color: false, io: fixture_io) }
+
+    describe 'headline' do
+      it 'formats the headline when short' do
+        ui.headline('test')
+        output.wont_include('\e[') # No ANSI escapes
+        output.wont_match(/[^[:ascii:]]/) # No non-ASCII chars (such as UTF-8 glyphs)
+        expected = ''
+        expected += ' ' + '-' * 36 + ' '
+        expected += 'test'
+        expected += ' ' + '-' * 36 + ' '
+        output.must_include(expected)
+      end
+    end
+
+    describe 'error' do
+      it 'formats the message without color' do
+        ui.error('Everything has gone terribly wrong')
+        output.wont_include('\e[') # No ANSI escapes
+        output.wont_match(/[^[:ascii:]]/) # No non-ASCII chars (such as UTF-8 glyphs)
+        expected = ''
+        expected += 'ERROR:'
+        expected += ' '
+        expected += 'Everything has gone terribly wrong'
+        output.must_include(expected)
+        output.must_match(/\n$/) # End with a newline
+      end
+    end
+
+    describe 'warning' do
+      it 'formats the message' do
+        ui.warning('Maybe we can still pull through this')
+        output.wont_include('\e[') # No ANSI escapes
+        output.wont_match(/[^[:ascii:]]/) # No non-ASCII chars (such as UTF-8 glyphs)
+        expected = ''
+        expected += 'WARNING:'
+        expected += ' '
+        expected += 'Maybe we can still pull through this'
+        output.must_include(expected)
+        output.must_match(/\n$/) # End with a newline
+      end
+    end
+  end
+end
 
 #=============================================================================#
 #                          Tables and Lists
