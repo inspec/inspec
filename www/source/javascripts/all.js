b0c77af72bce14c8869f3241bc1d2e98a5b6476b
Dominik Richter <dominik.richter@gmail.com>
2018-11-19 17:47:22 -0800

    www: remove jquery sticky on the sidebar
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- www/source/javascripts/all.js
+++ www/source/javascripts/all.js
@@ -1,7 +1,6 @@
 //= require vendor/jquery.min
 //= require vendor/foundation.min
 //= require jquery.enllax.min
-//= require jquery.sticky
 //= require highlight.min
 //= require nav
 //= require particles
