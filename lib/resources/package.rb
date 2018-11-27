b67236e485bfd6fa85a3422055941b347866ddeb
mhackethal <31733219+mhackethal@users.noreply.github.com>
2018-11-27 19:21:25 +0100

    package: fix package detection on windows (#3607)
    * Update package.rb to solve issue #3361
    
    Update the windows package search regex.
    According to issue #3361
    
    * Update package.rb
    Fix trailing spaces
    * Update helper.rb
    
    update sha for get-item-package. To fix the issue #3361
    Signed-off-by: markus hackethal <mh@it31.de>

--- lib/resources/package.rb
+++ lib/resources/package.rb
@@ -288,7 +288,7 @@ module Inspec::Resources
       # Find the package
       cmd = inspec.command <<-EOF.gsub(/^\s*/, '')
         Get-ItemProperty (@("#{search_paths.join('", "')}") | Where-Object { Test-Path $_ }) |
-        Where-Object { $_.DisplayName -match "^\\s*#{package_name.shellescape}\\s*$" -or $_.PSChildName -match "^\\s*#{package_name.shellescape}\\s*$" } |
+        Where-Object { $_.DisplayName -match "^\s*#{package_name.shellescape}\.*" -or $_.PSChildName -match "^\s*#{package_name.shellescape}\.*" } |
         Select-Object -Property DisplayName,DisplayVersion | ConvertTo-Json
       EOF
 
