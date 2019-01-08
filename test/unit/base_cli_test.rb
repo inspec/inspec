f1f5b2723768fa0c7a7923d6abfb822978f5144e
Clinton Wolfe <clintoncwolfe@gmail.com>
2019-01-07 20:07:59 -0500

    Create config object and units tests
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- test/unit/base_cli_test.rb
+++ test/unit/base_cli_test.rb
@@ -7,49 +7,7 @@ require 'thor'
 describe 'BaseCLI' do
   let(:cli) { Inspec::BaseCLI.new }
 
-  describe 'opts' do
-    it 'raises if `--password/--sudo-password` are used without value' do
-      default_options = { mock: { sudo_password: -1 } }
-      Inspec::BaseCLI.stubs(:default_options).returns(default_options)
-
-      e = proc { cli.send(:opts, :mock) }.must_raise(ArgumentError)
-      e.message.must_match(/Please provide a value for --sudo-password/)
-    end
-
-    it 'assumes `--sudo` if `--sudo-password` is used without it' do
-      default_options = { mock: { sudo_password: 'p@ssw0rd' } }
-      Inspec::BaseCLI.stubs(:default_options).returns(default_options)
-
-      opts = {}
-      out, err = capture_io do
-        cli.send(:opts, :mock)[:sudo].must_equal true
-      end
-      err.must_match /WARN: `--sudo-password` used without `--sudo`/
-    end
-
-    it 'calls `Compliance::API.login` if `opts[:compliance] is passed`' do
-      default_options = { mock: { compliance: 'mock' } }
-      Inspec::BaseCLI.stubs(:default_options).returns(default_options)
-
-      InspecPlugins::Compliance::API.expects(:login).with('mock')
-
-      cli.send(:opts, :mock)
-    end
-  end
-
-  describe 'merge_options' do
-    let(:default_options) do
-      { exec: { 'reporter' => ['json'], 'backend_cache' => false }}
-    end
-
-    it 'cli defaults populate correctly' do
-      Inspec::BaseCLI.stubs(:default_options).returns(default_options)
-
-      opts = cli.send(:merged_opts, :exec)
-      expected = {"backend_cache"=>false, "reporter"=>{"json"=>{"stdout"=>true}}, "type"=>:exec}
-      opts.must_equal expected
-    end
-
+  describe 'detect command' do
     it 'verify platform detect' do
       hash = { name: 'test-os', families: 'aws, cloud', release: 'aws-sdk-v1' }
       expect = <<EOF
@@ -57,54 +15,10 @@ describe 'BaseCLI' do
   Families:  \e[1m\e[35maws, cloud\e[0m
   Release:   \e[1m\e[35maws-sdk-v1\e[0m
 EOF
-      _(Inspec::BaseCLI.detect(params: hash, indent: 2, color: 35)).must_equal expect
-    end
-
-    it 'json-config options override cli defaults' do
-      Inspec::BaseCLI.stubs(:default_options).returns(default_options)
-
-      parsed_json = { 'backend_cache' => true }
-      cli.expects(:options_json).returns(parsed_json)
-
-      opts = cli.send(:merged_opts, :exec)
-      expected = {"backend_cache"=>true, "reporter"=>{"json"=>{"stdout"=>true}}, "type"=>:exec}
-      opts.must_equal expected
-    end
-
-    it 'cli options override json-config and default' do
-      Inspec::BaseCLI.stubs(:default_options).returns(default_options)
-
-      parsed_json = { 'backend_cache' => false }
-      cli.expects(:options_json).returns(parsed_json)
-
-      cli_options = { 'backend_cache' => true }
-      cli.instance_variable_set(:@options, cli_options)
-
-      opts = cli.send(:merged_opts, :exec)
-      expected = {"backend_cache"=>true, "reporter"=>{"json"=>{"stdout"=>true}}, "type"=>:exec}
-      opts.must_equal expected
-    end
-
-    it 'make sure shell does not get exec defaults' do
-      Inspec::BaseCLI.stubs(:default_options).returns(default_options)
-
-      opts = cli.send(:merged_opts)
-      expected = {}
-      opts.must_equal expected
-    end
-
-    it 'make sure default reporter is overriden by json-config reporter' do
-      default_options['reporter'] = ['cli']
-      Inspec::BaseCLI.stubs(:default_options).returns(default_options)
-
-      parsed_json = { 'reporter' => ['json'] }
-      cli.expects(:options_json).returns(parsed_json)
-
-      opts = cli.send(:merged_opts, :exec)
-      expected = {"backend_cache"=>false, "reporter"=>{"json"=>{"stdout"=>true}}, "type"=>:exec}
-      opts.must_equal expected
+      _(Inspec::BaseCLI.format_platform_info(params: hash, indent: 2, color: 35)).must_equal expect
     end
   end
+
 
   describe 'configure_logger' do
     let(:options) do
@@ -146,40 +60,6 @@ EOF
     end
   end
 
-  describe 'parse_reporters' do
-    it 'parse cli reporters' do
-      opts = { 'reporter' => ['cli'] }
-      parsed = Inspec::BaseCLI.parse_reporters(opts)
-      expected_value = { 'reporter' => { 'cli' => { 'stdout' => true }}}
-      parsed.must_equal expected_value
-    end
-
-    it 'parses cli report and attaches target_id' do
-      opts = { 'reporter' => ['cli'], 'target_id' => '1d3e399f-4d71-4863-ac54-84d437fbc444' }
-      parsed = Inspec::BaseCLI.parse_reporters(opts)
-      expected_value = {"reporter"=>{"cli"=>{"stdout"=>true, "target_id"=>"1d3e399f-4d71-4863-ac54-84d437fbc444"}}, "target_id"=>"1d3e399f-4d71-4863-ac54-84d437fbc444"}
-      parsed.must_equal expected_value
-    end
-  end
-
-  describe 'validate_reporters' do
-    it 'valid reporter' do
-      stdout = { 'stdout' => true }
-      reporters = { 'json' => stdout }
-      Inspec::BaseCLI.validate_reporters(reporters)
-    end
-
-    it 'invalid reporter type' do
-      reporters = ['json', 'magenta']
-      proc { Inspec::BaseCLI.validate_reporters(reporters) }.must_raise NotImplementedError
-    end
-
-    it 'two reporters outputting to stdout' do
-      stdout = { 'stdout' => true }
-      reporters = { 'json' => stdout, 'cli' => stdout }
-      proc { Inspec::BaseCLI.validate_reporters(reporters) }.must_raise ArgumentError
-    end
-  end
 
   describe 'suppress_log_output?' do
     it 'suppresses json' do
