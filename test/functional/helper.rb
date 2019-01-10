ad84987357f839c136e0f23710d645b92161990d
Clinton Wolfe <clintoncwolfe@gmail.com>
2019-01-09 17:16:44 -0800

    Fix spurious ampersands in windows invocation
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- test/functional/helper.rb
+++ test/functional/helper.rb
@@ -187,7 +187,7 @@ module FunctionalHelper
   def assemble_env_prefix(env = {})
     if is_windows?
       env_prefix = env.to_a.map { |assignment| "set #{assignment[0]}=#{assignment[1]}" }.join('&& ')
-      env_prefix += '&& '
+      env_prefix += '&& ' unless env_prefix.empty?
     else
       env_prefix = env.to_a.map { |assignment| "#{assignment[0]}=#{assignment[1]}" }.join(' ')
       env_prefix += ' '
