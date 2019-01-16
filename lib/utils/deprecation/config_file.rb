b89681350ef4b16d1d41efdc4fab5fd772ce6c09
Clinton Wolfe <clintoncwolfe@gmail.com>
2019-01-16 00:26:29 -0500

    Correct comment reference exit code
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- lib/utils/deprecation/config_file.rb
+++ lib/utils/deprecation/config_file.rb
@@ -9,7 +9,7 @@ module Inspec
 
       # What actions may you specify to be taken when a deprecation is encountered?
       VALID_ACTIONS = [
-        :exit, # Hard exit `inspec`, no stacktrace, exit code specified or DEFAULT_HARD_EXIT_STATUS
+        :exit, # Hard exit `inspec`, no stacktrace, exit code specified or Inspec::UI::EXIT_FATAL_DEPRECATION
         :fail_control, # Fail the control with a message. If not in a control, do :warn action instead.
         :ignore, # Do nothing.
         :warn, # Issue a warning
