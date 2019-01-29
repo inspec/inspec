c1a9de7003af0d40b3943d6f9c18d4fb6a5b0d0e
James Massardo <jmassardo@chef.io>
2019-01-29 15:56:20 -0600

    update helpers and mocks so tests run successfully
    Signed-off-by: James Massardo <jmassardo@chef.io>

--- test/helper.rb
+++ test/helper.rb
@@ -425,7 +425,7 @@ class MockLoader
       '/sbin/zpool get -Hp all tank' => cmd.call('zpool-get-all-tank'),
       # docker
       "4f8e24022ea8b7d3b117041ec32e55d9bf08f11f4065c700e7c1dc606c84fd17" => cmd.call('docker-ps-a'),
-      "9ef45813d4545f35d6fe9d6456c0b1063f9f5a80c699d3bb19da0699ab2d6ecc" => cmd.call('df'),
+      "b40ed61c006b54f155b28a85dc944dc0352b30222087b47c6279568ec0e59d05" => cmd.call('df-t'),
       "docker version --format '{{ json . }}'"  => cmd.call('docker-version'),
       "docker info --format '{{ json . }}'" => cmd.call('docker-info'),
       "docker inspect 71b5df59442b" => cmd.call('docker-inspec'),
@@ -551,7 +551,7 @@ class MockLoader
       'apk info -vv --no-network | grep git' => cmd.call('apk-info-grep-git'),
 
       # filesystem command
-      '2df93b941844efec1eee05c311cb4b4e8e3fd8b31808b785a70bd1a46298c716' => cmd.call('get-wmiobject-filesystem'),
+      '3744388bdaf198fad4754076ed3a2250fef2097171f68a4c9e3a830ff2637926' => cmd.call('get-wmiobject-filesystem'),
     }
 
     # ports on linux
