18011ccfec65a6ed42920ae6d2db42121ac94794
Clinton Wolfe <clintoncwolfe@gmail.com>
2019-01-30 14:05:11 -0500

    Also issue default: option for backwards compatibility
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- lib/inspec/objects/attribute.rb
+++ lib/inspec/objects/attribute.rb
@@ -94,6 +94,10 @@ module Inspec
       res = ["#{ruby_var_identifier} = attribute('#{@name}',{"]
       res.push "  title: '#{title}'," unless title.to_s.empty?
       res.push "  value: #{value.inspect}," unless value.to_s.empty?
+      # to_ruby may generate code that is to be used by older versions of inspec.
+      # Anything older than 3.4 will not recognize the value: option, so
+      # send the default: option as well. See #3759
+      res.push "  default: #{value.inspect}," unless value.to_s.empty?
       res.push "  description: '#{description}'," unless description.to_s.empty?
       res.push '})'
       res.join("\n")
