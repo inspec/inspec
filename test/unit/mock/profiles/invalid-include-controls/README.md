94d8d11120e7f7ff75873a6e4f84a2ba5c163915
Jerry Aldrich <jerryaldrichiii@gmail.com>
2019-01-30 09:26:04 -0800

    Respond to feedback
    Signed-off-by: Jerry Aldrich <jerryaldrichiii@gmail.com>

--- test/unit/mock/profiles/invalid-include-controls/README.md
+++
@@ -1,3 +0,0 @@
-# Invalid Include Controls
-
-This profile contains an `include_controls` line with a invalid reference
--- test/unit/mock/profiles/invalid-include-controls/controls/example.rb
+++ test/unit/mock/profiles/invalid-include-controls/controls/example.rb
@@ -1,4 +1,4 @@
 # encoding: utf-8
 # copyright: 2018, The Authors
 
-include_controls 'invalid_name'
+include_controls 'no_such_profile'
