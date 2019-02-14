ec29727838091b1180b3c3dcd5e2c6aa00fe6773
Tim Smith <tsmith@chef.io>
2019-02-13 16:04:53 -0800

    Let Chef handle the root_group itself
    We have node['root_group'] and Chef will apply this on its own.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/cookbooks/os_prepare/recipes/json_yaml_csv_ini_xml.rb
+++ test/cookbooks/os_prepare/recipes/json_yaml_csv_ini_xml.rb
@@ -4,24 +4,11 @@
 #
 # adds a yaml file
 
-gid = case node['platform_family']
-      when 'aix'
-        'system'
-      when 'freebsd'
-        'wheel'
-      when 'solaris', 'solaris2'
-        'sys'
-      else
-        'root'
-      end
-
 ['yml', 'json', 'csv', 'ini', 'toml', 'xml'].each { |filetype|
 
   if node['platform_family'] != 'windows'
     cookbook_file "/tmp/example.#{filetype}" do
       source "example.#{filetype}"
-      owner 'root'
-      group gid
       mode '0755'
       action :create
     end
