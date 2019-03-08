3c834e1a27dff6f235fc786e39dc5b1a3d89b7e0
Stuart Paterson <spaterson@chef.io>
2019-03-08 15:55:46 +0000

    Replace port 22 ingress control.
    Signed-off-by: Stuart Paterson <spaterson@chef.io>

--- lib/plugins/inspec-init/templates/profiles/aws/controls/example.rb
+++ lib/plugins/inspec-init/templates/profiles/aws/controls/example.rb
@@ -15,7 +15,17 @@ control 'aws-single-vpc-exists-check' do                                    # A 
   end
 end
 
-# Plural resources can be inspected to check for specific resource details
+# Plural resources can be inspected to check for specific resource details.
+control 'aws-vpcs-check' do
+  impact 1.0
+  title 'Check in all the VPCs for default sg not allowing 22 inwards'
+  aws_vpcs.vpc_ids.each do |vpc_id|
+    describe aws_security_group(vpc_id: vpc_id, group_name: 'default') do
+      it { should allow_in(port: 22) }
+    end
+  end
+end
+
 control 'aws-vpcs-multi-region-status-check' do                             # A unique ID for this control.
   impact 1.0                                                                # The criticality, if this control fails.
   title 'Check AWS VPCs in all regions have status "available"'             # A human-readable title.
