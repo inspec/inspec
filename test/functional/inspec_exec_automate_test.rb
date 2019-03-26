4b96929bf5f2a13afc309d4173dda914fd728164
Clinton Wolfe <clintoncwolfe@gmail.com>
2019-03-26 10:44:01 -0400

    Modernize automate output test
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- test/functional/inspec_exec_automate_test.rb
+++ test/functional/inspec_exec_automate_test.rb
@@ -6,32 +6,43 @@ require 'tempfile'
 describe 'inspec exec automate' do
   include FunctionalHelper
 
-  let(:json_file) do
-    file = Tempfile.new('json.conf')
-    json = <<~EOF
-    {
-    "reporter": {
-        "automate" : {
-            "stdout" : false,
-            "url" : "https://fake_url_a2.com/data-collector/v0/",
-            "token" : "faketoken123",
-            "insecure" : true,
-            "node_uuid" : "test123"
-            }
-        }
-    }
-    EOF
-
-    file.write(json)
+  let(:config_path) do
+    file = Tempfile.new('config.json')
+    file.write(config_data)
     file.close
     file.path
   end
 
-  it 'fails when trying to send a report to a fake url' do
-    out = inspec('exec ' + example_profile  + ' --no-create-lockfile --json-config ' + json_file)
-    out.stderr.must_equal "Error generating reporter 'automate'\n"
-    out.exit_status.must_equal 1
-    stdout = out.stdout.force_encoding(Encoding::UTF_8)
-    stdout.must_include "ERROR: send_report: POST to /data-collector/v0/"
+  let(:invocation) do
+    cmd = 'exec '
+    cmd += example_profile + ' '
+    cmd += '--config ' + config_path
+    cmd += ' --no-create-lockfile '
+  end
+
+  let(:run_result) { run_inspec_process(invocation) }
+
+  describe 'when the the URL is fake' do
+    let(:config_data) do
+      data = <<~EOF
+      {
+      "reporter": {
+          "automate" : {
+              "stdout" : false,
+              "url" : "https://fake_url_a2.com/data-collector/v0/",
+              "token" : "faketoken123",
+              "insecure" : true,
+              "node_uuid" : "test123"
+              }
+          }
+      }
+      EOF
+    end
+
+    it 'should fail' do
+      run_result.stderr.must_equal "Error generating reporter 'automate'\n"
+      run_result.exit_status.must_equal 1
+      run_result.stdout.must_include "ERROR: send_report: POST to /data-collector/v0/"
+    end
   end
 end
