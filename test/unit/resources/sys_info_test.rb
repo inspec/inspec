474d70b78d696bc8faa07233f064f1fc5fcdffbc
Eike Waldt <waldt@b1-systems.de>
2019-06-21 16:06:54 +0200

    fix description on sys_info.hostname tests
    Signed-off-by: Eike Waldt <waldt@b1-systems.de>

--- test/unit/resources/sys_info_test.rb
+++ test/unit/resources/sys_info_test.rb
@@ -4,17 +4,17 @@ require "inspec/resources/sys_info"
 
 describe "Inspec::Resources::SysInfo" do
   describe "sys_info" do
-    it "check ssh config parsing for Ubuntu" do
+    it "check sys_info.hostname on Ubuntu" do
       resource = MockLoader.new(:ubuntu1504).load_resource("sys_info")
       _(resource.hostname).must_equal "example.com"
     end
 
-    it "check ssh config parsing for Ubuntu" do
+    it "check sys_info.hostname on Windows" do
       resource = MockLoader.new(:windows).load_resource("sys_info")
       _(resource.hostname).must_equal "WIN-CIV7VMLVHLD"
     end
 
-    it "check ssh config parsing for freebsd" do
+    it "check sysinfo.hostname on freebsd" do
       resource = MockLoader.new(:freebsd10).load_resource("sys_info")
       _(resource.hostname).must_equal "The `sys_info.hostname` resource is not supported on your OS yet."
     end
