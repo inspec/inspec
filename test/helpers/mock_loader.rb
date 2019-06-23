3263d7662741f51de230fdc1657ffadf37c82240
Lance Albertson <lance@osuosl.org>
2019-06-22 17:35:32 -0700

    Add ip6tables resource
    This should resolve #1011 which provides an ip6tables resource to test IPv6
    iptables rules. This is essentially a copy of the iptables resource with a few
    renames.
    
    In addition, I've pulled in the integration tests for iptables into ip6tables
    and enabled it on docker so that it properly gets tested regularly. The test
    cookbook recipe has been updated to support all of the current platforms that
    are being tested.
    
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- test/helpers/mock_loader.rb
+++ test/helpers/mock_loader.rb
@@ -323,6 +323,9 @@ class MockLoader
       # iptables
       "/usr/sbin/iptables  -S" => cmd.call("iptables-s"),
       %{bash -c 'type "/usr/sbin/iptables"'} => empty.call,
+      # ip6tables
+      "/usr/sbin/ip6tables  -S" => cmd.call("ip6tables-s"),
+      %{bash -c 'type "/usr/sbin/ip6tables"'} => empty.call,
       # apache_conf
       "sh -c 'find /etc/apache2/ports.conf -type f -maxdepth 1'" => cmd.call("find-apache2-ports-conf"),
       "sh -c 'find /etc/httpd/conf.d/*.conf -type f -maxdepth 1'" => cmd.call("find-httpd-ssl-conf"),
