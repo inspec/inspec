668b8d82bf0862aa0d2629fda24a5c343f3063b7
Jerry Aldrich <jerryaldrichiii@gmail.com>
2019-06-28 00:14:24 -0700

    windows_task: Fix handling of multiple triggers
    When using `schtasks` a list is returned if the task has multiple
    triggers. This merges that list with the last item taking precedence.
    This is how `Get-ScheduledTask` behaves.
    
    Initially, I was going to rewrite this resource to use
    `Get-ScheduledTask` but the original author purposely did not do this
    so that PowerShell v3 would be supported. We only support PowerShell
    v5, but I don't want to break any current users and this change didn't
    seem to controversial to me.
    
    If it gives us trouble, I recommend rewriting it to use
    `Get-ScheduledTask`.
    
    Signed-off-by: Jerry Aldrich <jerryaldrichiii@gmail.com>

--- test/unit/resources/windows_task_test.rb
+++ test/unit/resources/windows_task_test.rb
@@ -3,11 +3,11 @@ require "inspec/resource"
 require "inspec/resources/windows_task"
 
 describe "Inspec::Resources::WindowsTasks" do
-  it "verify parsing a windows task that does not exist" do
+  it "can handle a Windows task that does not exist" do
     resource_fail = MockLoader.new(:windows).load_resource("windows_task", "does-not-exist")
     _(resource_fail.exists?).must_equal false
   end
-  it "verify parsing a windows task which is valid" do
+  it "parses a windows task with one trigger correctly" do
     resource = MockLoader.new(:windows).load_resource("windows_task", "WeLovePizza")
     _(resource.enabled?).must_equal true
     _(resource.disabled?).must_equal false
@@ -17,5 +17,16 @@ describe "Inspec::Resources::WindowsTasks" do
     _(resource.run_as_user).must_equal "LOCAL SERVICE"
     _(resource.type).must_equal "windows-task"
     _(resource.to_s).must_equal "Windows Task 'WeLovePizza'"
+  end
+  it "parses a windows task with more than one trigger correctly" do
+    resource = MockLoader.new(:windows).load_resource("windows_task", "WeLoveMultiplePizzas")
+    _(resource.enabled?).must_equal true
+    _(resource.disabled?).must_equal false
+    _(resource.logon_mode).must_equal "Interactive/Background"
+    _(resource.last_result).must_equal "1056"
+    _(resource.task_to_run).must_equal '%windir%\\system32\\sc.exe start w32time task_started'
+    _(resource.run_as_user).must_equal "LOCAL SERVICE"
+    _(resource.type).must_equal "windows-task"
+    _(resource.to_s).must_equal "Windows Task 'WeLoveMultiplePizzas'"
   end
 end
