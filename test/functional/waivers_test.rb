168c77e472cd075a64fbdafacc09750bce3a2005
Miah Johnson <miah@chia-pet.org>
2019-09-10 16:10:07 -0700

    Refactor tests.
    Signed-off-by: Miah Johnson <miah@chia-pet.org>

--- test/functional/waivers_test.rb
+++ test/functional/waivers_test.rb
@@ -2,65 +2,57 @@ require "functional/helper"
 
 describe "waivers" do
   include FunctionalHelper
+
   let(:waivers_profiles_path) { "#{profile_path}/waivers" }
-  let(:run_result) { run_inspec_process(cmd, json: true) }
-  let(:controls_by_id) { run_result.payload.json.dig("profiles", 0, "controls").map { |c| [c["id"], c] }.to_h }
-  let(:cmd) { "exec #{waivers_profiles_path}/#{profile_name} --input-file #{waivers_profiles_path}/#{profile_name}/files/#{waiver_file}" }
+  let(:run_result)            { run_inspec_process(cmd, json: true) }
+  let(:controls_by_id)        { run_result.payload.json.dig("profiles", 0, "controls").map { |c| [c["id"], c] }.to_h }
+  let(:cmd)                   { "exec #{waivers_profiles_path}/#{profile_name} --input-file #{waivers_profiles_path}/#{profile_name}/files/#{waiver_file}" }
 
   def assert_test_outcome(expected, control_id)
-    assert_equal "#{control_id}_#{expected}", "#{control_id}_#{controls_by_id.dig(control_id, "results", 0, "status")}"
+    assert_equal expected, controls_by_id.dig(control_id, "results", 0, "status")
   end
 
   def assert_waiver_annotation(control_id)
-    # TODO - test JSON for waiver annotation
+    # TODO assert_equal 42, controls_by_id.dig(control_id)
   end
 
   def refute_waiver_annotation(control_id)
-    # TODO - test JSON for waiver annotation
-    # Don't suppose we get this for free by defining assert_waiver_annotation ...
+    # TODO assert_equal 42, controls_by_id.dig(control_id)
+  end
+
+  def assert_skip_message(yea, nay)
+    msg = controls_by_id.dig("01_only_if", "results", 0, "skip_message")
+    assert_includes msg, yea
+    refute_includes msg, nay
   end
 
   describe "a fully pre-slugged control file" do
     let(:profile_name) { "basic" }
     let(:waiver_file) { "waivers.yaml" }
 
-    it "has all of the expected outcomes" do
-      [
-        "01_not_waivered_passes",
-        "03_waivered_no_expiry_not_skipped_passes", # this had a waiver but still passed - no annotation?
-        "06_waivered_expiry_in_past_not_skipped_passes", # a stale waiver
-        "08_waivered_expiry_in_past_skipped", # another stale waiver
-        "09_waivered_expiry_in_future_not_skipped_passes", # unneeded waiver
-      ].each do |control_id|
-        assert_test_outcome "passed", control_id
-        refute_waiver_annotation control_id
-      end
+    # rubocop:disable Layout/AlignHash
+    {
+      "01_not_waivered_passes"                          => "passed",
+      "02_not_waivered_fails"                           => "failed",
+      "03_waivered_no_expiry_not_skipped_passes"        => "passed",
+      "04_waivered_no_expiry_not_skipped_fails"         => "failed",
+      "05_waivered_no_expiry_skipped"                   => "skipped",
+      "06_waivered_expiry_in_past_not_skipped_passes"   => "passed",
+      "07_waivered_expiry_in_past_not_skipped_fails"    => "failed",
+      "08_waivered_expiry_in_past_skipped"              => "passed",
+      "09_waivered_expiry_in_future_not_skipped_passes" => "passed",
+      "10_waivered_expiry_in_future_not_skipped_fails"  => "failed",
+      "11_waivered_expiry_in_future_skipped"            => "skipped",
+    }.each do |control_id, expected|
+      it "has all of the expected outcomes #{control_id}" do
+        assert_test_outcome expected, control_id
 
-      [
-        "02_not_waivered_fails",
-        "07_waivered_expiry_in_past_not_skipped_fails", # Should this give a special waiver expired message?
-      ].each do |control_id|
-        assert_test_outcome "failed", control_id
-        refute_waiver_annotation control_id
-      end
-
-      # Each of these should have been forced to skip by the waiver system
-      %w{
-        05_waivered_no_expiry_skipped
-        11_waivered_expiry_in_future_skipped
-      }.each do |control_id|
-        assert_test_outcome "skipped", control_id
-        assert_waiver_annotation control_id
-      end
-
-      # Each of these should have had a failure, but had a waiver annotation
-      # added to the output.
-      %w{
-        04_waivered_no_expiry_not_skipped_fails
-        10_waivered_expiry_in_future_not_skipped_fails
-      }.each do |control_id|
-        assert_test_outcome "failed", control_id
-        assert_waiver_annotation control_id
+        case expected
+        when "passed", "skipped"
+          assert_waiver_annotation control_id
+        when "failed"
+          refute_waiver_annotation control_id
+        end
       end
     end
   end
@@ -73,9 +65,9 @@ describe "waivers" do
     let(:waiver_file) { "bad-date.yaml" }
     it "gracefully errors" do
       result = run_result
-      assert_includes "ERROR", result.stdout # the error level
+      assert_includes "ERROR",    result.stdout # the error level
       assert_includes "01_small", result.stdout # the offending control ID
-      assert_includes "never", result.stdout # The bad value
+      assert_includes "never",    result.stdout # The bad value
       assert_equal 1, result.exit_status
     end
   end
@@ -86,18 +78,14 @@ describe "waivers" do
     describe "when an only_if is used with no waiver" do
       let(:waiver_file) { "empty.yaml" }
       it "skips the control with an only_if message" do
-        msg = controls_by_id.dig("01_only_if", "results", 0, "skip_message")
-        assert_includes msg, "due to only_if"
-        refute_includes msg, "waiver"
+        assert_skip_message "due to only_if", "waiver"
       end
     end
 
     describe "when both a skipping waiver and an only_if are present" do
       let(:waiver_file) { "waiver.yaml" }
       it "skips the control with a waiver message" do
-        msg = controls_by_id.dig("01_only_if", "results", 0, "skip_message")
-        refute_includes msg, "due to only_if"
-        assert_includes msg, "waiver"
+        assert_skip_message "waiver", "due to only_if"
       end
     end
   end
