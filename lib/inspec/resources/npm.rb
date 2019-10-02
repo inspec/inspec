2092984fdd0729ee3cc3d847e91dc1ef0efc4e30
Clinton Wolfe <clintoncwolfe@gmail.com>
2019-10-02 12:28:14 -0400

    Remove erroneous windows support declaration
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- lib/inspec/resources/npm.rb
+++ lib/inspec/resources/npm.rb
@@ -5,7 +5,6 @@ module Inspec::Resources
   class NpmPackage < Inspec.resource(1)
     name "npm"
     supports platform: "unix"
-    supports platform: "windows"
     desc "Use the npm InSpec audit resource to test if a global npm package is installed. npm is the the package manager for Nodejs packages, such as bower and StatsD."
     example <<~EXAMPLE
       describe npm('bower') do
