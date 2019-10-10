26ffea8d3999d19a2fdf316be6ec5c282d2d566b
Ryan Davis <ryand-ruby@zenspider.com>
2019-10-09 17:40:53 -0700

    Make my diffs pretty for jsonmin tests.
    Signed-off-by: Ryan Davis <zenspider@chef.io>

--- test/unit/reporters/json_automate_test.rb
+++ test/unit/reporters/json_automate_test.rb
@@ -2,6 +2,8 @@ require "helper"
 require "inspec/reporters"
 
 describe Inspec::Reporters::JsonAutomate do
+  make_my_diffs_pretty!
+
   let(:path) { File.expand_path(File.dirname(__FILE__)) }
   let(:report) do
     data = JSON.parse(File.read(path + "/../mock/reporters/run_data_wrapper.json"), symbolize_names: true)
