6e88b3ceb9dc1c33065550843e29123ecebd6719
Miah Johnson <miah@chia-pet.org>
2019-10-16 20:38:37 -0700

    Fix rubocop warnings
    Signed-off-by: Miah Johnson <miah@chia-pet.org>

--- test/artifact/inspec_detect_test.rb
+++ test/artifact/inspec_detect_test.rb
@@ -2,11 +2,11 @@ require "minitest/autorun"
 
 class TestArtifactDetect < Minitest::Test
   def test_detect
-    out, err = capture_subprocess_io {
+    out, err = capture_subprocess_io do
       assert system("inspec detect --no-color")
-    }
+    end
 
-    assert_match %r%Platform Details%, out
+    assert_match %r{/Platform Details/}, out
     assert_empty err
   end
 end
