544db45c5edad6b370f5600bf321df5341e4e691
Miah Johnson <miah@chia-pet.org>
2019-10-16 19:57:45 -0700

    Untested and probably broken test scripts =)
    Signed-off-by: Miah Johnson <miah@chia-pet.org>

---
+++ .expeditor/buildkite/habitat_test.ps1
@@ -0,0 +1,36 @@
+#!/usr/bin/env powershell
+
+#Requires -Version 5
+
+$env:HAB_ORIGIN = 'ci'
+$Plan = 'inspec'
+
+Write-Host "--- system details"
+$Properties = 'Caption', 'CSName', 'Version', 'BuildType', 'OSArchitecture'
+Get-CimInstance Win32_OperatingSystem | Select-Object $Properties | Format-Table -AutoSize
+
+Write-Host "--- Installing the version of Habitat required"
+Install-Habitat --version 0.85.0.20190916
+
+Write-Host "--- Generating fake origin key"
+hab origin key generate $env:HAB_ORIGIN
+
+Write-Host "--- Building $Plan"
+$project_root = "$(git rev-parse --show-toplevel)"
+Set-Location $project_root
+
+$env:DO_CHECK=$true; hab pkg build .
+if (-not $?) { throw "unable to build" }
+
+. results/last_build.ps1
+if (-not $?) { throw "unable to determine details about this build" }
+
+Write-Host "--- Installing $pkg_ident"
+hab pkg install results/$pkg_artifact
+if (-not $?) { throw "unable to install this build" }
+
+Write-Host "+++ Testing $Plan"
+Push-Location $project_root/test/artifact
+rake
+if (-not $?) { throw "rake failed" }
+Pop-Location
---
+++ .expeditor/buildkite/habitat_test.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -ueo pipefail
+
+export HAB_ORIGIN='ci'
+export PLAN='inspec'
+
+echo "--- system details"
+uname -a
+
+echo "--- Installing the version of Habitat required"
+# Install-Habitat --version 0.85.0.20190916
+# TODO: Install Hab?
+hab --version
+
+echo "--- Generating fake origin key"
+hab origin key generate ${HAB_ORIGIN}
+
+echo "--- Building $PLAN"
+project_root="$(git rev-parse --show-toplevel)"
+cd "$project_root"
+
+DO_CHECK=true hab pkg build .
+
+. results/last_build.sh
+
+echo "--- Installing $pkg_ident"
+hab pkg install "results/$pkg_artifact"
+
+echo "+++ Testing $PLAN"
+pushd "$project_root/test/artifact"
+rake
+popd
