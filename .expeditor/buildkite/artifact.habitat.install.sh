d4badc6ad3c5cd7f29980eb27683da2c18b43584
Miah Johnson <miah@chia-pet.org>
2019-10-22 11:00:01 -0700

    set up binlink in shell script
    Signed-off-by: Miah Johnson <miah@chia-pet.org>

--- .expeditor/buildkite/artifact.habitat.install.sh
+++ .expeditor/buildkite/artifact.habitat.install.sh
@@ -3,4 +3,4 @@
 set -ueo pipefail
 
 hab origin key import < "$HAB_CI_KEY"
-hab pkg install "./results/${pkg_artifact:?is undefined}"
+hab pkg install --binlink "./results/${pkg_artifact:?is undefined}"
