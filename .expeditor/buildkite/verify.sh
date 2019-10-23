c93d61da1ce0aa0f6572489d9d6e4a4985fd18b1
Ryan Davis <ryand-ruby@zenspider.com>
2019-10-22 17:36:00 -0700

    Add a non-root user for testing on BK.
    Unfortunately, we have a lot of failing tests when we run as a
    non-root user and the files are owned by root. We can't chmod the
    checkout to the non-root user as it is a shared mount in docker and
    that's not allowed to the outside world.
    
    Signed-off-by: Ryan Davis <zenspider@chef.io>

--- .expeditor/buildkite/verify.sh
+++ .expeditor/buildkite/verify.sh
@@ -3,6 +3,12 @@
 set -ueo pipefail
 
 export LANG=C.UTF-8 LANGUAGE=C.UTF-8
+apt-get update -y
+apt-get install sudo -y
+
+echo "--- setting up user"
+useradd -m -U --uid 2000 normal
+echo "normal ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/normal
 
 echo "--- updating rubygems"
 gem update --system -N
@@ -16,4 +22,7 @@ echo "--- bundle install"
 bundle install --jobs=7 --retry=3 --without tools maintenance deploy
 
 echo "+++ bundle exec rake"
+# TODO: run this as non-root:
+# chown -R normal:normal /home/normal /workdir
+# su normal -c "bundle exec rake ${RAKE_TASK:-}"
 bundle exec rake ${RAKE_TASK:-}
