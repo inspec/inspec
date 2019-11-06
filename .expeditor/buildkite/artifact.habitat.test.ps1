13a1c30ba68ab7335743087c5755a91b302765e9
Miah Johnson <miah@chia-pet.org>
2019-11-06 14:31:47 -0800

    Corret build scripts
    correct path to hab artifact
    Rather than throwing, lets try to install the version
    Make sure hab is in the path
    we use -b on windows. lets be consistent
    export project_root too
    picardface
    fix regex
    execute rake _through_ habitat
    make test vrooom
    test presence of inspec
    strip out clixml bs
    ... no which on bk.
    full path to which
    export PATH
    test exist on linux/windows
    
    Signed-off-by: Miah Johnson <miah@chia-pet.org>

--- .expeditor/buildkite/artifact.habitat.test.ps1
+++ .expeditor/buildkite/artifact.habitat.test.ps1
@@ -15,13 +15,11 @@ Write-Host "--- Installing the version of Habitat required"
 $hab_version = (hab --version)
 $hab_minor_version = $hab_version.split('.')[1]
 if ( -not $? -Or $hab_minor_version -lt 85 ) {
-    Write-Host "I'm being built with $hab_version. I need at least Hab 0.85.0, because I use the -IsPath option for setting/pushing paths in SetupEnvironment."
-throw "unable to build: required minimum version of Habitat not installed"
+    Install-Habitat --version 0.85.0.20190916
 } else {
     Write-Host ":habicat: I think I have the version I need to build."
 }
 
-Install-Habitat --version 0.85.0.20190916
 
 Write-Host "--- Generating fake origin key"
 hab origin key generate $env:HAB_ORIGIN
@@ -41,6 +39,8 @@ hab pkg install -b $project_root/results/$pkg_artifact
 if (-not $?) { throw "unable to install this build" }
 
 Write-Host "+++ Testing $Plan"
+
+$env:PATH = "C:\hab\bin;$env:PATH"
 Push-Location $project_root/test/artifact
 rake
 if (-not $?) { throw "rake failed" }
