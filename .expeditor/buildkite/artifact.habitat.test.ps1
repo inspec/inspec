9eff871be197d96df845127db6b6727540e0ef18
Miah Johnson <miah@chia-pet.org>
2019-11-06 14:20:27 -0800

    crossing fingers
    Signed-off-by: Miah Johnson <miah@chia-pet.org>

--- .expeditor/buildkite/artifact.habitat.test.ps1
+++ .expeditor/buildkite/artifact.habitat.test.ps1
@@ -12,6 +12,15 @@ $Properties = 'Caption', 'CSName', 'Version', 'BuildType', 'OSArchitecture'
 Get-CimInstance Win32_OperatingSystem | Select-Object $Properties | Format-Table -AutoSize
 
 Write-Host "--- Installing the version of Habitat required"
+$hab_version = (hab --version)
+$hab_minor_version = $hab_version.split('.')[1]
+if ( -not $? -Or $hab_minor_version -lt 85 ) {
+    Write-Host "I'm being built with $hab_version. I need at least Hab 0.85.0, because I use the -IsPath option for setting/pushing paths in SetupEnvironment."
+throw "unable to build: required minimum version of Habitat not installed"
+} else {
+    Write-Host ":habicat: I think I have the version I need to build."
+}
+
 Install-Habitat --version 0.85.0.20190916
 
 Write-Host "--- Generating fake origin key"
@@ -21,14 +30,14 @@ Write-Host "--- Building $Plan"
 $project_root = "$(git rev-parse --show-toplevel)"
 Set-Location $project_root
 
-$env:DO_CHECK=$true; hab pkg build . -D
+$env:DO_CHECK=$true; hab pkg build .
 if (-not $?) { throw "unable to build" }
 
 . $project_root/results/last_build.ps1
 if (-not $?) { throw "unable to determine details about this build" }
 
 Write-Host "--- Installing $pkg_ident/$pkg_artifact"
-hab pkg install $project_root/results/$pkg_artifact
+hab pkg install -b $project_root/results/$pkg_artifact
 if (-not $?) { throw "unable to install this build" }
 
 Write-Host "+++ Testing $Plan"
