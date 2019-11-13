a299e93a4a0f8a1af74ffe88f366310ffd5a5530
Miah Johnson <miah@chia-pet.org>
2019-11-13 12:45:48 -0800

    oops
    Signed-off-by: Miah Johnson <miah@chia-pet.org>

--- .dir-locals.el
+++
@@ -1,8 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((nil
-  (projectile-test-command . "bundle exec rake test")
-  (projectile-project-name . "InSpec"))
- (ruby-mode
-  (flycheck-checker . chefstyle)))
