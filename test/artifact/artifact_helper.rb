c993f91ca61d2fdf79dec940ab7e79820039c6ae
Miah Johnson <miah@chia-pet.org>
2019-11-22 12:48:56 -0800

    rubocop is on the beat
    Signed-off-by: Miah Johnson <miah@chia-pet.org>

--- test/artifact/artifact_helper.rb
+++ test/artifact/artifact_helper.rb
@@ -1,7 +1,7 @@
 require "minitest/autorun"
 require "open3"
 
-TEST_CLI_OPTS="--chef-license=accept-no-persist"
+TEST_CLI_OPTS = "--chef-license=accept-no-persist".freeze
 
 class ArtifactTest < Minitest::Test
   make_my_diffs_pretty!
@@ -10,7 +10,7 @@ class ArtifactTest < Minitest::Test
     command = "inspec #{inspec_command} #{TEST_CLI_OPTS}"
     stdout, stderr, status = Open3.capture3({ "PATH" => ENV["PATH"] },
                                             command,
-                                            { :chdir => ENV["project_root"]})
+                                            { chdir: ENV["project_root"] })
 
     assert_empty stderr.sub(/#< CLIXML\n/, "")
     assert stdout
