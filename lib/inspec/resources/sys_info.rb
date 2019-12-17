c44e3e53ceb1d5ac6c2c94428a6c4a97f5c2fce5
mhackethal <31733219+mhackethal@users.noreply.github.com>
2019-12-17 16:33:17 +0100

    re-structure of sys_info
    rename calss from system to sysinfo
    split between windos + linux
    harmonize output for windows and linux
    Signed-off-by: mhackethal <mh@it31.de>

--- lib/inspec/resources/sys_info.rb
+++ lib/inspec/resources/sys_info.rb
@@ -3,187 +3,140 @@ require "inspec/resources/powershell"
 
 module Inspec::Resources
   # this resource returns additional system informatio
-  class System < Inspec.resource(1)
+  class SysInfo < Inspec.resource(1)
     name "sys_info"
-    supports platform: "unix"
+    supports platform: "linux"
     supports platform: "windows"
 
-    desc "Use the user InSpec system resource to test for operating system properties."
-    example <<~EXAMPLE
+    desc "Use the sys_info resource to test for operating system properties."
+   example <<~EXAMPLE
       describe sys_info do
-        its('hostname') { should eq 'example.com' }
+        its('hostname') { should eq 'hostname' }
       end
 
       describe sys_info do
-        its('fqdn') { should eq 'user.example.com' }
+        its('fqdn') { should eq 'hostname.example.com' }
       end
 
     EXAMPLE
 
-    %w{ domain fqdn ip_address short }.each do |opt|
-      define_method(opt.to_sym) do
-        hostname(opt)
-      end
-    end
-    @cache = nil
-    
-    def win_cache
-      return {} if !inspec.os.windows?
-      cmd = inspec.command <<-EOF
-        Get-WmiObject Win32_ComputerSystem -Property * | ConvertTo-Json
-      EOF
-
-      return {} if cmd.stdout == ""
-      begin
-        sysinfo = JSON.parse(cmd.stdout)
-      rescue JSON::ParserError => each
-        raise Inspec::Exceptions::ResourceFailed,
-          "Failed to parse JSON from Powershell. " \
-          "Error: #{e}"
-      end
-      return sysinfo
-    end
-
-    # returns the hostname of the local system
-    def hostname(opt = nil)
+    def initialize
       os = inspec.os
+      @cache = nil
+
       if os.linux?
-        linux_hostname(opt)
-      elsif os.darwin?
-        mac_hostname(opt)
+        @cache = LinuxInfo.new(inspec)
       elsif os.windows?
-        opt = "s" if opt.nil?
-        windows_hostname(opt)
+        @cache = WindowsInfo.new(inspec)
       else
-        skip_resource "The `sys_info.hostname` resource is not supported on your OS yet."
-      end
-    end
-    
-    def windows_hostname(opt = nil)
-      if @cache.nil?
-        @cache = win_cache
-      end
-      if !opt.nil?
-        opt = case opt
-          when "f", "long", "fqdn", "full"
-            #https://docs.microsoft.com/en-us/windows/win32/cimwin32prov/win32-computersystem
-            return @cache["DNSHostName"] if (@cache["DomainRole"] == 2 || @cache["DomainRole"] == 0)
-            return "#{@cache["DNSHostName"]}.#{@cache["Domain"]}"
-          when "d", "domain"
-            return nil if (@cache["DomainRole"] == 2 || @cache["DomainRole"] == 0)
-            return @cache["Domain"]
-          when "i", "ip_address"
-            cmd = inspec.command("(Get-WmiObject Win32_NetWorkAdapterConfiguration | Where IPAddress -ne $null | select -Property ipaddress).IPAddress | ConvertTo-Json")
-            return {} if cmd.stdout == ""
-            begin
-              ip = JSON.parse(cmd.stdout)
-            rescue JSON::ParserError => each
-              raise Inspec::Exceptions::ResourceFailed,
-                "Failed to parse JSON from Powershell. " \
-                "Error: #{e}"
-            end
-            return ip
-          when "s", "short"
-            return @cache["DNSHostName"]
-          else "ERROR"
-        end
-      end
-      if opt == "ERROR"
-        skip_resource "The `sys_info.hostname` resource is not supported with that option on your OS."
+        return skip_resource "The `sys_info` resource is not supported on your OS yet."
       end
     end
 
-    def linux_hostname(opt = nil)
-      if !opt.nil?
-        opt = case opt
-              when "f", "long", "fqdn", "full"
-                " -f"
-              when "d", "domain"
-                " -d"
-              when "i", "ip_address"
-                " -I"
-              when "s", "short"
-                " -s"
-              else
-                "ERROR"
-              end
-      end
-      if opt == "ERROR"
-        skip_resource "The `sys_info.hostname` resource is not supported with that option on your OS."
-      else
-        inspec.command("hostname#{opt}").stdout.chomp
-      end
+    def domain
+      @cache.info[:domain]
     end
 
-    def mac_hostname(opt = nil)
-      if !opt.nil?
-        opt = case opt
-              when "f", "long", "fqdn", "full"
-                " -f"
-              when "s", "short"
-                " -s"
-              else
-                "ERROR"
-              end
-      end
-      if opt == "ERROR"
-        skip_resource "The `sys_info.hostname` resource is not supported with that option on your OS."
-      else
-        inspec.command("hostname#{opt}").stdout.chomp
-      end
+    def ip_address
+      @cache.info[:ip]
     end
 
-    # returns the Manufacturer of the local system
     def manufacturer
-      os = inspec.os
-      if os.darwin?
-        "Apple Inc."
-      elsif os.linux?
-        inspec.command("cat /sys/class/dmi/id/sys_vendor").stdout.chomp
-      elsif os.windows?
-        if @cache.nil?
-          @cache = win_cache
-        end
-        @cache["Manufacturer"]
-      else
-        skip_resource "The `sys_info.manufacturer` resource is not supported on your OS yet."
-      end
+      @cache.info[:manufacturer]
     end
 
-    # returns the ServerModel of the local system
     def model
-      os = inspec.os
-      if os.darwin?
-        inspec.command("sysctl -n hw.model").stdout.chomp
-      elsif os.linux?
-        inspec.command("cat /sys/class/dmi/id/product_name").stdout.chomp
-      elsif os.windows?
-        if @cache.nil?
-          @cache = win_cache
-        end
-        @cache["Model"]
-      else
-        skip_resource "The `sys_info.model` resource is not supported on your OS yet."
-      end
+      @cache.info[:model]
     end
-    
-    def totalmemory
-      os = inspec.os
-      if os.linux?
-        inspec.command("awk '/MemTotal/ {print $2}' /proc/meminfo").stdout.chomp
-      elsif os.windows?
-        if @cache.nil?
-          @cache = win_cache
-        end
-        #Calculation requried - Windows return bytes where linux returns kB
-        @cache["TotalPhysicalMemory"] / 1024 rescue nil
-      else
-        skip_resource "The `sys_info.totalmemory` resource is not supported on your OS yet."
-      end
+
+    def totalmemory_kb
+      @cache.info[:totalmemory_kb]
+    end
+
+    def hostname 
+      @cache.info[:hostname]
+    end
+
+    def fqdn
+      @cache.info[:fqdn]
     end
 
     def to_s
       "System Information"
+    end
+  end
+  
+  class LinuxInfo < SysInfo
+    attr_reader :inspec
+    def initialize(inspec)
+      @inspec = inspec
+    end
+
+    def info
+      fqdn = inspec.command("hostname -f").stdout.chomp
+      domain = inspec.command("hostname -d").stdout.chomp
+      ip = inspec.command("hostname -I").stdout.chomp
+      short = inspec.command("hostname -s").stdout.chomp
+      manufacturer = inspec.command("cat /sys/class/dmi/id/sys_vendor").stdout.chomp
+      model =  inspec.command("cat /sys/class/dmi/id/product_name").stdout.chomp
+      totalmemory_kb = inspec.command("awk '/MemTotal/ {print $2}' /proc/meminfo").stdout.chomp
+
+      {
+        fqdn: fqdn,
+        domain: domain,
+        ip: ip,
+        hostname: short,
+        manufacturer: manufacturer,
+        model: model,
+        totalmemory_kb: totalmemory_kb,
+      }
+    end
+  end
+
+  class WindowsInfo < SysInfo
+    attr_reader :inspec
+    def initialize(inspec)
+      @inspec = inspec
+    end
+
+    def info
+      cmd = inspec.command <<-EOF
+        Get-WmiObject Win32_ComputerSystem -Property * | convertto-json
+      EOF
+
+      cmd = {} if cmd.stdout == ""
+      begin
+        info = JSON.parse(cmd.stdout)
+      rescue JSON::ParserError => _e
+        return nil
+      end
+
+      ip_cmd = inspec.command("(Get-WmiObject Win32_NetWorkAdapterConfiguration | Where IPAddress -ne $null | select -Property ipaddress).IPAddress | ConvertTo-Json")
+      ip_cmd = {} if ip_cmd.stdout == ""
+      begin
+        ip_info = JSON.parse(ip_cmd.stdout)
+      rescue JSON::ParserError => _e
+        return nil
+      end
+
+      if (info["DomainRole"] == 2 || info["DomainRole"] == 0)
+        fqdn = info["DNSHostName"]
+        domain = nil
+      else
+        fqdn = "#{info["DNSHostName"]}.#{info["Domain"]}"
+        domain = info["Domain"]
+      end
+
+      memory = info["TotalPhysicalMemory"] / 1024 rescue nil
+      {
+        fqdn: fqdn,
+        domain: domain,
+        ip: ip_info,
+        hostname: info["DNSHostName"],
+        manufacturer: info["Manufacturer"],
+        model: info["Model"],
+        totalmemory_kb: memory,
+      }
     end
   end
 end
