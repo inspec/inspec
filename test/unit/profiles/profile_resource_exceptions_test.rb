ddb0db12eff65d86d1ad6acbbee6f50fc0aec52c
Ryan Davis <ryand-ruby@zenspider.com>
2019-12-24 04:14:15 -0800

    Completely refactored profile_resource_exceptions_test.rb
    Half... half.
    
    Signed-off-by: Ryan Davis <zenspider@chef.io>

--- test/unit/profiles/profile_resource_exceptions_test.rb
+++ test/unit/profiles/profile_resource_exceptions_test.rb
@@ -6,107 +6,68 @@ describe "resource exception" do
   let(:profile) do
     profile = MockLoader.load_profile("profile-with-resource-exceptions")
     profile.load_libraries
-    profile.collect_tests
+    profile.collect_tests # TODO: bad name?
     profile
   end
 
   let(:checks) do
-    checks = []
-    profile.runner_context.rules.values.each do |rule|
-      checks.push(Inspec::Rule.prepare_checks(rule))
-    end
-    checks
-  end
-
-  describe "within initialize" do
-    it "skips resource when `Inspec::Exceptions::ResourceSkipped` is raised" do
-      _(checks[0][0][1][0].resource_skipped?).must_equal true
-      _(checks[0][0][1][0].resource_exception_message).must_equal "Skipping because reasons"
-      _(checks[0][0][1][0].resource_failed?).must_equal false
-    end
-
-    it "fails resource when `Inspec::Exceptions::ResourceFailed` is raised" do
-      _(checks[1][0][1][0].resource_failed?).must_equal true
-      _(checks[1][0][1][0].resource_exception_message).must_equal "Failing because reasons"
-      _(checks[1][0][1][0].resource_skipped?).must_equal false
-    end
-
-    it "does not affect other tests" do
-      _(checks[2][0][1][0].resource_skipped?).must_equal false
-      _(checks[2][0][1][0].resource_failed?).must_equal false
-      _(checks[2][0][1][0].resource_exception_message).must_be_nil
+    profile.runner_context.rules.map do |_, rule|
+      Inspec::Rule.prepare_checks(rule)
     end
   end
 
-  describe "within a matcher" do
-    it "fails resource when `Inspec::Exceptions::ResourceFailed` is raised" do
-      _(checks[3][0][1][0].resource_failed?).must_equal true
-      _(checks[3][0][1][0].resource_exception_message).must_equal "Failing inside matcher"
-      _(checks[3][0][1][0].resource_skipped?).must_equal false
-    end
+  def assert_check(nth, skipped, failed, msg = nil)
+    nth = [nth, 0, 1, 0] unless Array === nth
+    result = checks.dig(*nth)
 
-    it "skips resource when `Inspec::Exceptions::ResourceSkipped` is raised" do
-      _(checks[4][0][1][0].resource_skipped?).must_equal true
-      _(checks[4][0][1][0].resource_exception_message).must_equal "Skipping inside matcher"
-      _(checks[4][0][1][0].resource_failed?).must_equal false
+    if msg
+      _(result.resource_exception_message).must_equal msg
+    else
+      _(result.resource_exception_message).must_be_nil
     end
+    _(result.resource_failed?).must_equal failed
+    _(result.resource_skipped?).must_equal skipped
   end
 
-  describe "within a control" do
-    it "skips resource when `Inspec::Exceptions::ResourceSkipped` is raised" do
-      _(checks[5][0][1][0].resource_skipped?).must_equal true
-      _(checks[5][0][1][0].resource_exception_message).must_equal "Skipping because reasons"
-      _(checks[5][0][1][0].resource_failed?).must_equal false
-    end
-
-    it "fails resource when `Inspec::Exceptions::ResourceFailed` is raised" do
-      _(checks[5][1][1][0].resource_failed?).must_equal true
-      _(checks[5][1][1][0].resource_exception_message).must_equal "Failing because reasons"
-      _(checks[5][1][1][0].resource_skipped?).must_equal false
-    end
+  def assert_skipped(nth, msg) # skips resource when `Inspec::Exceptions::ResourceSkipped` is raised
+    assert_check nth, true, false, msg
   end
 
-  describe "within FilterTable" do
-    it "skips resource when `Inspec::Exceptions::ResourceSkipped` is raised" do
-      _(checks[6][0][1][0].resource_skipped?).must_equal true
-      _(checks[6][0][1][0].resource_exception_message).must_equal "Skipping inside FilterTable"
-      _(checks[6][0][1][0].resource_failed?).must_equal false
-    end
+  def assert_failed(nth, msg) # fails resource when `Inspec::Exceptions::ResourceFailed` is raised
+    assert_check nth, false, true, msg
+  end
 
-    it "fails resource when `Inspec::Exceptions::ResourceFailed` is raised" do
-      _(checks[7][0][1][0].resource_failed?).must_equal true
-      _(checks[7][0][1][0].resource_exception_message).must_equal "Failing inside FilterTable"
-      _(checks[7][0][1][0].resource_skipped?).must_equal false
-    end
+  it "within initialize" do
+    assert_skipped  0, "Skipping because reasons"
+    assert_failed   1, "Failing because reasons"
+    assert_check    2, false, false # does not affect other tests
+  end
 
-    describe "and multiple filters are used" do
-      it "skips resource when `Inspec::Exceptions::ResourceSkipped` is raised" do
-        _(checks[8][0][1][0].resource_skipped?).must_equal true
-        _(checks[8][0][1][0].resource_exception_message).must_equal "Skipping inside FilterTable"
-        _(checks[8][0][1][0].resource_failed?).must_equal false
-      end
+  it "within a matcher" do
+    assert_failed   3, "Failing inside matcher"
+    assert_skipped  4, "Skipping inside matcher"
+  end
 
-      it "fails resource when `Inspec::Exceptions::ResourceFailed` is raised" do
-        _(checks[9][0][1][0].resource_failed?).must_equal true
-        _(checks[9][0][1][0].resource_exception_message).must_equal "Failing inside FilterTable"
-        _(checks[9][0][1][0].resource_skipped?).must_equal false
-      end
+  it "within a control" do
+    idx = [5, 1, 1, 0]
+    assert_skipped  5, "Skipping because reasons"
+    assert_failed idx, "Failing because reasons"
+  end
 
-      it "does not halt the run/fail all tests when an incorrect filter is used" do
-        _(checks[10][0][1][0].resource_skipped?).must_equal true
-        _(checks[10][0][1][0].resource_exception_message).must_equal "Skipping inside FilterTable"
-        _(checks[10][0][1][0].resource_failed?).must_equal false
-      end
+  it "within FilterTable" do
+    assert_skipped  6, "Skipping inside FilterTable"
+    assert_failed   7, "Failing inside FilterTable"
+  end
 
-      it "does not halt the run/fail all tests when an incorrect filter is used" do
-        _(checks[11][0][1][0].resource_failed?).must_equal true
-        _(checks[11][0][1][0].resource_exception_message).must_equal "Failing inside FilterTable"
-        _(checks[11][0][1][0].resource_skipped?).must_equal false
-      end
-    end
+  it "within FilterTable when multiple filters are used" do
+    assert_skipped  8, "Skipping inside FilterTable"
+    assert_failed   9, "Failing inside FilterTable"
+    # does not halt the run/fail all tests when an incorrect filter is used:
+    assert_skipped 10, "Skipping inside FilterTable"
+    assert_failed  11, "Failing inside FilterTable"
+  end
 
-    it "does not affect regular FilterTable usage" do
-      _(checks[12][0][1][0].another_filter).must_equal ["example"]
-    end
+  it "within FilterTable does not affect regular FilterTable usage" do
+    _(checks.dig(12, 0, 1, 0).another_filter).must_equal ["example"]
   end
 end
