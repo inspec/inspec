07631a53e778d0be8a11453e242b2b01c3514b4a
Ryan Davis <ryand-ruby@zenspider.com>
2019-12-30 11:05:29 -0800

    Added extra doco on our require method.
    Signed-off-by: Ryan Davis <zenspider@chef.io>

--- lib/inspec/dsl_shared.rb
+++ lib/inspec/dsl_shared.rb
@@ -8,6 +8,26 @@ module Inspec
       # It is used whenever the `require 'lib'` is not in libraries.
       alias __ruby_require require
 
+      ##
+      # This is our own require override, to be used in
+      # LibraryEvalContext and ControlEvalContext.
+      #
+      # Any top level libraries file (autoloaded) that requires a
+      # second-level libraries file.
+      #
+      # in load_libraries
+      #   in top level libraries file to be autoloaded
+      #     that has a require to a known file that is NOT loaded yet
+      #
+      # ProfileContext#initialize
+      #   -> library_eval_context
+      #
+      # ProfileContext#load_libraries autoload
+      #   -> load_library_file(@library_eval_context)
+      #
+      # probably most of this comment is useless, but it was hard to
+      # discover so I'm adding it for others.
+
       def require(path)
         rbpath = path + ".rb"
         return __ruby_require(path) unless @require_loader.exists?(rbpath)
