9bf8f2f4cd87f2eb726374bca421decd0760eb5c
Clinton Wolfe <clintoncwolfe@gmail.com>
2020-01-03 13:33:19 -0500

    Adjust usage message for --airgap option
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- lib/inspec/cli.rb
+++ lib/inspec/cli.rb
@@ -173,7 +173,7 @@ class Inspec::InspecCLI < Inspec::BaseCLI
     desc: "Generates a tar.gz archive."
   option :overwrite, type: :boolean, default: false,
     desc: "Overwrite existing archive."
-  option :airgap, type: :boolean, default: false,
+  option :airgap, type: :string, default: false, banner: "", # Actually a boolean, but this suppresses the creation of a --no-airgap...
     desc: "Prefer using existing local archives to fetching profiles."
   option :ignore_errors, type: :boolean, default: false,
     desc: "Ignore profile warnings."
