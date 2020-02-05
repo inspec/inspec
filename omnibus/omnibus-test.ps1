70723000a8f7e2052deb3984604e94b972c876a4
Tim Smith <tsmith84@gmail.com>
2020-02-05 10:26:24 -0800

    Pull Ruby installer for windows from S3 not GitHub
    This should prevent failures when we get throttled pulling it from GitHub.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- omnibus/omnibus-test.ps1
+++ omnibus/omnibus-test.ps1
@@ -23,7 +23,7 @@ $Env:PATH = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";
 
 Write-Host "--- Downloading Ruby + DevKit"
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-(New-Object System.Net.WebClient).DownloadFile('https://github.com/oneclick/rubyinstaller2/releases/download/RubyInstaller-2.6.5-1/rubyinstaller-devkit-2.6.5-1-x64.exe', 'c:\\rubyinstaller-devkit-2.6.5-1-x64.exe')
+(New-Object System.Net.WebClient).DownloadFile('https://public-cd-buildkite-cache.s3-us-west-2.amazonaws.com/rubyinstaller-devkit-2.6.5-1-x64.exe', 'c:\\rubyinstaller-devkit-2.6.5-1-x64.exe')
 
 Write-Host "--- Installing Ruby + DevKit"
 Start-Process c:\rubyinstaller-devkit-2.6.5-1-x64.exe -ArgumentList '/verysilent /dir=C:\\ruby26' -Wait
