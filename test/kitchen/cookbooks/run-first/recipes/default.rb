58bcef218b8d532a1d7149393deab13551bd2806
Miah Johnson <miah@chia-pet.org>
2020-02-06 17:07:16 -0800

    Clean up gem generation stuff
    Signed-off-by: Miah Johnson <miah@chia-pet.org>

--- test/kitchen/cookbooks/run-first/recipes/default.rb
+++ test/kitchen/cookbooks/run-first/recipes/default.rb
@@ -1,11 +1,9 @@
-# inject the current inspec gem for use with audit cookbook
-# this is generated via Rake test:integration
-cookbook_file "/root/inspec-core-local.gem" do
-  source "inspec-core-local.gem"
+cookbook_file "/root/inspec.gem" do
+  source "inspec.gem"
   action :create
 end
 
 chef_gem "inspec" do
-  source "/root/inspec-core-local.gem"
+  source "/root/inspec.gem"
 end
 
