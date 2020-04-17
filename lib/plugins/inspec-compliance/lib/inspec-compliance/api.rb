03c012e3c9dc92f6fd429db38f0e45f5f084e04c
John Snow <thelunaticscripter@outlook.com>
2020-04-17 15:50:40 -0400

    Add additional debug logging for automate login
    Signed-off-by: John Snow <thelunaticscripter@outlook.com>

--- lib/plugins/inspec-compliance/lib/inspec-compliance/api.rb
+++ lib/plugins/inspec-compliance/lib/inspec-compliance/api.rb
@@ -302,6 +302,10 @@ module InspecPlugins
           )
           true
         else
+          Inspec::Log.debug(
+            "Reviecved #{response.code} form #{url}#{automate_endpoint} - " \
+            "assuming target is not an #{AUTOMATE_PRODUCT_NAME}2 instance"
+          )
           false
         end
       end
