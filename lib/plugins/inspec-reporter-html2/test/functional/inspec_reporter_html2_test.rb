adc59ea4ad4d048131d406a51db5c90d6dcbb68a
Clinton Wolfe <clintoncwolfe@gmail.com>
2020-05-23 23:35:32 -0400

    Correct html validation assertion syntax
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- lib/plugins/inspec-reporter-html2/test/functional/inspec_reporter_html2_test.rb
+++ lib/plugins/inspec-reporter-html2/test/functional/inspec_reporter_html2_test.rb
@@ -28,7 +28,7 @@ describe "inspec-reporter-html2" do
       }
       run_result
 
-      _(HTMLProofer.check_file(output_file, proofer_opts).run).must_output("")
+      _(proc {HTMLProofer.check_file(output_file, proofer_opts).run} ).must_be_silent
     end
   end
 
