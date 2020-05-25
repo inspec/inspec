45e3421595cf449d7010e103019ddad616b28dc0
Clinton Wolfe <clintoncwolfe@gmail.com>
2020-05-24 23:47:20 -0400

    Remove too-brittle frame ranges on stack locations
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- lib/inspec/utils/telemetry/run_context_probe.rb
+++ lib/inspec/utils/telemetry/run_context_probe.rb
@@ -5,29 +5,32 @@ module Inspec
 
     class RunContextProbe
       def self.guess_run_context
-        stack = caller_locations(4)
+        stack = caller_locations
         return "test-kitchen" if kitchen?(stack)
         return "cli" if run_by_thor?(stack)
         return "audit-cookbook" if audit_cookbook?(stack)
+
         "unknown"
       end
 
       def self.run_by_thor?(stack)
-        stack_match(stack: stack[-14..-9], path: "thor/command", label: "run") &&
-        stack_match(stack: stack[-14..-9], path: "thor/invocation", label: "invoke_command")
+        stack_match(stack: stack, path: "thor/command", label: "run") &&
+          stack_match(stack: stack, path: "thor/invocation", label: "invoke_command")
       end
 
       def self.kitchen?(stack)
-        stack_match(stack: stack[-21..-11], path: "kitchen/instance", label: "verify_action") &&
-        stack_match(stack: stack[-14..-4], path: "kitchen/instance", label: "verify")
+        stack_match(stack: stack, path: "kitchen/instance", label: "verify_action") &&
+          stack_match(stack: stack, path: "kitchen/instance", label: "verify")
       end
 
       def self.audit_cookbook?(stack)
-        stack_match(stack: stack[-21..-11], path: "chef/handler", label: "run_report_handlers") &&
-        stack_match(stack: stack[-26..-16], path: "handler/audit_report", label: "report")
+        stack_match(stack: stack, path: "chef/handler", label: "run_report_handlers") &&
+          stack_match(stack: stack, path: "handler/audit_report", label: "report")
       end
 
-      def self.stack_match(stack: nil, label: nil, path: nil)
+      def self.stack_match(stack: [], label: nil, path: nil)
+        return false if stack.nil?
+
         stack.any? do |frame|
           if label && path
             frame.label == label && frame.absolute_path.include?(path)
