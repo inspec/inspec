99ec27b337b8c57a60bef60dd24692d477717d0e
Clinton Wolfe <clintoncwolfe@gmail.com>
2020-06-02 21:35:16 -0400

    Fix json-min reporter unit test
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- test/unit/reporters/json_min_test.rb
+++ test/unit/reporters/json_min_test.rb
@@ -1,17 +1,18 @@
 require "helper"
-require "inspec/reporters"
+require_relative "../../../lib/plugins/inspec-reporter-json-min/lib/inspec-reporter-json-min"
+require_relative "../../../lib/plugins/inspec-reporter-json-min/lib/inspec-reporter-json-min/reporter"
 
-describe Inspec::Reporters::JsonMin do
-  let(:report) do
+describe InspecPlugins::JsonMinReporter::Reporter do
+  let(:reporter) do
     data = JSON.parse(File.read("test/fixtures/reporters/run_data.json"), symbolize_names: true)
-    Inspec::Reporters::JsonMin.new({ run_data: data })
+    InspecPlugins::JsonMinReporter::Reporter.new({ run_data: data })
   end
 
   describe "#render" do
     it "confirm render output" do
       output = File.read("test/fixtures/reporters/json_min_output")
-      report.render
-      _(report.rendered_output).must_equal output
+      reporter.render
+      _(reporter.rendered_output).must_equal output
     end
   end
 
@@ -19,7 +20,7 @@ describe Inspec::Reporters::JsonMin do
     it "confirm report output" do
       output = File.read("test/fixtures/reporters/json_min_output")
       output = JSON.parse(output, symbolize_names: true)
-      _(report.report).must_equal output
+      _(reporter.report).must_equal output
     end
   end
 end
