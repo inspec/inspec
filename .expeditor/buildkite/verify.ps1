2b8e73b3bb3eaffc7a5cee89ccd31589ed0d4262
James Stocks <jstocks@chef.io>
2020-06-03 15:35:42 +0100

    Limit testing to linting and unit testing
    The functional tests require a large port of test improvements from the 4.x branch.
    
    Signed-off-by: James Stocks <jstocks@chef.io>

--- .expeditor/buildkite/verify.ps1
+++ .expeditor/buildkite/verify.ps1
@@ -7,7 +7,11 @@ bundle --version
 echo "--- bundle install"
 bundle install --jobs=7 --retry=3 --without tools maintenance deploy
 
-echo "+++ bundle exec rake test:parallel"
-bundle exec rake test:parallel K=4
+#echo "+++ bundle exec rake test:parallel"
+#bundle exec rake test:parallel K=4
+
+# Only run unit tests for now
+echo "+++ bundle exec rake test:unit"
+bundle exec rake test:unit
 
 exit $LASTEXITCODE
