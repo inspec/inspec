7bf8e9b6e58836809906224ff1b1d24648db731a
Chef Expeditor <chef-ci@chef.io>
2020-08-04 07:55:13 +0000

    Bump version to 4.22.5 by Chef Expeditor
    Obvious fix; these changes are the result of automation not creative thinking.

--- inspec-bin/lib/inspec-bin/version.rb
+++ inspec-bin/lib/inspec-bin/version.rb
@@ -1,5 +1,5 @@
 # This file managed by automation - do not edit manually
 module InspecBin
   INSPECBIN_ROOT = File.expand_path("../..", __FILE__)
-  VERSION = "4.22.4".freeze
+  VERSION = "4.22.5".freeze
 end
