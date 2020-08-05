f0f7bb99377b3166dc79c3278c9aa0c097ef25c7
Nick Schwaderer <nschwaderer@chef.io>
2020-08-05 15:11:44 +0100

    Try to finish pipeline for 4.22.8 and update cdn purge step
    Signed-off-by: Nick Schwaderer <nschwaderer@chef.io>

--- .expeditor/purge-cdn.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -eou pipefail
-
-echo "Purging '${EXPEDITOR_CHANNEL}/${EXPEDITOR_PRODUCT_KEY}/latest' Surrogate Key group from Fastly"
-curl -X POST -H "Fastly-Key: $FASTLY_API_TOKEN" "https://api.fastly.com/service/1ga2Kt6KclvVvCeUYJ3MRp/purge/${EXPEDITOR_CHANNEL}/${EXPEDITOR_PRODUCT_KEY}/latest"
