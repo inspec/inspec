37a822bb8239f07f68ff8b904014ae936f4bb71c
Faizan <60912283+FaizanSRE@users.noreply.github.com>
2020-12-07 14:20:56 +0530

    set environment after changing path

--- omnibus/omnibus-test.ps1
+++ omnibus/omnibus-test.ps1
@@ -21,9 +21,12 @@ Write-Output "--- Running verification for $channel $product $version"
 # reload Env:PATH to ensure it gets any changes that the install made (e.g. C:\opscode\inspec\bin\ )
 $Env:PATH = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
 
+Set-Location test/artifact
+
 $Env:Path = "C:\opscode\$product\bin;C:\opscode\$product\embedded\bin;$Env:PATH"
 Write-Host "+++ Testing $Plan"
 
-Set-Location test/artifact
+
+
 rake
 If ($lastexitcode -ne 0) { Exit $lastexitcode }
