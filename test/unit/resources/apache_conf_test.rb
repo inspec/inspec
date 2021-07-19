1e8363587ed98a476831883f5aa960126f5ac6ce
Nikita Mathur <nikita.mathur@chef.io>
2021-07-19 14:39:25 +0530

    Test case to test apache_conf when server root is not configured
    Signed-off-by: Nikita Mathur <nikita.mathur@chef.io>

--- test/unit/resources/apache_conf_test.rb
+++ test/unit/resources/apache_conf_test.rb
@@ -21,6 +21,15 @@ describe "Inspec::Resources::ApacheConf" do
                                              ENABLE_USR_LIB_CGI_BIN}
   end
 
+  it "reads values successfully from apache2.conf and ignores Include, IncludeOptional params when server root is not configured" do
+    resource = MockLoader.new(:ubuntu1504).load_resource("apache_conf", "/etc/apache2/apache2.conf")
+    _(resource.params).must_be_kind_of Hash
+    _(resource.content).must_be_kind_of String
+    _(resource.params("ServerAlias")).must_equal ["inspec.test www.inspec.test io.inspec.test"]
+    assert_nil(resource.params("ServerRoot"))
+    assert_nil(resource.params("Listen"))
+  end
+
   # non debian style httpd
   it "reads values in httpd.conf and from Include, IncludeOptional params" do
     resource = MockLoader.new(:centos6).load_resource("apache_conf",
