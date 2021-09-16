cc0d86a185d3e98575fbc24c34ee2f1e0d62cd77
Vasu1105 <vasundhara.jagdale@chef.io>
2021-09-16 15:56:07 +0530

    Add csv without headers support in csv resource
    Signed-off-by: Vasu1105 <vasundhara.jagdale@chef.io>

--- docs-chef-io/content/inspec/resources/csv.md
+++ docs-chef-io/content/inspec/resources/csv.md
@@ -27,19 +27,34 @@ This resource first became available in v1.0.0 of InSpec.
 
 A `csv` resource block declares the configuration data to be tested:
 
-    describe csv('file') do
+    describe csv('file', true) do
       its('name') { should cmp 'foo' }
+    end
+
+Test ``csv`` without headers
+
+    describe csv('file', false).params do
+      its([0]) { should cmp 'name' }
     end
 
 where
 
 - `'file'` is the path to a CSV file
+- 'true' is the value for headers by default value is true. If set to false it consider csv file does not have headers,
 - `name` is a configuration setting in a CSV file
 - `should eq 'foo'` tests a value of `name` as read from a CSV file versus the value declared in the test
+- `params` when headers are set to false use this method to fetch the data.
+- `[0]` is the array element position.
 
 ## Examples
 
 The following examples show how to use this Chef InSpec audit resource.
+
+### Test a csv file without headers
+
+    describe csv('some_file.csv', false).params do
+      its([0]) { should eq ["name"] }
+    end
 
 ### Test a CSV file
 
--- lib/inspec/resources/csv.rb
+++ lib/inspec/resources/csv.rb
@@ -11,13 +11,27 @@ module Inspec::Resources
       describe csv('example.csv') do
         its('name') { should eq(['John', 'Alice']) }
       end
+
+      describe csv('example.csv', false).params do
+        its[[0]] { should eq (['name', 'col1', 'col2']) }
+      emd
     EXAMPLE
+
+    def initialize(path, headers = true)
+      @headers = headers
+      super(path)
+    end
 
     # override the parse method from JsonConfig
     # Assuming a header row of name,col1,col2, it will output an array of hashes like so:
     #    [
     #      { 'name' => 'row1', 'col1' => 'value1', 'col2' => 'value2' },
     #      { 'name' => 'row2', 'col1' => 'value3', 'col2' => 'value4' }
+    #    ]
+    # When headers is set to false it will return data as array of array
+    #    [
+    #      ['name', col1', 'col2'],
+    #      ['row2', 'value3', 'value4']
     #    ]
     def parse(content)
       require "csv" unless defined?(CSV)
@@ -28,10 +42,14 @@ module Inspec::Resources
       end
 
       # implicit conversion of values
-      csv = CSV.new(content, headers: true, converters: %i{all blank_to_nil})
+      csv = CSV.new(content, headers: @headers, converters: %i{all blank_to_nil})
 
       # convert to hash
-      csv.to_a.map(&:to_hash)
+      if @headers
+        csv.to_a.map(&:to_hash)
+      else
+        csv.to_a
+      end
     rescue => e
       raise Inspec::Exceptions::ResourceFailed, "Unable to parse CSV: #{e.message}"
     end
@@ -42,7 +60,12 @@ module Inspec::Resources
     # #value method from JsonConfig (which uses ObjectTraverser.extract_value)
     # doesn't make sense here.
     def value(key)
-      @params.map { |x| x[key.first.to_s] }.compact
+      if @headers
+        @params.map { |x| x[key.first.to_s] }.compact
+      else
+        # when headers is set to false send the array as it is.
+        @params
+      end
     end
 
     private
