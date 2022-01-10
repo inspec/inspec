5b308c2a7db16aa95c623e98e5409e896fde9e01
Chef Expeditor <chef-ci@chef.io>
2022-01-10 01:12:10 +0000

    Bump version to 4.52.12 by Chef Expeditor
    Obvious fix; these changes are the result of automation not creative thinking.

--- inspec-bin/lib/inspec-bin/version.rb
+++ inspec-bin/lib/inspec-bin/version.rb
@@ -1,5 +1,5 @@
 # This file managed by automation - do not edit manually
 module InspecBin
   INSPECBIN_ROOT = File.expand_path("..", __dir__)
-  VERSION = "4.52.11".freeze
+  VERSION = "4.52.12".freeze
 end
--- lib/inspec/version.rb
+++ lib/inspec/version.rb
@@ -1,3 +1,3 @@
 module Inspec
-  VERSION = "4.52.11".freeze
+  VERSION = "4.52.12".freeze
 end
