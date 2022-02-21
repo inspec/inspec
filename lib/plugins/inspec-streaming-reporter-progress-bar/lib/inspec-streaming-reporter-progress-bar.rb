bf116e95176f3b3cfc580f7cfd280ce66c651d1c
Nikita Mathur <nikita.mathur@chef.io>
2022-02-21 17:58:19 +0530

    Test cases added for streaming progress bar reporter
    Signed-off-by: Nikita Mathur <nikita.mathur@chef.io>

--- lib/plugins/inspec-streaming-reporter-progress-bar/lib/inspec-streaming-reporter-progress-bar.rb
+++ lib/plugins/inspec-streaming-reporter-progress-bar/lib/inspec-streaming-reporter-progress-bar.rb
@@ -9,8 +9,7 @@
 # This is not needed when being loaded as a gem; but when doing
 # plugin development, you may need it.  Either way, it's harmless.
 libdir = File.dirname(__FILE__)
-# ["/Users/nmathur/chef/inspec/vendor/bundle/ruby/2.7.0/gems/ruby-progressbar-1.11.0/lib"]
-# /Users/nmathur/.rbenv/versions/2.7.4/lib/ruby/gems/2.7.0/gems/progress_bar-1.3.3/lib/progress_bar.rb
+
 $LOAD_PATH.unshift(libdir) unless $LOAD_PATH.include?(libdir)
 
 require "inspec-streaming-reporter-progress-bar/plugin"
