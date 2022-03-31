5ec479fff33944d0b5c6aeb078c83d10bae22836
Sonu Saha <sonu.saha@progress.com>
2022-03-31 19:44:30 +0530

    CFINSPEC-94: Remove user specific matchers from universal matchers & include in user
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

--- lib/matchers/matchers.rb
+++ lib/matchers/matchers.rb
@@ -340,16 +340,3 @@ RSpec::Matchers.define :be_mounted do
     end
   end
 end
-
-# user resource belong_to matcher for serverspec compatibility
-RSpec::Matchers.define :belong_to_primary_group do |group|
-  match do |user|
-    user.belongs_to_primary_group?(group)
-  end
-end
-
-RSpec::Matchers.define :belong_to_group do |group|
-  match do |user|
-    user.belongs_to_group?(group)
-  end
-end
