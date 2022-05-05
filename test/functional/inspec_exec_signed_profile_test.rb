6500b0d8bd8a964d6329a5158cb44b75baa9526d
Vasu1105 <vasundhara.jagdale@chef.io>
2022-05-05 13:32:08 +0530

    Fixed failing test and adds test for signed dependent profile
    Signed-off-by: Vasu1105 <vasundhara.jagdale@chef.io>

--- test/functional/inspec_exec_signed_profile_test.rb
+++ test/functional/inspec_exec_signed_profile_test.rb
@@ -2,14 +2,13 @@ require "functional/helper"
 
 describe "inspec exec" do
   include FunctionalHelper
+  parallelize_me!
 
   def test_inspec_exec_signed_profile
     prepare_examples do |dir|
       skip_windows!
 
       unique_key_name = SecureRandom.uuid
-      install_dir = File.join(dir, SecureRandom.uuid)
-      FileUtils.mkdir(install_dir)
 
       # create profile
       profile = File.join(dir, "profile_a")
@@ -20,8 +19,6 @@ describe "inspec exec" do
 
       out = run_inspec_process("sign profile --profile #{profile} --keyname #{unique_key_name}", prefix: "cd #{dir};")
       assert_exit_code 0, out
-
-      return if is_windows?
 
       out = run_inspec_process("exec profile_a-0.1.0.iaf --no-create-lockfile", prefix: "cd #{dir};")
       assert_exit_code 0, out
@@ -33,8 +30,6 @@ describe "inspec exec" do
       skip_windows!
 
       unique_key_name = SecureRandom.uuid
-      install_dir = File.join(dir, SecureRandom.uuid)
-      FileUtils.mkdir(install_dir)
 
       # create profile
       profile = File.join(dir, "profile_a")
@@ -46,14 +41,36 @@ describe "inspec exec" do
       out = run_inspec_process("sign profile --profile #{profile} --keyname #{unique_key_name}", prefix: "cd #{dir};")
       assert_exit_code 0, out
 
-      return if is_windows?
-
       File.delete("#{dir}/#{unique_key_name}.pem.pub")
 
       out = run_inspec_process("exec profile_a-0.1.0.iaf --no-create-lockfile", prefix: "cd #{dir};")
       assert_exit_code 1, out
 
-      assert_includes out.stderr.force_encoding(Encoding::UTF_8), "Profile validation key not found."
+      assert_includes out.stderr.force_encoding(Encoding::UTF_8), "Validation key #{unique_key_name} not found"
+    end
+  end
+
+  def test_inspec_exec_signed_profile_with_dependency
+    prepare_examples do |dir|
+      skip_windows!
+
+      unique_key_name = SecureRandom.uuid
+
+      profile = "#{profile_path}/dependencies/profile_a"
+
+      out = run_inspec_process("sign generate-keys --keyname #{unique_key_name}", prefix: "cd #{dir};")
+      assert_exit_code 0, out
+
+      out = run_inspec_process("sign profile --profile #{profile} --keyname #{unique_key_name}", prefix: "cd #{dir};")
+      assert_exit_code 0, out
+
+      out = run_inspec_process("exec profile_a-0.1.0.iaf --no-create-lockfile", prefix: "cd #{dir};")
+      assert_exit_code 0, out
+
+      assert_includes out.stdout.force_encoding(Encoding::UTF_8), "InSpec Profile (profile_c)"
+
+      # Removing vendors directory from the profile once the test is performed.
+      FileUtils.rm_rf("#{profile}/vendor/")
     end
   end
 end
