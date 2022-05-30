a093731f23146982b0b74f6a7dbb27d40a5d588f
Nikita Mathur <nikita.mathur@chef.io>
2022-05-30 16:59:18 +0530

    Fix attempt 3
    Signed-off-by: Nikita Mathur <nikita.mathur@chef.io>

--- .expeditor/buildkite/license_scout.sh
+++ .expeditor/buildkite/license_scout.sh
@@ -21,11 +21,16 @@ pull_bundle
 echo "--- bundle"
 bundle config --local path vendor/bundle
 bundle config set --local without tools maintenance deploy
-#bundle install --jobs=7 --retry=3
-bundle update --jobs=7 --retry=3
+bundle install --jobs=7 --retry=3
+#bundle update --jobs=7 --retry=3
 
 echo "--- push bundle cache"
-push_bundle
+# push_bundle
+
+shasum -a 256 Gemfile.lock > bundle.sha256
+tar -czf bundle.tar.gz Gemfile.lock vendor/
+push_s3_file bundle.tar.gz
+push_s3_file bundle.sha256
 
 echo "--- running license_scout"
 bundle exec license_scout
