cc8229dfd00c8a7b523844c94b562e2308f93343
Nikita Mathur <nikita.mathur@chef.io>
2022-05-30 17:33:47 +0530

    fix attempt 5
    Signed-off-by: Nikita Mathur <nikita.mathur@chef.io>

--- .expeditor/buildkite/license_scout.sh
+++ .expeditor/buildkite/license_scout.sh
@@ -1,35 +1,36 @@
 #!/bin/bash
 
 echo "--- dependencies"
-. .expeditor/buildkite/cache_support.sh
-install_cache_deps sudo
+# . .expeditor/buildkite/cache_support.sh
+# install_cache_deps sudo
 
 echo "--- updating rubygems"
-gem update -N --system 3.2.3 --force
+# gem update -N --system 3.2.3 --force
 
 echo "--- system details"
-uname -a
-gem env
-bundle --version
-mount
-df /tmp
-echo ${TMPDIR:-unknown}
+# uname -a
+# gem env
+# bundle --version
+# mount
+# df /tmp
+# echo ${TMPDIR:-unknown}
 
 echo "--- pull bundle cache"
-pull_bundle
+# pull_bundle
 
 echo "--- bundle"
-bundle config --local path vendor/bundle
-bundle config set --local without tools maintenance deploy
-bundle install --jobs=7 --retry=3
-#bundle update --jobs=7 --retry=3
+# bundle config --local path vendor/bundle
+# bundle config set --local without tools maintenance deploy
+# bundle install --jobs=7 --retry=3
 
 echo "--- push bundle cache"
-push_bundle
+# push_bundle
 
 echo "--- running license_scout"
-gem install license_scout -v 1.3.1
+#gem install license_scout -v 1.3.1
+gem install license_scout -v 2.5.1
 license_scout
 LICENSE_SCOUT_EXIT=$?
-
+echo "--- License scout exited"
+echo LICENSE_SCOUT_EXIT
 exit $LICENSE_SCOUT_EXIT
