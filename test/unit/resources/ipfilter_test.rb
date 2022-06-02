389495793f96bad833c4595424f42dcd69186373
Sonu Saha <sonu.saha@progress.com>
2022-06-02 16:55:18 +0530

    CFINSPEC-266: Add resource_id & unit test: ipfilter
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

--- test/unit/resources/ipfilter_test.rb
+++ test/unit/resources/ipfilter_test.rb
@@ -9,6 +9,7 @@ describe "Inspec::Resources::Ipfilter" do
     _(resource.has_rule?("pass in quick on lo0 all")).must_equal true
     _(resource.has_rule?("rule which does not exist")).must_equal false
     _(resource.has_rule?(nil)).must_equal false
+    _(resource.resource_id).must_equal "Ipfilter"
   end
 
   # solaris11
@@ -16,24 +17,28 @@ describe "Inspec::Resources::Ipfilter" do
     resource = MockLoader.new(:solaris11).load_resource("ipfilter")
     _(resource.has_rule?("pass out quick on lo0 all")).must_equal true
     _(resource.has_rule?("rule which does not exist")).must_equal false
+    _(resource.resource_id).must_equal "Ipfilter"
   end
 
   # ubuntu
   it "verify ipfilter on ubuntu" do
     resource = MockLoader.new(:ubuntu).load_resource("ipfilter")
     _(resource.has_rule?("pass out quick on lo0 all")).must_equal false
+    _(resource.resource_id).must_equal "Ipfilter"
   end
 
   # windows
   it "verify ipfilter on windows" do
     resource = MockLoader.new(:windows).load_resource("ipfilter")
     _(resource.has_rule?("pass out quick on lo0 all")).must_equal false
+    _(resource.resource_id).must_equal "Ipfilter"
   end
 
   # undefined
   it "verify ipfilter on unsupported os" do
     resource = MockLoader.new(:undefined).load_resource("ipfilter")
     _(resource.has_rule?("pass out quick on lo0 all")).must_equal false
+    _(resource.resource_id).must_equal "Ipfilter"
   end
 
 end
