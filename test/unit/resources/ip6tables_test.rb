56e5a67f979075e477bac6576c41c102a45dc395
Sonu Saha <sonu.saha@progress.com>
2022-06-02 16:54:50 +0530

    CFINSPEC-266: Add resource_id & unit test: ip6tables
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

--- test/unit/resources/ip6tables_test.rb
+++ test/unit/resources/ip6tables_test.rb
@@ -9,17 +9,20 @@ describe "Inspec::Resources::Ip6tables" do
     resource = MockLoader.new(:ubuntu1404).load_resource("ip6tables")
     _(resource.has_rule?("-P OUTPUT ACCEPT")).must_equal true
     _(resource.has_rule?("-P OUTPUT DROP")).must_equal false
+    _(resource.resource_id).must_equal "Ip6tables"
   end
 
   it "verify ip6tables with comments on ubuntu" do
     resource = MockLoader.new(:ubuntu1404).load_resource("ip6tables")
     _(resource.has_rule?('-A INPUT -i eth0 -p tcp -m tcp --dport 80 -m state --state NEW -m comment --comment "http-v6 like its 1990" -j ACCEPT')).must_equal true
+    _(resource.resource_id).must_equal "Ip6tables"
   end
 
   it "verify ip6tables on windows" do
     resource = MockLoader.new(:windows).load_resource("ip6tables")
     _(resource.has_rule?("-P OUTPUT ACCEPT")).must_equal false
     _(resource.has_rule?("-P OUTPUT DROP")).must_equal false
+    _(resource.resource_id).must_equal "Ip6tables"
   end
 
   # undefined
@@ -27,6 +30,7 @@ describe "Inspec::Resources::Ip6tables" do
     resource = MockLoader.new(:undefined).load_resource("ip6tables")
     _(resource.has_rule?("-P OUTPUT ACCEPT")).must_equal false
     _(resource.has_rule?("-P OUTPUT DROP")).must_equal false
+    _(resource.resource_id).must_equal "Ip6tables"
   end
 
 end
