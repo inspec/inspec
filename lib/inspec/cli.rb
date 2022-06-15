23de0c3902d7fb4a8c5a5208ebf4c3dcf80ac0ac
Nikita Mathur <nikita.mathur@chef.io>
2022-06-15 17:52:34 +0530

    Added enhanced_outcomes option for exec and shell
    Signed-off-by: Nikita Mathur <nikita.mathur@chef.io>

--- lib/inspec/cli.rb
+++ lib/inspec/cli.rb
@@ -415,6 +415,8 @@ class Inspec::InspecCLI < Inspec::BaseCLI
     desc: "Load one or more input files, a YAML file with values for the shell to use"
   option :input, type: :array, banner: "name1=value1 name2=value2",
     desc: "Specify one or more inputs directly on the command line to the shell, as --input NAME=VALUE. Accepts single-quoted YAML and JSON structures."
+  option :enhanced_outcomes, type: :boolean,
+    desc: "Show enhanced outcomes in output"
   def shell_func
     o = config
     deprecate_target_id(config)
