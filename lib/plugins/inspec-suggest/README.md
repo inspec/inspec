f5fb421da09b9c03cd9480e7123b3da078298fec
Sonu Saha <98935583+ahasunos@users.noreply.github.com>
2022-06-18 23:35:44 +0530

    [WIP] CFINSPEC-349: Initialize suggest plugin (#6143)
    * Add inspec-6 branch as release branch
    
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>
    
    * Bump version to 5.18.4 by Chef Expeditor
    
    Obvious fix; these changes are the result of automation not creative thinking.
    
    * add ruby test 3.1 in verify pipeline (#5892)
    
    * add ruby test 3.1 in verify pipeline
    
    Signed-off-by: jayashri garud <jgarud@msystechnologies.com>
    
    * updating rubygem to 3.3.3
    
    Signed-off-by: jayashri garud <jgarud@msystechnologies.com>
    
    * Test fixture plugin files for ABI 3.1.0
    
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>
    
    * Use kwargs to match new calling style for ERB in ruby 2.7 and 3.0
    
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>
    
    * Allow Date and Time data types, needed for waivers, since safe load is now the default for YAML
    
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>
    
    * Use Time.parse, not Time.new since the rules tightened for what you can pass the constructor for waiver dates
    
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>
    
    * Add ruby platform debug message to test parallel output
    
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>
    
    * Conditionalize permitted_classes in inputs yaml_load for <= 3.1.x only
    
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>
    
    * In ruby 3.1.0 net/pop3 is no more a default gem and that will require us to add it in Gemfile so trying with the byebug gem which we are bundling already
    
    Signed-off-by: Vasu1105 <vasundhara.jagdale@chef.io>
    
    * CFINSPEC-166 Updated url fetcher to handle Net::OpenTimeout Exception on Windows
    
    Signed-off-by: Vasu1105 <vasundhara.jagdale@chef.io>
    
    Co-authored-by: Clinton Wolfe <clintoncwolfe@gmail.com>
    Co-authored-by: Vasu1105 <vasundhara.jagdale@chef.io>
    
    * Bump version to 5.18.5 by Chef Expeditor
    
    Obvious fix; these changes are the result of automation not creative thinking.
    
    * CFINSPEC-349: Initialize suggest plugin
    
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>
    
    * CFINSPEC-349: Delete license
    All plugins use the parent license of the project
    
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>
    
    * CFINSPEC-349: Test plugin list listes inspec-suggest
    
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>
    
    * CFINSPEC-349: Remove unit test
    
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>
    
    * CFINSPEC-349: Remove Rakefile
    
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>
    
    * Delete Gemfile and gemspec, not needed for a core plugin
    
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>
    
    * Update and trim plugin skeleton files
    
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>
    
    Co-authored-by: Clinton Wolfe <clintoncwolfe@gmail.com>
    Co-authored-by: Chef Expeditor <chef-ci@chef.io>
    Co-authored-by: jayashrig158 <61735743+jayashrig158@users.noreply.github.com>
    Co-authored-by: Vasu1105 <vasundhara.jagdale@chef.io>

---
+++ lib/plugins/inspec-suggest/README.md
@@ -0,0 +1,16 @@
+# Suggest Plugin
+
+This plugin contains all the functionality needed to implement the InSpec Suggest recommender engine.
+
+## What This Plugin Does
+
+TODO
+
+## Developing This Plugin
+
+The generated plugin contains everything a real-world, industrial grade plugin would have, including:
+
+* an implementation of one or more InSpec Plugin Types
+* documentation (you are reading it now)
+* tests, at the unit and functional level
+
---
+++ lib/plugins/inspec-suggest/lib/inspec-suggest.rb
@@ -0,0 +1,14 @@
+# This file is known as the "entry point."
+# This is the file InSpec will try to load if it
+# thinks your plugin is installed.
+
+# The *only* thing this file should do is setup the
+# load path, then load the plugin definition file.
+
+# Next two lines simply add the path of the gem to the load path.
+# This is not needed when being loaded as a gem; but when doing
+# plugin development, you may need it.  Either way, it's harmless.
+libdir = File.dirname(__FILE__)
+$LOAD_PATH.unshift(libdir) unless $LOAD_PATH.include?(libdir)
+
+require "inspec-suggest/plugin"
