3a95cfa4697f9ffc60ea1aab8dc70e64195d2461
Clinton Wolfe <clintoncwolfe@gmail.com>
2022-06-27 16:29:34 -0400

    Linting
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- lib/inspec/formatters/base.rb
+++ lib/inspec/formatters/base.rb
@@ -126,9 +126,9 @@ module Inspec::Formatters
       elsif control_has_all_tests_skipped(control)
         { name: "Not Reviewed", abbrev: "N/R" }
       elsif control[:results].any? { |r| r[:status] == "failed" }
-        { name: "Failed", abbrev: "fail"}
+        { name: "Failed", abbrev: "fail" }
       else
-        { name: "Passed", abbrev: "pass"}
+        { name: "Passed", abbrev: "pass" }
       end
     end
 
