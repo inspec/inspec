67feff85560573a6eb962a5246728f76378d512b
Clinton Wolfe <clintoncwolfe@gmail.com>
2022-06-29 17:38:17 -0400

    Resource Type check
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- lib/plugins/inspec-suggest/lib/inspec-suggest/checker.rb
+++ lib/plugins/inspec-suggest/lib/inspec-suggest/checker.rb
@@ -44,6 +44,6 @@ require_relative "checker/syntax"           # must run first
 require_relative "checker/trailing_newline"
 require_relative "checker/load_as_json"
 require_relative "checker/control_id"       # must run after load_as_json
-# require_relative "checker/resource_type"    # must run after load_as_json
+require_relative "checker/resource_type"    # must run after load_as_json
 require_relative "checker/root_path_input" # must run after load_as_json
 require_relative "checker/category_tag"     # must run after load_as_json
