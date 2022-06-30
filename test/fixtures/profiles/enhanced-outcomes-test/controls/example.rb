8d8de0b08f8f24325c2bf1b0069999d8386a32cc
Nikita Mathur <nikita.mathur@chef.io>
2022-06-30 14:09:59 +0530

    Enhanced outcomes test profiles made consistent for testing across diff platforms
    Signed-off-by: Nikita Mathur <nikita.mathur@chef.io>

--- test/fixtures/profiles/enhanced-outcomes-test/controls/example.rb
+++ test/fixtures/profiles/enhanced-outcomes-test/controls/example.rb
@@ -1,67 +1,62 @@
 # Error
 control "tmp-1.0.1" do
   impact 0.7
-  describe file("/tmp") do
-    it { should_bot be_directory }
+  describe "a.1" do
+    it { should_bot "a.1" }
   end
 end
 
 control "tmp-1.0.2" do
   impact 0.0
-  describe file("/tmp") do
-    it { should_bot be_directory }
+  describe "a.2" do
+    it { should_bot "a.2" }
   end
 end
 
 # Not Applicable
 control "tmp-2.0.1" do
   impact 0.0
-  describe file("/tmp") do
-    it { should be_directory }
+  describe "b.1" do
+    it { should cmp "b.1" }
   end
 end
 
 control "tmp-2.0.2" do
   impact 0.0
   only_if { false }
-  describe file("/tmp") do
-    it { should be_directory }
+  describe "b.2" do
+    it { should cmp "b.2" }
   end
 end
 
 # Not Reviewed
 control "tmp-3.0.1" do
   only_if { false }
-  describe file("/tmp") do
-    it { should be_directory }
+  describe "c.1" do
+    it { should cmp "c.1" }
   end
 end
 
 control "tmp-3.0.2" do
   only_if { false }
-  describe file("/tmp") do
-    it { should_bot be_directory }
+  describe "c.2" do
+    it { should_bot "c.2" }
   end
 end
 
 # Failed
 control "tmp-4.0" do
   impact 0.7
-  title "Create /tmp directory"
-  desc "An optional description..."
-  describe file("/tmp") do
-    it { should_not be_directory }
-    it { should be_directory }
+  describe "d.1" do
+    it { should_not cmp "d.1" }
+    it { should cmp "d.1" }
   end
 end
 
 # Passed
 control "tmp-5.0" do
   impact 0.7
-  title "Create /tmp directory"
-  desc "An optional description..."
-  describe file("/tmp") do
-    it { should be_directory }
-    it { should exist }
+  describe "e.1" do
+    it { should cmp "e.1" }
   end
 end
