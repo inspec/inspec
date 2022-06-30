c689d953c6a82b798cb4be60f899ca59c7a2dcf5
Vasu1105 <vasundhara.jagdale@chef.io>
2022-06-30 22:55:30 +0530

    Fix lint errors
    Signed-off-by: Vasu1105 <vasundhara.jagdale@chef.io>

--- lib/inspec/resources/podman.rb
+++ lib/inspec/resources/podman.rb
@@ -30,7 +30,6 @@ module Inspec::Resources
     private
 
     def get_containers
-      #labels = %w{AutoRemove, Command, CreatedAt, Exited, ExitedAt, ExitCode, Id, Image, ImageID, IsInfra, Labels, Mounts, Names, Namespaces, Networks, Pid, Pod, PodName, Ports, Size, StartedAt, State, Status, Created}
       sub_cmd = "ps -a --no-trunc"
       output = run_command(sub_cmd)
       parse(output)
@@ -63,22 +62,18 @@ module Inspec::Resources
   end
 
   class PodmanContainerFilter
-    # TODO : Clear this commented labels once the code is complere. This is just for refernce
-    # AutoRemove, Command, CreatedAt, Exited, ExitedAt, ExitCode, Id, Image,
-    #ImageID, IsInfra, Labels, Mounts, Names,
-    #Namespaces, Networks, Pid, Pod, PodName, Ports, Size, StartedAt, State, Status, Created
     filter = FilterTable.create
     filter.register_custom_matcher(:exists?) { |x| !x.entries.empty? }
-    filter.register_column(:commands, field: "command")
+    filter.register_column(:commands,   field: "command")
       .register_column(:auto_remove,    field: "autoremove")
       .register_column(:created_at,     field: "createdat")
       .register_column(:ids,            field: "id")
       .register_column(:images,         field: "image")
       .register_column(:names,          field: "names")
       .register_column(:status,         field: "status")
-      .register_column(:exited,        field: "exited")
+      .register_column(:exited,         field: "exited")
       .register_column(:exit_code,      field: "exitcode")
-      .register_column(:image_ids,       field: "imageid")
+      .register_column(:image_ids,      field: "imageid")
       .register_column(:labels,         field: "labels")
       .register_column(:mounts,         field: "mounts")
       .register_column(:namespaces,     field: "namespaces")
