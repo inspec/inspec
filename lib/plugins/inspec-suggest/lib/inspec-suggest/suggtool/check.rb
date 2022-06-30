5aa6360bcf56324de3b916d3a9f2bd9c029ea9a3
Clinton Wolfe <clintoncwolfe@gmail.com>
2022-06-29 21:10:01 -0400

    Functional tests for suggtool check
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- lib/plugins/inspec-suggest/lib/inspec-suggest/suggtool/check.rb
+++ lib/plugins/inspec-suggest/lib/inspec-suggest/suggtool/check.rb
@@ -16,44 +16,40 @@ module InspecPlugins::Suggest
       setup(requested_sets)
 
       # Option disable-check is handled by Checker
-      checker = InspecPlugins::Suggest::Checker.new(cli_options: options, config: @cfg)
-      error_count = 0
+      checker = InspecPlugins::Suggest::Checker.new(cli_options: options, config: @cfg, ui: ui)
+      error_count, warning_count, file_count = 0, 0, 0
 
       with_requested_sets do |set_name, set_cfg|
-        set_cfg["paths"].each do |path|
-          Dir.glob(File.join(@working_dir, path, "suggest", "*.rb")).each do |control_file_path|
+        with_matching_files(set_cfg) do |control_file_path|
+          file_info = {
+            full_path: control_file_path,
+            rel_path: control_file_path.sub(Inspec.src_root + "/", ""),
+            errors: [],
+            warnings: [],
+            # checks may add fields for message passing
+          }
 
-            file_info = {
-              full_path: control_file_path,
-              rel_path: control_file_path.sub(Inspec.src_root + "/", ""),
-              content: File.read(control_file_path),
-              as_json: nil,
-              errors: [],
-              warnings: [],
-            }
+          ui.info("checking #{file_info[:rel_path]}")
 
-            ui.info("checking #{file_info[:rel_path]}")
+          checker.each_check(set_name) do |check|
+            # Runs the check and modifies file_info with errors and warnings
+            # Returns false if processing should stop
+            break unless check.run(file_info)
+          end
 
-            checker.each_check do |check|
-              # Runs the check and modifies file_info with errors and warnings
-              # Returns false if processing should stop
-              break unless check.run(file_info)
-            end
+          file_info[:warnings].each { |warning| ui.warning("#{file_info[:rel_path]}: #{warning}") }
+          file_info[:errors].each { |error| ui.error("#{file_info[:rel_path]}: #{error}") }
 
-            file_info[:warnings].each { |warning| ui.warn("#{file_info[:rel_path]}: #{warning}") }
-            file_info[:errors].each { |error| ui.error("#{file_info[:rel_path]}: #{error}") }
+          error_count += file_info[:errors].count
+          warning_count += file_info[:warnings].count
+          file_count += 1
 
-            error_count += file_info[:errors].count
-
-            ui.exit(:usage_error) if options[:fail_fast] && !file_info[:errors].empty
-
-          end # dir.glob
+          ui.exit(:usage_error) if options[:fail_fast] && !file_info[:errors].empty?
         end # each path
       end # each set
 
-      ui.info("check: Saw #{error_count} error(s)")
+      ui.info("check: Saw #{error_count} error(s), #{warning_count} warning(s) over #{file_count} files")
       ui.exit(:usage_error) unless options[:ignore_errors] || error_count == 0
-
     end # def check
   end
 end
