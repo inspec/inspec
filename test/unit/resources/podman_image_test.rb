2a73c5cfbd076ed99a31a631c39e909fc26438a2
Sonu Saha <sonu.saha@progress.com>
2022-07-08 14:30:43 +0530

    CFINSPEC-361: Modify resource id to avoid dependency on other methods
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

--- test/unit/resources/podman_image_test.rb
+++ test/unit/resources/podman_image_test.rb
@@ -11,7 +11,7 @@ describe Inspec::Resources::PodmanImage do
     _(resource.tag).must_equal "latest"
     _(resource.image).must_equal "docker.io/library/nginx:latest"
     _(resource.repo).must_equal "docker.io/library/nginx"
-    _(resource.resource_id).must_equal "sha256:55f4b40fe486a5b734b46bb7bf28f52fa31426bf23be068c8e7b19e58d9b8deb"
+    _(resource.resource_id).must_equal "docker.io/library/nginx:latest"
     _(resource.size).must_equal "146 MB"
     _(resource.digest).must_equal "sha256:10f14ffa93f8dedf1057897b745e5ac72ac5655c299dade0aa434c71557697ea"
     _(resource.created_at).must_equal "2022-06-23 04:13:24 +0000 UTC"
@@ -19,6 +19,6 @@ describe Inspec::Resources::PodmanImage do
     _(resource.history).must_equal "docker.io/library/nginx:latest"
     _(resource.inspect_info).must_include "Architecture"
     _(resource.inspect_info.Architecture).must_equal "arm64"
-    _(resource.to_s).must_equal "podman_image sha256:55f4b40fe486a5b734b46bb7bf28f52fa31426bf23be068c8e7b19e58d9b8deb"
+    _(resource.to_s).must_equal "podman_image docker.io/library/nginx:latest"
   end
 end
