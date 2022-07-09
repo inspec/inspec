338c55d49fe9ef25bf3e4c703191b407d065eea8
Sonu Saha <sonu.saha@progress.com>
2022-07-10 01:44:45 +0530

    CFINSPEC-361: Modify unit test and reqd fixtures
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

--- test/unit/resources/podman_image_test.rb
+++ test/unit/resources/podman_image_test.rb
@@ -5,20 +5,27 @@ require_relative "../../../lib/inspec/resources/podman_image"
 
 describe Inspec::Resources::PodmanImage do
   it "test podman image properties and matchers" do
-    resource = MockLoader.new("unix".to_sym).load_resource("podman_image", "docker.io/library/nginx")
+    resource = MockLoader.new("unix".to_sym).load_resource("podman_image", "docker.io/library/busybox")
     _(resource.exist?).must_equal true
-    _(resource.id).must_equal "sha256:55f4b40fe486a5b734b46bb7bf28f52fa31426bf23be068c8e7b19e58d9b8deb"
-    _(resource.tag).must_equal "latest"
-    _(resource.image).must_equal "docker.io/library/nginx:latest"
-    _(resource.repo).must_equal "docker.io/library/nginx"
-    _(resource.resource_id).must_equal "docker.io/library/nginx:latest"
-    _(resource.size).must_equal "146 MB"
-    _(resource.digest).must_equal "sha256:10f14ffa93f8dedf1057897b745e5ac72ac5655c299dade0aa434c71557697ea"
-    _(resource.created_at).must_equal "2022-06-23 04:13:24 +0000 UTC"
-    _(resource.created_since).must_equal "13 days ago"
-    _(resource.history).must_equal "docker.io/library/nginx:latest"
-    _(resource.inspect_info).must_include "Architecture"
-    _(resource.inspect_info.Architecture).must_equal "arm64"
-    _(resource.to_s).must_equal "podman_image docker.io/library/nginx:latest"
+    _(resource.repo_tags).must_include "docker.io/library/busybox:latest"
+    _(resource.created_at).must_equal "2022-06-08T00:39:28.175020858Z"
+    _(resource.version).must_equal "20.10.12"
+    _(resource.size).must_equal 1636053
+    _(resource.digest).must_equal "sha256:3614ca5eacf0a3a1bcc361c939202a974b4902b9334ff36eb29ffe9011aaad83"
+    _(resource.names_history).must_include "docker.io/library/busybox:latest"
+    _(resource.repo_digests).must_include "docker.io/library/busybox@sha256:2c5e2045f35086c019e80c86880fd5b7c7a619878b59e3b7592711e1781df51a"
+    _(resource.architecture).must_equal "arm64"
+    _(resource.os).must_equal "linux"
+    _(resource.virtual_size).must_equal 1636053
+    _(resource.resource_id).must_equal "docker.io/library/busybox:latest"
+    _(resource.to_s).must_equal "podman_image docker.io/library/busybox:latest"
+  end
+
+  it "test for a non-existing container image" do
+    resource = MockLoader.new("ubuntu".to_sym).load_resource("podman_image", "nginx")
+    _(resource.exist?).must_equal false
+    assert_nil resource.repo_tags
+    assert_nil resource.size
+    assert_nil resource.digest
   end
 end
