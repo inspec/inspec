c33ea15538711b98bd9f8c0981102d7d9061c183
Sonu Saha <sonu.saha@progress.com>
2022-07-11 15:11:35 +0530

    CFINSPEC-370: Modify test for patch, minor and major
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

--- lib/plugins/inspec-suggest/test/functional/suggtool_bump_test.rb
+++ lib/plugins/inspec-suggest/test/functional/suggtool_bump_test.rb
@@ -8,11 +8,25 @@ describe "inspec suggtool bump" do
   let(:config) { File.join(fixture_path, "sugg-conf", "suggest-bump.yaml") }
 
   describe "when packaging the basic test fixture set" do
-    it "should work correctly" do
+    it "bump:patch version default" do
       result = run_inspec_process("suggtool bump --suggest-config #{config}")
       _(result.stderr).must_equal ""
       _(result.exit_status).must_equal 0
-      _(result.stdout).must_include "Bumped version of"
+      _(result.stdout).must_include "Bumped:Patch version of"
+    end
+
+    it "bump:minor version" do
+      result = run_inspec_process("suggtool bump --suggest-config #{config} --minor")
+      _(result.stderr).must_equal ""
+      _(result.exit_status).must_equal 0
+      _(result.stdout).must_include "Bumped:Minor version of"
+    end
+
+    it "bump:major version" do
+      result = run_inspec_process("suggtool bump --suggest-config #{config} --major")
+      _(result.stderr).must_equal ""
+      _(result.exit_status).must_equal 0
+      _(result.stdout).must_include "Bumped:Major version of"
     end
   end
 end
