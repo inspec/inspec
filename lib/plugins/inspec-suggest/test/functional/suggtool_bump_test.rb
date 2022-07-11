cd8035f0e3cbaccfa7f85cd8c0f90c4260e847f3
Sonu Saha <sonu.saha@progress.com>
2022-07-11 23:05:25 +0530

    CFINSPEC-370: Add test for wrong bump level
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

--- lib/plugins/inspec-suggest/test/functional/suggtool_bump_test.rb
+++ lib/plugins/inspec-suggest/test/functional/suggtool_bump_test.rb
@@ -67,5 +67,19 @@ describe "inspec suggtool bump" do
         _(run_suggtool_bump.stdout).must_include "Bumped:major version of"
       end # Dir.mktmpdir
     end # it ends
+
+    it "should fail because of wrong bump level" do
+      Dir.mktmpdir do |workdir|
+        FileUtils.cp_r(File.join(fixture_path, "src"), workdir)
+        run_suggtool_package = run_inspec_process("suggtool package test-fixture --work-dir #{workdir} --suggest-config #{config}")
+        _(run_suggtool_package.stderr).must_equal ""
+        _(run_suggtool_package.exit_status).must_equal 0
+
+        # wrong bump level
+        run_suggtool_bump = run_inspec_process("suggtool bump --level unknown --suggest-config #{config} --work-dir #{workdir}")
+        _(run_suggtool_bump.exit_status).must_equal 1
+        _(run_suggtool_bump.stdout).must_include "Invalid bump level 'unknown'. Valid levels are: patch, minor, major"
+      end # Dir.mktmpdir
+    end # it ends
   end # describe end (when bumping version on different levels)
 end # describe end (inspec suggtool bump)
