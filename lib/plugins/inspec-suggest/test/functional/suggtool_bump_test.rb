3c99a061adb56894a3d2bfb81f6f4562fe8f908d
Sonu Saha <sonu.saha@progress.com>
2022-07-11 22:25:53 +0530

    CFINSPEC-370: Refactor test, remove fixture, rename config
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

--- lib/plugins/inspec-suggest/test/functional/suggtool_bump_test.rb
+++ lib/plugins/inspec-suggest/test/functional/suggtool_bump_test.rb
@@ -8,56 +8,37 @@ describe "inspec suggtool bump" do
   let(:config) { File.join(fixture_path, "sugg-conf", "suggest-bump.yaml") }
 
   describe "when bumping version on different levels" do
-    it "should bump version for default level i.e. patch" do
+    it "should bump version for all three levels" do
       Dir.mktmpdir do |workdir|
         FileUtils.cp_r(File.join(fixture_path, "src"), workdir)
-        run_suggtool_package = run_inspec_process("suggtool package --work-dir #{workdir} --suggest-config #{config}")
+        run_suggtool_package = run_inspec_process("suggtool package test-fixture --work-dir #{workdir} --suggest-config #{config}")
         _(run_suggtool_package.stderr).must_equal ""
         _(run_suggtool_package.exit_status).must_equal 0
+
+        # default bump i.e. patch
         run_suggtool_bump = run_inspec_process("suggtool bump --suggest-config #{config} --work-dir #{workdir}")
         _(run_suggtool_bump.stderr).must_equal ""
         _(run_suggtool_bump.exit_status).must_equal 0
         _(run_suggtool_bump.stdout).must_include "Bumped:patch version of"
-      end
-    end # default end
 
-    it "should bump version for --level patch" do
-      Dir.mktmpdir do |workdir|
-        FileUtils.cp_r(File.join(fixture_path, "src"), workdir)
-        run_suggtool_package = run_inspec_process("suggtool package --work-dir #{workdir} --suggest-config #{config}")
-        _(run_suggtool_package.stderr).must_equal ""
-        _(run_suggtool_package.exit_status).must_equal 0
+        # patch bump
         run_suggtool_bump = run_inspec_process("suggtool bump --level patch --suggest-config #{config} --work-dir #{workdir}")
         _(run_suggtool_bump.stderr).must_equal ""
         _(run_suggtool_bump.exit_status).must_equal 0
         _(run_suggtool_bump.stdout).must_include "Bumped:patch version of"
-      end
-    end # patch end
 
-    it "should bump version for --level minor" do
-      Dir.mktmpdir do |workdir|
-        FileUtils.cp_r(File.join(fixture_path, "src"), workdir)
-        run_suggtool_package = run_inspec_process("suggtool package --work-dir #{workdir} --suggest-config #{config}")
-        _(run_suggtool_package.stderr).must_equal ""
-        _(run_suggtool_package.exit_status).must_equal 0
+        # minor bump
         run_suggtool_bump = run_inspec_process("suggtool bump --level minor --suggest-config #{config} --work-dir #{workdir}")
         _(run_suggtool_bump.stderr).must_equal ""
         _(run_suggtool_bump.exit_status).must_equal 0
         _(run_suggtool_bump.stdout).must_include "Bumped:minor version of"
-      end
-    end # minor end
 
-    it "should bump version for --level major" do
-      Dir.mktmpdir do |workdir|
-        FileUtils.cp_r(File.join(fixture_path, "src"), workdir)
-        run_suggtool_package = run_inspec_process("suggtool package --work-dir #{workdir} --suggest-config #{config}")
-        _(run_suggtool_package.stderr).must_equal ""
-        _(run_suggtool_package.exit_status).must_equal 0
+        # major bump
         run_suggtool_bump = run_inspec_process("suggtool bump --level major --suggest-config #{config} --work-dir #{workdir}")
         _(run_suggtool_bump.stderr).must_equal ""
         _(run_suggtool_bump.exit_status).must_equal 0
         _(run_suggtool_bump.stdout).must_include "Bumped:major version of"
-      end
-    end # major end
+      end # Dir.mktmpdir
+    end # it ends
   end # describe end (when bumping version on different levels)
 end # describe end (inspec suggtool bump)
