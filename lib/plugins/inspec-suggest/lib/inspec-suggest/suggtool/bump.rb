3157ce652203095498802b70ae8828e097fe4207
Sonu Saha <sonu.saha@progress.com>
2022-07-11 22:42:38 +0530

    CFINSPEC-370: Handle error situation first then logic
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

--- lib/plugins/inspec-suggest/lib/inspec-suggest/suggtool/bump.rb
+++ lib/plugins/inspec-suggest/lib/inspec-suggest/suggtool/bump.rb
@@ -19,33 +19,33 @@ module InspecPlugins::Suggest
 
     no_commands do
       def bump_version(file_path)
-        require "yaml" unless defined? YAML
-        yaml_data = YAML.load_file(file_path)
         valid_bump_levels = %w{patch minor major}
 
-        if valid_bump_levels.include?(options[:level])
-          current_version = yaml_data["version"].split(".")
-
-          case options[:level]
-          when "patch"
-            new_version = current_version[0] + "." + current_version[1] + "." + current_version[2].succ
-          when "minor"
-            new_version = current_version[0] + "." + current_version[1].succ + ".0"
-          when "major"
-            new_version = current_version[0].succ + ".0.0"
-          end
-
-          # Update the version in yaml_data
-          yaml_data["version"] = new_version
-
-          File.open(file_path, "w") do |file|
-            file.write(yaml_data.to_yaml)
-          end
-          ui.info "Bumped:#{options[:level]} version of #{file_path} to #{new_version}"
-        else
-          ui.error("Invalid level '#{options[:level]}'. Valid levels are: patch, minor, major")
+        unless valid_bump_levels.include? options[:level]
+          ui.error("Invalid bump level '#{options[:level]}'. Valid levels are: #{valid_bump_levels.join(", ")}")
           ui.exit(:usage_error)
         end
+
+        require "yaml" unless defined? YAML
+        yaml_data = YAML.load_file(file_path)
+        current_version = yaml_data["version"].split(".")
+
+        case options[:level]
+        when "patch"
+          new_version = current_version[0] + "." + current_version[1] + "." + current_version[2].succ
+        when "minor"
+          new_version = current_version[0] + "." + current_version[1].succ + ".0"
+        when "major"
+          new_version = current_version[0].succ + ".0.0"
+        end
+
+        # Update the version in yaml_data
+        yaml_data["version"] = new_version
+
+        File.open(file_path, "w") do |file|
+          file.write(yaml_data.to_yaml)
+        end
+        ui.info "Bumped:#{options[:level]} version of #{file_path} to #{new_version}"
       end
     end
   end
