b68d95a8c6ac8ebb428298ea96a5c10956a3cab3
Vasu1105 <vasundhara.jagdale@chef.io>
2022-07-13 00:17:58 +0530

    CFINSPEC-351 Adds podman_network resource properties
    Signed-off-by: Vasu1105 <vasundhara.jagdale@chef.io>

--- lib/inspec/resources/podman_network.rb
+++ lib/inspec/resources/podman_network.rb
@@ -1,3 +1,4 @@
+require "inspec/resources/command"
 module Inspec::Resources
   class PodmanNetwork < Inspec.resource(1)
     name "podman_network"
@@ -15,77 +16,64 @@ module Inspec::Resources
       end
     EXAMPLE
 
-    def initialize(id)
+    LABELS = {
+      id: "ID",
+      name: "Name",
+      driver: "Driver",
+      labels: "Labels",
+      options: "Options",
+      ipam_options: "IPAMOptions",
+      internal: "Internal",
+      created: "Created",
+      ipv6_enabled: "IPv6Enabled",
+      dns_enabled: "DNSEnabled",
+      network_interface: "NetworkInterface",
+      subnets: "Subnets",
+    }.freeze
+
+    attr_reader :param, :network_info
+    def initialize(param)
       skip_resource "The `podman_network` resource is not yet available on your OS." unless inspec.os.unix?
-      @id = id
-      @network_info = network_info
+      @param = param
+      @network_info = get_network_info
+    end
+
+    ## This creates all the required properties methods dynamically.
+    LABELS.each do |k, v|
+      define_method(k) do
+        network_info[k.to_s]
+      end
     end
 
     def exist?
-      false
-    end
-
-    def id
-      "abc"
-    end
-
-    def name
-      "abc"
-    end
-
-    def network_interface
-      "abc"
-    end
-
-    def driver
-      "abc"
-    end
-
-    def labels
-      "abc"
-    end
-
-    def options
-      {}
-    end
-
-    def ipv6_enabled
-      false
-    end
-
-    def ipam_options
-      {}
-    end
-
-    def dns_enabled
-      false
-    end
-
-    def subnets
-      []
-    end
-
-    def internal
-      false
-    end
-
-    def created
-      "timestamp"
+      ! network_info.empty?
     end
 
     def resource_id
       id || ""
     end
 
-    # Define how you want your resource to appear in test reports. Commonly, this is just the resource name and the resource ID.
     def to_s
       "podman_network"
     end
 
     private
 
-    def network_info
-      # Run podman command to get nework information
+    def get_network_info
+      go_template_format = (LABELS.map { |k, v| "\"#{k}\": {{json .#{v}}}" }).join(", ")
+      result = inspec.command("podman network inspect #{param} --format '{#{go_template_format}}'")
+
+      if result.exit_status != 0 && result.exit_status != 125
+        raise Inspec::Exceptions::ResourceFailed, "Unable to retrieve podman network information for #{param}.\nError message: #{result.stderr}"
+      elsif result.stdout.empty?
+        {}
+      else
+        require "json" unless defined?(JSON)
+        JSON.parse(result.stdout)
+      end
+    rescue JSON::ParserError => _e
+      warn "Could not parse `podman command` output"
+      {}
     end
   end
 end
