4bb7f3a22ed3bbe5dd5234e9d2227fa7c2c0e9c6
Sonu Saha <98935583+ahasunos@users.noreply.github.com>
2022-09-15 18:53:45 +0530

    Enhance `lxc` resource to test properties (#6243)
    * ENHANCE: Extend lxc resource to test the properties
    
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>
    
    * TEST: Extend test for introduced properties
    
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>
    
    * DOCS: Extend test for introduced properties
    
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>
    
    * FIX: Correct exception class to handle bad yaml
    
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>
    
    * Doc Review
    
    Signed-off-by: Deepa Kumaraswamy <dkumaras@progress.com>
    
    * FIX: Handle specific exception to handle bad yaml
    
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>
    
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>
    Signed-off-by: Deepa Kumaraswamy <dkumaras@progress.com>
    Co-authored-by: Deepa Kumaraswamy <dkumaras@progress.com>

--- lib/inspec/resources/lxc.rb
+++ lib/inspec/resources/lxc.rb
@@ -9,14 +9,26 @@ module Inspec::Resources
       describe lxc("ubuntu-container") do
         it { should exist }
         it { should be_running }
+        its("name") { should eq "ubuntu-container" }
+        its("status") { should cmp "Running" }
+        its("type") { should eq "container" }
+        its("architecture") { should eq "x86_64" }
+        its("pid") { should eq 1378 }
+        its("created_at") { should eq "2022/08/16 12:07 UTC" }
+        its("last_used_at") { should eq "2022/08/17 05:06 UTC" }
+        its("resources") { should include "Disk usage" }
       end
     EXAMPLE
+
+    attr_reader :container_info, :container_name
 
     # Resource initialization.
     def initialize(container_name)
       @container_name = container_name
 
       raise Inspec::Exceptions::ResourceSkipped, "The `lxc` resource is not supported on your OS yet." unless inspec.os.linux?
+
+      @container_info = populate_container_info
     end
 
     def resource_id
@@ -28,17 +40,60 @@ module Inspec::Resources
     end
 
     def exists?
-      lxc_info_cmd.exit_status.to_i == 0
+      !@container_info.empty?
     end
 
     def running?
-      container_info = lxc_info_cmd.stdout.split(":").map(&:strip)
-      container_info[0] == "Status" && container_info[1] == "Running"
+      @container_info.key?("Status") && @container_info["Status"].casecmp("Running") == 0
+    end
+
+    def name
+      @container_info["Name"]
+    end
+
+    def status
+      @container_info["Status"]
+    end
+
+    def type
+      @container_info["Type"]
+    end
+
+    def architecture
+      @container_info["Architecture"]
+    end
+
+    def pid
+      @container_info["PID"]
+    end
+
+    def created_at
+      @container_info["Created"]
+    end
+
+    def last_used_at
+      @container_info["Last Used"]
+    end
+
+    def resources
+      @container_info["Resources"]
     end
 
     private
 
-    # Method to find lxc
+    def populate_container_info
+      lxc_util = find_lxc_or_error
+      lxc_info_cmd = inspec.command("#{lxc_util} info #{@container_name}")
+
+      if lxc_info_cmd.exit_status.to_i == 0
+        parse_command_output(lxc_info_cmd.stdout)
+      elsif lxc_info_cmd.stderr =~ /Error: Instance not found/
+        {}
+      else
+        raise Inspec::Exceptions::ResourceFailed, "Unable to retrieve information for #{container_name}.\n#{lxc_info_cmd.stderr}"
+      end
+    end
+
     def find_lxc_or_error
       %w{/usr/sbin/lxc /sbin/lxc lxc}.each do |cmd|
         return cmd if inspec.command(cmd).exist?
@@ -47,11 +102,12 @@ module Inspec::Resources
       raise Inspec::Exceptions::ResourceFailed, "Could not find `lxc`"
     end
 
-    def lxc_info_cmd
-      bin = find_lxc_or_error
-      info_cmd = "info #{@container_name} | grep -i Status"
-      lxc_cmd = format("%s %s", bin, info_cmd).strip
-      inspec.command(lxc_cmd)
+    def parse_command_output(output)
+      require "yaml" unless defined?(YAML)
+      YAML.load(output)
+    rescue Psych::SyntaxError => e
+      warn "Could not parse the command output.\n#{e.message}"
+      {}
     end
   end
 end
