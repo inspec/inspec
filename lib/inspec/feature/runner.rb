76c8ddb76fa6374358a76e080a2d8f326038e997
Clinton Wolfe <clintoncwolfe@gmail.com>
2022-09-16 12:31:25 -0400

    Basic logger integration
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- lib/inspec/feature/runner.rb
+++ lib/inspec/feature/runner.rb
@@ -2,6 +2,12 @@ module Inspec
   class Feature
     class Runner
       def self.with_feature(feature_name, opts = {}, &feature_implementation)
+        config = opts[:config] || Inspec::Feature::Config.new
+        logger = opts[:logger] || Inspec::Log
+
+        # Emit log message saying we're running a feature
+        logger.debug("Prepping to run feature '#{feature_name}'")
+
         yield feature_implementation
       end
     end
