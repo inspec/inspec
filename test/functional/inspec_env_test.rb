9f8e53385621fa9e0724a83519358862a9beb946
Clinton Wolfe <clintoncwolfe@gmail.com>
2022-12-06 22:10:46 -0500

    Restrict test to non-windows systems
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- test/functional/inspec_env_test.rb
+++ test/functional/inspec_env_test.rb
@@ -6,14 +6,18 @@ describe "inspec env" do
   parallelize_me!
 
   describe "inspec env with no args" do
-    it "runs without warnings" do
-      result = run_inspec_process("env")
+    # env is a command only supported for 
+    # bash, zsh, and fish shells - so mac and linux
+    unless windows?
+      it "runs without warnings" do
+        result = run_inspec_process("env")
 
-      assert_empty result.stderr
-      assert_exit_code 0, result
-      
-      # Part of the successful help usage message at the end, not likely to change
-      _(result.stdout).must_include "inspec env SHELLNAME"
+        assert_empty result.stderr
+        assert_exit_code 0, result
+
+        # Part of the successful help usage message at the end, not likely to change
+        _(result.stdout).must_include "inspec env SHELLNAME"
+      end
     end
   end
 end
