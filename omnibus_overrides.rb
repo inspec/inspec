7a4f6e4e2fe7f3cdce60693e8d802e1c196ff3d4
Sean Simmons <ssimmons@progress.com>
2022-12-22 11:08:00 -0500

    adding more overrides for ruby version, windows settings and nokogiri
    Signed-off-by: Sean Simmons <ssimmons@progress.com>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -3,8 +3,12 @@
 # grab the current train release from rubygems.org
 train_stable = /^train \((.*)\)/.match(`gem list ^train$ --remote`)[1]
 override "train", version: "v#{train_stable}"
-override "ruby", version: "3.1.2"
+override :ruby, version: aix? ? "3.0.3" : "3.1.2"
 
 # Mac m1
 override "openssl", version: "1.1.1m" if mac_os_x?
-#override "ruby-msys2-devkit", version: "2.7.6-1"
+override "nokogiri", version: "1.13.6"
+
+# windows
+override "ruby-windows-devkit-bash", version: "3.1.23-4-msys-1.0.18"
+override "ruby-msys2-devkit", version: "3.1.2-1"
