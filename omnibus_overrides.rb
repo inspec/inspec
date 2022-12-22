d28d7a85a9a91df05845249693d6b796fb2f9f05
Vasu1105 <vasundhara.jagdale@chef.io>
2022-12-22 16:03:17 +0530

    Checking with nokogiri verison 1.13.6 and just trying to match other things with other chef project settings
    Signed-off-by: Vasu1105 <vasundhara.jagdale@chef.io>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -5,6 +5,9 @@ train_stable = /^train \((.*)\)/.match(`gem list ^train$ --remote`)[1]
 override "train", version: "v#{train_stable}"
 override "ruby", version: "3.0.3"
 
+override "libxml2", version: windows? ? "2.9.10" : "2.9.13"
+override "libxslt", version: windows? ? "1.1.34" : "1.1.35"
+
 # Mac m1
 override "openssl", version: "1.1.1m" if mac_os_x?
-#override "ruby-msys2-devkit", version: "2.7.6-1"
+override "nokogiri", version: "1.13.6"
