6940d590270b3fbda42f92c8a8c0b54484ca14dc
Chef Expeditor <chef-ci@chef.io>
2023-02-24 09:30:11 +0000

    Bump version to 6.2.15 by Chef Expeditor
    Obvious fix; these changes are the result of automation not creative thinking.

--- inspec-bin/lib/inspec-bin/version.rb
+++ inspec-bin/lib/inspec-bin/version.rb
@@ -1,5 +1,5 @@
 # This file managed by automation - do not edit manually
 module InspecBin
   INSPECBIN_ROOT = File.expand_path("..", __dir__)
-  VERSION = "6.2.14".freeze
+  VERSION = "6.2.15".freeze
 end
--- lib/inspec/version.rb
+++ lib/inspec/version.rb
@@ -1,3 +1,3 @@
 module Inspec
-  VERSION = "6.2.14".freeze
+  VERSION = "6.2.15".freeze
 end
