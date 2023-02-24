be7671da8e26f3baf37850b842656e718c547830
Vasundhara Jagdale <vasundhara.jagdale@chef.io>
2023-02-24 17:15:28 +0530

    Fixing typo in user_permissions (#6344) (#6465)
    * Fixing typo in user_permissions
    
    
    
    * Corrected spacing
    
    Signed-off-by: Magruder, Micah <Micah.Magruder@oracle.com>
    Co-authored-by: Micah Magruder <micah.magruder@cerner.com>

--- lib/inspec/resources/file.rb
+++ lib/inspec/resources/file.rb
@@ -66,7 +66,7 @@ module Inspec::Resources
     def user_permissions
       return {} unless exist?
 
-      return skip_reource"`user_permissions` is not supported on your OS yet." unless inspec.os.windows?
+      return skip_resource "`user_permissions` is not supported on your OS yet." unless inspec.os.windows?
 
       @perms_provider.user_permissions(file)
     end
