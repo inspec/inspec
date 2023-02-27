ee35f4ed0845a0d1a99b8db1b294541605780187
Vasundhara Jagdale <vasundhara.jagdale@chef.io>
2023-02-27 14:01:37 +0530

    CFINSPEC-576 : Fix profile gem dependency loading issue when dependent gem is required inside profile libraries. (#6408) (#6467)
    * Test profile consist of custom resource which has a gem dependency
    
    
    
    * CFINSPEC-576 Fix for profile gem depedency loading issue if depedent gem is required in libraries files of the profile
    
    
    
    * Fix windows test failure for profile gem dependency
    
    
    
    ---------
    
    Signed-off-by: Vasu1105 <vasundhara.jagdale@chef.io>

--- test/fixtures/profiles/profile-with-gem-dependency/controls/example.rb
+++ test/fixtures/profiles/profile-with-gem-dependency/controls/example.rb
@@ -1,11 +1,5 @@
-require "money"
-
 control "tmp-1.0" do
-  Money.rounding_mode = BigDecimal::ROUND_HALF_UP
-  m = Money.from_cents(1000, "USD")
-  cents = m.cents
-
-  describe cents do
+  describe money_converter.cents do
     it { should eq 1000 }
   end
 end
