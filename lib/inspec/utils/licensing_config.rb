81f299ea500097171c1c845be573f2b22d7e0c09
Sonu Saha <98935583+ahasunos@users.noreply.github.com>
2023-06-08 18:47:21 +0530

    CONFIG: Update licensing config to set default server url (#50)
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

--- lib/inspec/utils/licensing_config.rb
+++ lib/inspec/utils/licensing_config.rb
@@ -3,4 +3,5 @@ ChefLicensing.configure do |config|
   config.chef_product_name = "Inspec"
   config.chef_entitlement_id = "3ff52c37-e41f-4f6c-ad4d-365192205968"
   config.chef_executable_name = "inspec"
+  config.license_server_url = "https://licensing.chef.co/License"
 end
