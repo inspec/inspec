8323ecd5c50270e5a5467dcee8a14b6fef25bbea
Sonu Saha <98935583+ahasunos@users.noreply.github.com>
2023-07-07 04:58:23 +0530

    CHORE: Casing correction of product name from Inspec to InSpec (#78)
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

--- lib/inspec/utils/licensing_config.rb
+++ lib/inspec/utils/licensing_config.rb
@@ -1,6 +1,6 @@
 require "chef-licensing"
 ChefLicensing.configure do |config|
-  config.chef_product_name = "Inspec"
+  config.chef_product_name = "InSpec"
   config.chef_entitlement_id = "3ff52c37-e41f-4f6c-ad4d-365192205968"
   config.chef_executable_name = "inspec"
   config.license_server_url = "https://licensing.chef.co/License"
