c8b5bc43f2ec4b5a9f68ea11f825b7b01ef04970
Vasu1105 <vasundhara.jagdale@progress.com>
2023-09-07 17:37:17 +0530

    Updates code to better handle errors and warnings related to missing required parameters and extra parameters in waivers file in all format i.e (yaml, json and csv).
    Signed-off-by: Vasu1105 <vasundhara.jagdale@progress.com>

--- lib/inspec/waiver_file_reader.rb
+++ lib/inspec/waiver_file_reader.rb
@@ -24,7 +24,7 @@ module Inspec
               "Check to make sure file has the appropriate extension."
         end
       rescue Inspec::Exceptions::WaiversFileNotReadable, Inspec::Exceptions::WaiversFileInvalidFormatting => e
-        Inspec::Log.error "Error reading waivers file #{file_path}.#{e.message}"
+        Inspec::Log.error "Error reading waivers file #{file_path}. #{e.message}"
         Inspec::UI.new.exit(:usage_error)
       end
 
@@ -41,7 +41,7 @@ module Inspec
       elsif file_extension == ".csv"
         data = Waivers::CSVFileReader.resolve(file_path)
         headers = Waivers::CSVFileReader.headers
-        validate_headers(headers)
+        validate_csv_headers(headers)
       elsif file_extension == ".json"
         data = Waivers::JSONFileReader.resolve(file_path)
         validate_json_yaml(data) unless data.nil?
@@ -53,24 +53,55 @@ module Inspec
       %w{control_id justification expiration_date run}
     end
 
+    def self.validate_csv_headers(headers)
+      missing_required_field, blank_column, extra_headers = validate_headers(headers)
+      # Warn if blank column found in csv file
+      Inspec::Log.warn "Invalid column headers: Column can't be nil" if blank_column
+      # Warn if extra header found in csv file
+      Inspec::Log.warn "Extra header/s #{extra_headers}" unless extra_headers.empty?
+      unless missing_required_field.empty?
+        raise Inspec::Exceptions::WaiversFileInvalidFormatting,
+        "Missing required header/s #{missing_required_field}. Fix headers in file to proceed."
+      end
+    end
+
     def self.validate_headers(headers, json_yaml = false)
       required_fields = json_yaml ? %w{justification} : %w{control_id justification}
-
-      # In case of invalid headers raise error to provide valid waiver file.
+      missing_required_field = []
+      extra_headers = []
+      # Finds missing required fields
       unless (required_fields - headers).empty?
-        raise Inspec::Exceptions::WaiversFileInvalidFormatting,
-            "\n Missing required parameters: #{required_fields}.\n Valid parameters are #{all_fields}."
+        missing_required_field = required_fields - headers
       end
-      Inspec::Log.warn "Invalid column headers: Column can't be nil" if (headers.include? nil) && !json_yaml
-      Inspec::Log.warn "Extra parameters: #{(headers - all_fields)}" unless (headers - all_fields).empty?
+      # If column with no header found set the blank_column flag. Only applicable for csv
+      blank_column = true if headers.include?(nil)
+      # Find extra headers/parameters
+      extra_headers = (headers - all_fields)
+      if json_yaml
+        [missing_required_field, extra_headers]
+      else
+        [missing_required_field, blank_column, extra_headers]
+      end
     end
 
     def self.validate_json_yaml(data)
-      headers = []
-      data.each_value do |value|
+      missing_required_fields = false
+      data.each do |key, value|
         # In case of yaml or json we need to validate headers/parametes for each value
-        validate_headers(value.keys, true)
-        headers.push value.keys
+        missing_required_field, extra_parameters = validate_headers(value.keys, true)
+        # WARN in case of extra parameters found in each waived control
+        Inspec::Log.warn "Control ID #{key}: extra parameter/s #{extra_parameters}" unless extra_parameters.empty?
+        unless missing_required_field.empty?
+          missing_required_fields = true
+          # Log error for each waived control
+          Inspec::Log.error "Control ID #{key}: missing required parameter/s #{missing_required_field}"
+        end
+      end
+
+      # Raise error if any of the waived control has missing required filed
+      if missing_required_fields
+        raise Inspec::Exceptions::WaiversFileInvalidFormatting,
+             "Missing required parameter [justification]. Fix parameters in file to proceed."
       end
     end
   end
