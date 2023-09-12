95c17d4e7fd3f634243219d53024661d91edc2c9
Vasundhara Jagdale <vasundhara.jagdale@chef.io>
2023-09-12 14:36:40 +0000

    CHEF-3849: InSpec should exit quickly and clearly if waiver file is malformed/corrupt (#6644)
    * Functional test for malformed waiver file
    
    Signed-off-by: Vasu1105 <vasundhara.jagdale@progress.com>
    
    * Raise error for malformed yaml content and exit
    
    Signed-off-by: Vasu1105 <vasundhara.jagdale@progress.com>
    
    * Updates functional test for malformed yaml waiver file and for empty waiver file
    
    Signed-off-by: Vasu1105 <vasundhara.jagdale@progress.com>
    
    * Raises error in case of missing required parameters in waiver file
    
    Signed-off-by: Vasu1105 <vasundhara.jagdale@progress.com>
    
    * Update functional test for missing parameters, extra parameters or column without headers in waiver file
    
    Signed-off-by: Vasu1105 <vasundhara.jagdale@progress.com>
    
    * Fix linting
    
    Signed-off-by: Vasu1105 <vasundhara.jagdale@progress.com>
    
    * Fix warning and error messages
    
    Signed-off-by: Vasu1105 <vasundhara.jagdale@progress.com>
    
    * Updates nil and false check for yaml data and adds additional empty check.
    
    Co-authored-by: Sathish Babu <80091550+sathish-progress@users.noreply.github.com>
    Signed-off-by: Vasu1105 <vasundhara.jagdale@progress.com>
    
    * Adds more generic message as this yaml reader is now getting used by other functionalities like waiver file
    
    Signed-off-by: Vasu1105 <vasundhara.jagdale@progress.com>
    
    * Fixed test description to reflect correct use case
    
    Signed-off-by: Vasu1105 <vasundhara.jagdale@progress.com>
    
    * Fix validate headers was not validating the required parametes for all the data fields as it was not called inside the loop where we are iterating over the data and fetching the headers.
    
    Signed-off-by: Vasu1105 <vasundhara.jagdale@progress.com>
    
    * Updates the test files for the use case to missing parameters and extra parameters
    
    Signed-off-by: Vasu1105 <vasundhara.jagdale@progress.com>
    
    * Updates code to better handle errors and warnings related to missing required parameters and extra parameters in waivers file in all format i.e (yaml, json and csv).
    
    Signed-off-by: Vasu1105 <vasundhara.jagdale@progress.com>
    
    * Updated functional test to capture the updated error and warning messages for waiver file validation
    
    Signed-off-by: Vasu1105 <vasundhara.jagdale@progress.com>
    
    * Fix linting
    
    Signed-off-by: Vasu1105 <vasundhara.jagdale@progress.com>
    
    * Deleted fixture file which is not required
    
    Signed-off-by: Vasu1105 <vasundhara.jagdale@progress.com>
    
    * Refactor: Renamed method validate_headers to reflect whats it's doing and instead of return data in array it will now return the data in hash
    
    Signed-off-by: Vasu1105 <vasundhara.jagdale@progress.com>
    
    ---------
    
    Signed-off-by: Vasu1105 <vasundhara.jagdale@progress.com>
    Co-authored-by: Sathish Babu <80091550+sathish-progress@users.noreply.github.com>

--- lib/inspec/waiver_file_reader.rb
+++ lib/inspec/waiver_file_reader.rb
@@ -20,9 +20,12 @@ module Inspec
 
         if data.nil?
           raise Inspec::Exceptions::WaiversFileNotReadable,
-              "Cannot find parser for waivers file '#{file_path}'. " \
+              "Cannot find parser for waivers file." \
               "Check to make sure file has the appropriate extension."
         end
+      rescue Inspec::Exceptions::WaiversFileNotReadable, Inspec::Exceptions::WaiversFileInvalidFormatting => e
+        Inspec::Log.error "Error reading waivers file #{file_path}. #{e.message}"
+        Inspec::UI.new.exit(:usage_error)
       end
 
       @waivers_data[profile_id] = output
@@ -38,7 +41,7 @@ module Inspec
       elsif file_extension == ".csv"
         data = Waivers::CSVFileReader.resolve(file_path)
         headers = Waivers::CSVFileReader.headers
-        validate_headers(headers)
+        validate_csv_headers(headers)
       elsif file_extension == ".json"
         data = Waivers::JSONFileReader.resolve(file_path)
         validate_json_yaml(data) unless data.nil?
@@ -50,19 +53,52 @@ module Inspec
       %w{control_id justification expiration_date run}
     end
 
-    def self.validate_headers(headers, json_yaml = false)
+    def self.validate_csv_headers(headers)
+      invalid_headers_info = fetch_invalid_headers_info(headers)
+      # Warn if blank column found in csv file
+      Inspec::Log.warn "Invalid column headers: Column can't be nil" if invalid_headers_info[:blank_column]
+      # Warn if extra header found in csv file
+      Inspec::Log.warn "Extra header/s #{invalid_headers_info[:extra_headers]}" unless invalid_headers_info[:extra_headers].empty?
+      unless invalid_headers_info[:missing_required_fields].empty?
+        raise Inspec::Exceptions::WaiversFileInvalidFormatting,
+        "Missing required header/s #{invalid_headers_info[:missing_required_fields]}. Fix headers in file to proceed."
+      end
+    end
+
+    def self.fetch_invalid_headers_info(headers, json_yaml = false)
       required_fields = json_yaml ? %w{justification} : %w{control_id justification}
-      Inspec::Log.warn "Missing column headers: #{(required_fields - headers)}" unless (required_fields - headers).empty?
-      Inspec::Log.warn "Invalid column header: Column can't be nil" if headers.include? nil
-      Inspec::Log.warn "Extra column headers: #{(headers - all_fields)}" unless (headers - all_fields).empty?
+      data = {}
+      data[:missing_required_fields] = []
+      # Finds missing required fields
+      unless (required_fields - headers).empty?
+        data[:missing_required_fields] = required_fields - headers
+      end
+      # If column with no header found set the blank_column flag. Only applicable for csv
+      data[:blank_column] = headers.include?(nil) ? true : false
+      # Find extra headers/parameters
+      data[:extra_headers] = (headers - all_fields)
+      data
     end
 
     def self.validate_json_yaml(data)
-      headers = []
-      data.each_value do |value|
-        headers.push value.keys
+      missing_required_field = false
+      data.each do |key, value|
+        # In case of yaml or json we need to validate headers/parametes for each value
+        invalid_headers_info = fetch_invalid_headers_info(value.keys, true)
+        # WARN in case of extra parameters found in each waived control
+        Inspec::Log.warn "Control ID #{key}: extra parameter/s #{invalid_headers_info[:extra_headers]}" unless invalid_headers_info[:extra_headers].empty?
+        unless invalid_headers_info[:missing_required_fields].empty?
+          missing_required_field = true
+          # Log error for each waived control
+          Inspec::Log.error "Control ID #{key}: missing required parameter/s #{invalid_headers_info[:missing_required_fields]}"
+        end
       end
-      validate_headers(headers.flatten.uniq, true)
+
+      # Raise error if any of the waived control has missing required filed
+      if missing_required_field
+        raise Inspec::Exceptions::WaiversFileInvalidFormatting,
+             "Missing required parameter [justification]. Fix parameters in file to proceed."
+      end
     end
   end
 end
