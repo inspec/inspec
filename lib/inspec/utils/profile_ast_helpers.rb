a8ea715c08a73579ee99d03660fef9cddf665705
Clinton Wolfe <clintoncwolfe@gmail.com>
2023-09-20 10:52:34 -0400

    Rework per-control metadata collectors to be class-based
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- lib/inspec/utils/profile_ast_helpers.rb
+++ lib/inspec/utils/profile_ast_helpers.rb
@@ -3,6 +3,61 @@ require "rubocop-ast"
 module Inspec
   class Profile
     class AstHelper
+
+      class ImpactCollector
+        include Parser::AST::Processor::Mixin
+        include RuboCop::AST::Traversal
+
+        attr_reader :memo
+        def initialize(memo)
+          @memo = memo
+        end
+
+        def on_send(node)
+          if RuboCop::AST::NodePattern.new("(send nil? :impact ...)").match(node)
+            # TODO - any special processing for impact (float)
+            memo[:impact] = node.children[2].value
+          end
+        end
+      end
+
+      class DescCollector
+        include Parser::AST::Processor::Mixin
+        include RuboCop::AST::Traversal
+
+        attr_reader :memo
+        def initialize(memo)
+          @memo = memo
+        end
+
+        def on_send(node)
+          # TODO - description is also available as "description"
+          if RuboCop::AST::NodePattern.new("(send nil? :desc ...)").match(node)
+            # TODO - description may be read as a hash or a string
+            memo[:desc] = node.children[2].value
+          end
+        end
+      end
+
+      class TitleCollector
+        include Parser::AST::Processor::Mixin
+        include RuboCop::AST::Traversal
+
+        attr_reader :memo
+        def initialize(memo)
+          @memo = memo
+        end
+
+        def on_send(node)
+          if RuboCop::AST::NodePattern.new("(send nil? :title ...)").match(node)
+            # TODO - title may not be a simple string
+            memo[:title] = node.children[2].value
+          end
+        end
+      end
+
+
+
       class ControlIDCollector
         include Parser::AST::Processor::Mixin
         include RuboCop::AST::Traversal
@@ -17,29 +72,10 @@ module Inspec
 
         def on_block(block_node)
           if RuboCop::AST::NodePattern.new("(block (send nil? :control ...) ...)").match(block_node)
-
-            impact_value = 0.0 # Some controls don't have impacts
-            title_value = nil # Some controls don't have titles
-            desc_value = nil # Some controls don't have descriptions
-
             # NOTE: Assuming begin block is at the index 2
-            child = block_node.children[2]
-
-            child.children.each do |grand_child|
-              if impact_node = RuboCop::AST::NodePattern.new("$(send nil? :impact ...)").match(grand_child)
-                impact_value = impact_node.children[2].value
-              end
-
-              if title_node = RuboCop::AST::NodePattern.new("$(send nil? :title ...)").match(grand_child)
-                title_value = title_node.children[2].value
-              end
-
-              if desc_node = RuboCop::AST::NodePattern.new("$(send nil? :desc ...)").match(grand_child)
-                desc_value = desc_node.children[2].value
-              end
-            end
-
+            begin_block = block_node.children[2]
             control_node = block_node.children[0]
+            
             # TODO - This assumes the control ID is always a plain string, which we know it is often not!
             control_id = control_node.children[2].value
             # TODO - BUG - this keeps seeing the same nodes over and over againa, and so repeating control IDs. We are ignoring duplicate control IDs, which is incorrect.
@@ -47,14 +83,19 @@ module Inspec
 
             seen_control_ids[control_id] = true
 
-            # TODO - search down within block_node.children to find other per-control metadata like impact, tags, refs, desc
-
             control_data = {
-              id: control_id,
-              impact: impact_value,
-              desc: desc_value,
-              title: title_value
+              id: control_id
             }
+
+            # Scan the code block for per-control metadata
+            collectors = []
+            collectors.push ImpactCollector.new(control_data)
+            collectors.push DescCollector.new(control_data)
+            collectors.push TitleCollector.new(control_data)
+          
+            begin_block.each_node do |node_within_control| 
+              collectors.each { |collector| collector.process(node_within_control) }
+            end
 
             memo[:controls].push control_data
           end
