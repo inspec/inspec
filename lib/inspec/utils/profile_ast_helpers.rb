662af1464c2cafded3e04cb6ce5750fa189bc743
Clinton Wolfe <clintoncwolfe@gmail.com>
2023-09-20 02:45:09 -0400

    Modify to capture entire block
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- lib/inspec/utils/profile_ast_helpers.rb
+++ lib/inspec/utils/profile_ast_helpers.rb
@@ -14,28 +14,26 @@ module Inspec
           @seen_control_ids = {} 
         end
 
-        # See docs of Parser::AST::Processor::Mixin#process
-        # There will be a hook defined for each node type - on_send, on_sym, etc.
-        def on_send (node)
-          if RuboCop::AST::NodePattern.new("(send nil? :control ...)").match(node)
 
+        def on_block(block_node)
+          if RuboCop::AST::NodePattern.new("(block (send nil? :control ...) ...)").match(block_node)
+            control_node = block_node.children[0]
             # TODO - This assumes the control ID is always a plain string, which we know it is often not!
-            control_id = node.children[2].value
+            control_id = control_node.children[2].value
             # TODO - BUG - this keeps seeing the same nodes over and over againa, and so repeating control IDs. We are ignoring duplicate control IDs, which is incorrect.
             return if seen_control_ids[control_id]
 
             seen_control_ids[control_id] = true
 
-            # TODO - search down within node to find other per-control metadata like impact, tags, refs, desc
+            # TODO - search down within block_node.children to find other per-control metadata like impact, tags, refs, desc
 
             control_data = {
               id: control_id
             }
 
             memo[:controls].push control_data
-
           end
-        end
+        end        
       end
     end
   end
