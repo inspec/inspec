dbb7c2d25927ec5486e359dc1a13214cc78f22c2
poorndm <poorndm@progress.com>
2023-10-04 16:30:55 +0530

    Pin openssl version
    Signed-off-by: poorndm <poorndm@progress.com>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -3,9 +3,9 @@
 # grab the current train release from rubygems.org
 train_stable = /^train \((.*)\)/.match(`gem list ^train$ --remote`)[1]
 override "train", version: "v#{train_stable}"
-override "ruby", version: "3.1.2"
+override "ruby", version: "3.2.2"
 
 # Mac m1
-override "openssl", version: "1.1.1m" if mac_os_x?
-
+override "openssl", version: mac_os_x? ? "1.1.1w" : "1.0.2zi"
 override "ruby-msys2-devkit", version: "3.1.2-1"
+
