e81151e967014a7e78e657a521b6da95707b3636
Sonu Saha <sonu.saha@progress.com>
2023-10-10 19:24:13 +0530

    REVERT: UPGRADE: Upgrade ruby version from 3.1.2 to 3.1.4
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -3,9 +3,9 @@
 # grab the current train release from rubygems.org
 train_stable = /^train \((.*)\)/.match(`gem list ^train$ --remote`)[1]
 override "train", version: "v#{train_stable}"
-override "ruby", version: "3.1.4"
+override "ruby", version: "3.1.2"
 
 # Mac m1
 override "openssl", version: "1.1.1w" if mac_os_x?
 
-override "ruby-msys2-devkit", version: "3.1.4-1"
+override "ruby-msys2-devkit", version: "3.1.2-1"
