6ee21986bb4c198702c6929ecf6d1d5ccd3b19f5
Sonu Saha <98935583+ahasunos@users.noreply.github.com>
2023-10-11 18:37:52 +0530

    Backport-6410:  Dependent profiles fix for any scheme of version used in profiles (#6471)
    * CFINSPEC-580 Dependent profiles fix for any scheme of version used in profiles (#6410)
    
    * Dependent profiles fix for any scheme of version used in profiles
    
    Signed-off-by: Nikita Mathur <nikita.mathur@chef.io>
    
    * Dependent profile fix when version is blank in dependent profiles
    
    Signed-off-by: Nikita Mathur <nikita.mathur@chef.io>
    
    ---------
    
    Signed-off-by: Nikita Mathur <nikita.mathur@chef.io>
    
    * LINT: Fix lint offense
    
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>
    
    * Remove tests for enhanced outcomes and resource_ids
    
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>
    
    * Add missing changes for test fixtures
    
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>
    
    ---------
    
    Signed-off-by: Nikita Mathur <nikita.mathur@chef.io>
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>
    Co-authored-by: Nikita Mathur <Nik08@users.noreply.github.com>

--- test/functional/inspec_exec_test.rb
+++ test/functional/inspec_exec_test.rb
@@ -1304,15 +1304,53 @@ EOT
   end
 
   unless windows?
-    describe "when profiles are dependent on different versions of same profile - test in unix" do
-      let(:profile) { "#{profile_path}/git-fetcher/inheritance/parent-profile" }
-      let(:run_result) { run_inspec_process("exec #{profile}") }
-      it "should evaluate all test controls of all versions correctly" do
-        _(run_result.stderr).must_be_empty
-        _(run_result.stdout).must_include "2.7.0"
-        _(run_result.stdout).must_include "2.6.0"
-        _(run_result.stdout).must_include "sshd-01"
-        _(run_result.stdout).must_include "sshd-50"
+    unless windows?
+      describe "when profiles are dependent on different versions of same profile - test in unix - test in unix" do
+        let(:profile) { "#{profile_path}/git-fetcher/inheritance/parent-profile" }
+        let(:run_result) { run_inspec_process("exec #{profile}") }
+        it "should evaluate all test controls of all versions correctly" do
+          skip_windows!
+          _(run_result.stderr).must_be_empty
+          _(run_result.stdout).must_include "2.7.0"
+          _(run_result.stdout).must_include "2.6.0"
+          _(run_result.stdout).must_include "sshd-01"
+          _(run_result.stdout).must_include "sshd-50"
+          # Test with version which is not following semver scheme.
+          _(run_result.stdout).must_include "0.1.0 Release Inspec 5 NotASemverVersion 1"
+        end
+      end
+
+      describe "Using require control with version: when profiles are dependent on different versions of same profile" do
+        let(:profile) { "#{profile_path}/git-fetcher/inheritance/child-profile-3" }
+        let(:run_result) { run_inspec_process("exec #{profile}") }
+        it "should evaluate all test controls of all versions correctly" do
+          skip_windows!
+          _(run_result.stderr).must_be_empty
+          _(run_result.stdout).must_include "2.6.0"
+          _(run_result.stdout).must_include "sshd-01"
+        end
+      end
+
+      describe "When profiles are dependent on a profile with no version" do
+        let(:profile) { "#{profile_path}/git-fetcher/inheritance/child-profile-4" }
+        let(:run_result) { run_inspec_process("exec #{profile}") }
+        it "should evaluate all test controls of all versions correctly" do
+          skip_windows!
+          _(run_result.stderr).must_be_empty
+        end
+      end
+    end
+
+    if windows?
+      describe "when profiles are dependent on different versions of same profile - test in windows" do
+        let(:profile) { "#{profile_path}/git-fetcher/inheritance-windows/parent-profile" }
+        let(:run_result) { run_inspec_process("exec #{profile}") }
+        it "should evaluate all test controls of all versions correctly" do
+          _(run_result.stdout).must_include "1.1.2"
+          _(run_result.stdout).must_include "1.1.0"
+          # Test with version which is not following semver scheme.
+          _(run_result.stdout).must_include "0.1.0 Release Inspec 5 NotASemverVersion 1"
+        end
       end
     end
   end
