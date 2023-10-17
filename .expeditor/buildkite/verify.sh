679e831d742160b31a939ab5f6627e07a2780d06
Vasu1105 <vasundhara.jagdale@progress.com>
2023-10-17 13:44:58 +0530

    Revert "Configure SonarQube on inspec-4 branch"
    This reverts commit 30f12cc8cb92bbef7a72b24c4eba1ca1b80a4c01.

--- .expeditor/buildkite/verify.sh
+++ .expeditor/buildkite/verify.sh
@@ -20,18 +20,6 @@ mount
 df /tmp
 echo ${TMPDIR:-unknown}
 
-# Fetch tokens from vault ASAP so that long-running tests don't cause our vault token to expire
-echo "--- installing vault"
-export VAULT_VERSION=1.13.0
-export VAULT_HOME=$HOME/vault
-curl --create-dirs -sSLo $VAULT_HOME/vault.zip https://releases.hashicorp.com/vault/$VAULT_VERSION/vault_${VAULT_VERSION}_linux_amd64.zip
-unzip -o $VAULT_HOME/vault.zip -d $VAULT_HOME
-
-if [ -n "${CI_ENABLE_COVERAGE:-}" ]; then
-  echo "--- fetching Sonar token from vault"
-  export SONAR_TOKEN=$($VAULT_HOME/vault kv get -field token secret/inspec/sonar)
-fi
-
 echo "--- pull bundle cache"
 pull_bundle
 
@@ -49,29 +37,3 @@ echo "+++ bundle exec rake ${RAKE_TASK:-}"
 # su normal -c "bundle exec rake ${RAKE_TASK:-}"
 # shellcheck disable=SC2086
 bundle exec rake ${RAKE_TASK:-}
-RAKE_EXIT=$?
-
-# If coverage is enabled, then we need to pick up the coverage/coverage.json file
-if [ -n "${CI_ENABLE_COVERAGE:-}" ]; then
-  echo "--- installing sonarscanner"
-  export SONAR_SCANNER_VERSION=4.7.0.2747
-  export SONAR_SCANNER_HOME=$HOME/.sonar/sonar-scanner-$SONAR_SCANNER_VERSION-linux
-  curl --create-dirs -sSLo $HOME/.sonar/sonar-scanner.zip https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-$SONAR_SCANNER_VERSION-linux.zip
-  unzip -o $HOME/.sonar/sonar-scanner.zip -d $HOME/.sonar/
-  export PATH=$SONAR_SCANNER_HOME/bin:$PATH
-  export SONAR_SCANNER_OPTS="-server"
-
-  # Delete the vendor/ directory. I've tried to exclude it using sonar.exclusions,
-  # but that appears to get ignored, and we end up analyzing the gemfile install
-  # which blows our analysis.
-  echo "--- deleting installed gems"
-  rm -rf vendor/
-
-  # See sonar-project.properties for additional settings
-  echo "--- running sonarscanner"
-  sonar-scanner \
-  -Dsonar.sources=. \
-  -Dsonar.host.url=https://sonar.progress.com
-fi
-
-exit $RAKE_EXIT
