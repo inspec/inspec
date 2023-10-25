02af80866e7758c4f520d18d61423ab3688ff28c
Sonu Saha <sonu.saha@progress.com>
2023-10-25 16:37:06 +0530

    CHORE: Fix lint offense, TrailingWhitespace & EmptyLines
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -5,12 +5,11 @@ train_stable = /^train \((.*)\)/.match(`gem list ^train$ --remote`)[1]
 override "train", version: "v#{train_stable}"
 override "ruby", version: "3.1.2"
 
-
 # Mac Apple Silicon requires 1.1.1 series instead of 1.0.2 series
 if mac_os_x?
   override "openssl", version: "1.1.1w"
 else
-  # Hopefully temporary, in October 2023 the default is 1.0.2zg which 
+  # Hopefully temporary, in October 2023 the default is 1.0.2zg which
   # has an open high cve, while zi is available. Temporarily pin until
   # default in omnibus-software has no cves.
   override "openssl", version: "1.0.2zi"
