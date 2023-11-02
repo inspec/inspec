c5eaf5379b1350bacc57fbc184bd86f629080e8a
Clinton Wolfe <clintoncwolfe@gmail.com>
2023-11-02 02:28:13 -0400

    Pull release notes from v5 wiki page instead of main (#6840)
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- .expeditor/publish-release-notes.sh
+++ .expeditor/publish-release-notes.sh
@@ -6,11 +6,11 @@ git clone https://x-access-token:${GITHUB_TOKEN}@github.com/inspec/inspec.wiki.g
 
 pushd ./inspec.wiki
   # Publish release notes to S3
-  aws s3 cp Pending-Release-Notes.md "s3://chef-automate-artifacts/release-notes/${EXPEDITOR_PRODUCT_KEY}/${EXPEDITOR_VERSION}.md" --acl public-read --content-type "text/plain" --profile chef-cd
-  aws s3 cp Pending-Release-Notes.md "s3://chef-automate-artifacts/${EXPEDITOR_CHANNEL}/latest/${EXPEDITOR_PRODUCT_KEY}/release-notes.md" --acl public-read --content-type "text/plain" --profile chef-cd
+  aws s3 cp Pending-Release-Notes-v5.md "s3://chef-automate-artifacts/release-notes/${EXPEDITOR_PRODUCT_KEY}/${EXPEDITOR_VERSION}.md" --acl public-read --content-type "text/plain" --profile chef-cd
+  aws s3 cp Pending-Release-Notes-v5.md "s3://chef-automate-artifacts/${EXPEDITOR_CHANNEL}/latest/${EXPEDITOR_PRODUCT_KEY}/release-notes.md" --acl public-read --content-type "text/plain" --profile chef-cd
 
   # Reset "Pending Release Notes" wiki page
-  cat >./Pending-Release-Notes.md <<EOH
+  cat >./Pending-Release-Notes-v5.md <<EOH
 ## New Features
 -
 ## Improvements
