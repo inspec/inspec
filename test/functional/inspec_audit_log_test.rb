2a21b7569e897b5dddd7faec2210106e9d502295
Vasu1105 <vasundhara.jagdale@progress.com>
2023-11-07 18:01:21 +0530

    Updates functional test for audit logging feature
    Signed-off-by: Vasu1105 <vasundhara.jagdale@progress.com>

--- test/functional/inspec_audit_log_test.rb
+++ test/functional/inspec_audit_log_test.rb
@@ -1,40 +1,48 @@
 require "functional/helper"
 
-describe "inspec exec and inspec shell with audit logging on " do
+describe "inspec audit log feature" do
   parallelize_me!
 
   include FunctionalHelper
 
-  before do
-    delete_audit_log_file("#{Inspec.log_dir}/inspec-audit.log")
+  describe "when audit log is off" do
+    let(:audit_log_file) { "#{Inspec.log_dir}/inspec-audit.log" }
+
+    before do
+      delete_audit_log_file(audit_log_file.to_s)
+    end
+
+    it "should not create inspec-audit.log file in the default location when inpsec exec run" do
+      cli_args = " --audit-log-location #{audit_log_file}"
+      run_result = run_inspec_process("exec " + File.join(profile_path, "basic_profile") + " " + cli_args)
+      _(run_result.exit_status).must_equal 0
+      _(File.exist?(audit_log_file)).must_equal false
+    end
   end
 
-  it "should create inspec-audit.log file in the default location when inpsec exec run" do
-    run_result = run_inspec_process("exec " + File.join(profile_path, "basic_profile") + " ", env: { CHEF_PREVIEW_AUDIT_LOGGING: "1" })
-    _(run_result.stdout).must_include "1 successful control"
-    _(run_result.exit_status).must_equal 0
-    _(File.exist?("#{Inspec.log_dir}/inspec-audit.log")).must_equal true
-    delete_audit_log_file("#{Inspec.log_dir}/inspec-audit.log")
-  end
+  describe "When audit logging on" do
+    let(:audit_log_file) { "#{Inspec.log_dir}/inspec-audit.log" }
 
-  it "shoudl not create inspec-audit.log file if preview feature flag is not set" do
-    run_result = run_inspec_process("exec " + File.join(profile_path, "basic_profile"))
-    _(run_result.stdout).must_include "1 successful control"
-    _(run_result.exit_status).must_equal 0
-    _(File.exist?"#{Inspec.log_dir}/inspec-audit.log").must_equal false
-  end
+    before do
+      delete_audit_log_file(audit_log_file.to_s)
+    end
 
-  it "should create inspec-audit.log file in the default location" do
-    run_result = run_inspec_process("shell " + " ", env: { CHEF_PREVIEW_AUDIT_LOGGING: "1" })
-    _(run_result.exit_status).must_equal 0
-    _(File.exist?("#{Inspec.log_dir}/inspec-audit.log")).must_equal true
-    delete_audit_log_file("#{Inspec.log_dir}/inspec-audit.log")
-  end
+    it "should create inspec-audit.log file in the default location when inpsec exec run" do
+      cli_args = " --audit-log-location #{audit_log_file}"
+      run_result = run_inspec_process("exec " + File.join(profile_path, "basic_profile") + " " + cli_args, env: { CHEF_PREVIEW_AUDIT_LOGGING: "1" })
+      _(run_result.stdout).must_include "1 successful control"
+      _(run_result.exit_status).must_equal 0
+      _(File.exist?(audit_log_file)).must_equal true
+      delete_audit_log_file("#{Inspec.log_dir}/inspec-audit.log")
+    end
 
-  it "should create inspec-audit.log file in the default location" do
-    run_result = run_inspec_process("shell " + " ")
-    _(run_result.exit_status).must_equal 0
-    _(File.exist?("#{Inspec.log_dir}/inspec-audit.log")).must_equal false
+    it "should create inspec-audit.log file in the default location" do
+      cli_args = " --audit-log-location #{Inspec.log_dir}/inspec-audit.log"
+      run_result = run_inspec_process("shell " + " " + cli_args, env: { CHEF_PREVIEW_AUDIT_LOGGING: "1" })
+      _(run_result.exit_status).must_equal 0
+      _(File.exist?("#{Inspec.log_dir}/inspec-audit.log")).must_equal true
+      delete_audit_log_file("#{Inspec.log_dir}/inspec-audit.log")
+    end
   end
 
   def delete_audit_log_file(file_path)
