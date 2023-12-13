b79ad46e5a897ebaefc9c9aa7f2e77ffccdfea5f
Sonu Saha <98935583+ahasunos@users.noreply.github.com>
2023-12-13 18:38:52 +0530

    Fix examples for `file` resource provided in docs (#6899)
    * Update docs and introduce test for file content
    
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>
    
    * TEST: Fix broken test due to changes in fixture
    
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>
    
    ---------
    
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

--- test/unit/resources/postgres_hba_conf_test.rb
+++ test/unit/resources/postgres_hba_conf_test.rb
@@ -11,7 +11,7 @@ describe "Inspec::Resources::PGHbaConf" do
     it "Verify postgres_hba_conf filtering by `type`" do
       entries = resource.where { type == "local" }
       _(entries.database).must_include "all"
-      _(entries.auth_method).must_equal ["peer"]
+      _(entries.auth_method).must_equal %w{peer md5}
     end
     it "Verify postgres_hba_conf filtering by `database`" do
       entries = resource.where { database == "acme_test" }
@@ -29,12 +29,12 @@ describe "Inspec::Resources::PGHbaConf" do
       _(resource.type).must_include "hostssl"
     end
     it "parses the pg_hba.conf file correctly" do
-      _(resource.type).must_equal %w{local host host host host hostssl hostssl hostssl hostssl}
-      _(resource.database).must_equal %w{all acme_test_db acme_test_db acme_test acme_test acme_test_db acme_test_db acme_test acme_test}
-      _(resource.user).must_equal %w{all all all all all all all all all}
-      _(resource.address).must_equal ["", "::1/0", "127.0.0.1/0", "::1/0", "127.0.0.1/0", "::/0", "0.0.0.0/0", "::/0", "0.0.0.0/0"]
-      _(resource.auth_method).must_equal %w{peer md5 md5 md5 md5 cert cert cert cert}
-      _(resource.auth_params).must_equal ["", "", "", "", "", "clientcert=1 map=ssl-test", "clientcert=1 map=ssl-test", "clientcert=1 map=ssl-test", "clientcert=1 map=ssl-test"]
+      _(resource.type).must_equal %w{local local host host host host host host hostssl hostssl hostssl hostssl}
+      _(resource.database).must_equal %w{all all all all acme_test_db acme_test_db acme_test acme_test acme_test_db acme_test_db acme_test acme_test}
+      _(resource.user).must_equal %w{all all all all all all all all all all all all}
+      _(resource.address).must_equal ["", "", "::1/128", "127.0.0.1/32", "::1/0", "127.0.0.1/0", "::1/0", "127.0.0.1/0", "::/0", "0.0.0.0/0", "::/0", "0.0.0.0/0"]
+      _(resource.auth_method).must_equal %w{peer md5 md5 md5 md5 md5 md5 md5 cert cert cert cert}
+      _(resource.auth_params).must_equal ["", "", "", "", "", "", "", "", "clientcert=1 map=ssl-test", "clientcert=1 map=ssl-test", "clientcert=1 map=ssl-test", "clientcert=1 map=ssl-test"]
     end
   end
 end
