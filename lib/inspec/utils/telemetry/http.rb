7818f1f4deb705309fa1dccd35fb03aa66af2a54
Sathish <sbabu@progress.com>
2024-02-06 17:56:13 +0530

    make HTTP client post requests
    Signed-off-by: Sathish <sbabu@progress.com>

---
+++ lib/inspec/utils/telemetry/http.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+require_relative "base"
+require "faraday" unless defined?(Faraday)
+module Inspec
+  class Telemetry
+    class HTTP < Base
+      TELEMETRY_JOBS_PATH = "/jobs"
+      def run_ending(opts)
+        payload = super
+        response = connection.post(TELEMETRY_JOBS_PATH) do |req|
+          req.body = payload.to_json
+        end
+        response.success?
+      end
+
+      def connection
+        Faraday.new(url: ENV["TELEMETRY_URL"]) do |config|
+          config.request :json
+          config.response :json
+        end
+      end
+    end
+  end
+end
