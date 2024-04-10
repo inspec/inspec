502ef2537fffc399a746383593817cdd5d8c3628
Nikita Mathur <Nik08@users.noreply.github.com>
2024-04-10 17:09:20 +0530

    CHEF-4017 Telemetry job api updations (#6965)
    * Added initial changes to jobs api
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    * Feature flag changes for telemetry
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    * move base, debug and null to its own file structure
    
    Signed-off-by: Sathish <sbabu@progress.com>
    
    * make HTTP client post requests
    
    Signed-off-by: Sathish <sbabu@progress.com>
    
    * remove old logic
    
    Signed-off-by: Sathish <sbabu@progress.com>
    
    * make backend class as `HTTP`
    
    Signed-off-by: Sathish <sbabu@progress.com>
    
    * CHEF-7258 Fetch and use licensing information for telemetry (#6964)
    
    * Added method to fetch license ids for inspec
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    * Added free license check for performing telemetry api call
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    * move base, debug and null to its own file structure
    
    Signed-off-by: Sathish <sbabu@progress.com>
    
    * make HTTP client post requests
    
    Signed-off-by: Sathish <sbabu@progress.com>
    
    * remove old logic
    
    Signed-off-by: Sathish <sbabu@progress.com>
    
    * make backend class as `HTTP`
    
    Signed-off-by: Sathish <sbabu@progress.com>
    
    ---------
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    Signed-off-by: Sathish <sbabu@progress.com>
    Co-authored-by: Sathish <sbabu@progress.com>
    
    * Updated control tags and desc value to be used in jobs api
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    * Added checks for automate run context and free license check
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    * capture target mode and id
    
    Signed-off-by: Sathish <sbabu@progress.com>
    
    * profile doesn't need ID
    
    Signed-off-by: Sathish <sbabu@progress.com>
    
    * use run context to set environment data
    
    Signed-off-by: Sathish <sbabu@progress.com>
    
    * refactor `create_wrapper` to be localized
    
    Signed-off-by: Sathish <sbabu@progress.com>
    
    * change all timestamps to be UTC
    
    Signed-off-by: Sathish <sbabu@progress.com>
    
    * Null checks for response and corrected job api endpoint
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    * Fixed tag values to be sent as string in api call
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    * make version as float
    
    Signed-off-by: Sathish <sbabu@progress.com>
    
    * add platform name
    
    Signed-off-by: Sathish <sbabu@progress.com>
    
    * Added control result data in jobs api payload
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    * Debug logs added for telemetry call
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    * Removed unwanted telemetry debug class
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    * Payload fix to pass features data only on per control basis
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    * Added class function to list all invoked features by feature sub system
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    * Using feature system to get all invoked features list to be used in jobs api
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    * Unit tests cases updated and fixed
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    * License type check downcased
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    * Lint fix
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    * CHEF-7265 Telemetry opt-in for CINC users (#6966)
    
    * Enabled telemtry opt-in
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    * Removed old comments
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    * Unit test case added to validate the disabling telemetry behaviour for inspec user
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    ---------
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    ---------
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    Signed-off-by: Sathish <sbabu@progress.com>
    Co-authored-by: Sathish <sbabu@progress.com>

--- test/unit/utils/telemetry_test.rb
+++ test/unit/utils/telemetry_test.rb
@@ -23,7 +23,24 @@ describe "Telemetry" do
   let(:runner) { Inspec::Runner.new({ command_runner: :generic, reporter: [] }) }
   let(:repo_path) { File.expand_path("../../..", __dir__) }
   let(:mock_path) { File.join(repo_path, "test", "fixtures") }
+  let(:valid_list_licenses_api_response) { File.read("#{repo_path}/test/fixtures/valid_list_licenses_api_response.json") }
+  let(:valid_client_api_data) { File.read("#{repo_path}/test/fixtures/valid_client_api_data.json") }
   let(:profile_path) { File.join(mock_path, "profiles") }
+
+  before do
+    stub_request(:get, "#{ChefLicensing::Config.license_server_url}/v1/listLicenses")
+      .to_return(
+        body: valid_list_licenses_api_response,
+        headers: { content_type: "application/json" }
+      )
+
+    stub_request(:get, "#{ChefLicensing::Config.license_server_url}/v1/client")
+      .with(query: { licenseId: "free-42727540-ddc8-4d4b-0000-80662e03cd73-0000", entitlementId: ChefLicensing::Config.chef_entitlement_id })
+      .to_return(
+        body: valid_client_api_data ,
+        headers: { content_type: "application/json" }
+      )
+  end
 
   describe "when it runs with a nested profile" do
     let(:profile) { File.join(profile_path, "dependencies", "profile_a") }
@@ -36,10 +53,9 @@ describe "Telemetry" do
       _(tm.run_ending_payload).wont_be_empty
       _(tm.run_ending_payload).must_be_kind_of Hash
       _(tm.run_ending_payload[:source]).must_match(/^inspec:\d+\.\d+\.\d+$/)
-      _(tm.run_ending_payload[:customerId]).must_match(/^\S+$/) # TODO - Validate customer ID
-      _(tm.run_ending_payload[:correlationId]).must_match(REGEX[:uuid])
+      _(tm.run_ending_payload[:licenseIds]).wont_be_empty
       _(tm.run_ending_payload[:createdTimeUTC]).must_match(REGEX[:datetime])
-      _(tm.run_ending_payload[:type]).must_match(/^job|event$/)
+      _(tm.run_ending_payload[:type]).must_match(/^job$/)
     end
 
     it "sets the job fields" do
@@ -72,12 +88,31 @@ describe "Telemetry" do
         _(s[:name]).must_equal "inspec-control"
         _(s[:id]).must_match(REGEX[:sha256])
         _(s[:resources]).must_be_kind_of Array
+        _(s[:features]).wont_be_empty
+        _(s[:tags]).wont_be_empty
         s[:resources].each do |r|
           _(r[:type]).must_equal "inspec-resource"
           _(r[:name]).wont_be_empty
           _(r[:id]).must_match(REGEX[:sha256])
         end
       end
+    end
+  end
+
+  describe "when telemetry is run for Inspec" do
+    let(:profile) { File.join(profile_path, "dependencies", "profile_a") }
+    let(:tm) { Inspec::Telemetry::Mock.new }
+    let(:conf) { Inspec::Config.new({ "enable_telemetry" => false }) }
+    let(:runner) { Inspec::Runner.new({ command_runner: :generic, reporter: [], conf: conf }) }
+
+    it "Inspec user can not disable telemetry using --no-enable-telemetry option" do
+      Inspec::Telemetry.expects(:instance).returns(tm).at_least_once
+      runner.add_target(profile)
+      runner.run
+      # Inspec user attempted to disable telemetry
+      _(Inspec::Telemetry.telemetry_disabled?).must_equal true
+      # Returns HTTP backend meaning the telemetry call is not disabled and will make an API call
+      _(Inspec::Telemetry.determine_backend_class).must_equal Inspec::Telemetry::HTTP
     end
   end
 end
