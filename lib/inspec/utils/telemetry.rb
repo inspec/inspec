96b359b9d8807e6c4eb06577fec80166dea2f377
Nikita Mathur <Nik08@users.noreply.github.com>
2024-04-10 17:09:20 +0530

    CHEF-4017 Telemetry job api updations (#6965)
    * Added initial changes to jobs api
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    * Feature flag changes for telemetry
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    * move base, debug and null to its own file structure
    
    Signed-off-by: Sathish <sbabu@progress.com>
    
    * make HTTP client post requests
    
    Signed-off-by: Sathish <sbabu@progress.com>
    
    * remove old logic
    
    Signed-off-by: Sathish <sbabu@progress.com>
    
    * make backend class as `HTTP`
    
    Signed-off-by: Sathish <sbabu@progress.com>
    
    * CHEF-7258 Fetch and use licensing information for telemetry (#6964)
    
    * Added method to fetch license ids for inspec
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    * Added free license check for performing telemetry api call
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    * move base, debug and null to its own file structure
    
    Signed-off-by: Sathish <sbabu@progress.com>
    
    * make HTTP client post requests
    
    Signed-off-by: Sathish <sbabu@progress.com>
    
    * remove old logic
    
    Signed-off-by: Sathish <sbabu@progress.com>
    
    * make backend class as `HTTP`
    
    Signed-off-by: Sathish <sbabu@progress.com>
    
    ---------
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    Signed-off-by: Sathish <sbabu@progress.com>
    Co-authored-by: Sathish <sbabu@progress.com>
    
    * Updated control tags and desc value to be used in jobs api
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    * Added checks for automate run context and free license check
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    * capture target mode and id
    
    Signed-off-by: Sathish <sbabu@progress.com>
    
    * profile doesn't need ID
    
    Signed-off-by: Sathish <sbabu@progress.com>
    
    * use run context to set environment data
    
    Signed-off-by: Sathish <sbabu@progress.com>
    
    * refactor `create_wrapper` to be localized
    
    Signed-off-by: Sathish <sbabu@progress.com>
    
    * change all timestamps to be UTC
    
    Signed-off-by: Sathish <sbabu@progress.com>
    
    * Null checks for response and corrected job api endpoint
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    * Fixed tag values to be sent as string in api call
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    * make version as float
    
    Signed-off-by: Sathish <sbabu@progress.com>
    
    * add platform name
    
    Signed-off-by: Sathish <sbabu@progress.com>
    
    * Added control result data in jobs api payload
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    * Debug logs added for telemetry call
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    * Removed unwanted telemetry debug class
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    * Payload fix to pass features data only on per control basis
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    * Added class function to list all invoked features by feature sub system
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    * Using feature system to get all invoked features list to be used in jobs api
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    * Unit tests cases updated and fixed
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    * License type check downcased
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    * Lint fix
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    * CHEF-7265 Telemetry opt-in for CINC users (#6966)
    
    * Enabled telemtry opt-in
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    * Removed old comments
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    * Unit test case added to validate the disabling telemetry behaviour for inspec user
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    ---------
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    ---------
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    Signed-off-by: Sathish <sbabu@progress.com>
    Co-authored-by: Sathish <sbabu@progress.com>

--- lib/inspec/utils/telemetry.rb
+++ lib/inspec/utils/telemetry.rb
@@ -1,36 +1,54 @@
 require "time" unless defined?(Time.zone_offset)
-require_relative "../dist"
-require "securerandom" unless defined?(SecureRandom)
-require "digest" unless defined?(Digest)
+require "chef-licensing"
+require_relative "telemetry/null"
+require_relative "telemetry/http"
+require_relative "telemetry/run_context_probe"
 
 module Inspec
   class Telemetry
 
     @@instance = nil
+    @@config = nil
 
     def self.instance
       @@instance ||= determine_backend_class.new
     end
 
     def self.determine_backend_class
-      # Don't perform telemetry unless feature flag is enabled
-      return Inspec::Telemetry::Null unless ENV["CHEF_FEATURE_TELEMETRY"]
+      # Don't perform telemetry call if license is not a free license
+      return Inspec::Telemetry::Null unless license&.license_type&.downcase == "free"
 
-      # TODO: ask chef-telemetry gem if we should be enabled or not
+      if Inspec::Dist::EXEC_NAME == "inspec" && telemetry_disabled?
+        # Issue a warning if an InSpec user is explicitly trying to opt out of telemetry using cli option
+        Inspec::Log.warn "Telemetry opt-out is not permissible."
+      end
 
-      # TODO: implement online telemetry collection using chef-telemtry gem
+      # Telemetry opt-in/out enabled for other Inspec distros
+      return Inspec::Telemetry::Null if Inspec::Dist::EXEC_NAME != "inspec" && telemetry_disabled?
 
-      Inspec::Telemetry::Debug
+      # Don't perform telemetry action if running under Automate - Automate does LDC tracking for us
+      return Inspec::Telemetry::Null if Inspec::Telemetry::RunContextProbe.under_automate?
+
+      Inspec::Log.debug "Determined HTTP instance for telemetry"
+
+      Inspec::Telemetry::HTTP
+    end
+
+    def self.license
+      Inspec::Log.debug "Fetching license context for telemetry"
+      @license ||= ChefLicensing.license_context
     end
 
     ######
     # These class methods make it convenient to call from anywhere within the InSpec codebase.
     ######
     def self.run_starting(opts)
+      @@config ||= opts[:conf]
       instance.run_starting(opts)
     end
 
     def self.run_ending(opts)
+      @@config ||= opts[:conf]
       instance.run_ending(opts)
     end
 
@@ -38,147 +56,12 @@ module Inspec
       instance.note_feature_usage(feature_name)
     end
 
-    class Base
-      attr_accessor :scratch
-      attr_reader :session_id
-
-      def fetch_customer_id
-        # TODO: obtain customer ID from some mechanism
-        "unknown"
-      end
-
-      def fetch_session_id
-        @session_id ||= SecureRandom.uuid
-      end
-
-      def create_wrapper(payload_type)
-        {
-          version: "2.0",
-          createdTimeUTC: Time.now.getutc.iso8601, # like 3339 but always uses T
-          environment: "CLI", # TODO: Consider replacing this with run context probe
-          # licenseId # We will never have this
-          customerId: fetch_customer_id,
-          source: "#{Inspec::Dist::EXEC_NAME}:#{Inspec::VERSION}",
-          type: payload_type,
-          correlationId: fetch_session_id,
-        }
-      end
-
-      def note_feature_usage(feature_name)
-        @scratch ||= {}
-        @scratch[:features] ||= []
-        @scratch[:features] << feature_name
-      end
-
-      def run_starting(_opts)
-        @scratch ||= {}
-        @scratch[:features] ||= []
-        @scratch[:run_start_time] = Time.now # TODO: This is not used
-      end
-
-      def run_ending(opts)
-        note_per_run_features(opts)
-
-        payload = create_wrapper("job")
-
-        train_platform = opts[:runner].backend.backend.platform
-
-        payload[:jobs] = [{
-          type: "InSpec",
-
-          # Target platform info
-          environment: {
-            host: obscure(URI(opts[:runner].backend.backend.uri).host) || "unknown",
-            os: train_platform.name,
-            version: train_platform.release,
-            architecture: train_platform.arch || "",
-            id: train_platform.uuid,
-          },
-
-          runtime: Inspec::VERSION,
-          content: [],  # one content == one profile
-          steps: [],    # one step == one control
-          features: scratch[:features].dup, # TODO: proposed new field (promoted up from steps)
-        }]
-
-        opts[:run_data][:profiles].each do |profile|
-          payload[:jobs][0][:content] << {
-            name: obscure(profile[:name]),
-            version: profile[:version],
-            sha256: profile[:sha256],
-            maintainer: profile[:maintainer],
-            parentProfile: obscure(profile[:parent_profile]) || "",
-            # TODO: add profileContentId
-          }
-
-          profile[:controls].each do |control|
-            payload[:jobs][0][:steps] << {
-              id: obscure(control[:id]),
-              name: "inspec-control",
-              resources: [],
-              features: [],
-            }
-
-            control[:results]&.each do |resource_block|
-              payload[:jobs][0][:steps].last[:resources] << {
-                type: "inspec-resource",
-                name: resource_block[:resource_class],
-                id: obscure(resource_block[:resource_title].respond_to?(:resource_id) ? resource_block[:resource_title].resource_id : nil) || "unknown",
-              }
-            end
-
-            # Per-control features.
-            # Waivers
-            payload[:jobs][0][:steps].last[:features] << "waivers" unless control[:waiver_data].empty?
-          end
-        end
-
-        # Return payload object for testing
-        payload
-      end
-
-      # Hash text if non-nil
-      def obscure(cleartext)
-        return nil if cleartext.nil?
-        return nil if cleartext.empty?
-
-        Digest::SHA2.new(256).hexdigest(cleartext)
-      end
-
-      def note_per_run_features(opts)
-        note_reporters
-        note_gem_dependency_usage(opts)
-        # TODO: what other features should be observed?
-      end
-
-      def note_reporters
-        Inspec::Config.cached[:reporter]
-          .keys
-          .each { |name| Inspec::Telemetry.note_feature_usage("reporter:#{name}") }
-      end
-
-      def note_gem_dependency_usage(opts)
-        unless opts[:runner].target_profiles.map do |tp|
-          tp.metadata.gem_dependencies + \
-              tp.locked_dependencies.list.map { |_k, v| v.profile.metadata.gem_dependencies }.flatten
-        end.flatten.empty?
-          Inspec::Telemetry.note_feature_usage("gem-deps-in-profiles")
-        end
-      end
+    def self.config
+      @@config
     end
 
-    class Null < Base
-      def run_starting(_opts); end
-      def run_ending(_opts); end
+    def self.telemetry_disabled?
+      !config.telemetry_options["enable_telemetry"]
     end
-
-    class Debug < Base
-      def run_ending(opts)
-        # payload = super(opts)
-        # require "byebug"; byebug
-        # 1
-      end
-    end
-
   end
 end
