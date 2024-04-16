a6f426add3e3c783a6259f33c24c372fb01e8f84
Nik08 <nikita.mathur@progress.com>
2024-04-16 14:45:32 +0530

    Connection failure handling for telemetry http call
    Signed-off-by: Nik08 <nikita.mathur@progress.com>

--- lib/inspec/utils/telemetry/http.rb
+++ lib/inspec/utils/telemetry/http.rb
@@ -5,22 +5,25 @@ module Inspec
   class Telemetry
     class HTTP < Base
       TELEMETRY_JOBS_PATH = "v1/job"
+      TELEMETRY_URL = ENV["TELEMETRY_URL"] # TODO Set to production telemetry URL before release
       def run_ending(opts)
         payload = super
         response = connection.post(TELEMETRY_JOBS_PATH) do |req|
           req.body = payload.to_json
         end
         if response.success?
-          Inspec::Log.debug "HTTP connection with Telemetry Client successful"
+          Inspec::Log.debug "HTTP connection with Telemetry Client successful."
           Inspec::Log.debug "HTTP response from Telemetry Client -> #{response.to_hash}"
           true
         else
           false
         end
+      rescue Faraday::ConnectionFailed
+        Inspec::Log.debug "HTTP connection failure with telemetry url -> #{TELEMETRY_URL}"
       end
 
       def connection
-        Faraday.new(url: ENV["TELEMETRY_URL"]) do |config|
+        Faraday.new(url: TELEMETRY_URL) do |config|
           config.request :json
           config.response :json
         end
