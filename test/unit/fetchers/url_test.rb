084efb37f87f061c2a39bb30ebfa9faee64dbab7
Sonu Saha <sonu.saha@progress.com>
2024-04-26 12:53:10 +0530

    fix: update url fetcher to use ls-remote to make it work in non-git dirs
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

--- test/unit/fetchers/url_test.rb
+++ test/unit/fetchers/url_test.rb
@@ -18,6 +18,20 @@ describe Inspec::Fetcher::Url do
       m
     end
 
+    let(:git_remote_head_main) do
+      out = mock
+      out.stubs(:stdout).returns("ref: refs/heads/main\tHEAD\n")
+      out.stubs(:exitstatus).returns(0)
+      out.stubs(:stderr).returns("")
+      out.stubs(:error!).returns(false)
+      out.stubs(:run_command).returns(true)
+      out
+    end
+
+    def expect_git_remote_head_main(remote_url)
+      Mixlib::ShellOut.expects(:new).returns(git_remote_head_main)
+    end
+
     it "handles a http url" do
       url = "http://chef.io/some.tar.gz"
       res = Inspec::Fetcher::Url.resolve(url)
@@ -51,31 +65,25 @@ describe Inspec::Fetcher::Url do
     end
 
     %w{https://github.com/chef/inspec
-    https://github.com/chef/inspec.git
-    https://www.github.com/chef/inspec.git
-    http://github.com/chef/inspec
-    http://github.com/chef/inspec.git
-    http://www.github.com/chef/inspec.git}.each do |github|
-      it "resolves a github url #{github}" do
-        Inspec::Fetcher::Url.stub :default_ref, "main" do
-          res = Inspec::Fetcher::Url.resolve(github)
-          res.expects(:open).returns(mock_open)
-          res.stubs(:default_ref).returns("main") # Stub the default_ref method
-          _(res).wont_be_nil
-          _(res.resolved_source).must_equal({ url: "https://github.com/chef/inspec/archive/main.tar.gz", sha256: expected_shasum })
-        end
-      end
-    end
+       https://github.com/chef/inspec.git
+       https://www.github.com/chef/inspec.git
+       http://github.com/chef/inspec
+       http://github.com/chef/inspec.git
+       http://www.github.com/chef/inspec.git}.each do |github|
+         it "resolves a github url #{github}" do
+           res = Inspec::Fetcher::Url.resolve(github)
+           res.expects(:open).returns(mock_open)
+           _(res).wont_be_nil
+           _(res.resolved_source).must_equal({ url: "https://github.com/chef/inspec/archive/main.tar.gz", sha256: expected_shasum })
+         end
+       end
 
     it "resolves a github url with dot" do
-      Inspec::Fetcher::Url.stub :default_ref, "master" do
-        github = "https://github.com/mitre/aws-rds-oracle-mysql-ee-5.7-cis-baseline"
-        res = Inspec::Fetcher::Url.resolve(github)
-        res.expects(:open).returns(mock_open)
-        res.stubs(:default_ref).returns("master") # Stub the default_ref method
-        _(res).wont_be_nil
-        _(res.resolved_source).must_equal({ url: "https://github.com/mitre/aws-rds-oracle-mysql-ee-5.7-cis-baseline/archive/master.tar.gz", sha256: expected_shasum })
-      end
+      github = "https://github.com/mitre/aws-rds-oracle-mysql-ee-5.7-cis-baseline"
+      res = Inspec::Fetcher::Url.resolve(github)
+      res.expects(:open).returns(mock_open)
+      _(res).wont_be_nil
+      _(res.resolved_source).must_equal({ url: "https://github.com/mitre/aws-rds-oracle-mysql-ee-5.7-cis-baseline/archive/master.tar.gz", sha256: expected_shasum })
     end
 
     it "resolves a github branch url" do
@@ -102,13 +110,11 @@ describe Inspec::Fetcher::Url do
        http://bitbucket.org/chef/inspec.git
        http://www.bitbucket.org/chef/inspec.git}.each do |bitbucket|
          it "resolves a bitbucket url #{bitbucket}" do
-           Inspec::Fetcher::Url.stub :default_ref, "main" do
-             res = Inspec::Fetcher::Url.resolve(bitbucket)
-             res.expects(:open).returns(mock_open)
-             res.stubs(:default_ref).returns("main") # Stub the default_ref method
-             _(res).wont_be_nil
-             _(res.resolved_source).must_equal({ url: "https://bitbucket.org/chef/inspec/get/main.tar.gz", sha256: expected_shasum })
-           end
+           expect_git_remote_head_main(bitbucket)
+           res = Inspec::Fetcher::Url.resolve(bitbucket)
+           res.expects(:open).returns(mock_open)
+           _(res).wont_be_nil
+           _(res.resolved_source).must_equal({ url: "https://bitbucket.org/chef/inspec/get/main.tar.gz", sha256: expected_shasum })
          end
        end
 
@@ -127,6 +133,7 @@ describe Inspec::Fetcher::Url do
       _(res).wont_be_nil
       _(res.resolved_source).must_equal({ url: "https://bitbucket.org/chef/inspec/get/48bd4388ddffde68badd83aefa654e7af3231876.tar.gz", sha256: expected_shasum })
     end
+
   end
 
   describe "download_automate2_archive_to_temp" do
