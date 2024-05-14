e9540ef033ccbb7a50dab71dff074e07a47a8833
Nik08 <nikita.mathur@progress.com>
2024-05-14 16:31:17 +0530

    WIP unit test for profile context which differs based on ruby versions
    Signed-off-by: Nik08 <nikita.mathur@progress.com>

--- test/unit/profiles/profile_context_test.rb
+++ test/unit/profiles/profile_context_test.rb
@@ -8,6 +8,7 @@ class Module
 end
 
 RUBY3_1_PLUS = Gem.ruby_version >= Gem::Version.new("3.1")
+RUBY3_3_PLUS = Gem.ruby_version >= Gem::Version.new("3.3")
 
 module DescribeOneTest
   it "loads an empty describe.one" do
@@ -95,14 +96,22 @@ describe Inspec::ProfileContext do
     it "supports empty describe calls" do
       assert_load("describe", "")
       _(profile.rules.keys.length).must_equal 1
-      _(profile.rules.keys[0]).must_match(/^\(generated from \(eval at .+:\d+\):\d+ [0-9a-f]+\)$/)
+      if RUBY3_3_PLUS
+        _(profile.rules.keys[0]).must_match(/^\(generated from \(eval at .+:\d+\):\d+ [0-9a-f]+\)$/)
+      else
+        _(profile.rules.keys[0]).must_match(/^\(generated from \(eval\):1 [0-9a-f]+\)$/)
+      end
       _(profile.rules.values[0]).must_be_kind_of Inspec::Rule
     end
 
     it "provides the describe keyword in the global DSL" do
       assert_load("describe true do; it { should_eq true }; end", "")
       _(profile.rules.keys.length).must_equal 1
-      _(profile.rules.keys[0]).must_match(/^\(generated from profile_context\.rb:\d+\):\d+ [0-9a-f]+\)$/)
+      if RUBY3_3_PLUS
+        _(profile.rules.keys[0]).must_match(/^\(generated from profile_context\.rb:\d+\):\d+ [0-9a-f]+\)$/)
+      else
+        _(profile.rules.keys[0]).must_match(/^\(generated from \(eval\):1 [0-9a-f]+\)$/)
+      end
       _(profile.rules.values[0]).must_be_kind_of Inspec::Rule
     end
 
@@ -110,7 +119,11 @@ describe Inspec::ProfileContext do
       assert_load("%w{1 2 3}.each do\ndescribe true do; it { should_eq true }; end\nend", "")
       _(profile.rules.keys.length).must_equal 3
       [0, 1, 2].each do |i|
-        _(profile.rules.keys[i]).must_match(/^\(generated from profile_context\.rb:\d+\):\d+ [0-9a-f]+\)$/)
+        if RUBY3_3_PLUS
+          _(profile.rules.keys[i]).must_match(/^\(generated from profile_context\.rb:\d+\):\d+ [0-9a-f]+\)$/)
+        else
+          _(profile.rules.keys[i]).must_match(/^\(generated from \(eval\):2 [0-9a-f]+\)$/)
+        end
         _(profile.rules.values[i]).must_be_kind_of Inspec::Rule
       end
     end
