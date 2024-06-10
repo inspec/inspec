daf8580ce673fab2eb447e062c9ba234b14bf34b
Vasu1105 <vjagdale@progress.com>
2024-06-10 16:41:21 +0530

    Fix failing test after remmoval of support for ruby 3.0
    Signed-off-by: Vasu1105 <vjagdale@progress.com>

--- test/unit/dependency_loader_test.rb
+++ test/unit/dependency_loader_test.rb
@@ -11,7 +11,7 @@ describe "dependency_loader" do
 
   before(:each) do
     reset_globals
-    ENV["HOME"] = File.join(config_dir_path, "test-fixture-1-float/gems/2.7.0")
+    ENV["HOME"] = File.join(config_dir_path, "test-fixture-1-float/gems/3.1.0")
   end
 
   after(:each) do
