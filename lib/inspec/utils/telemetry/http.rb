321c56ee81f929df5fd74f1da271aca87ce702d2
Nikita Mathur <Nik08@users.noreply.github.com>
2024-06-12 17:47:36 +0530

    (Restoring) CHEF-10392 load default telemetry url conditionally (#7059)
    * load default telemetry url conditionally
    
    Signed-off-by: Sathish <sbabu@progress.com>
    
    * remove version base path
    version base path is defined in jobs path already
    
    Signed-off-by: Sathish <sbabu@progress.com>
    
    * use `CHEF_` prefix for the ENV
    
    Signed-off-by: Sathish <sbabu@progress.com>
    
    ---------
    
    Signed-off-by: Sathish <sbabu@progress.com>
    Co-authored-by: Sathish <sbabu@progress.com>

--- lib/inspec/utils/telemetry/http.rb
+++ lib/inspec/utils/telemetry/http.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 require_relative "base"
 require "faraday" unless defined?(Faraday)
+require "inspec/utils/licensing_config"
 module Inspec
   class Telemetry
     class HTTP < Base
       TELEMETRY_JOBS_PATH = "v1/job"
-      TELEMETRY_URL = ENV["TELEMETRY_URL"] # TODO Set to production telemetry URL before release
+      TELEMETRY_URL = if ChefLicensing::Config.license_server_url&.match?("acceptance")
+                        ENV["CHEF_TELEMETRY_URL"]
+                      else
+                        "https://services.chef.io/telemetry/"
+                      end
       def run_ending(opts)
         payload = super
         response = connection.post(TELEMETRY_JOBS_PATH) do |req|
