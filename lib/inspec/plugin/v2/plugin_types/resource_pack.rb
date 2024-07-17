658c813c126385d0ca46789ada6186441e14e7e9
Clinton Wolfe <clintoncwolfe@gmail.com>
2024-07-17 11:14:06 -0400

    resource pack plugin type defintion file
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

---
+++ lib/inspec/plugin/v2/plugin_types/resource_pack.rb
@@ -0,0 +1,8 @@
+module Inspec::Plugin::V2::PluginType
+  class ResourcePack < Inspec::Plugin::V2::PluginBase
+    register_plugin_type(:resource_pack)
+
+    # Any DSL definitions would go here
+
+  end
+end
