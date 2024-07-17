84b7d8f70475cad3062703f0779957e5f6d5aee8
Clinton Wolfe <clintoncwolfe@gmail.com>
2024-07-17 11:04:25 -0400

    Test fixture for resource pack plugin type - test properly fail
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

---
+++ test/fixtures/plugins/inspec-test-resources/README.md
@@ -0,0 +1,3 @@
+# inspec-test-resources
+
+Resource Pack plugin used to test resource_pack plugin type in test/functional/plugins_test.rb
--- test/functional/plugins_test.rb
+++ test/functional/plugins_test.rb
@@ -201,6 +201,27 @@ describe "Streaming-reporter plugin type support" do
 end
 
 #=========================================================================================#
+#                           Resource Pack Plugin Support
+#=========================================================================================#
+
+describe "Resource Pack Plugin support" do
+  include PluginFunctionalHelper
+
+  let(:resource_pack_plugin_path) { File.join(mock_path, "plugins", "inspec-test-resources", "lib", "inspec-test-resources.rb") }
+
+  describe "just loading the plugin" do
+    let(:fixture_path) { File.join(profile_path, "basic_profile") }
+    let(:run_result) { run_inspec_with_plugin("exec #{fixture_path}", plugin_path: resource_pack_plugin_path) }
+
+    it "runs the resource pack plugin type successfully" do
+      _(run_result.stderr).must_be_empty
+      _(run_result.exit_status).must_equal 0
+    end
+  end
+
+end
+
+#=========================================================================================#
 #                           DSL Plugin Support
 #=========================================================================================#
 
