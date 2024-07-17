aee76e7b83d8801dba7484b36d23e8b1bb533984
Clinton Wolfe <clintoncwolfe@gmail.com>
2024-07-17 11:04:25 -0400

    Test fixture for resource pack plugin type - test properly fail
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- test/fixtures/plugins/inspec-test-resources/README.md
+++ test/fixtures/plugins/inspec-test-resources/README.md
@@ -1,5 +1,3 @@
 # inspec-test-resources
 
 Resource Pack plugin used to test resource_pack plugin type in test/functional/plugins_test.rb
-
-Defines one resource, demo_resource.
--- test/functional/plugins_test.rb
+++ test/functional/plugins_test.rb
@@ -219,16 +219,6 @@ describe "Resource Pack Plugin support" do
     end
   end
 
-  describe "loading via profile gem dependency" do
-    let(:fixture_path) { File.join(profile_path, "dependencies", "uses-resource-pack-gem") }
-    let(:run_result) { run_inspec_process("exec #{fixture_path}", json: true) }
-
-    it "runs the resource pack plugin type successfully" do
-      _(run_result.stderr).must_be_empty
-      _(run_result.exit_status).must_equal 0
-      assert_json_controls_passing
-    end
-  end
 end
 
 #=========================================================================================#
