b37baf68d972359a0c807c708a7e52b26e48dbf7
Clinton Wolfe <clintoncwolfe@gmail.com>
2024-07-18 07:01:34 -0400

    Failing test for a gem profile dependency
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- test/functional/plugins_test.rb
+++ test/functional/plugins_test.rb
@@ -219,6 +219,15 @@ describe "Resource Pack Plugin support" do
     end
   end
 
+  describe "loading via profile gem dependency" do
+    let(:fixture_path) { File.join(profile_path, "dependencies", "uses-resource-pack-gem") }
+    let(:run_result) { run_inspec_with_plugin("exec #{fixture_path}", plugin_path: resource_pack_plugin_path, do_install: false) }
+
+    it "runs the resource pack plugin type successfully" do
+      _(run_result.stderr).must_be_empty
+      _(run_result.exit_status).must_equal 0
+    end
+  end
 end
 
 #=========================================================================================#
