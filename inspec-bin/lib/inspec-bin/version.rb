d641254dcbd1c267e4c1c59fb4dcbbed271c1630
Clinton Wolfe <clintoncwolfe@gmail.com>
2024-07-19 03:44:32 -0400

    Temporarily bump major version to 7 so we can pin against it. This change should be reverted and the bump performed by the Bump Major Version tag when the inspec-7 branch merges. (#7095)
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- inspec-bin/lib/inspec-bin/version.rb
+++ inspec-bin/lib/inspec-bin/version.rb
@@ -1,5 +1,5 @@
 # This file managed by automation - do not edit manually
 module InspecBin
   INSPECBIN_ROOT = File.expand_path("..", __dir__)
-  VERSION = "6.8.7".freeze
+  VERSION = "7.0.0".freeze
 end
