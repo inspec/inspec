ce258b051326d4782dc92efc6358dcad15879113
Clinton Wolfe <clintoncwolfe@gmail.com>
2024-07-20 09:00:06 -0400

    Linting fixes
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- test/fixtures/plugins/inspec-test-resources/lib/inspec-test-resources/resource_pack.rb
+++ test/fixtures/plugins/inspec-test-resources/lib/inspec-test-resources/resource_pack.rb
@@ -7,10 +7,8 @@ module InspecPlugins::TestResources
   # version 2 of the Plugins API.  The second says we are making a Resource
   # Pack plugin component, so please make available any DSL needed
   # for that.
-    class ResourcePack < Inspec.plugin(2, :resource_pack)
-      # TBD
-      # load_timing :early  <-- isn't that implicit in the rewuire statements
-      # train relationship declarations?  <-- that should be in the gemspec
-    end
+  class ResourcePack < Inspec.plugin(2, :resource_pack)
+    # load_timing :early  <-- isn't that implicit in the rewuire statements
+    # train relationship declarations?  <-- that should be in the gemspec
   end
 end
