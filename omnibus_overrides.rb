a9dbdda8a72dc38e76e7c0b48e84cf8bf871d895
Sonu Saha <sonu.saha@progress.com>
2024-10-01 14:08:07 +0530

    fix: set OpenSSL version to 1.1.1t for Windows systems
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -5,6 +5,11 @@ train_stable = /^train \((.*)\)/.match(`gem list ^train$ --remote`)[1]
 override "train", version: "v#{train_stable}"
 override "ruby", version: "3.1.6"
 
-override :openssl, version: "3.0.11"
+# Check the platform and use OpenSSL 1.1.1t for Windows, else use 3.0.11
+if Gem::Platform.local.os == 'mswin' || Gem::Platform.local.os == 'mingw'
+  override :openssl, version: "1.1.1t"
+else
+  override :openssl, version: "3.0.11"
+end
 
 override "ruby-msys2-devkit", version: "3.1.6-1"
