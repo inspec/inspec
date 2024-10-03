07364e34864fdf452fce413ab42fa7e96e20c70a
Nik08 <nikita.mathur@progress.com>
2024-10-03 18:56:49 +0530

    Added control summarisation implementation
    Signed-off-by: Nik08 <nikita.mathur@progress.com>

--- lib/inspec/utils/ai.rb
+++ lib/inspec/utils/ai.rb
@@ -1,34 +1,54 @@
 require "openai"
 
-# Integration goes here
 module Inspec
   class AI
+    PROMPT_TEMPLATE_CHAT = " For this requested question, Pretend as a professional Compliance and Chef Inspec Expert and respond only with Chef Inspec and Compliance related answers. Make sure the question is related to Chef Inspec or Compliance and politely refuse to respond when it is an unrelated question.".freeze
+    PROMPT_TEMPLATE_SUMMARY = " For this requested question, Pretend as a professional Compliance and Chef Inspec Expert and analyse this InSpec profile control result.".freeze
+
     def self.start
-      client = Inspec::OpenAIClient.new
-      # TODO: Add some user docs
-      puts "InspecAI  - type 'exit' to end the conversation"
+      client = OpenAIClient.new
+      puts "InspecAI - type 'exit' to end the conversation"
       conversation_history = []
-      prompt_template_chat = ' For this requested question, Pretend as a professional Compliance and Chef Inspec Expert and respond only with Chef Inspec and Complaince related answers. Make sure if the question is only related to Chef Inspec or Compliance and refuse politely to respond when it is an unrelated question'
 
       loop do
         print 'You: '
         user_input = $stdin.gets.chomp
 
-        # simple way to exit
-        # TODO: catch CTL+C
-        exit if user_input.downcase == 'exit'
+        # Exit on 'exit' command
+        break if user_input.downcase == 'exit'
 
-        # TODO: Find a way to send prompts
-        conversation_history << { role: 'user', content: user_input + prompt_template_chat }
+        conversation_history << { role: 'user', content: "#{user_input}#{PROMPT_TEMPLATE_CHAT}" }
         response = client.get_chat_completion(conversation_history)
 
         conversation_history << { role: 'assistant', content: response }
-
-        puts "\n"
-        puts "InspecAI: #{response}"
+        puts "\nInspecAI: #{response}\n"
       end
     rescue StandardError => e
       puts "Failed to get response from OpenAI: #{e.message}"
+    end
+
+    def self.summarise_control
+      client = OpenAIClient.new
+      latest_file_path = get_latest_file_path('inspec-ai-control-logs')
+      control_results = File.read(latest_file_path)
+
+      conversation_history = [
+        { role: 'user', content: "#{control_results}#{PROMPT_TEMPLATE_SUMMARY}" }
+      ]
+      response = client.get_chat_completion(conversation_history)
+
+      puts "\nInspecAI: #{response}\n"
+    rescue StandardError => e
+      puts "Failed to summarize control: #{e.message}"
+    end
+
+    private
+
+    def self.get_latest_file_path(directory)
+      latest_file = Dir.entries(directory)
+                       .select { |file| File.file?(File.join(directory, file)) }
+                       .max_by { |file| File.ctime(File.join(directory, file)) }
+      File.join(directory, latest_file)
     end
   end
 
@@ -41,13 +61,12 @@ module Inspec
       @client = OpenAI::Client.new(log_errors: true)
     end
 
-    # Method to get a chat completion response
     def get_chat_completion(messages, model: DEFAULT_MODEL, max_tokens: DEFAULT_MAX_TOKENS)
       response = @client.chat(
         parameters: {
-          model:,
-          messages:,
-          max_tokens:
+          model: model,
+          messages: messages,
+          max_tokens: max_tokens
         }
       )
       response.dig('choices', 0, 'message', 'content')&.strip
--- lib/plugins/inspec-ai/lib/inspec-ai/cli.rb
+++ lib/plugins/inspec-ai/lib/inspec-ai/cli.rb
@@ -1,18 +1,17 @@
+require "inspec/utils/ai.rb"
 module InspecPlugins::AI
-  
   class Cli < Inspec.plugin(2, :cli_command)
     
     subcommand_desc "ai SUBCOMMAND [options]", "InSpec AI"
 
     desc "assistant", "InSpec AI for help"
     def assistant
-      require "inspec/utils/ai.rb"
       Inspec::AI.start
     end
 
     desc "summarise", "InSpec AI helping in providing summarisation to control results"
     def summarise
-      # Implement control summarisation
+      Inspec::AI.summarise_control
     end
 
     desc "remediation", "InSpec AI helping in providing remeditation suggetion on control results"
