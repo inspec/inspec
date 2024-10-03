1f7d2578cd8621a8da8f8b4db86c8d1a0f05f184
Sathish <sbabu@progress.com>
2024-10-03 15:58:34 +0530

    print basic chat completion
    Signed-off-by: Sathish <sbabu@progress.com>

--- lib/inspec/utils/ai.rb
+++ lib/inspec/utils/ai.rb
@@ -9,11 +9,11 @@ module Inspec
       client = Inspec::OpenAIClient.new
 
       puts client.get_models
+      puts client.get_chat_completion([{ role: 'user', content: 'Hello!' }])
 
       # prompt = "Once upon a time in a land far away,"
       # completion = client.get_completion(prompt)
       # puts "Completion: #{completion}"
-
 
       # messages = [{ role: 'user', content: "Tell me a story about a dragon." }]
       # chat_completion = client.get_chat_completion(messages)
