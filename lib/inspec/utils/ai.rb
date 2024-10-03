b9f4474aebad4194a16e1ee8a453ef2407f1bc13
Sonu Saha <sonu.saha@progress.com>
2024-10-03 15:28:04 +0530

    get the client atleast able to return models
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

--- lib/inspec/utils/ai.rb
+++ lib/inspec/utils/ai.rb
@@ -6,15 +6,18 @@ module Inspec
   class AI
     def self.start
       # Feel free to change this
-      client = OpenAIClient.new
-      prompt = "Once upon a time in a land far away,"
-      completion = client.get_completion(prompt)
-      puts "Completion: #{completion}"
+      client = Inspec::OpenAIClient.new
+
+      puts client.get_models
+
+      # prompt = "Once upon a time in a land far away,"
+      # completion = client.get_completion(prompt)
+      # puts "Completion: #{completion}"
 
 
-      messages = [{ role: 'user', content: "Tell me a story about a dragon." }]
-      chat_completion = client.get_chat_completion(messages)
-      puts "Chat Completion: #{chat_completion}"
+      # messages = [{ role: 'user', content: "Tell me a story about a dragon." }]
+      # chat_completion = client.get_chat_completion(messages)
+      # puts "Chat Completion: #{chat_completion}"
     end
   end
 end
