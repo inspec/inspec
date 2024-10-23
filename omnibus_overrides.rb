db57cc8b8b59368cb752cfe6d4c0c2dcc58b0362
Sonu Saha <sosaha@progress.com>
2024-10-23 14:26:32 +0530

    test dev branch with openssl dir changes
    Signed-off-by: Sonu Saha <sosaha@progress.com>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -3,7 +3,7 @@
 # grab the current train release from rubygems.org
 train_stable = /^train \((.*)\)/.match(`gem list ^train$ --remote`)[1]
 override "train", version: "v#{train_stable}"
-override "ruby", version: "3.1.6", openssl_gem: "3.2.0"
+override "ruby", version: "3.1.6"
 
 override "openssl", version: "3.0.11"
 
