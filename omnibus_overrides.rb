b56689ea30611f3cb386e88a2112014c436eb1f9
Sonu Saha <sosaha@progress.com>
2024-10-24 11:44:03 +0530

    chore: remove with_openssl; it didnt help
    Signed-off-by: Sonu Saha <sosaha@progress.com>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -3,8 +3,8 @@
 # grab the current train release from rubygems.org
 train_stable = /^train \((.*)\)/.match(`gem list ^train$ --remote`)[1]
 override "train", version: "v#{train_stable}"
-override "ruby", version: "3.1.6", openssl_gem: "3.2.0"
+override "ruby", version: "3.1.6"
 
-override "openssl", version: "3.0.11"
+override :openssl, version: "3.0.11"
 
 override "ruby-msys2-devkit", version: "3.1.6-1"
