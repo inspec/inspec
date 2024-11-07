c336c60b9a9c36724389e1e112721f09e2eeb01f
Vasu1105 <vjagdale@progress.com>
2024-11-07 16:44:34 +0530

    Updated omnibus overrides file for ruby 3.1.6 upgrade openssl version 3.0.9
    Signed-off-by: Vasu1105 <vjagdale@progress.com>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -2,9 +2,20 @@
 
 # grab the current train release from rubygems.org
 train_stable = /^train \((.*)\)/.match(`gem list ^train$ --remote`)[1]
-override "train", version: "v#{train_stable}"
-override "ruby", version: "3.1.2"
+override 'train', version: "v#{train_stable}"
+override 'ruby', version: '3.1.6'
 
-override :openssl, version: "3.0.11"
+openssl_version_default =
+  if macos?
+    '1.1.1m'
+  else
+    '3.0.9'
+  end
 
-override "ruby-msys2-devkit", version: "3.1.2-1"
+# set OPENSSL_OVERRIDE variable to a different openssl version to test
+# builds for other versions in omnibus_software
+override 'openssl', version: ENV.fetch('OPENSSL_OVERRIDE', openssl_version_default)
+
+override 'ruby-msys2-devkit', version: '3.1.2-1'
+
+override 'ruby-msys2-devkit', version: ENV.fetch('MSYS_OVERRIDE', '3.1.6-1')
