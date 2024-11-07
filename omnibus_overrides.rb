a952e1ddfd417aadb54c34b5fe336c37e0490cac
Sonu Saha <ahasunos@gmail.com>
2024-11-07 12:38:49 +0530

    undo changes to openssl version
    Signed-off-by: Sonu Saha <ahasunos@gmail.com>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -5,7 +5,6 @@ train_stable = /^train \((.*)\)/.match(`gem list ^train$ --remote`)[1]
 override "train", version: "v#{train_stable}"
 override "ruby", version: "3.1.6"
 
-openssl_version = windows? ? "1.1.1w" : "3.0.11"
-override :openssl, version: openssl_version
+override :openssl, version:  "3.0.11"
 
 override "ruby-msys2-devkit", version: "3.1.6-1"
