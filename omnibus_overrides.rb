2883203498bf689607b62a1fe0189243860189c6
Sonu Saha <sonu.saha@progress.com>
2024-11-14 15:03:43 +0530

    test ruby 3.1.6
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -5,6 +5,6 @@ train_stable = /^train \((.*)\)/.match(`gem list ^train$ --remote`)[1]
 override "train", version: "v#{train_stable}"
 override "ruby", version: "3.1.6"
 
-override :openssl, version:  "3.0.11"
+override :openssl, version: "3.0.11"
 
 override "ruby-msys2-devkit", version: "3.1.6-1"
