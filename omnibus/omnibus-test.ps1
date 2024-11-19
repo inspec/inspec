e0a43cec1470a2121a1a6b6fbed8244fffb2cde4
poorndm <poorndm@progress.com>
2024-11-19 13:22:12 +0530

    add winwods-2019 as builder
    Signed-off-by: poorndm <poorndm@progress.com>

--- omnibus/omnibus-test.ps1
+++ omnibus/omnibus-test.ps1
@@ -1,7 +1,7 @@
 # Stop script execution when a non-terminating error occurs
 $ErrorActionPreference = "Stop"
 
-# reload Env:PATH to ensure it gets any changes that the install made (e.g. C:\opscode\inspec\bin\ )
+# Reload Env:PATH to ensure it gets any changes that the install made (e.g. C:\opscode\inspec\bin\ )
 $Env:PATH = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
 
 $Env:Path = "C:\opscode\inspec\bin;C:\opscode\inspec\embedded\bin;$Env:PATH"
@@ -10,17 +10,21 @@ Write-Host "+++ Testing $Plan"
 # *********Debugging ***************
 Write-Host ":ruby: Validating Ruby can run"
 & $embedded_bin_dir\ruby.exe --version
-If ($lastexitcode -ne 0) { Throw $lastexit }
+if ($LASTEXITCODE -ne 0) { Throw "Ruby validation failed with exit code $LASTEXITCODE" }
 
 Write-Host ":bundler: Validating Bundler can run"
 & $embedded_bin_dir\bundle.bat --version
-If ($lastexitcode -ne 0) { Throw $lastexitcode }
+if ($LASTEXITCODE -ne 0) { Throw "Bundler validation failed with exit code $LASTEXITCODE" }
 
 Write-Host ":lock: Validating OpenSSL can run"
-$version=(&"$embedded_bin_dir\openssl.exe" version)
-If ($lastexitcode -ne 0) { Throw $lastexitcode }
+$version = (&"$embedded_bin_dir\openssl.exe" version)
+if ($LASTEXITCODE -ne 0) { Throw "OpenSSL validation failed with exit code $LASTEXITCODE" }
 
-
-Set-Location test/artifact
-rake
-If ($lastexitcode -ne 0) { Throw $lastexitcode }
+# Check if directory exists before changing location
+if (Test-Path test/artifact) {
+    Set-Location test/artifact
+    rake
+    if ($LASTEXITCODE -ne 0) { Throw "Rake command failed with exit code $LASTEXITCODE" }
+} else {
+    Write-Host "Directory test/artifact does not exist."
+}
