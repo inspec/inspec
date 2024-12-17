c07200eafa38161f35ff743a97b7a7bfdf42822b
Nik08 <nikita.mathur@progress.com>
2024-12-17 17:00:05 +0530

    Implemented git fetcher logic to fetch multiple versions of resource packs
    Signed-off-by: Nik08 <nikita.mathur@progress.com>

--- lib/inspec/plugin/v2/installer.rb
+++ lib/inspec/plugin/v2/installer.rb
@@ -217,7 +217,10 @@ module Inspec::Plugin::V2
         if opts.key?(:version) && plugin_version_installed?(plugin_name, opts[:version])
           raise InstallError, "#{plugin_name} version #{opts[:version]} is already installed."
         else
-          raise InstallError, "#{plugin_name} is already installed. Use 'inspec plugin update' to change version."
+          # Do not redirect to plugin update when using resource packs plugin
+          unless plugin_name =~ /^(inspec)-/ && plugin_name =~ /-resources/
+            raise InstallError, "#{plugin_name} is already installed. Use 'inspec plugin update' to change version."
+          end
         end
       end
 
@@ -340,15 +343,21 @@ module Inspec::Plugin::V2
         raise ex
       end
 
+      it_is_resource_pack_gem = new_plugin_dependency.name =~ /^(inspec)-/ && new_plugin_dependency.name =~ /-resources/
+
       # Activate all current plugins before trying to activate the new one
       loader.list_managed_gems.each do |spec|
-        next if spec.name == new_plugin_dependency.name && update_mode
+        # Skip in case of update mode
+        # Skip in case using a resource pack plugin
+        next if spec.name == new_plugin_dependency.name && (update_mode || it_is_resource_pack_gem)
 
         spec.activate
       end
 
       # Make sure we remove any previously loaded gem on update
-      Gem.loaded_specs.delete(new_plugin_dependency.name) if update_mode
+      # Make sure we remove any previously loaded gem when trying to use resource pack gem
+      # Resource pack gems when updated need to deactivate older version of gem
+      Gem.loaded_specs.delete(new_plugin_dependency.name) if update_mode || it_is_resource_pack_gem
 
       # Test activating the solution. This makes sure we do not try to load two different versions
       # of the same gem on the stack or a malformed dependency.
