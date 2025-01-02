1bbf6293322f737af6e1b1770d736675612e2d3b
Sonu Saha <sonu.saha@progress.com>
2025-01-02 12:56:52 +0530

    chore: update profile fixture for deprecated core resources
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

--- test/fixtures/profiles/deprecated-core-resources/controls/example.rb
+++ test/fixtures/profiles/deprecated-core-resources/controls/example.rb
@@ -11,13 +11,3 @@ control "mongodb_conf" do
     its(%w{storage dbPath}) { should eq "/var/lib/mongodb" }
   end
 end
-
-control "rabbitmq config test" do
-  impact 1.0
-  title "RabbitMQ Config Test"
-  desc "Verifies the configuration of RabbitMQ"
-
-  describe rabbitmq_config("./test/fixtures/files/rabbitmq.config").params("rabbit", "ssl_listeners") do
-    it { should eq [5671] }
-  end
-end
