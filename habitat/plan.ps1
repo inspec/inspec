c1263d6331767b6a27155d62e08fe17ab0773bda
Nikita Mathur <Nik08@users.noreply.github.com>
2025-01-07 17:37:00 +0530

    Plan file changes to fix Hab build fix for LTS channel (#7257)
    * Exporting hab channel for testing in plan file
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    * Revert "Exporting hab channel for testing in plan file"
    
    This reverts commit e43edec223c9e1dd036c6043d88d51c7b01e806b.
    
    * Attempt #1 to fix hab pipeline using lts channel for linux
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    * Attempt to fix hab pipeline using lts channel for windows
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    ---------
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>

--- habitat/plan.ps1
+++ habitat/plan.ps1
@@ -1,7 +1,6 @@
 # https://stackoverflow.com/questions/9948517
 # TODO: Set-StrictMode -Version Latest
-$env:HAB_BLDR_CHANNEL = "base-2025"
-$env:HAB_REFRESH_CHANNEL = "base-2025"
+$env:HAB_BLDR_CHANNEL = "LTS-2024"
 $ErrorActionPreference = "Stop"
 $PSDefaultParameterValues['*:ErrorAction']='Stop'
 
