21dc0a73b1bdbf8046e99f34a7b2420ac78a4cdc
Sonu Saha <98935583+ahasunos@users.noreply.github.com>
2025-01-07 14:26:54 +0530

    CHEF-16823: Introduce deprecation warning for core resources moving to resource packs in InSpec 7 (#7219)
    * chore: add deprecation message for core resource that will be moved as resource pack
    
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>
    
    * chore: invoke deprecation for docker resources
    
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>
    
    * chore: invoke deprecation for mongodb resources
    
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>
    
    * chore: invoke deprecation for rabbitmq resources
    
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>
    
    * chore: invoke deprecation for ibmdb2 resources
    
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>
    
    * chore: invoke deprecation for sybase resources
    
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>
    
    * chore: expect deprecation for docker resources
    
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>
    
    * chore: expect deprecation for mongodb resources
    
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>
    
    * chore: expect deprecation for rabbitmq resources
    
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>
    
    * chore: expect deprecation for ibmdb2 resources
    
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>
    
    * chore: update keyword to display to change notice for core resources moving to resource pack
    
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>
    
    * chore: add the common message to be displayed as part of the warning message
    
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>
    
    * chore: update warning message for the core resources moving to resource pack
    
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>
    
    * chore: add warning message for elasticsearch resource as it will be moved to resource pack in InSpec 7
    
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>
    
    * chore: add warning message for podman resources as it will be moved to resource pack in InSpec 7
    
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>
    
    * chore: add warning message for ssh resources as it will be moved to resource pack in InSpec 7
    
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>
    
    * spec: update test for ignoring change notice warning
    
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>
    
    * refactor: invoke warning during loading of the resource
    
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>
    
    * revert: undo changes to unit test files of core resources
    
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>
    
    * spec: add functional test to verify warning message and successful run
    
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>
    
    * chore: add fixture profile for deprecated resource
    
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>
    
    * chore: update profile fixture for deprecated core resources
    
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>
    
    * chore: minor cleanup and doc change
    
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>
    
    ---------
    
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

---
+++ lib/inspec/utils/deprecated_core_resources_list.rb
@@ -0,0 +1,25 @@
+module DeprecatedCoreResourcesList
+  CORE_RESOURCES_DEPRECATED = %i{
+    docker_container
+    docker_image
+    docker_plugin
+    docker_service
+    elasticsearch
+    ibmdb2_conf
+    ibmdb2_session
+    mongodb
+    mongodb_conf
+    mongodb_session
+    podman
+    podman_container
+    podman_image
+    podman_network
+    podman_pod
+    podman_volume
+    rabbitmq_config
+    ssh_config
+    ssh_key
+    sybase_conf
+    sybase_session
+  }.freeze
+end
