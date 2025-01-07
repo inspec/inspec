c0c6a461726eb5dff9550725bf1315237dbc0a3c
Nik08 <nikita.mathur@progress.com>
2025-01-07 15:18:22 +0530

    Revert "Exporting hab channel for testing in plan file"
    This reverts commit e43edec223c9e1dd036c6043d88d51c7b01e806b.

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -22,8 +22,6 @@ pkg_build_deps=(
 pkg_bin_dirs=(bin)
 
 do_setup_environment() {
-  build_line 'Setting HAB_LTS_CHANNEL for testing'
-  export HAB_BLDR_CHANNEL="LTS-2024"
   build_line 'Setting GEM_HOME="$pkg_prefix/lib"'
   export GEM_HOME="$pkg_prefix/lib"
 
