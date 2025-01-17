6e95e744fc9a2820778f57d8e93db4918206d49b
Nikita Mathur <Nik08@users.noreply.github.com>
2025-01-17 14:33:10 +0530

    CHEF-18598 Update Hab packages to use refresh channel (#7265)
    * Testing HAB_REFRESH_CHANNEL change for linux
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    * Testing windows artifacts with HAB_REFRESH_CHANNEL
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    * Update plan files with HAB_REFRESH_CHANNEL for lts hab pkgs
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    ---------
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>

--- .expeditor/buildkite/artifact.habitat.test.ps1
+++ .expeditor/buildkite/artifact.habitat.test.ps1
@@ -8,6 +8,7 @@ $ErrorActionPreference = 'Stop'
 $env:CHEF_LICENSE = 'accept-no-persist'
 $env:HAB_LICENSE = 'accept-no-persist'
 $env:HAB_BLDR_CHANNEL = 'LTS-2024'
+$env:HAB_REFRESH_CHANNEL = 'LTS-2024'
 $Plan = 'inspec'
 
 Write-Host "--- system details"
