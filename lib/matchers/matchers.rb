b3bc75733018dbb99aabb30b8d2582abe698b1dd
Aaron Lippold <lippold@gmail.com>
2025-01-17 20:05:06 -0500

    ran budle exec rake test:lint:auto_correct
    Signed-off-by: Aaron Lippold <lippold@gmail.com>

--- lib/matchers/matchers.rb
+++ lib/matchers/matchers.rb
@@ -1,6 +1,6 @@
 # copyright: 2015, Vulcano Security GmbH
 
-require 'rspec/matchers'
+require "rspec/matchers"
 
 RSpec::Matchers.define :be_readable do
   match do |file|
@@ -16,7 +16,7 @@ RSpec::Matchers.define :be_readable do
   end
 
   description do
-    res = 'be readable'
+    res = "be readable"
     res += " by #{@by}" unless @by.nil?
     res += " by user #{@by_user}" unless @by_user.nil?
     res
@@ -37,7 +37,7 @@ RSpec::Matchers.define :be_writable do
   end
 
   description do
-    res = 'be writable'
+    res = "be writable"
     res += " by #{@by}" unless @by.nil?
     res += " by user #{@by_user}" unless @by_user.nil?
     res
@@ -58,7 +58,7 @@ RSpec::Matchers.define :be_executable do
   end
 
   description do
-    res = 'be executable'
+    res = "be executable"
     res += " by #{@by}" unless @by.nil?
     res += " by user #{@by_user}" unless @by_user.nil?
     res
@@ -94,7 +94,7 @@ RSpec::Matchers.define :be_enabled do
   end
 
   chain :with_level do |_level|
-    raise '[UNSUPPORTED] with level is not supported'
+    raise "[UNSUPPORTED] with level is not supported"
   end
 
   failure_message do |service|
@@ -110,7 +110,7 @@ RSpec::Matchers.define :be_running do
   end
 
   chain :under do |_under|
-    raise '[UNSUPPORTED] under is not supported'
+    raise "[UNSUPPORTED] under is not supported"
   end
 
   failure_message do |service|
@@ -125,7 +125,7 @@ RSpec::Matchers.define :be_reachable do
   end
 
   chain :with do |_attr|
-    raise '[UNSUPPORTED] `with` is not supported in combination with `be_reachable`'
+    raise "[UNSUPPORTED] `with` is not supported in combination with `be_reachable`"
   end
 
   failure_message do |host|
@@ -231,8 +231,8 @@ RSpec::Matchers.define :cmp do |first_expected| # rubocop:disable Metrics/BlockL
   end
 
   def boolean?(value)
-    if value.respond_to?('downcase')
-      %w[true false].include?(value.downcase)
+    if value.respond_to?("downcase")
+      %w{true false}.include?(value.downcase)
     else
       value.is_a?(TrueClass) || value.is_a?(FalseClass)
     end
@@ -247,7 +247,7 @@ RSpec::Matchers.define :cmp do |first_expected| # rubocop:disable Metrics/BlockL
 
   # expects that the values have been checked with boolean?
   def to_boolean(value)
-    value.casecmp('true') == 0
+    value.casecmp("true") == 0
   end
 
   def try_match(actual, op, expected) # rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Metrics/AbcSize
@@ -262,7 +262,7 @@ RSpec::Matchers.define :cmp do |first_expected| # rubocop:disable Metrics/BlockL
       return actual.send(op, expected.to_i)
     elsif expected.is_a?(String) && boolean?(expected) && [true, false].include?(actual)
       return actual.send(op, to_boolean(expected))
-    elsif boolean?(expected) && %w[true false].include?(actual)
+    elsif boolean?(expected) && %w{true false}.include?(actual)
       return actual.send(op, expected.to_s)
     elsif expected.is_a?(Integer) && actual.is_a?(String) && integer?(actual)
       return actual.to_i.send(op, expected)
@@ -291,7 +291,7 @@ RSpec::Matchers.define :cmp do |first_expected| # rubocop:disable Metrics/BlockL
     try_match(actual, @operation, @expected)
   end
 
-  %i[== != < <= >= > === =~].each do |op|
+  %i{== != < <= >= > === =~}.each do |op|
     chain(op) do |x|
       @operation = op
       @expected = x
@@ -299,15 +299,15 @@ RSpec::Matchers.define :cmp do |first_expected| # rubocop:disable Metrics/BlockL
   end
 
   def format_actual(actual, negate = false)
-    actual = '0%o' % actual if octal?(@expected) && !actual.nil?
+    actual = "0%o" % actual if octal?(@expected) && !actual.nil?
     format("\n%s\n     got: %s\n\n(compared using `cmp` matcher)\n", format_expectation(negate), actual)
   end
 
   def format_expectation(negate)
-    return format('expected: %s', @expected) if @operation == :== && !negate
+    return format("expected: %s", @expected) if @operation == :== && !negate
 
-    negate_str = negate ? 'not ' : ''
-    format('expected it %sto be %s %p', negate_str, @operation, @expected)
+    negate_str = negate ? "not " : ""
+    format("expected it %sto be %s %p", negate_str, @operation, @expected)
   end
 
   failure_message do |actual|
@@ -319,7 +319,7 @@ RSpec::Matchers.define :cmp do |first_expected| # rubocop:disable Metrics/BlockL
   end
 
   description do
-    format('cmp %s %p', @operation, @expected)
+    format("cmp %s %p", @operation, @expected)
   end
 end
 
