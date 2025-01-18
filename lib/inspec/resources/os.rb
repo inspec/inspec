b0a7330cafe4775d10bb74da2fdc889799dbe846
Aaron Lippold <lippold@gmail.com>
2025-01-17 19:52:26 -0500

    updated the cmp matcher to handle multiple percision floats and update the os resource in the same spirit but just use semver
    Signed-off-by: Aaron Lippold <lippold@gmail.com>

--- lib/inspec/resources/os.rb
+++ lib/inspec/resources/os.rb
@@ -1,11 +1,11 @@
-require "inspec/resources/platform"
+require 'inspec/resources/platform'
 
 module Inspec::Resources
   class OSResource < PlatformResource
-    name "os"
-    supports platform: "unix"
-    supports platform: "windows"
-    desc "Use the os InSpec audit resource to test the platform on which the system is running."
+    name 'os'
+    supports platform: 'unix'
+    supports platform: 'windows'
+    desc 'Use the os InSpec audit resource to test the platform on which the system is running.'
     example <<~EXAMPLE
       describe os[:family] do
         it { should eq 'redhat' }
@@ -21,22 +21,23 @@ module Inspec::Resources
     EXAMPLE
 
     # reuse helper methods from backend
-    %w{aix? redhat? debian? suse? bsd? solaris? linux? unix? windows? hpux? darwin?}.each do |os_family|
+    %w[aix? redhat? debian? suse? bsd? solaris? linux? unix? windows? hpux? darwin?].each do |os_family|
       define_method(os_family.to_sym) do
         @platform.send(os_family)
       end
     end
 
+    # updated to support multiple precision float and semver release numbers
     def release
       Gem::Version.new(@platform.release).to_s
     end
 
     def resource_id
-      @platform.name || "OS"
+      @platform.name || 'OS'
     end
 
     def to_s
-      "Operating System Detection"
+      'Operating System Detection'
     end
   end
 end
