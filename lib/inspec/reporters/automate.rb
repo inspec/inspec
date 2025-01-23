388c67023d6df4de8c17c08f5efc9f0719ab5bc5
Nikita Mathur <Nik08@users.noreply.github.com>
2025-01-23 16:31:01 +0530

    Revert "CHEF-18596 Fix code scanning alert no. 62: Request without certificate validation" (#7280)
    Signed-off-by: Nik08 <nikita.mathur@progress.com>

--- lib/inspec/reporters/automate.rb
+++ lib/inspec/reporters/automate.rb
@@ -41,7 +41,11 @@ module Inspec::Reporters
         Inspec::Log.debug "Posting report to Chef Automate: #{uri.path}"
         http = Net::HTTP.new(uri.hostname, uri.port)
         http.use_ssl = uri.scheme == "https"
-        http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+        if @config["verify_ssl"] == true
+          http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+        else
+          http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        end
 
         res = http.request(req)
         if res.is_a?(Net::HTTPSuccess)
