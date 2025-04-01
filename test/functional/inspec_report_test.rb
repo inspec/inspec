f1613a15775e217c02676c0f1074f6d8ece1612a
Nik08 <nikita.mathur@progress.com>
2025-04-01 16:57:19 +0530

    Test fix since old format of hashes not supported in Ruby 3.4
    Signed-off-by: Nik08 <nikita.mathur@progress.com>

--- test/functional/inspec_report_test.rb
+++ test/functional/inspec_report_test.rb
@@ -14,8 +14,8 @@ describe "inspec report tests" do
       runner.add_target(example_profile)
       runner.run
       _(runner.report.count).must_equal 4
-      _(runner.report.inspect).must_include ':title=>"InSpec Example Profile"'
-      _(runner.report.inspect).must_include ':status=>"passed"'
+      _(runner.report.inspect).must_include 'title: "InSpec Example Profile"'
+      _(runner.report.inspect).must_include 'status: "passed"'
     end
 
     it "loads a json report twice" do
@@ -26,8 +26,8 @@ describe "inspec report tests" do
       runner.add_target(example_profile)
       runner.run
       _(runner.report.count).must_equal 4
-      _(runner.report.inspect).must_include ':title=>"InSpec Example Profile"'
-      _(runner.report.inspect).must_include ':status=>"passed"'
+      _(runner.report.inspect).must_include 'title: "InSpec Example Profile"'
+      _(runner.report.inspect).must_include 'status: "passed"'
     end
 
     # Due to the way we require/use rspec, you can only run one runner.
