45a9a6865fb35e441eaafa05a9f9d47a0b9dbfc3
Nik08 <nikita.mathur@progress.com>
2025-05-22 16:55:41 +0530

    Ruby version conditional added due to differences in symbol interpretation in diff versions
    Signed-off-by: Nik08 <nikita.mathur@progress.com>

--- test/unit/profiles/profile_context_test.rb
+++ test/unit/profiles/profile_context_test.rb
@@ -7,9 +7,8 @@ class Module
   include Minitest::Spec::DSL
 end
 
-RUBY3_1_PLUS = Gem.ruby_version >= Gem::Version.new("3.1")
-RUBY3_3_PLUS = Gem.ruby_version >= Gem::Version.new("3.3")
-RUBY3_4_PLUS = Gem.ruby_version >= Gem::Version.new("3.4")
+RUBY3_1_PLUS = Gem.ruby_version >= Gem::Version.new("3.1.0")
+RUBY3_4_PLUS = Gem.ruby_version >= Gem::Version.new("3.4.0")
 
 module DescribeOneTest
   it "loads an empty describe.one" do
@@ -99,8 +98,6 @@ describe Inspec::ProfileContext do
       _(profile.rules.keys.length).must_equal 1
       if RUBY3_4_PLUS
         _(profile.rules.keys[0]).must_match(/^\(generated from \(eval(?: at .+profile_context\.rb:\d+)?\):\d+ [0-9a-f]+\)$/)
-      elsif RUBY3_3_PLUS
-        _(profile.rules.keys[0]).must_match(/^\(generated from profile_context\.rb:\d+\):\d+ [0-9a-f]+\)$/)
       else
         _(profile.rules.keys[0]).must_match(/^\(generated from \(eval\):1 [0-9a-f]+\)$/)
       end
@@ -112,8 +109,6 @@ describe Inspec::ProfileContext do
       _(profile.rules.keys.length).must_equal 1
       if RUBY3_4_PLUS
         _(profile.rules.keys[0]).must_match(/^\(generated from profile_context\.rb:\d+\)?(?::\d+)? [0-9a-f]+\)?$/)
-      elsif RUBY3_3_PLUS
-        _(profile.rules.keys[0]).must_match(/^\(generated from profile_context\.rb:\d+\):\d+ [0-9a-f]+\)$/)
       else
         _(profile.rules.keys[0]).must_match(/^\(generated from \(eval\):1 [0-9a-f]+\)$/)
       end
@@ -126,10 +121,8 @@ describe Inspec::ProfileContext do
       [0, 1, 2].each do |i|
         if RUBY3_4_PLUS
           _(profile.rules.keys[i]).must_match(/^\(generated from profile_context\.rb:\d+\)?(?::\d+)? [0-9a-f]+\)?$/)
-        elsif RUBY3_3_PLUS
-          _(profile.rules.keys[0]).must_match(/^\(generated from profile_context\.rb:\d+\):\d+ [0-9a-f]+\)$/)
         else
-          _(profile.rules.keys[0]).must_match(/^\(generated from \(eval\):1 [0-9a-f]+\)$/)
+          _(profile.rules.keys[i]).must_match(/^\(generated from \(eval\):2 [0-9a-f]+\)$/)
         end
         _(profile.rules.values[i]).must_be_kind_of Inspec::Rule
       end
--- test/unit/utils/erlang_parser_test.rb
+++ test/unit/utils/erlang_parser_test.rb
@@ -23,79 +23,155 @@ describe ErlangParser do
   end
 
   it "parse simple root array" do
-    _(parsestr("[].\n")).must_equal "{array: nil}"
+    if Gem.ruby_version >= Gem::Version.new("3.4.0")
+      _(parsestr("[].\n")).must_equal "{array: nil}"
+    else
+      _(parsestr("[].\n")).must_equal "{:array=>nil}"
+    end
   end
 
   it "parses a root array with an int" do
-    _(parsestr("[1].")).must_equal '{array: [{integer: "1"@1}]}'
+    if Gem.ruby_version >= Gem::Version.new("3.4.0")
+      _(parsestr("[1].")).must_equal '{array: [{integer: "1"@1}]}'
+    else
+      _(parsestr("[1].")).must_equal '{:array=>[{:integer=>"1"@1}]}'
+    end
   end
 
   it "parses a root array with a float" do
-    _(parsestr("[1.1].")).must_equal '{array: [{float: {integer: "1"@1, e: ".1"@2}}]}'
+    if Gem.ruby_version >= Gem::Version.new("3.4.0")
+      _(parsestr("[1.1].")).must_equal '{array: [{float: {integer: "1"@1, e: ".1"@2}}]}'
+    else
+      _(parsestr("[1.1].")).must_equal '{:array=>[{:float=>{:integer=>"1"@1, :e=>".1"@2}}]}'
+    end
   end
 
   it "parses a root array with a double quoted string" do
-    _(parsestr('["st\"r"].')).must_equal '{array: [{string: "st\\\\\\"r"@2}]}'
+    if Gem.ruby_version >= Gem::Version.new("3.4.0")
+      _(parsestr('["st\"r"].')).must_equal '{array: [{string: "st\\\\\\"r"@2}]}'
+    else
+      _(parsestr('["st\"r"].')).must_equal '{:array=>[{:string=>"st\\\\\\"r"@2}]}'
+    end
   end
 
   it "parses a root array with a single quoted string" do
-    _(parsestr("['st\\'r'].")).must_equal '{array: [{string: "st\\\\\'r"@2}]}'
+    if Gem.ruby_version >= Gem::Version.new("3.4.0")
+      _(parsestr("['st\\'r'].")).must_equal '{array: [{string: "st\\\\\'r"@2}]}'
+    else
+      _(parsestr("['st\\'r'].")).must_equal '{:array=>[{:string=>"st\\\\\'r"@2}]}'
+    end
   end
 
   it "parses a root array with an empty binary" do
-    _(parsestr("[<<>>].")).must_equal "{array: [{binary: nil}]}"
+    if Gem.ruby_version >= Gem::Version.new("3.4.0")
+      _(parsestr("[<<>>].")).must_equal "{array: [{binary: nil}]}"
+    else
+      _(parsestr("[<<>>].")).must_equal "{:array=>[{:binary=>nil}]}"
+    end
   end
 
   it "parses a root array with a bit-stream with a string" do
-    _(parsestr('[<<"pwd">>].')).must_equal '{array: [{binary: [{string: "pwd"@4, size: nil, type: nil}]}]}'
+    if Gem.ruby_version >= Gem::Version.new("3.4.0")
+      _(parsestr('[<<"pwd">>].')).must_equal '{array: [{binary: [{string: "pwd"@4, size: nil, type: nil}]}]}'
+    else
+      _(parsestr('[<<"pwd">>].')).must_equal '{:array=>[{:binary=>[{:string=>"pwd"@4, :size=>nil, :type=>nil}]}]}'
+    end
   end
 
   it "parses a root array with a bit-stream with a string and type" do
-    _(parsestr('[<<"pwd"/utf8>>].')).must_equal '{array: [{binary: [{string: "pwd"@4, size: nil, type: {identifier: "utf8"@9}}]}]}'
+    if Gem.ruby_version >= Gem::Version.new("3.4.0")
+      _(parsestr('[<<"pwd"/utf8>>].')).must_equal '{array: [{binary: [{string: "pwd"@4, size: nil, type: {identifier: "utf8"@9}}]}]}'
+    else
+      _(parsestr('[<<"pwd"/utf8>>].')).must_equal '{:array=>[{:binary=>[{:string=>"pwd"@4, :size=>nil, :type=>{:identifier=>"utf8"@9}}]}]}'
+    end
   end
 
   it "parses a root array with a bit-stream of numbers" do
-    _(parsestr("[<<0, 1, 2>>].")).must_equal '{array: [{binary: [{integer: "0"@3, size: nil, type: nil}, {integer: "1"@6, size: nil, type: nil}, {integer: "2"@9, size: nil, type: nil}]}]}'
+    if Gem.ruby_version >= Gem::Version.new("3.4.0")
+      _(parsestr("[<<0, 1, 2>>].")).must_equal '{array: [{binary: [{integer: "0"@3, size: nil, type: nil}, {integer: "1"@6, size: nil, type: nil}, {integer: "2"@9, size: nil, type: nil}]}]}'
+    else
+      _(parsestr("[<<0, 1, 2>>].")).must_equal '{:array=>[{:binary=>[{:integer=>"0"@3, :size=>nil, :type=>nil}, {:integer=>"1"@6, :size=>nil, :type=>nil}, {:integer=>"2"@9, :size=>nil, :type=>nil}]}]}'
+    end
   end
 
   it "parses a root array with a mixed bit-stream of string+numbers" do
-    _(parsestr('[<<97, "b", 99>>].')).must_equal '{array: [{binary: [{integer: "97"@3, size: nil, type: nil}, {string: "b"@8, size: nil, type: nil}, {integer: "99"@12, size: nil, type: nil}]}]}'
+    if Gem.ruby_version >= Gem::Version.new("3.4.0")
+      _(parsestr('[<<97, "b", 99>>].')).must_equal '{array: [{binary: [{integer: "97"@3, size: nil, type: nil}, {string: "b"@8, size: nil, type: nil}, {integer: "99"@12, size: nil, type: nil}]}]}'
+    else
+      _(parsestr('[<<97, "b", 99>>].')).must_equal '{:array=>[{:binary=>[{:integer=>"97"@3, :size=>nil, :type=>nil}, {:string=>"b"@8, :size=>nil, :type=>nil}, {:integer=>"99"@12, :size=>nil, :type=>nil}]}]}'
+    end
   end
 
   it "parses a root array with a bit-stream of value:size" do
-    _(parsestr('[<<0, 1:8, "2":16>>].')).must_equal '{array: [{binary: [{integer: "0"@3, size: nil, type: nil}, {integer: "1"@6, size: {integer: "8"@8}, type: nil}, {string: "2"@12, size: {integer: "16"@15}, type: nil}]}]}'
+    if Gem.ruby_version >= Gem::Version.new("3.4.0")
+      _(parsestr('[<<0, 1:8, "2":16>>].')).must_equal '{array: [{binary: [{integer: "0"@3, size: nil, type: nil}, {integer: "1"@6, size: {integer: "8"@8}, type: nil}, {string: "2"@12, size: {integer: "16"@15}, type: nil}]}]}'
+    else
+      _(parsestr('[<<0, 1:8, "2":16>>].')).must_equal '{:array=>[{:binary=>[{:integer=>"0"@3, :size=>nil, :type=>nil}, {:integer=>"1"@6, :size=>{:integer=>"8"@8}, :type=>nil}, {:string=>"2"@12, :size=>{:integer=>"16"@15}, :type=>nil}]}]}'
+    end
   end
 
   it "parses a root array with a boolean" do
-    _(parsestr("[true].")).must_equal '{array: [{bool: "true"@1}]}'
+    if Gem.ruby_version >= Gem::Version.new("3.4.0")
+      _(parsestr("[true].")).must_equal '{array: [{bool: "true"@1}]}'
+    else
+      _(parsestr("[true].")).must_equal '{:array=>[{:bool=>"true"@1}]}'
+    end
   end
 
   it "parses a root array with an identifier" do
-    _(parsestr("[hello_world123].")).must_equal '{array: [{identifier: "hello_world123"@1}]}'
+    if Gem.ruby_version >= Gem::Version.new("3.4.0")
+      _(parsestr("[hello_world123].")).must_equal '{array: [{identifier: "hello_world123"@1}]}'
+    else
+      _(parsestr("[hello_world123].")).must_equal '{:array=>[{:identifier=>"hello_world123"@1}]}'
+    end
   end
 
   it "parses a root array with multiple elements" do
-    _(parsestr("[1, \"a\" %%\n, true\n\t].")).must_equal '{array: [{integer: "1"@1}, {string: "a"@5}, {bool: "true"@13}]}'
+    if Gem.ruby_version >= Gem::Version.new("3.4.0")
+      _(parsestr("[1, \"a\" %%\n, true\n\t].")).must_equal '{array: [{integer: "1"@1}, {string: "a"@5}, {bool: "true"@13}]}'
+    else
+      _(parsestr("[1, \"a\" %%\n, true\n\t].")).must_equal '{:array=>[{:integer=>"1"@1}, {:string=>"a"@5}, {:bool=>"true"@13}]}'
+    end
   end
 
   it "parses a root array with an tuple" do
-    _(parsestr("[{}].")).must_equal "{array: [{tuple: nil}]}"
+    if Gem.ruby_version >= Gem::Version.new("3.4.0")
+      _(parsestr("[{}].")).must_equal "{array: [{tuple: nil}]}"
+    else
+      _(parsestr("[{}].")).must_equal "{:array=>[{:tuple=>nil}]}"
+    end
   end
 
   it "parses a root array with an tuple" do
-    _(parsestr("[{handshake_timeout, 100}].")).must_equal '{array: [{tuple: [{identifier: "handshake_timeout"@2}, {integer: "100"@21}]}]}'
+    if Gem.ruby_version >= Gem::Version.new("3.4.0")
+      _(parsestr("[{handshake_timeout, 100}].")).must_equal '{array: [{tuple: [{identifier: "handshake_timeout"@2}, {integer: "100"@21}]}]}'
+    else
+      _(parsestr("[{handshake_timeout, 100}].")).must_equal '{:array=>[{:tuple=>[{:identifier=>"handshake_timeout"@2}, {:integer=>"100"@21}]}]}'
+    end
   end
 
   it "parses a root array with a stringy tuple" do
-    _(parsestr('[{ab, [{"c", 1},{"d",2}]}].')).must_equal '{array: [{tuple: [{identifier: "ab"@2}, {array: [{tuple: [{string: "c"@9}, {integer: "1"@13}]}, {tuple: [{string: "d"@18}, {integer: "2"@21}]}]}]}]}'
+    if Gem.ruby_version >= Gem::Version.new("3.4.0")
+      _(parsestr('[{ab, [{"c", 1},{"d",2}]}].')).must_equal '{array: [{tuple: [{identifier: "ab"@2}, {array: [{tuple: [{string: "c"@9}, {integer: "1"@13}]}, {tuple: [{string: "d"@18}, {integer: "2"@21}]}]}]}]}'
+    else
+      _(parsestr('[{ab, [{"c", 1},{"d",2}]}].')).must_equal '{:array=>[{:tuple=>[{:identifier=>"ab"@2}, {:array=>[{:tuple=>[{:string=>"c"@9}, {:integer=>"1"@13}]}, {:tuple=>[{:string=>"d"@18}, {:integer=>"2"@21}]}]}]}]}'
+    end
   end
 
   it "parses a complex deep array-tuple mix" do
-    _(parsestr("[{rabbit,[{two,[]}]}].")).must_equal '{array: [{tuple: [{identifier: "rabbit"@2}, {array: [{tuple: [{identifier: "two"@11}, {array: nil}]}]}]}]}'
+    if Gem.ruby_version >= Gem::Version.new("3.4.0")
+      _(parsestr("[{rabbit,[{two,[]}]}].")).must_equal '{array: [{tuple: [{identifier: "rabbit"@2}, {array: [{tuple: [{identifier: "two"@11}, {array: nil}]}]}]}]}'
+    else
+      _(parsestr("[{rabbit,[{two,[]}]}].")).must_equal '{:array=>[{:tuple=>[{:identifier=>"rabbit"@2}, {:array=>[{:tuple=>[{:identifier=>"two"@11}, {:array=>nil}]}]}]}]}'
+    end
   end
 
   it "parses a complex multi array tuple mix" do
-    _(parsestr("[{rabbit,[{two,[]}]}, 3, false].")).must_equal '{array: [{tuple: [{identifier: "rabbit"@2}, {array: [{tuple: [{identifier: "two"@11}, {array: nil}]}]}]}, {integer: "3"@22}, {bool: "false"@25}]}'
+    if Gem.ruby_version >= Gem::Version.new("3.4.0")
+      _(parsestr("[{rabbit,[{two,[]}]}, 3, false].")).must_equal '{array: [{tuple: [{identifier: "rabbit"@2}, {array: [{tuple: [{identifier: "two"@11}, {array: nil}]}]}]}, {integer: "3"@22}, {bool: "false"@25}]}'
+    else
+      _(parsestr("[{rabbit,[{two,[]}]}, 3, false].")).must_equal '{:array=>[{:tuple=>[{:identifier=>"rabbit"@2}, {:array=>[{:tuple=>[{:identifier=>"two"@11}, {:array=>nil}]}]}]}, {:integer=>"3"@22}, {:bool=>"false"@25}]}'
+    end
   end
 
   it "parses a complex array-tuple mix with comments" do
@@ -108,7 +184,11 @@ describe ErlangParser do
      {tcp_listeners, [5672]}
    ]}].
 '
-    _(parsestr(s)).must_equal '{array: [{tuple: [{identifier: "rabbit"@29}, {array: [{tuple: [{identifier: "tcp_listeners"@75}, {array: [{integer: "5672"@91}]}]}]}]}]}'
+    if Gem.ruby_version >= Gem::Version.new("3.4.0")
+      _(parsestr(s)).must_equal '{array: [{tuple: [{identifier: "rabbit"@29}, {array: [{tuple: [{identifier: "tcp_listeners"@75}, {array: [{integer: "5672"@91}]}]}]}]}]}'
+    else
+      _(parsestr(s)).must_equal '{:array=>[{:tuple=>[{:identifier=>"rabbit"@29}, {:array=>[{:tuple=>[{:identifier=>"tcp_listeners"@75}, {:array=>[{:integer=>"5672"@91}]}]}]}]}]}'
+    end
   end
 end
 
