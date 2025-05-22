ddfb0a92ce203b7c71ef1c8974b1d9bee7ae2040
sa-progress <85890442+sa-progress@users.noreply.github.com>
2025-05-22 12:17:44 +0530

    test-fixture-1-float version upgrade: 0.2.0 -> 0.3.0
    Signed-off-by: sa-progress <85890442+sa-progress@users.noreply.github.com>

--- lib/plugins/inspec-plugin-manager-cli/test/functional/update_test.rb
+++ lib/plugins/inspec-plugin-manager-cli/test/functional/update_test.rb
@@ -13,18 +13,18 @@ class PluginManagerCliUpdate < Minitest::Test
     end
 
     update_result = run_inspec_process_with_this_plugin("plugin update inspec-test-fixture", pre_run: pre_block, post_run: list_after_run)
-
     success_message = update_result.stdout.split("\n").grep(/updated/).last
     refute_nil success_message, "Should find a success message at the end"
+
     assert_includes success_message, "inspec-test-fixture"
     assert_includes success_message, "0.1.0"
-    assert_includes success_message, "0.2.0"
+    assert_includes success_message, "0.3.0"
     assert_includes success_message, "updated from rubygems.org"
 
     itf_plugin = @list_result.detect { |p| p[:name] == "inspec-test-fixture" }
     refute_nil itf_plugin, "plugin name should now appear in the output of inspec list"
     assert_equal "gem (user)", itf_plugin[:type]
-    assert_equal "0.2.0", itf_plugin[:version]
+    assert_equal "0.3.0", itf_plugin[:version]
 
     assert_empty_ignoring_27_warnings update_result.stderr
     assert_exit_code 0, update_result
@@ -43,9 +43,9 @@ class PluginManagerCliUpdate < Minitest::Test
     # Loosen pattern to allow for variations
     refusal_message = output.lines.grep(/(already.*(installed|current|latest)|up to date|nothing to update)/i).last
     refute_nil refusal_message, "Should find a failure message at the end"
-
+    require 'pry';binding.pry
     assert_includes refusal_message, "inspec-test-fixture"
-    assert_includes refusal_message, "0.2.0"
+    assert_includes refusal_message, "0.3.0"
     assert_match(/already.*version|up to date|nothing to update/i, refusal_message)
 
     assert_exit_code 0, update_result
