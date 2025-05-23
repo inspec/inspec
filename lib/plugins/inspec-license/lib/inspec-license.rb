75b61be48b0a7754adbf85853d3948d6e9d2fdfa
Nik08 <nikita.mathur@progress.com>
2025-05-23 16:22:14 +0530

    Disabled license add/list command
    Signed-off-by: Nik08 <nikita.mathur@progress.com>

--- lib/plugins/inspec-license/lib/inspec-license.rb
+++ lib/plugins/inspec-license/lib/inspec-license.rb
@@ -3,12 +3,13 @@ module InspecPlugins
     class Plugin < ::Inspec.plugin(2)
       plugin_name :"inspec-license"
 
-      if Inspec::Dist::EXEC_NAME == "inspec"
-        cli_command :license do
-          require_relative "inspec-license/cli"
-          InspecPlugins::License::CLI
-        end
-      end
+      ## Disabled licensing commands
+      # if Inspec::Dist::EXEC_NAME == "inspec"
+      #   cli_command :license do
+      #     require_relative "inspec-license/cli"
+      #     InspecPlugins::License::CLI
+      #   end
+      # end
     end
   end
 end
