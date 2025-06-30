52d6eb90c60b6a93657d77eb9a18caf5ec4d9266
Nik08 <nikita.mathur@progress.com>
2025-06-30 14:04:05 +0530

    Alternate solution to solve circular dep with pastel dependency
    Signed-off-by: Nik08 <nikita.mathur@progress.com>

--- lib/inspec/base_cli.rb
+++ lib/inspec/base_cli.rb
@@ -1,4 +1,5 @@
 require "thor" # rubocop:disable Chef/Ruby/UnlessDefinedRequire
+require "tty-prompt"
 require "inspec/log"
 require "inspec/ui"
 require "inspec/config"
@@ -72,16 +73,7 @@ module Inspec
     def self.check_license!
       allowed_commands = ["-h", "--help", "help", "-v", "--version", "version"]
 
-      begin
-        original_stderr = $stderr
-        # We are redirecting the stderr to a StringIO object
-        # This is a workaround to avoid the warning "circular require considered harmful"
-        $stderr = StringIO.new
-        require "license_acceptance/acceptor"
-      ensure
-        # Restore the original stderr
-        $stderr = original_stderr
-      end
+      require "license_acceptance/acceptor"
 
       begin
         if (allowed_commands & ARGV.map(&:downcase)).empty? && # Did they use a non-exempt command?
