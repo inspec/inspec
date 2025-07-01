0c74ee17b1bc8f6dd65c17bfa963449682e818f1
sa-progress <85890442+sa-progress@users.noreply.github.com>
2025-07-01 13:04:09 +0530

    Rake used habitat
    Signed-off-by: sa-progress <85890442+sa-progress@users.noreply.github.com>

--- .expeditor/buildkite/artifact.habitat.test.ps1
+++ .expeditor/buildkite/artifact.habitat.test.ps1
@@ -1,49 +1,22 @@
-#!/usr/bin/env powershell
-
-#Requires -Version 5
-# https://stackoverflow.com/questions/9948517
-# TODO: Set-StrictMode -Version Latest
-$PSDefaultParameterValues['*:ErrorAction']='Stop'
-$ErrorActionPreference = 'Stop'
-$env:HAB_ORIGIN = 'ci'
-$env:CHEF_LICENSE = 'accept-no-persist'
-$env:HAB_LICENSE = 'accept-no-persist'
-$env:HAB_BLDR_CHANNEL = 'base-2025'
-$env:HAB_REFRESH_CHANNEL = 'base-2025'
-$Plan = 'inspec'
-
-Write-Host "--- system details"
-$Properties = 'Caption', 'CSName', 'Version', 'BuildType', 'OSArchitecture'
-Get-CimInstance Win32_OperatingSystem | Select-Object $Properties | Format-Table -AutoSize
-
-Write-Host "--- Installing the version of Habitat required"
-try {
-  hab --version
-}
-catch {
-  Set-ExecutionPolicy Bypass -Scope Process -Force
-  Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/habitat-sh/habitat/main/components/hab/install.ps1'))
-}
-finally {
-  Write-Host ":habitat: I think I have the version I need to build."
-}
-
-
-Write-Host "--- Generating fake origin key"
-hab origin key generate $env:HAB_ORIGIN
-
 Write-Host "--- Building $Plan"
 $project_root = "$(git rev-parse --show-toplevel)"
 Set-Location $project_root
 
 $env:DO_CHECK=$true; hab pkg build .
 
+if (-not (Test-Path "$project_root/results/last_build.ps1")) {
+    Write-Host "Build failed: last_build.ps1 not found."
+    Exit 1
+}
 . $project_root/results/last_build.ps1
+
+Write-Host "--- Installing $pkg_ident/$pkg_artifact"
+hab pkg install -b $project_root/results/$pkg_artifact
 
 $Env:Path += ";C:\hab\bin"
 
 Write-Host "+++ Testing $Plan"
 
 Push-Location $project_root/test/artifact
-hab pkg exec chef/inspec rake
+hab pkg exec $pkg_ident rake
 If ($lastexitcode -ne 0) { Exit $lastexitcode }
