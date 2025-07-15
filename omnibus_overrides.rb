8e086a79d42e57f13ef72ebe7142ee4e77010a42
Nik08 <nikita.mathur@progress.com>
2025-07-15 17:03:06 +0530

    Testing inspec with omnibus test branch for Ruby 3.1.7
    Signed-off-by: Nik08 <nikita.mathur@progress.com>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -3,8 +3,8 @@
 # grab the current train release from rubygems.org
 train_stable = /^train \((.*)\)/.match(`gem list ^train$ --remote`)[1]
 override "train", version: "v#{train_stable}"
-override "ruby", version: "3.1.6"
+override "ruby", version: "3.1.7"
 
 override :openssl, version: "3.0.9"
 
-override "ruby-msys2-devkit", version: "3.1.6-1"
+override "ruby-msys2-devkit", version: "3.1.7-1"
