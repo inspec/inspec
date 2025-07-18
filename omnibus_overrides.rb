d2d2346dab8b023b5f9cbd09c2b743ae7a3eae0b
Nik08 <nikita.mathur@progress.com>
2025-07-18 14:41:42 +0530

    Revert openssl version - temporarily for testing
    Signed-off-by: Nik08 <nikita.mathur@progress.com>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -5,6 +5,6 @@ train_stable = /^train \((.*)\)/.match(`gem list ^train$ --remote`)[1]
 override "train", version: "v#{train_stable}"
 override "ruby", version: "3.1.7"
 
-override :openssl, version: "3.2.4"
+override :openssl, version: "3.0.9" #TODO Upgrade openssl version
 
 override "ruby-msys2-devkit", version: "3.1.7-1"
