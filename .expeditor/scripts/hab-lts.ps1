3b35d24610741c53df6596e2e441830f4ee038f9
Sean Simmons <sean.simmons@progress.com>
2025-07-25 11:31:04 -0400

    updating to use only github action
    Signed-off-by: Sean Simmons <sean.simmons@progress.com>

--- .expeditor/scripts/hab-lts.ps1
+++
@@ -1,7 +0,0 @@
-#todo how to figure out the queues for this
-write-output "-- building inspec linux x86_64 hab package"
-
-hab license accept
-hab origin key download $HAB_ORIGIN
-hab origin key download --auth ${PIPELINE_HAB_AUTH_TOKEN} --secret chef
-sudo hab pkg build ./habitat --refresh-channel $HAB_REFRESH_CHANNEL
--- .expeditor/scripts/hab-lts.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euox pipefail 
-
-echo "-- building inspec linux x86_64 hab package"
-hab license accept
-hab origin key download $HAB_ORIGIN
-hab origin key download --auth ${PIPELINE_HAB_AUTH_TOKEN} --secret chef
-sudo hab pkg build ./habitat --refresh-channel $HAB_REFRESH_CHANNEL
