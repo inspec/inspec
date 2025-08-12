6a584b7fb50ed214ffca7b112004d6d441f56147
Balasubramanian S <68114029+balasubramanian-s@users.noreply.github.com>
2025-08-12 18:12:52 +0530

    Revert "CHEF-21376 Disable licensing and telemetry from InSpec 7" (#7495)
    * Revert "CHEF-21376 Disable licensing and telemetry from InSpec 7"
    
    * trigger commit test
    
    Signed-off-by: balasubramanian-s <balasubramanian.s@progress.com>
    
    ---------
    
    Signed-off-by: balasubramanian-s <balasubramanian.s@progress.com>

--- test/unit/utils/license_config_test.rb
+++ test/unit/utils/license_config_test.rb
@@ -1,19 +1,18 @@
-## Disabled licensing - to enable, uncomment this line
-# require "helper"
-# require "inspec/utils/licensing_config"
+require "helper"
+require "inspec/utils/licensing_config"
 
-# describe "ChefLicensing::Config" do
-#   it "returns the default chef product name as foo" do
-#     expect(ChefLicensing::Config.chef_product_name).must_equal("InSpec")
-#   end
+describe "ChefLicensing::Config" do
+  it "returns the default chef product name as foo" do
+    expect(ChefLicensing::Config.chef_product_name).must_equal("InSpec")
+  end
 
-#   it "returns the default chef_entitlement_id" do
-#     expect(ChefLicensing::Config.chef_entitlement_id).must_equal("3ff52c37-e41f-4f6c-ad4d-365192205968")
-#   end
+  it "returns the default chef_entitlement_id" do
+    expect(ChefLicensing::Config.chef_entitlement_id).must_equal("3ff52c37-e41f-4f6c-ad4d-365192205968")
+  end
 
-#   it "returns the default chef_executable_name" do
-#     expect(ChefLicensing::Config.chef_executable_name).must_equal("inspec")
-#   end
+  it "returns the default chef_executable_name" do
+    expect(ChefLicensing::Config.chef_executable_name).must_equal("inspec")
+  end
 
-#   # TODO: Need to add the test for license_server_url.
-# end
+  # TODO: Need to add the test for license_server_url.
+end
