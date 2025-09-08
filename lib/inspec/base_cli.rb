b37b320ea04b4c0da24d6e57912e9269a8b1dfb0
balasubramanian-s <balasubramanian.s@progress.com>
2025-09-08 14:16:57 +0530

    comment entitlement checks for edge case
    Signed-off-by: balasubramanian-s <balasubramanian.s@progress.com>

--- lib/inspec/base_cli.rb
+++ lib/inspec/base_cli.rb
@@ -55,8 +55,13 @@ module Inspec
         Inspec::Log.error "#{Inspec::Dist::PRODUCT_NAME} cannot execute without valid licenses."
         Inspec::UI.new.exit(:license_not_set)
       rescue ChefLicensing::SoftwareNotEntitled
-        Inspec::Log.error "License is not entitled to use InSpec."
-        Inspec::UI.new.exit(:license_not_entitled)
+        true
+        # UNDO: uncomment if entitlement check is needed
+        # Edge Case by product: https://progresssoftware.atlassian.net/browse/CHEF-23438
+        # By default, all licensed users are assumed to be entitled to use this product.
+        #
+        # Inspec::Log.error "License is not entitled to use InSpec."
+        # Inspec::UI.new.exit(:license_not_entitled)
       rescue ChefLicensing::Error => e
         Inspec::Log.error e.message
         Inspec::UI.new.exit(:usage_error)
