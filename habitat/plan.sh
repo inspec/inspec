d51b093483f89a644a2e55cf587c946811aa52ea
Vasu1105 <vjagdale@progress.com>
2025-09-10 21:14:09 +0530

    Trying to pull gem using gem source add
    Signed-off-by: Vasu1105 <vjagdale@progress.com>

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -61,7 +61,11 @@ do_install() {
 
   # internal artifactory is not compatible to resolve gem deps and fails with gem install <URL>
   # so we are using the following workaround
-  curl -o chef-official-distribution-0.1.3.gem https://artifactory-internal.ps.chef.co/artifactory/omnibus-gems-local/gems/chef-official-distribution-0.1.3.gem && gem install chef-official-distribution-0.1.3.gem --local
+  # curl -o chef-official-distribution-0.1.3.gem https://artifactory-internal.ps.chef.co/artifactory/omnibus-gems-local/gems/chef-official-distribution-0.1.3.gem && gem install chef-official-distribution-0.1.3.gem --local
+
+  gem source --add https://artifactory-internal.ps.chef.co/artifactory/omnibus-gems-local/
+  gem install chef-official-distribution
+  gem sources -r https://artifactory-internal.ps.chef.co/artifactory/omnibus-gems-local/
 
   # MUST install inspec first because inspec-bin depends on it via gemspec
   pushd "$HAB_CACHE_SRC_PATH/$pkg_dirname/"
