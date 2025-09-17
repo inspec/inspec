a3f3076420af778e5f6c77dbe6c3c292969eb5de
muthuja <muthujabavaji.vempalli@progress.com>
2025-09-17 12:43:12 +0530

    fix verify issue
    Signed-off-by: muthuja <muthujabavaji.vempalli@progress.com>

--- omnibus/omnibus.rb
+++ omnibus/omnibus.rb
@@ -33,16 +33,16 @@
 use_s3_caching true
 
 # note, this is statically set in the omnibus-buildkite-plugin, you are always going to be forced to use internal sources. If you dont want internal sources, you must enable this to false.
-use_internal_sources ENV.fetch('OMNIBUS_USE_INTERNAL_SOURCES', true)
+use_internal_sources ENV.fetch("OMNIBUS_USE_INTERNAL_SOURCES", true)
 
 # Enable S3 asset caching
 # ------------------------------
 use_s3_caching true
-s3_access_key  ENV['AWS_ACCESS_KEY_ID']
-s3_secret_key  ENV['AWS_SECRET_ACCESS_KEY']
-s3_bucket      'opscode-omnibus-cache-private'
-s3_acl         'private'
-s3_region      'us-west-2'
+s3_access_key  ENV["AWS_ACCESS_KEY_ID"]
+s3_secret_key  ENV["AWS_SECRET_ACCESS_KEY"]
+s3_bucket      "opscode-omnibus-cache-private"
+s3_acl         "private"
+s3_region      "us-west-2"
 build_retries 3
 fetcher_retries 3
 fetcher_read_timeout 120
