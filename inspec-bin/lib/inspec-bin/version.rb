fbcdcbf83ec89d2f7c2cf84b7deebc326b115ffb
Chef Expeditor <chef-ci@chef.io>
2025-09-24 10:24:58 +0000

    Bump version to 7.0.65 by Chef Expeditor
    Obvious fix; these changes are the result of automation not creative thinking.

--- inspec-bin/lib/inspec-bin/version.rb
+++ inspec-bin/lib/inspec-bin/version.rb
@@ -1,5 +1,5 @@
 # This file managed by automation - do not edit manually
 module InspecBin
   INSPECBIN_ROOT = File.expand_path("..", __dir__)
-  VERSION = "7.0.64".freeze
+  VERSION = "7.0.65".freeze
 end
