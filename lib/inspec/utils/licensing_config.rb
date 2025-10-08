e16bea668821f2f294038506838d3ad39186ae02
Nikita Mathur <Nik08@users.noreply.github.com>
2025-10-08 10:20:02 +0530

    fix: Prioritize environment-set license server URL to address configuration override issue (#7203) (#7572)
    Signed-off-by: Sonu Saha <ahasunos@gmail.com>
    Co-authored-by: Sonu Saha <98935583+ahasunos@users.noreply.github.com>

--- lib/inspec/utils/licensing_config.rb
+++ lib/inspec/utils/licensing_config.rb
@@ -4,6 +4,6 @@ ChefLicensing.configure do |config|
   config.chef_product_name = "InSpec"
   config.chef_entitlement_id = "3ff52c37-e41f-4f6c-ad4d-365192205968"
   config.chef_executable_name = "inspec"
-  config.license_server_url = "https://services.chef.io/licensing"
+  config.license_server_url = ENV["CHEF_LICENSE_SERVER"] || "https://services.chef.io/licensing"
   config.logger = Inspec::Log
 end
