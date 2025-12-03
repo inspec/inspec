d6c67e9f66fd748441ecc30976da34fc4d8a6a80
Nik08 <nikita.mathur@progress.com>
2025-12-03 17:58:59 +0530

    Remove vulnerable net-imap gem
    Signed-off-by: Nik08 <nikita.mathur@progress.com>

--- omnibus/config/software/remove-old-gems.rb
+++ omnibus/config/software/remove-old-gems.rb
@@ -16,4 +16,18 @@ build do
     # Note: We do NOT use -x flag to avoid removing executables/binstubs
     command "#{gemfile} uninstall rexml -v \"<3.4.2\" -a -I", env: env
   end
+
+  block "Removing old versions of net-imap < 0.2.5" do
+    gembin = "#{install_dir}/embedded/bin/gem"
+
+    next unless File.exist?(gembin)
+
+    puts "Removing old versions of net-imap < 0.2.5"
+    env = with_standard_compiler_flags(with_embedded_path)
+
+    # remove [-a] all net-imap < 0.2.5 and [-I] ignore dependencies
+    # CVE: GHSA-j3g3-5qv5-52mj
+    # Note: We do NOT use -x flag to avoid removing executables/binstubs
+    command "#{gembin} uninstall net-imap -v \"<0.2.5\" -a -I", env: env
+  end
 end
