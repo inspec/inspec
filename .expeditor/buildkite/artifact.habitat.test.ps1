6953665f5815dcec43b3b5be46cac9d9bb3dc17f
balasubramanian-s <balasubramanian.s@progress.com>
2025-12-10 13:27:23 +0530

    Revert "Installing isolated Habitat safe for shared CI agents"
    This reverts commit 48d943d3d85e7789387feedfd82a1eeeccbcb060.

--- .expeditor/buildkite/artifact.habitat.test.ps1
+++ .expeditor/buildkite/artifact.habitat.test.ps1
@@ -15,27 +15,44 @@ Get-CimInstance Win32_OperatingSystem | Select-Object $Properties | Format-Table
 
 Write-Host "--- Installing the version of Habitat required"
 
-Write-Host "--- Installing isolated Habitat (safe for shared CI agents)"
-
-$habDir = "$env:WORKSPACE\hab"
-$habExe = "$habDir\hab.exe"
-
-if (-not (Test-Path $habExe)) {
-    Write-Host "Downloading Habitat CLI..."
-    Invoke-WebRequest `
-    -Uri "https://packages.chef.io/files/stable/habitat/latest/hab-x86_64-windows.zip" `
-    -OutFile "hab.zip"
-
-    Expand-Archive "hab.zip" -DestinationPath $habDir
-    Remove-Item "hab.zip"
+function Stop-HabProcess {
+  $habProcess = Get-Process hab -ErrorAction SilentlyContinue
+  if ($habProcess) {
+      Write-Host "Stopping hab process..."
+      Stop-Process -Name hab -Force
+  }
 }
 
-$env:PATH = "$habDir;$env:PATH"
+function Install-Habitat {
+  Write-Host "Downloading and installing Habitat..."
+  Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/habitat-sh/habitat/main/components/hab/install.ps1'))
+}
 
-Write-Host "Hab version: $(hab --version)"
+try {
+  hab --version
+}
+catch {
+  Set-ExecutionPolicy Bypass -Scope Process -Force
 
-$project_root = "$(git rev-parse --show-toplevel)"
-Set-Location $project_root
+  Stop-HabProcess
+
+  # Remove the existing hab.exe if it exists and if you have permissions
+  $habPath = "C:\ProgramData\Habitat\hab.exe"
+  if (Test-Path $habPath) {
+      Write-Host "Attempting to remove existing hab.exe..."
+      Remove-Item $habPath -Force -ErrorAction SilentlyContinue
+      if (Test-Path $habPath) {
+          Write-Host "Failed to remove hab.exe, re-running script with elevated permissions."
+          Start-Process powershell -Verb runAs -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`""
+          exit
+      }
+  }
+
+  Install-Habitat
+}
+finally {
+  Write-Host ":habitat: I think I have the version I need to build."
+}
 
 # Set HAB_ORIGIN after Habitat installation
 Write-Host "HAB_ORIGIN set to 'ci' after installation."
@@ -65,6 +82,9 @@ else {
 }
 
 Write-Host "--- Building $Plan"
+$project_root = "$(git rev-parse --show-toplevel)"
+Set-Location $project_root
+
 $env:DO_CHECK = $true
 hab pkg build .
 
