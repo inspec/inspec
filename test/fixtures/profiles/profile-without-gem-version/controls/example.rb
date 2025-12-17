9ba6fd1ac81c72aaa0f0536632f9e9f6f41ae78d
Nik08 <nikita.mathur@progress.com>
2025-12-17 18:33:31 +0530

    Updated test and dependency version in fixture to solve gem conflict issues
    Signed-off-by: Nik08 <nikita.mathur@progress.com>

--- test/fixtures/profiles/profile-without-gem-version/controls/example.rb
+++ test/fixtures/profiles/profile-without-gem-version/controls/example.rb
@@ -1,11 +1,10 @@
-require "money"
+require "colorize"
 
 control "tmp-1.0" do
-  Money.rounding_mode = BigDecimal::ROUND_HALF_UP
-  m = Money.from_cents(1000, "USD")
-  cents = m.cents
+  colored_text = "InSpec Test".colorize(:green)
 
-  describe cents do
-    it { should eq 1000 }
+  describe colored_text do
+    it { should_not be_nil }
+    it { should be_a(String) }
   end
 end
