95c135aa39fe1aede730116e1dbe08b7d98869ff
Sathish Babu <80091550+sathish-progress@users.noreply.github.com>
2025-12-18 16:05:33 +0530

    CHEF-29019 Optimise the CI pipeline (#7678)
    * Revert "Backport 7576 to fix windows pipeline failure in inspec 5 (#7676)"
    
    This reverts commit 1c1f49b1a18b961d85e2b5974ab4072a00209483.
    
    * optimise parallel test thread count from 20 to 16 for windows
    
    Signed-off-by: Sathish <sbabu@progress.com>
    
    * set parallel test thread count to 16 for ruby tests in linux platform
    
    Signed-off-by: Sathish <sbabu@progress.com>
    
    * Improve test execution order by interleaving functional and unit tests for better load balancing on Windows
    
    Signed-off-by: Sathish <sbabu@progress.com>
    
    * fix lint issues
    
    Signed-off-by: Sathish <sbabu@progress.com>
    
    * set build to false
    
    Signed-off-by: Sathish <sbabu@progress.com>
    
    * default to etc processors and print n threads
    
    Signed-off-by: Sathish <sbabu@progress.com>
    
    * remove kitchen from exclusion groups
    
    Signed-off-by: Sathish <sbabu@progress.com>
    
    ---------
    
    Signed-off-by: Sathish <sbabu@progress.com>

--- .expeditor/buildkite/verify.ps1
+++ .expeditor/buildkite/verify.ps1
@@ -66,6 +66,6 @@ bundle config set --local without deploy kitchen
 bundle install --jobs=7 --retry=3
 
 echo "+++ bundle exec rake test:parallel"
-bundle exec rake test:parallel K=4
+bundle exec rake test:parallel K=16
 
 exit $LASTEXITCODE
