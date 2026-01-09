422d06a3a3057945e3a5c883fb56cd49c137d273
Sean Simmons <ssimmons@progress.com>
2026-01-09 12:02:30 -0500

    fixing conflicts
    Signed-off-by: Sean Simmons <ssimmons@progress.com>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -5,7 +5,7 @@ train_stable = /^train \((.*)\)/.match(`gem list ^train$ --remote`)[1]
 override "train", version: "v#{train_stable}"
 override "ruby", version: "3.1.7"
 
-override :openssl, version: "3.2.4"
+override :openssl, version: "3.2.6"
 override :libffi, version: "3.4.6" # Pinning to this version, as 3.4.4 is incompatible with RHEL-10.
 
 override "ruby-msys2-devkit", version: "3.1.7-1"
