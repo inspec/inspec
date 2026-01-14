94e97a6d7f969b57b704f07140ac8d2505ae74c9
Nik08 <nikita.mathur@progress.com>
2026-01-14 14:07:03 +0530

    Pinning libxml2
    Signed-off-by: Nik08 <nikita.mathur@progress.com>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -2,10 +2,11 @@
 
 # grab the current train release from rubygems.org
 train_stable = /^train \((.*)\)/.match(`gem list ^train$ --remote`)[1]
-override "train", version: "v#{train_stable}"
-override "ruby", version: "3.1.7"
+override 'train', version: "v#{train_stable}"
+override 'ruby', version: '3.1.7'
 
-override :openssl, version: "3.2.6"
-override :libffi, version: "3.4.6" # Pinning to this version, as 3.4.4 is incompatible with RHEL-10.
+override :openssl, version: '3.2.6'
+override :libffi, version: '3.4.6' # Pinning to this version, as 3.4.4 is incompatible with RHEL-10.
+override :libxml2, version: '2.13.8'
 
-override "ruby-msys2-devkit", version: "3.1.7-1"
+override 'ruby-msys2-devkit', version: '3.1.7-1'
