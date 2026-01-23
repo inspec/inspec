a0e8814b57428cb8a92fbfae7ef55ef094871fc2
Vasundhara Jagdale <vasundhara.jagdale@chef.io>
2026-01-23 06:03:06 +0000

    Fixes warning which shows up after inspec execution while using winrm transport (#7753)
    * bump-train-winrm-chef-winrm-version
    
    Signed-off-by: Vasu1105 <vjagdale@progress.com>
    
    * Reduce parallel test jobs from 16 to 6 in verify.ps1
    
    Signed-off-by: Sathish <sbabu@progress.com>
    
    ---------
    
    Signed-off-by: Vasu1105 <vjagdale@progress.com>
    Signed-off-by: Sathish <sbabu@progress.com>
    Co-authored-by: Sathish <sbabu@progress.com>

--- .expeditor/buildkite/verify.ps1
+++ .expeditor/buildkite/verify.ps1
@@ -66,6 +66,6 @@ bundle config set --local without deploy
 bundle install --jobs=7 --retry=3
 
 echo "+++ bundle exec rake test:parallel"
-bundle exec rake test:parallel K=20
+bundle exec rake test:parallel K=6
 
 exit $LASTEXITCODE
