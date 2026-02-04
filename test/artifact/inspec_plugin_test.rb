ac23d333682491a465daf738fefdda48f88e7c96
Chandra <cprasad@progress.com>
2026-02-04 11:42:35 +0530

    check modified to catch the any type of errors
    Signed-off-by: Chandra <cprasad@progress.com>

--- test/artifact/inspec_plugin_test.rb
+++ test/artifact/inspec_plugin_test.rb
@@ -6,11 +6,9 @@ class TestInspecPlugin < ArtifactTest
     inspec_command = "plugin list"
     stdout, stderr, status = run_cmd "inspec #{inspec_command} #{TEST_CLI_OPTS}"
     
-    # Check for plugin loading errors in stdout (InSpec logs errors to stdout, not stderr)
-    refute_match(/ERROR: Could not load plugin/, stdout, 
-      "Plugin loading error detected in stdout")
-    refute_match(/Unable to activate/, stdout,
-      "Gem activation failure detected in stdout")
+    # This catches dependency conflicts, plugin loading failures, and any other errors
+    refute_match(/\[.*\] ERROR:/i, stdout, 
+      "Error detected in stdout - check for plugin loading or dependency issues")
     
     # Check stderr only for cosmetic warnings
     assert_empty stderr.sub(/#< CLIXML\n/, "").sub(/The table size exceeds the currently set width\.Defaulting to vertical orientation\.\n/, "")
