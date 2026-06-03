3d4e48c2d8ea8f533002f1bbaa4bd51cde76948e
Chandra <cprasad@progress.com>
2026-06-03 14:30:16 +0530

    using git2.45-test channel
    Signed-off-by: Chandra <cprasad@progress.com>

--- habitat/x86_64-linux/plan.sh
+++ habitat/x86_64-linux/plan.sh
@@ -1,5 +1,5 @@
-export HAB_BLDR_CHANNEL="stable"
-export HAB_REFRESH_CHANNEL="stable"
+export HAB_BLDR_CHANNEL="git2.45-test"  # "stable"
+export HAB_REFRESH_CHANNEL="git2.45-test"  # "stable"
 pkg_name=inspec
 pkg_origin=chef
 pkg_version=$(cat "$PLAN_CONTEXT/../../VERSION")
